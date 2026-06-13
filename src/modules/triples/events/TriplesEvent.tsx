328dc47d6a27207acb788226d0e5be12203b7abf
groomigroom <googjohn0143@gmail.com>
2026-06-13 11:26:15 +0900

    Create TriplesEvent.tsx

---
+++ src/modules/triples/events/TriplesEvent.tsx
@@ -0,0 +1,60 @@
+import "./TriplesEvent.css";
+
+export default function TriplesEvent() {
+    return (
+        <div className="triplesevent_full">
+            <form className="tri_e_form" action="">
+                <input className="tri_e_fo1" type="text" />
+                <input className="tri_e_fo2" type="text" />
+                <input className="tri_e_fo3" type="text" />
+                <input className="tri_e_fo4" type="text" />
+                <input className="tri_e_fo5" type="text" />
+                <input className="tri_e_fo6" type="text" />
+                <input className="tri_e_fo7" type="text" />
+                <input className="tri_e_fo8" type="text" />
+                <input className="tri_e_fo9" type="text" />
+                <input className="tri_e_fo10" type="text" />
+                <input className="tri_e_fo11" type="text" />
+                <input className="tri_e_fo12" type="text" />
+            </form>
+            <section className="tri_e_img">
+                <section className="triplesevent_imgt tri_e_i_1">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </section>
+                <section className="triplesevent_imgt tri_e_i_2">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </section>
+                <section className="triplesevent_imgt tri_e_i_3">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </section>
+                <section className="triplesevent_imgt tri_e_i_4">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </section>
+                <section className="triplesevent_imgt tri_e_i_5">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </section>
+                <section className="triplesevent_imgt tri_e_i_6">
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </section>
+            </section>
+        </div>
+    );
+};
