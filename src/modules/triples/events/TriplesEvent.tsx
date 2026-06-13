e0e0479a22e03ff4476fa2ab7586ae61d79489a8
groomigroom <googjohn0143@gmail.com>
2026-06-13 17:23:10 +0900

    Update TriplesEvent.tsx

--- src/modules/triples/events/TriplesEvent.tsx
+++ src/modules/triples/events/TriplesEvent.tsx
@@ -1,37 +1,41 @@
 import "./TriplesEvent.css";
+import TriEventLogo from "./img/tri_events_logo.png";
+import TriEventS1 from "./img/s1_img.jpg";
 
 export default function TriplesEvent() {
     return (
         <div className="triplesevent_full">
-            <form className="tri_e_form" action="">
-                <input className="tri_e_fo1" type="text" />
-                <input className="tri_e_fo2" type="text" />
-                <input className="tri_e_fo3" type="text" />
-                <input className="tri_e_fo4" type="text" />
-                <input className="tri_e_fo5" type="text" />
-                <input className="tri_e_fo6" type="text" />
-                <input className="tri_e_fo7" type="text" />
-                <input className="tri_e_fo8" type="text" />
-                <input className="tri_e_fo9" type="text" />
-                <input className="tri_e_fo10" type="text" />
-                <input className="tri_e_fo11" type="text" />
-                <input className="tri_e_fo12" type="text" />
-                <input className="tri_e_fo13" type="text" />
-                <input className="tri_e_fo14" type="text" />
-                <input className="tri_e_fo15" type="text" />
-                <input className="tri_e_fo16" type="text" />
-                <input className="tri_e_fo17" type="text" />
-                <input className="tri_e_fo18" type="text" />
-                <input className="tri_e_fo19" type="text" />
-                <input className="tri_e_fo20" type="text" />
-                <input className="tri_e_fo21" type="text" />
-                <input className="tri_e_fo22" type="text" />
-                <input className="tri_e_fo23" type="text" />
-                <input className="tri_e_fo24" type="text" />
-            </form>
+            <img src={TriEventLogo} alt="트리플에스 이벤트 로고" className="tri_e_logo" />
+            <h2 className="tri_e_h2">이름 맞추기 이벤트</h2>
             <section className="tri_e_img">
+                <form className="tri_e_form" action="">
+                    <input className="tri_e_fo1" type="text" />
+                    <input className="tri_e_fo2" type="text" />
+                    <input className="tri_e_fo3" type="text" />
+                    <input className="tri_e_fo4" type="text" />
+                    <input className="tri_e_fo5" type="text" />
+                    <input className="tri_e_fo6" type="text" />
+                    <input className="tri_e_fo7" type="text" />
+                    <input className="tri_e_fo8" type="text" />
+                    <input className="tri_e_fo9" type="text" />
+                    <input className="tri_e_fo10" type="text" />
+                    <input className="tri_e_fo11" type="text" />
+                    <input className="tri_e_fo12" type="text" />
+                    <input className="tri_e_fo13" type="text" />
+                    <input className="tri_e_fo14" type="text" />
+                    <input className="tri_e_fo15" type="text" />
+                    <input className="tri_e_fo16" type="text" />
+                    <input className="tri_e_fo17" type="text" />
+                    <input className="tri_e_fo18" type="text" />
+                    <input className="tri_e_fo19" type="text" />
+                    <input className="tri_e_fo20" type="text" />
+                    <input className="tri_e_fo21" type="text" />
+                    <input className="tri_e_fo22" type="text" />
+                    <input className="tri_e_fo23" type="text" />
+                    <input className="tri_e_fo24" type="text" />
+                </form>
                 <section className="triplesevent_imgt tri_e_i_1">
-                    <img src="" alt="" />
+                    <img src={TriEventS1} alt="윤서연 이미지" />
                     <img src="" alt="" />
                     <img src="" alt="" />
                     <img src="" alt="" />
