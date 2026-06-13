48a8b52baefebb155a45b797f0b33153820112ac
groomigroom <googjohn0143@gmail.com>
2026-06-13 11:21:54 +0900

    Create home.tsx

---
+++ src/modules/home.tsx
@@ -0,0 +1,19 @@
+import "./home.css";
+
+
+export default function Home() {
+    return (
+        <div className="home_full">
+            <section className="home_p1">
+
+            </section>
+            <section className="home_p2">
+                <section className="home_p2_con">
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                </section>
+            </section>
+        </div>
+    )
+}
