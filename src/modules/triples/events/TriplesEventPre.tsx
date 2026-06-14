e141a828e377f34aad42530e37ac0563ce75d621
groomigroom <googjohn0143@gmail.com>
2026-06-14 16:06:44 +0900

    Add TriplesEventPre component with basic structure

---
+++ src/modules/triples/events/TriplesEventPre.tsx
@@ -0,0 +1,9 @@
+import "./TriplesEventPre.css";
+
+export default function TriplesEventPre() {
+    return (
+        <div className="tri_e_p_full">
+            <section className="tri_e_p_ticket"></section>
+        </div>
+    );
+};
