06725a1b4b8c1a03bc2f4ed9a120605a6c0998f5
groomigroom <googjohn0143@gmail.com>
2026-06-14 16:21:16 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import "./App.css";
 import Home from "./modules/home";
-import TriplesEvent from "./modules/triples/events/TriplesEvent";
+import TxtEvent from "./modules/txt/events/TxtEvent";
+// import TriplesEvent from "./modules/triples/events/TriplesEvent";
 
 function App() {
 
 
   return (
     <div className="app_full">
-      <TriplesEvent />
+      <TxtEvent />
+      {/* <TriplesEvent /> */}
       <Home />
     </div>
   )
