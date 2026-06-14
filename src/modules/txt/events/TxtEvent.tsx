f56534962096f70d908d11513ba07fcf4a6092e5
groomigroom <googjohn0143@gmail.com>
2026-06-14 16:22:45 +0900

    Add TxtEvent component with lyrics display

---
+++ src/modules/txt/events/TxtEvent.tsx
@@ -0,0 +1,20 @@
+import "./TxtEvent.css";
+
+export default function TxtEvent() {
+    return (
+        <div className="txt_e_full">
+            <h2 className="txt_e_h2">"하루에 하루만 더" 가사 맟주기 이벤트</h2>
+            <p className="txt_e_mp">
+                머물러 줘<br />
+                하루에 하루만 더<br />
+                헤어질 수 없어<br />
+                I’ll just stick with you<br />
+                <br />
+                언제부턴지 몰라도<br />
+                미지근한 너의 태도<br />
+                ㅁㅁㅁㅁ 한 표정, 말투도<br />
+                {/* 낯설기만 */}
+            </p>
+        </div>
+    );
+};
