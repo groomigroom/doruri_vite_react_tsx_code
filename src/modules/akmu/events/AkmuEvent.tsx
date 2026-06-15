d21ef9de487364e065b90513885966ab79caea68
groomigroom <googjohn0143@gmail.com>
2026-06-16 06:29:24 +0900

    Update AkmuEvent.tsx

--- src/modules/akmu/events/AkmuEvent.tsx
+++ src/modules/akmu/events/AkmuEvent.tsx
@@ -2,6 +2,8 @@ import "./AkmuEvent.css";
 
 export default function AkmuEvent() {
     return (
-        <div className="akmuevent_full"></div>
+        <div className="akmuevent_full">
+            악뮤이벤트
+        </div>
     );
 }
