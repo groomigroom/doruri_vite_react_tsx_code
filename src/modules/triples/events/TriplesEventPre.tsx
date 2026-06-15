a3b91166aedcced063f7b6464e4b70acfdccbaef
groomigroom <googjohn0143@gmail.com>
2026-06-15 16:27:00 +0900

    Add onClick handler to TriplesEventPre component

--- src/modules/triples/events/TriplesEventPre.tsx
+++ src/modules/triples/events/TriplesEventPre.tsx
@@ -1,9 +1,14 @@
 import "./TriplesEventPre.css";
 
-export default function TriplesEventPre() {
+interface Tri_e_popProp {
+    tri_e_OnCl: () => void;
+}
+
+export default function TriplesEventPre({ tri_e_OnCl }: Tri_e_popProp) {
     return (
         <div className="tri_e_p_full">
             <section className="tri_e_p_ticket"></section>
+            <button onClick={tri_e_OnCl}>닫기</button>
         </div>
     );
 };
