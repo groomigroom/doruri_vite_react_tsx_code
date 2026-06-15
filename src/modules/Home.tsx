dc650593a4fbd3ea2b5488fbcf94551591a1f22f
groomigroom <googjohn0143@gmail.com>
2026-06-15 21:11:55 +0900

    Add navigation links to Home component

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -1,11 +1,15 @@
 import "./home.css";
 
+import { Link } from 'react-router-dom';
+
 
 export default function Home() {
     return (
         <div className="home_full">
             <section className="home_p1">
-
+                <Link to="/">Home</Link>
+                <Link to="/TriplesEvent">TriplesEvent</Link>
+                <Link to="/TxtEvent">TxtEvent</Link>
             </section>
             <section className="home_p2">
                 <section className="home_p2_con">
