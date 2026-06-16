22a26faef3aded0c967b6f84e12d781a9aa2ac0f
groomigroom <googjohn0143@gmail.com>
2026-06-16 20:54:02 +0900

    Update TriplesEventPre.tsx

--- src/modules/triples/events/TriplesEventPre.tsx
+++ src/modules/triples/events/TriplesEventPre.tsx
@@ -2,12 +2,17 @@ import "./TriplesEventPre.css";
 
 interface Tri_e_popProp {
     tri_e_OnCl: () => void;
+    tri_e_result: number;
 }
 
-export default function TriplesEventPre({ tri_e_OnCl }: Tri_e_popProp) {
+
+export default function TriplesEventPre({ tri_e_OnCl, tri_e_result }: Tri_e_popProp) {
     return (
         <div className="tri_e_p_full">
             <section className="tri_e_p_ticket"></section>
+            <section className="tri_e_p_pp">
+                <h2>정답 : {tri_e_result}개</h2>
+            </section>
             <button onClick={tri_e_OnCl}>닫기</button>
         </div>
     );
