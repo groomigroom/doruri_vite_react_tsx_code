6be7de5a46b7f3059050322b43f1f5c6c55ee7c1
groomigroom <googjohn0143@gmail.com>
2026-06-17 12:31:48 +0900

    Update TxtMain.tsx

--- src/modules/txt/main/TxtMain.tsx
+++ src/modules/txt/main/TxtMain.tsx
@@ -1,5 +1,9 @@
+import TxtEvent from "../events/TxtEvent";
+
 export default function TxtMain() {
     return (
-        <div></div>
+        <div>
+            <TxtEvent />
+        </div>
     );
 };
