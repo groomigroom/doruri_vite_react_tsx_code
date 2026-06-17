127f9233c10ec5a1f8ddeb013f2e9305488c808d
groomigroom <googjohn0143@gmail.com>
2026-06-17 10:54:17 +0900

    Update TriplesMain.tsx

--- src/modules/triples/main/TriplesMain.tsx
+++ src/modules/triples/main/TriplesMain.tsx
@@ -1,5 +1,10 @@
+import { Link } from 'react-router-dom';
+
+
 export default function TriplesMain() {
     return (
-        
+        <div>
+            <Link to="/TriplesEvent">이벤트</Link>
+        </div>
     );
 };
