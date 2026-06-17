6966d0b14687b228f71736317583c707ed4a2b2d
groomigroom <googjohn0143@gmail.com>
2026-06-17 16:33:33 +0900

    Update TriplesMain.tsx

--- src/modules/triples/main/TriplesMain.tsx
+++ src/modules/triples/main/TriplesMain.tsx
@@ -1,10 +1,18 @@
 import { Link } from 'react-router-dom';
 
+import './TriplesMain.css';
+
+import Tri_m_logo from './img/tri_m_logo.svg';
 
 export default function TriplesMain() {
     return (
-        <div>
+        <div className='tri_m_full'>
+            <img src={Tri_m_logo} alt="트리플에스 로고" className='tri_m_logo' />
+            <div className='tri_m_logo'></div>
+            <p>우리는 하나이자 스물넷입니다.</p>
+            <h2>tripleS</h2>
             <Link to="/TriplesEvent">이벤트</Link>
-        </div>
+        </div >
     );
 };
+
