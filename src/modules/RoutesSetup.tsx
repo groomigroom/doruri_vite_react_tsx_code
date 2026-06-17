d960cf92956ab20cabc4dd5742b5c01d986da8cc
groomigroom <googjohn0143@gmail.com>
2026-06-17 12:21:49 +0900

    Update RoutesSetup.tsx

--- src/modules/RoutesSetup.tsx
+++ src/modules/RoutesSetup.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import TriplesMain from './triples/main/TriplesMain';
 import TriplesEvent from "./triples/events/TriplesEvent";
+import TxtMain from './txt/main/TxtMain';
 import TxtEvent from "./txt/events/TxtEvent";
 import AkmuEvent from "./akmu/events/AkmuEvent";
 
@@ -12,6 +13,7 @@ export default function RoutesSetup() {
             <Route path='/' element={<Home />}></Route>
             <Route path='/TriplesMain' element={<TriplesMain />}></Route>
             <Route path='/TriplesEvent' element={<TriplesEvent />}></Route>
+            <Route path='/TxtMain' element={<TxtMain />}></Route>
             <Route path='/TxtEvent' element={<TxtEvent />}></Route>
             <Route path='/AkmuEvent' element={<AkmuEvent />}></Route>
         </Routes>
