816e19d6223a2ecae88c83cb632f8847f0c50f6e
groomigroom <googjohn0143@gmail.com>
2026-06-17 12:34:25 +0900

    Update Home.tsx

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
                     <section className="home_p1_train">
                         <Link to="/" className="home_link">Home</Link>
                         <Link to="/TriplesMain" className="home_link home_tri_link">TriplesEvent</Link>
-                        <Link to="/TxtEvent" className="home_link home_txt_link">TxtEvent</Link>
+                        <Link to="/TxtMain" className="home_link home_txt_link">TxtMain</Link>
                         <Link to="/AkmuEvent" className="home_link home_akm_link">AkmuEvent</Link>
                     </section>
                     <section className="home_p1_train">
