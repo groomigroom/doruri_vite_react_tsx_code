d68311db1ff4af80abb801cb747adbdb98368014
groomigroom <googjohn0143@gmail.com>
2026-06-17 16:01:50 +0900

    Update AkmuMain.tsx

--- src/modules/akmu/main/AkmuMain.tsx
+++ src/modules/akmu/main/AkmuMain.tsx
@@ -1,7 +1,9 @@
+import { Link } from 'react-router-dom';
+
 export default function AkmuMain() {
     return (
         <div>
-
+            <Link to='/AkmuEvent'>이벤트로 가기</Link>
         </div>
     );
 };
