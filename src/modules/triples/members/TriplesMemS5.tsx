4f20f4ae2b0ee378cbca2cb783a7c99713e70b77
groomigroom <googjohn0143@gmail.com>
2026-06-18 15:02:06 +0900

    Add files via upload

---
+++ src/modules/triples/members/TriplesMemS5.tsx
@@ -0,0 +1,16 @@
+import './TriplesMemS5.css';
+
+export default function TriplesMemS5() {
+    return (
+        <div className="tri_mm_s5_full">
+            김유연
+        </div>
+    );
+
+};
+
+
+
+
+
+
