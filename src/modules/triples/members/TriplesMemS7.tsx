496fa00804d0cd97dae109db0623892175d00bd5
groomigroom <googjohn0143@gmail.com>
2026-06-18 15:17:01 +0900

    Add files via upload

---
+++ src/modules/triples/members/TriplesMemS7.tsx
@@ -0,0 +1,21 @@
+import './TriplesMemS7.css';
+
+export default function TriplesMemS7() {
+    return (
+        <div className="tri_mm_s5_full">
+            김나경
+        </div>
+    );
+
+};
+
+
+
+
+
+
+
+
+
+
+
