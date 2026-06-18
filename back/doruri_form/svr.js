045b44b944cd4c15a99dd011cc92a30f7672d660
groomigroom <googjohn0143@gmail.com>
2026-06-19 06:30:07 +0900

    Add files via upload

---
+++ back/doruri_form/svr.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const mysql = require('mysql');
+const path = require('path');
+const static = require('serve_static');
+
+//https://youtu.be/t5PZ9ICjp1w?si=4vFvEOSNN8DG4Up1
+//까지봄
