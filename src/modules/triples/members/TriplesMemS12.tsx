f8ae510f8cd896c1b16d4e1c54137c096e09c0ca
groomigroom <googjohn0143@gmail.com>
2026-06-19 08:28:13 +0900

    Add files via upload

---
+++ src/modules/triples/members/TriplesMemS12.tsx
@@ -0,0 +1,27 @@
+import './TriplesMemS12.css';
+
+export default function TriplesMemS12() {
+    return (
+        <div className="tri_mm_s5_full">
+            곽연지
+        </div>
+    );
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
