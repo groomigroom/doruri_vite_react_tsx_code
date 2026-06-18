5fe3ec2380a5105c5438d13a880a151aebf9026a
groomigroom <googjohn0143@gmail.com>
2026-06-19 08:59:46 +0900

    Add files via upload

---
+++ src/modules/triples/members/TriplesMemS14.tsx
@@ -0,0 +1,27 @@
+import './TriplesMemS14.css';
+
+export default function TriplesMemS14() {
+    return (
+        <div className="tri_mm_s5_full">
+            박소현
+        </div>
+    );
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
