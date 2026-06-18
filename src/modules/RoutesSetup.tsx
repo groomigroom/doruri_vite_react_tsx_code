1398531373131b92e0411d6e91d32fe828b42196
groomigroom <googjohn0143@gmail.com>
2026-06-18 21:04:03 +0900

    Update RoutesSetup.tsx

--- src/modules/RoutesSetup.tsx
+++ src/modules/RoutesSetup.tsx
@@ -9,6 +9,7 @@ import TriplesMemS4 from './triples/members/TriplesMemS4';
 import TriplesMemS5 from './triples/members/TriplesMemS5';
 import TriplesMemS6 from './triples/members/TriplesMemS6';
 import TriplesMemS7 from './triples/members/TriplesMemS7';
+import TriplesMemS8 from './triples/members/TriplesMemS8';
 import TriplesEvent from "./triples/events/TriplesEvent";
 import TxtMain from './txt/main/TxtMain';
 import TxtEvent from "./txt/events/TxtEvent";
@@ -27,6 +28,7 @@ export default function RoutesSetup() {
             <Route path='/TriplesMemS5' element={<TriplesMemS5 />}></Route>
             <Route path='/TriplesMemS6' element={<TriplesMemS6 />}></Route>
             <Route path='/TriplesMemS7' element={<TriplesMemS7 />}></Route>
+            <Route path='/TriplesMemS8' element={<TriplesMemS8 />}></Route>
             <Route path='/TriplesEvent' element={<TriplesEvent />}></Route>
             <Route path='/TxtMain' element={<TxtMain />}></Route>
             <Route path='/TxtEvent' element={<TxtEvent />}></Route>
