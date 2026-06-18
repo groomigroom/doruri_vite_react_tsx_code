2434660eded463ecac2ae584fb3e63dac7ffb548
groomigroom <googjohn0143@gmail.com>
2026-06-18 21:17:35 +0900

    Add files via upload

---
+++ src/modules/triples/members/TriplesMemS10.tsx
@@ -0,0 +1,24 @@
+import './TriplesMemS10.css';
+
+export default function TriplesMemS10() {
+    return (
+        <div className="tri_mm_s5_full">
+            서다현
+        </div>
+    );
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
