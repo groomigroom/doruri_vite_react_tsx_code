0a8e3c5441de4f2f5424548df8f49efb182b470a
groomigroom <googjohn0143@gmail.com>
2026-06-18 14:02:53 +0900

    Update TriplesMemS3.tsx

--- src/modules/triples/members/TriplesMemS3.tsx
+++ src/modules/triples/members/TriplesMemS3.tsx
@@ -1,6 +1,6 @@
 import './TriplesMemS3.css';
 
-export default function TriplesMemS2() {
+export default function TriplesMemS3() {
     return (
         <div className="tri_mm_s3_full">
             이지우
