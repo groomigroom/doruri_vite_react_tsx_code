ce7ed7debf7938dd5e6d345cf8a6a28bac8bdc41
groomigroom <googjohn0143@gmail.com>
2026-06-19 09:40:54 +0900

    Add files via upload

---
+++ src/modules/triples/members/TriplesMemS17.tsx
@@ -0,0 +1,26 @@
+import './TriplesMemS17.css';
+
+export default function TriplesMemS17() {
+    return (
+        <div className="tri_mm_s5_full">
+            린
+        </div>
+    );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
