9779660f41aad54a9fbf47c06d19e3c2352ed668
groomigroom <googjohn0143@gmail.com>
2026-06-19 12:23:32 +0900

    Update TriplesMemS1.tsx

--- src/modules/triples/members/TriplesMemS1.tsx
+++ src/modules/triples/members/TriplesMemS1.tsx
@@ -3,7 +3,26 @@ import './TriplesMemS1.css';
 export default function TriplesMemS1() {
     return (
         <div className="tri_mm_s1_full">
-            윤서연
+            <header>
+                <h2>윤서연</h2>
+            </header>
+            <section className='tri_mm_s1_intro'>
+                <section className='tri_mm_s1_intropp'>
+                    <p></p>
+                </section>
+                <section className='tri_mm_s1_intropa'></section>
+                <section className='tri_mm_s1_intropa'>
+                    <table className='tri_mm_s1_intropa_tb'>
+                        <tbody>
+                            <tr>
+                                <td>성격</td>
+                                <td>좋음</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </section>
+
+            </section>
         </div>
     );
 };
