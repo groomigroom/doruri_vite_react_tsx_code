6ad6a653ed910483a7ac61f2547540f53430a9d1
groomigroom <googjohn0143@gmail.com>
2026-06-19 17:48:26 +0900

    Refactor TxtMain component layout and styling
    Updated the TxtMain component to include a logo and modified the structure of the JSX.

--- src/modules/txt/main/TxtMain.tsx
+++ src/modules/txt/main/TxtMain.tsx
@@ -1,15 +1,21 @@
 import { Link } from 'react-router-dom';
 
+import '../../triples/main/TriplesMain.css'
+import './TxtMain.css';
+
+import Txt_m_logo from './img/txt_m_logo.svg';
+
 export default function TxtMain() {
     return (
-        <div>
-            ##### logo 넣기
+        <div className='txt_m_full'>
+            <img src={Txt_m_logo} alt="트리플에스 로고" className='tri_m_logo' />
+            <h2>투모로우바이투게더</h2>
             <p>
                 #####
                 하루에 하루만 더<br />
                 &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
-            <h2>투모로우바이투게더</h2>
-            #####          txt_m_img
+            
+
             <h3>멤버</h3>
             <article className='txt_m_mem'>
                 <img src="" alt="" />
