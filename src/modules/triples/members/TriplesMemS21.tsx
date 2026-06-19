932bcdd6f3e7946c10cf195c44d6c8f67e3edf83
groomigroom <googjohn0143@gmail.com>
2026-06-19 10:15:36 +0900

    Add files via upload

---
+++ src/modules/triples/members/TriplesMemS21.tsx
@@ -0,0 +1,26 @@
+import './TriplesMemS21.css';
+
+export default function TriplesMemS21() {
+    return (
+        <div className="tri_mm_s5_full">
+            김채원
+        </div>
+    );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
