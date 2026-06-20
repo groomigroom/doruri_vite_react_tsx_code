db8017329297df5e068a192319082dd8224e3858
groomigroom <googjohn0143@gmail.com>
2026-06-20 09:53:48 +0900

    Add files via upload

---
+++ src/modules/Menu.tsx
@@ -0,0 +1,18 @@
+import './Menu.css';
+
+export default function Menu() {
+    return (
+        <div className="Menu_full">
+            <header>
+                <nav className='menu_nav'>
+                    <ul>
+                        <li>메뉴1</li>
+                        <li>메뉴1</li>
+                        <li>메뉴1</li>
+                        <li>메뉴1</li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
