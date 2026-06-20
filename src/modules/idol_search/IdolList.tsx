c3fe1d664cfb438d9eb7f0ef246e0ca0a2ba4a7d
groomigroom <googjohn0143@gmail.com>
2026-06-20 17:14:00 +0900

    Update IdolList.tsx

--- src/modules/idol_search/IdolList.tsx
+++ src/modules/idol_search/IdolList.tsx
@@ -8,25 +8,25 @@ export default function IdolList() {
     return (
         <div className="idolList_full">
             <Menu />
-            <section className="home_p1">
-                <section className="home_p1_trainf">
-                    <section className="home_p1_train">
-                        <Link to="/" className="home_link">Home</Link>
-                        <Link to="/TriplesMain" className="home_link home_tri_link">TriplesEvent</Link>
-                        <Link to="/TxtMain" className="home_link home_txt_link">TxtMain</Link>
-                        <Link to="/AkmuMain" className="home_link home_akm_link">AkmuMain</Link>
+            <section className="idolList_part">
+                <section className="idolList_trainf">
+                    <section className="idolList_train">
+                        <Link to="/" className="idolList_link">Home</Link>
+                        <Link to="/TriplesMain" className="idolList_link">TriplesEvent</Link>
+                        <Link to="/TxtMain" className="idolList_link">TxtMain</Link>
+                        <Link to="/AkmuMain" className="idolList_link">AkmuMain</Link>
                     </section>
-                    <section className="home_p1_train">
-                        <Link to="/" className="home_link">Home</Link>
-                        <Link to="/TriplesEvent" className="home_link home_tri_link">TriplesEvent</Link>
-                        <Link to="/TxtEvent" className="home_link home_txt_link">TxtEvent</Link>
-                        <Link to="/AkmuEvent" className="home_link home_akm_link">AkmuEvent</Link>
+                    <section className="idolList_train">
+                        <Link to="/" className="idolList_link">Home</Link>
+                        <Link to="/TriplesEvent" className="idolList_link">TriplesEvent</Link>
+                        <Link to="/TxtEvent" className="idolList_link">TxtEvent</Link>
+                        <Link to="/AkmuEvent" className="idolList_link">AkmuEvent</Link>
                     </section>
-                    <section className="home_p1_train">
-                        <Link to="/" className="home_link">Home</Link>
-                        <Link to="/TriplesEvent" className="home_link home_tri_link">TriplesEvent</Link>
-                        <Link to="/TxtEvent" className="home_link home_txt_link">TxtEvent</Link>
-                        <Link to="/AkmuEvent" className="home_link home_akm_link">AkmuEvent</Link>
+                    <section className="idolList_train">
+                        <Link to="/" className="idolList_link">Home</Link>
+                        <Link to="/TriplesEvent" className="idolList_link">TriplesEvent</Link>
+                        <Link to="/TxtEvent" className="idolList_link">TxtEvent</Link>
+                        <Link to="/AkmuEvent" className="idolList_link">AkmuEvent</Link>
                     </section>
                 </section>
             </section>
