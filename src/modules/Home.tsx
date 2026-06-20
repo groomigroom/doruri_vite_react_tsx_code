fc8e987f92f7c0dd267617ea0f97388712e6aa38
groomigroom <googjohn0143@gmail.com>
2026-06-21 07:38:03 +0900

    Update Home.tsx

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+
 
 import "./home.css";
 
@@ -8,35 +8,7 @@ export default function Home() {
     return (
         <div className="home_full">
             <Menu />
-            <section className="home_p1">
-                <section className="home_p1_trainf">
-                    <section className="home_p1_train">
-                        <Link to="/" className="home_link">Home</Link>
-                        <Link to="/TriplesMain" className="home_link home_tri_link">TriplesEvent</Link>
-                        <Link to="/TxtMain" className="home_link home_txt_link">TxtMain</Link>
-                        <Link to="/AkmuMain" className="home_link home_akm_link">AkmuMain</Link>
-                    </section>
-                    <section className="home_p1_train">
-                        <Link to="/" className="home_link">Home</Link>
-                        <Link to="/TriplesEvent" className="home_link home_tri_link">TriplesEvent</Link>
-                        <Link to="/TxtEvent" className="home_link home_txt_link">TxtEvent</Link>
-                        <Link to="/AkmuEvent" className="home_link home_akm_link">AkmuEvent</Link>
-                    </section>
-                    <section className="home_p1_train">
-                        <Link to="/" className="home_link">Home</Link>
-                        <Link to="/TriplesEvent" className="home_link home_tri_link">TriplesEvent</Link>
-                        <Link to="/TxtEvent" className="home_link home_txt_link">TxtEvent</Link>
-                        <Link to="/AkmuEvent" className="home_link home_akm_link">AkmuEvent</Link>
-                    </section>
-                </section>
-            </section>
-            <section className="home_p2">
-                <section className="home_p2_con">
-                    <article></article>
-                    <article></article>
-                    <article></article>
-                </section>
-            </section>
+
         </div>
     )
 }
