44494d4637f4fc19e720dfaae98eee361d9327aa
groomigroom <googjohn0143@gmail.com>
2026-06-29 13:40:42 +0900

    Update AkmuMain.tsx

--- src/modules/akmu/main/AkmuMain.tsx
+++ src/modules/akmu/main/AkmuMain.tsx
@@ -1,8 +1,14 @@
 import { Link } from 'react-router-dom';
 
+import "./AkmuMain.css";
+import "../../triples/main/TriplesMain.css";
+
+import Menu from '../../Menu';
+
 export default function AkmuMain() {
     return (
-        <div>
+        <div className='akmu_m_full'>
+            <Menu />
             <Link to='/AkmuEvent'>이벤트로 가기</Link>
         </div>
     );
