9cdb816105427b64f17434fcb12b72d15e0fdcd4
groomigroom <googjohn0143@gmail.com>
2026-06-29 21:29:09 +0900

    Update logos and text in TxtMain component

--- src/modules/txt/main/TxtMain.tsx
+++ src/modules/txt/main/TxtMain.tsx
@@ -7,20 +7,20 @@ import Menu from '../../Menu';
 
 import Txt_m_logo from './img/txt_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
+import txt_m_img from './img/txt_m_txtimg.jpg';
 
 export default function TxtMain() {
     return (
         <div className='txt_m_full'>
             <Menu />
-            <img src={Txt_m_logo} alt="트리플에스 로고" className='tri_m_logo' />
+            <img src={Txt_m_logo} alt="투바투 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="투바투 로고 꾸미기" />투모로우바이투게더<img src={MainH2Deco} alt="투바투 로고 꾸미기" /></h2>
-            <p>
-                #####
+            <p className='tri_m_intropp'>
+                #####<br />
                 하루에 하루만 더<br />
                 &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
-
-
-            <h3>멤버</h3>
+            <img src={txt_m_img} className='tri_m_img txt_m_img' />
+            <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <article className='txt_m_mem'>
                 <img src="" alt="" />
                 <h4>수빈</h4>
