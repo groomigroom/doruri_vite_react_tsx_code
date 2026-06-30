d4e0cface8b4759d448a87070f1ecede7eef02c2
groomigroom <googjohn0143@gmail.com>
2026-07-01 08:40:30 +0900

    Add files via upload

---
+++ src/modules/doruri_intro/DoruriMapModules/DoruriMapIdolList.tsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+export default function DoruriMapIdolList() {
+    return (
+        <div className='doruri_map_idollist_full'>
+            <ul className='doruri_map_IdolList'>
+                <li className='doruri_map_TriplesMain'><Link to='/TriplesMain'>트리플에스 메인 페이지</Link>
+                    <ul>
+                        <li><Link to='/TriplesMemS1'>S1 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS2'>S2 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS3'>S3 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS4'>S4 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS5'>S5 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS6'>S6 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS7'>S7 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS8'>S8 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS9'>S9 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS10'>S10 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS11'>S11 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS12'>S12 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS13'>S13 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS14'>S14 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS15'>S15 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS16'>S16 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS17'>S17 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS18'>S18 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS19'>S19 멤버 페이지</Link></li>
+                        <li><Link to='/TriplesMemS20'>S20 멤버 페이지</Link></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    );
+}
