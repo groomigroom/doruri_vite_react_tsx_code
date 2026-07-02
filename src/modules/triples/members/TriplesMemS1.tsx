b76ecf43785dadd17305ba1b758eedba934e7754
groomigroom <googjohn0143@gmail.com>
2026-07-02 12:10:09 +0900

    Update TriplesMemS1.tsx

--- src/modules/triples/members/TriplesMemS1.tsx
+++ src/modules/triples/members/TriplesMemS1.tsx
@@ -2,7 +2,7 @@ import './TriplesMemS1.css';
 
 import Menu from '../../Menu';
 
-import Tri_mm_s1_img from '../main/img/tri_m_memS1.jpg'
+import Tri_mm_s1_img from '../main/img/tri_m_memS1.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -15,55 +15,50 @@ export default function TriplesMemS1() {
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
             <img src={Tri_mm_s1_img} alt="윤서연 이미지" className='tri_mm_s1_img' />
-            <section className='tri_mm_s1_intro'>
+            <table className='tri_mm_s1_intropa_tb'>
+                <tbody>
+                    <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2003년 8월 6일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>161cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>B형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>햄햄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>취미</p></td>
+                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>특기</p></td>
+                        <td><p>셀카찍기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td><p>아이스크림</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>소속 유닛</p></td>
+                        <td>
+                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                LOVElution, neptune</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>좋음</p></td>
+                    </tr>
+                </tbody>
+            </table>
 
-                <section className='tri_mm_s1_intropa'></section>
-                <section className='tri_mm_s1_intropa'>
-                    <table className='tri_mm_s1_intropa_tb'>
-                        <tbody>
-                            <tr className='tri_mm_s1_intropa_tb_tr1'>
-                                <td><p>출생</p></td>
-                                <td><p>2003년 8월 6일</p></td>
-                            </tr>
-                            <tr>
-                                <td><p>키</p></td>
-                                <td><p>161cm</p></td>
-                            </tr>
-                            <tr>
-                                <td><p>혈액형</p></td>
-                                <td><p>B형</p></td>
-                            </tr>
-                            <tr>
-                                <td><p>별명</p></td>
-                                <td><p>햄햄</p></td>
-                            </tr>
-                            <tr>
-                                <td><p>취미</p></td>
-                                <td><p>좋아하는 영화 여러번 보기</p></td>
-                            </tr>
-                            <tr>
-                                <td><p>특기</p></td>
-                                <td><p>셀카찍기</p></td>
-                            </tr>
-                            <tr>
-                                <td><p>좋아하는 음식</p></td>
-                                <td><p>아이스크림</p></td>
-                            </tr>
-                            <tr>
-                                <td><p>소속 유닛</p></td>
-                                <td>
-                                    <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                        LOVElution, neptune</p></td>
-                            </tr>
-                            <tr className='tri_mm_s1_intropa_tb_tr9'>
-                                <td><p>성격</p></td>
-                                <td><p>좋음</p></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </section>
 
-            </section>
 
         </div>
     );
