49a28053b7eb199bcf75d90b62afc49aab52dc40
groomigroom <googjohn0143@gmail.com>
2026-07-02 09:49:59 +0900

    Update TriplesMemS20.tsx

--- src/modules/triples/members/TriplesMemS20.tsx
+++ src/modules/triples/members/TriplesMemS20.tsx
@@ -1,9 +1,22 @@
+import './TriplesMemS1.css';
 import './TriplesMemS20.css';
+
+import Menu from '../../Menu';
+
+import Tri_mm_s20_img from '../main/img/tri_m_memS20.jpg';
+
+import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS20() {
     return (
-        <div className="tri_mm_s5_full">
-            박시온
+        <div className="tri_mm_s20_full">
+            <Menu />
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="트리플에스 로고 꾸미기" />박시온<img src={MainH2Deco} alt="트리플에스 로고 꾸미기" /></h2>
+            <p className='tri_mm_s1_intropp'>#####24명은 너무 많지 않아요?<br />
+                그리고... 그리고... 그리고...<br />
+                더 열심히 더 열심히</p>
+            <img src={Tri_mm_s20_img} alt="박시온 이미지" className='tri_mm_s1_img' />
+
         </div>
     );
 };
