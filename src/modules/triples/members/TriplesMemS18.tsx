4ae8604d4cd07f11478128b292c811a0a1309147
groomigroom <googjohn0143@gmail.com>
2026-07-02 09:38:15 +0900

    Update TriplesMemS18.tsx

--- src/modules/triples/members/TriplesMemS18.tsx
+++ src/modules/triples/members/TriplesMemS18.tsx
@@ -1,9 +1,22 @@
+import './TriplesMemS1.css';
 import './TriplesMemS18.css';
+
+import Menu from '../../Menu';
+
+import Tri_mm_s18_img from '../main/img/tri_m_memS18.jpg';
+
+import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS18() {
     return (
-        <div className="tri_mm_s5_full">
-            주빈
+        <div className="tri_mm_s18_full">
+            <Menu />
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="트리플에스 로고 꾸미기" />주빈<img src={MainH2Deco} alt="트리플에스 로고 꾸미기" /></h2>
+            <p className='tri_mm_s1_intropp'>#####24명은 너무 많지 않아요?<br />
+                그리고... 그리고... 그리고...<br />
+                더 열심히 더 열심히</p>
+            <img src={Tri_mm_s18_img} alt="주빈 이미지" className='tri_mm_s1_img' />
+
         </div>
     );
 };
