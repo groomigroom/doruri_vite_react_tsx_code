330f86070b781eed48d730a693c535e1204b69be
groomigroom <googjohn0143@gmail.com>
2026-07-02 20:02:19 +0900

    Update IoiMain.tsx

--- src/modules/ioi/IoiMain.tsx
+++ src/modules/ioi/IoiMain.tsx
@@ -1,1 +1,17 @@
+// import { Link } from 'react-router-dom';
 
+import './TriplesMain.css';
+import './IoiMain.css';
+
+// import Tri_m_logo
+
+import Menu from '../../Menu';
+
+export default function IoiMain() {
+    return (
+        <div className='ioi_m_full'>
+            <Menu />
+            <img src="" alt="아이오아이 로고" className='tri_m_logo' />
+        </div>
+    );
+};
