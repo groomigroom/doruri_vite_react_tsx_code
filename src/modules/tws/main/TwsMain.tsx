a3479a42ebce22f526fcf7f1f0a9d37e140767b1
groomigroom <googjohn0143@gmail.com>
2026-07-03 20:44:29 +0900

    Update TwsMain.tsx

--- src/modules/tws/main/TwsMain.tsx
+++ src/modules/tws/main/TwsMain.tsx
@@ -1,4 +1,4 @@
-// 신유  도훈  영재  한진  지훈  경민
+
 
 import { Link } from 'react-router-dom';
 
@@ -21,45 +21,45 @@ export default function TwsMain() {
                 &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
             <img src="" className='tri_m_img txt_m_img' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-            <section className='tri_m_mem_tr_full '>
+            <section className='tri_m_mem_tr_full tws_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/IllMemYunah' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="윤아 이미지" />
-                            <h4>윤아</h4>
+                            <img src="" alt="신유 이미지" />
+                            <h4>신유</h4>
                         </article>
                     </Link>
                     <Link to='/IllMemMinju' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="민주 이미지" />
-                            <h4>민주</h4>
+                            <img src="" alt="도훈 이미지" />
+                            <h4>도훈</h4>
                         </article>
                     </Link>
                     <Link to='/IllMemMoka' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="모카 이미지" />
-                            <h4>모카</h4>
+                            <img src="" alt="영재 이미지" />
+                            <h4>영재</h4>
                         </article>
                     </Link>
                 </section>
 
-                <section className='tri_m_mem_tr txt_m_mem_tr'>
+                <section className='tri_m_mem_tr'>
                     <Link to='/IllMemWonhee' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="원희 이미지" />
-                            <h4>원희</h4>
+                            <img src="" alt="한진 이미지" />
+                            <h4>한진</h4>
                         </article>
                     </Link>
                     <Link to='/IllMemEroha' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="이로하 이미지" />
-                            <h4>이로하</h4>
+                            <img src="" alt="지훈 이미지" />
+                            <h4>지훈</h4>
                         </article>
                     </Link>
                     <Link to='/IllMemEroha' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="이로하 이미지" />
-                            <h4>이로하</h4>
+                            <img src="" alt="경민 이미지" />
+                            <h4>경민</h4>
                         </article>
                     </Link>
 
