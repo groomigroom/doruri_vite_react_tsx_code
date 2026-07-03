8b8e11db075de28e8e3d11987da7f42668cce880
groomigroom <googjohn0143@gmail.com>
2026-07-03 12:20:26 +0900

    Update StrayKidsMain.tsx

--- src/modules/Stray_Kids/main/StrayKidsMain.tsx
+++ src/modules/Stray_Kids/main/StrayKidsMain.tsx
@@ -16,15 +16,17 @@ import MainH2Deco from '../../img/main_h2_deco.png';
 // import Txt_m_memHueningkai from './img/txt_m_mem_hueningkai.jpg';
 
 export default function StrayKidsMain() {
-    <div className='txt_m_full'>
-        <Menu />
-        <img src="" alt="스트레이 키즈 로고" className='tri_m_logo' />
-        <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" />투모로우바이투게더<img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" /></h2>
-        <p className='tri_m_intropp'>
-            #####<br />
-            하루에 하루만 더<br />
-            &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
-        <img src="" className='tri_m_img txt_m_img' />
-        <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-    </div>
+    return (
+        <div className='txt_m_full'>
+            <Menu />
+            <img src="" alt="스트레이 키즈 로고" className='tri_m_logo' />
+            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" />투모로우바이투게더<img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" /></h2>
+            <p className='tri_m_intropp'>
+                #####<br />
+                하루에 하루만 더<br />
+                &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
+            <img src="" className='tri_m_img txt_m_img' />
+            <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
+        </div>
+    );
 };
