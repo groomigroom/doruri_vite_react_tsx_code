0cc3ff7337dd8405242e36a5183e1a9b7c26f04a
groomigroom <googjohn0143@gmail.com>
2026-07-05 14:07:13 +0900

    Update DAY6Main.tsx

--- src/modules/DAY6/main/DAY6Main.tsx
+++ src/modules/DAY6/main/DAY6Main.tsx
@@ -1,1 +1,63 @@
+import { Link } from 'react-router-dom';
 
+import '../../triples/main/TriplesMain.css'
+import '../../aespa/main/AespaMain.css';
+import './DAY6Main.css';
+
+import Menu from '../../Menu';
+
+// import Txt_m_logo from './img/txt_m_logo.svg';
+import MainH2Deco from '../../img/main_h2_deco.png';
+// import txt_m_img from './img/txt_m_txtimg.jpg';
+
+export default function DAY6Main() {
+    return (
+        <div className='day_m_full'>
+            <Menu />
+            <img src="" alt="aespa 로고" className='tri_m_logo' />
+            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="DAY6 꾸미기" />DAY6<img src={MainH2Deco} alt="DAY6 꾸미기" /></h2>
+            <p className='tri_m_intropp'>
+                #####<br />
+                하루에 하루만 더<br />
+                &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
+            <img src="" className='tri_m_img txt_m_img' />
+            <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
+            <section className='tri_m_mem_tr_full aes_m_mem_tr_full'>
+                <section className='tri_m_mem_tr'>
+                    <Link to='/AesMemKarina' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src="" alt="성진 이미지" />
+                            <h4>성진</h4>
+                        </article>
+                    </Link>
+                    <Link to='/AesMemJijell' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src="" alt="Young K 이미지" />
+                            <h4>Young K</h4>
+                        </article>
+                    </Link>
+
+                </section>
+
+                <section className='tri_m_mem_tr'>
+                    <Link to='/AesMemWinter' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src="" alt="원필 이미지" />
+                            <h4>원필</h4>
+                        </article>
+                    </Link>
+                    <Link to='/AesMemNingning' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src="" alt="도운 이미지" />
+                            <h4>도운</h4>
+                        </article>
+                    </Link>
+
+
+                </section>
+
+
+            </section>
+        </div>
+    );
+};
