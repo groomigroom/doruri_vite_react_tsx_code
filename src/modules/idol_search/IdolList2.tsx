e0e11f011d5f73f58f58eb720433c4b693b5d664
groomigroom <googjohn0143@gmail.com>
2026-07-05 16:08:44 +0900

    Update IdolList2.tsx

--- src/modules/idol_search/IdolList2.tsx
+++ src/modules/idol_search/IdolList2.tsx
@@ -1,1 +1,148 @@
+import { Link } from 'react-router-dom';
 
+import './IdolList.css';
+import './IdolList2.css';
+
+import IdL_Triples from '../triples/main/img/tri_m_img.jpg'
+import IdL_Txt from '../txt/main/img/txt_m_txtimg.jpg'
+import IdL_Akmu from '../akmu/main/img/aknu_m_akmuimg.jpg'
+
+import Menu from '../Menu';
+
+export default function IdolList2() {
+    return (
+        <div className="idolList2_full">
+            <Menu />
+            <section className="idolList_part">
+                <section className="idolList_trainf">
+                    <section className="idolList_train">
+                        <div className="idolList_link">
+                            <Link to="/TriplesMain" className="idolList_link_a idolList_link_tri">
+                                <img src={IdL_Triples} alt="트리플에스" />
+                                <div>
+                                    <h4>tripleS</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/TxtMain" className="idolList_link_a idolList_link_txt">
+                                <img src={IdL_Txt} alt="투바투" />
+                                <div>
+                                    <h4>투모로우<br />
+                                        바이<br />
+                                        투게더
+                                    </h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/AkmuMain" className="idolList_link_a idolList_link_akmu">
+                                <img src={IdL_Akmu} alt="악뮤" />
+                                <div>
+                                    <h4>악뮤</h4>
+
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/IoiMain" className="idolList_link_a idolList_link_ioi">
+                                <img src="" alt="아이오아이" />
+                                <div>
+                                    <h4>아이오아이</h4>
+                                </div>
+                            </Link>
+                        </div>
+                    </section>
+                    <section className="idolList_train">
+                        <div className="idolList_link">
+                            <Link to="/StrayKidsMain" className="idolList_link_a idolList_link_str">
+                                <img src="" alt="Stray Kids" />
+                                <div>
+                                    <h4>Stray Kids</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/IllitMain" className="idolList_link_a idolList_link_str">
+                                <img src="" alt="ILLIT" />
+                                <div>
+                                    <h4>ILLIT</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/TwsMain" className="idolList_link_a idolList_link_str">
+                                <img src="" alt="TWS" />
+                                <div>
+                                    <h4>TWS</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/ResMain" className="idolList_link_a idolList_link_str">
+                                <img src="" alt="RESCENE" />
+                                <div>
+                                    <h4>RESCENE</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                    </section>
+
+                    <section className="idolList_train">
+                        <div className="idolList_link">
+                            <Link to="/TenCMMain" className="idolList_link_a idolList_link_str">
+                                <img src="" alt="10CM" />
+                                <div>
+                                    <h4>10CM</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/AespaMain" className="idolList_link_a idolList_link_str">
+                                <img src="" alt="aespa" />
+                                <div>
+                                    <h4>aespa</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/ALLDAY_PROJECTMain" className="idolList_link_a idolList_link_str">
+                                <img src="" alt="ALLDAY PROJECT" />
+                                <div>
+                                    <h4>ALLDAY<br />
+                                        PROJECT</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                        <div className="idolList_link">
+                            <Link to="/DAY6Main" className="idolList_link_a idolList_link_str">
+                                <img src="" alt="DAY6" />
+                                <div>
+                                    <h4>DAY6</h4>
+                                </div>
+                            </Link>
+                        </div>
+
+                    </section>
+                </section>
+            </section>
+            <section className="idolList_12345">
+                <Link to='/IdolList'>1</Link>
+                <Link to='/'>2</Link>
+                <Link to='/'>3</Link>
+                <Link to='/'>4</Link>
+                <Link to='/'>5</Link>
+            </section>
+        </div>
+    );
+};
