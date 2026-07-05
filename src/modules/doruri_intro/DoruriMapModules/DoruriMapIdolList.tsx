a1ff422963b7a32157a84edcd7b3be243113265e
groomigroom <googjohn0143@gmail.com>
2026-07-05 19:07:08 +0900

    Update DoruriMapIdolList.tsx

--- src/modules/doruri_intro/DoruriMapModules/DoruriMapIdolList.tsx
+++ src/modules/doruri_intro/DoruriMapModules/DoruriMapIdolList.tsx
@@ -9,6 +9,7 @@ export default function DoruriMapIdolList() {
         <div className='doruri_map_idollist_full'>
             <Menu />
             <Link to='/DoruriMap' className='doruri_map_idollist_goback'>뒤로가기</Link>
+            <Link to='/IdolList' className='doruri_map_idollist_idollist'>목록 보러가기</Link>
             <nav className='doruri_map_idollist_nav'>
                 <ul>
                     <li className='doruri_map_triplesmain_li'><Link to='/TriplesMain'>tripleS 메인 페이지</Link>
