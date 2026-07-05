e305e64725f1c469097602d77f6e4a65507302a6
groomigroom <googjohn0143@gmail.com>
2026-07-06 08:37:28 +0900

    Update DoruriMapIdolList2.tsx

--- src/modules/doruri_intro/DoruriMapModules/DoruriMapIdolList2.tsx
+++ src/modules/doruri_intro/DoruriMapModules/DoruriMapIdolList2.tsx
@@ -1,1 +1,27 @@
+import { Link } from 'react-router-dom';
 
+import './DoruriMapIdolList.css';
+
+import Menu from '../../Menu';
+
+export default function DoruriMapIdolList2() {
+    return (
+        <div className='doruri_map_idollist_full'>
+            <Menu />
+            <Link to='/DoruriMap' className='doruri_map_idollist_goback'>뒤로가기</Link>
+            <Link to='/IdolList' className='doruri_map_idollist_idollist'>목록 보러가기</Link>
+            <nav className='doruri_map_idollist_nav'>
+                <ul>
+                    <li className='doruri_map_triplesmain_li'><Link to='/TriplesMain'>tripleS 메인 페이지</Link>
+                        <ul>
+
+                        </ul>
+                    </li>
+
+                </ul>
+
+
+            </nav>
+        </div >
+    );
+}
