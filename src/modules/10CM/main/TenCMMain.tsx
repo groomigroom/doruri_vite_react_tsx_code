6a4b7aec9a93118762e2a6bfadc7276e97ad4abd
groomigroom <googjohn0143@gmail.com>
2026-07-06 10:12:40 +0900

    Update TenCMMain.tsx

--- src/modules/10CM/main/TenCMMain.tsx
+++ src/modules/10CM/main/TenCMMain.tsx
@@ -16,9 +16,10 @@ export default function TenCMMain() {
             <img src="" alt="10CM 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="10CM 꾸미기" />10CM<img src={MainH2Deco} alt="10CM 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                #####<br />
-                하루에 하루만 더<br />
-                &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
+                일말의 가능성이 없더라도<br />
+                &apos;10CM&apos;와 함께<br />
+                오늘도 힘내 보아요!
+            </p>
             <img src="" className='tri_m_img txt_m_img' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full ten_m_mem_tr_full'>
