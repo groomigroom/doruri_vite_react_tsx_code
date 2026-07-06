4de9901a291a5dbfbe8768fb25450f9f744d0032
groomigroom <googjohn0143@gmail.com>
2026-07-07 08:22:09 +0900

    Update IllitMain.tsx

--- src/modules/illit/main/IllitMain.tsx
+++ src/modules/illit/main/IllitMain.tsx
@@ -25,9 +25,10 @@ export default function IllitMain() {
             <img src={Ill_m_logo} alt="illit 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="illit 로고 꾸미기" />ILLIT<img src={MainH2Deco} alt="illit 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                #####<br />
-                하루에 하루만 더<br />
-                &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
+                &apos;ILLIT&apos;과 함께<br />
+                Who's your bias?<br />
+                I'm your bias!
+            </p>
             <img src={Ill_m_img} className='tri_m_img' alt='illit' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full ill_m_mem_tr_full'>
