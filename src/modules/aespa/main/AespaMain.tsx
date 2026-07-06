f94e3c61eb17e112c01c141469480dc77b6b9d21
groomigroom <googjohn0143@gmail.com>
2026-07-07 07:04:56 +0900

    Update AespaMain.tsx

--- src/modules/aespa/main/AespaMain.tsx
+++ src/modules/aespa/main/AespaMain.tsx
@@ -27,7 +27,7 @@ export default function AespaMain() {
                 #####<br />
                 하루에 하루만 더<br />
                 &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
-            <img src={Aes_m_img} className='tri_m_img txt_m_img' alt='aespa' />
+            <img src={Aes_m_img} className='tri_m_img' alt='aespa' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full aes_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
