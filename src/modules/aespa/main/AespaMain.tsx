c7998b5b6d646e1dd5bc0b0b2b2625a37f590f36
groomigroom <googjohn0143@gmail.com>
2026-07-06 14:04:42 +0900

    Update AespaMain.tsx

--- src/modules/aespa/main/AespaMain.tsx
+++ src/modules/aespa/main/AespaMain.tsx
@@ -5,31 +5,41 @@ import './AespaMain.css';
 
 import Menu from '../../Menu';
 
+import Aes_m_logo from './img/Aes_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
+import Aes_m_img from './img/aespa_m_aespa.jpg';
+
+import Aes_m_MemKarina_img from './img/aespa_m_mem_Karina.jpg';
+import Aes_m_MemJijell_img from './img/aespa_m_mem_Jijel.jpg';
+import Aes_m_MemWinter_img from './img/aespa_m_mem_Winter.jpg';
+import Aes_m_MemNingning_img from './img/aespa_m_mem_Ningning.jpg';
+
+//191919
+//ffffff
 
 export default function AespaMain() {
     return (
         <div className='aes_m_full'>
             <Menu />
-            <img src="" alt="aespa 로고" className='tri_m_logo' />
+            <img src={Aes_m_logo} alt="aespa 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="aespa 로고 꾸미기" />aespa<img src={MainH2Deco} alt="aespa 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
                 #####<br />
                 하루에 하루만 더<br />
                 &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
-            <img src="" className='tri_m_img txt_m_img' />
+            <img src={Aes_m_img} className='tri_m_img txt_m_img' alt='aespa' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full aes_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/AesMemKarina' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="카리나 이미지" />
+                            <img src={Aes_m_MemKarina_img} alt="카리나 이미지" />
                             <h4>카리나</h4>
                         </article>
                     </Link>
                     <Link to='/AesMemJijell' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="지젤 이미지" />
+                            <img src={Aes_m_MemJijell_img} alt="지젤 이미지" />
                             <h4>지젤</h4>
                         </article>
                     </Link>
@@ -39,13 +49,13 @@ export default function AespaMain() {
                 <section className='tri_m_mem_tr'>
                     <Link to='/AesMemWinter' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="윈터 이미지" />
+                            <img src={Aes_m_MemWinter_img} alt="윈터 이미지" />
                             <h4>윈터</h4>
                         </article>
                     </Link>
                     <Link to='/AesMemNingning' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="닝닝 이미지" />
+                            <img src={Aes_m_MemNingning_img} alt="닝닝 이미지" />
                             <h4>닝닝</h4>
                         </article>
                     </Link>
