e552c3d3e0f9d28ae2a7f0b0bd7e41b8a4208794
groomigroom <googjohn0143@gmail.com>
2026-07-07 06:45:22 +0900

    Update TwsMain.tsx

--- src/modules/tws/main/TwsMain.tsx
+++ src/modules/tws/main/TwsMain.tsx
@@ -29,7 +29,7 @@ export default function TwsMain() {
                 #####<br />
                 하루에 하루만 더<br />
                 &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
-            <img src={Tws_m_img} className='tri_m_img txt_m_img' alt='tws' />
+            <img src={Tws_m_img} className='tri_m_img' alt='tws' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full tws_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
