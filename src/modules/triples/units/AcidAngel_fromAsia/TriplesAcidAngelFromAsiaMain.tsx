85e340fb8f5207889a1a8fe35f228e44e97c398d
groomigroom <googjohn0143@gmail.com>
2026-07-07 08:55:44 +0900

    Update TriplesAcidAngelFromAsiaMain.tsx

--- src/modules/triples/units/AcidAngel_fromAsia/TriplesAcidAngelFromAsiaMain.tsx
+++ src/modules/triples/units/AcidAngel_fromAsia/TriplesAcidAngelFromAsiaMain.tsx
@@ -1,11 +1,11 @@
 import { Link } from 'react-router-dom';
 
 import '../../main/TriplesMain.css';
-import './AespaMain.css';
+import './TriplesAcidAngelFromAsiaMain.css';
 
 import Menu from '../../../Menu';
 
-// import Aes_m_logo from './img/Aes_m_logo.svg';
+import TriplesAAA_m_logo from './img/TriplesAcidAngelFromAsia_m_logo.svg';
 import MainH2Deco from '../../../img/main_h2_deco.png';
 // import Aes_m_img from './img/aespa_m_aespa.jpg';
 
@@ -20,26 +20,26 @@ export default function TriplesAcidAngelFromAsiaMain() {
     return (
         <div className='tri_aaa_m_full'>
             <Menu />
-            <img src="" alt="aespa 로고" className='tri_m_logo' />
-            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="tri_aaa 로고 꾸미기" />aespa<img src={MainH2Deco} alt="tri_aaa 로고 꾸미기" /></h2>
+            <img src={TriplesAAA_m_logo} alt="tri_aaa 로고" className='tri_m_logo' />
+            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="tri_aaa 로고 꾸미기" />Acid Angel from Asia<img src={MainH2Deco} alt="tri_aaa 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                &apos;aespa&apos;와 함께<br />
-                다른 세계에서<br />
-                Lemonade를 만들어 봐요!
+                &apos;Acid Angel from Asia&apos;와 함께<br />
+                여기에서 Together<br />
+                무질서한 그대로
             </p>
-            <img src={Aes_m_img} className='tri_m_img' alt='aespa' />
+            <img src="" className='tri_m_img' alt='tri_aaa' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full aes_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/AesMemKarina' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Aes_m_MemKarina_img} alt="카리나 이미지" />
+                            <img src="" alt="카리나 이미지" />
                             <h4>카리나</h4>
                         </article>
                     </Link>
                     <Link to='/AesMemJijell' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Aes_m_MemJijell_img} alt="지젤 이미지" />
+                            <img src="" alt="지젤 이미지" />
                             <h4>지젤</h4>
                         </article>
                     </Link>
@@ -49,13 +49,13 @@ export default function TriplesAcidAngelFromAsiaMain() {
                 <section className='tri_m_mem_tr'>
                     <Link to='/AesMemWinter' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Aes_m_MemWinter_img} alt="윈터 이미지" />
+                            <img src="" alt="윈터 이미지" />
                             <h4>윈터</h4>
                         </article>
                     </Link>
                     <Link to='/AesMemNingning' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Aes_m_MemNingning_img} alt="닝닝 이미지" />
+                            <img src="" alt="닝닝 이미지" />
                             <h4>닝닝</h4>
                         </article>
                     </Link>
