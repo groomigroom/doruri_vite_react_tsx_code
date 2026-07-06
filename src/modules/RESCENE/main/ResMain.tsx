8df4efb69c07df889452e25adbbf4f7eefb68bb8
groomigroom <googjohn0143@gmail.com>
2026-07-07 08:16:47 +0900

    Update ResMain.tsx

--- src/modules/RESCENE/main/ResMain.tsx
+++ src/modules/RESCENE/main/ResMain.tsx
@@ -25,9 +25,10 @@ export default function ResMain() {
             <img src={Res_m_logo} alt="RESCENE 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="RESCENE 꾸미기" />RESCENE<img src={MainH2Deco} alt="RESCENE 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                #####<br />
-                하루에 하루만 더<br />
-                &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
+                &apos;RESCENE&apos;와 함께<br />
+                좋은 곳으로<br />
+                Runaway!
+            </p>
             <img src={Res_m_img} className='tri_m_img' alt="rescene" />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full res_m_mem_tr_full'>
