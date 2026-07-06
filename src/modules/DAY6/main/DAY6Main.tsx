0c539d3411ac1a2a753856232474071d42862572
groomigroom <googjohn0143@gmail.com>
2026-07-06 10:13:49 +0900

    Update DAY6Main.tsx

--- src/modules/DAY6/main/DAY6Main.tsx
+++ src/modules/DAY6/main/DAY6Main.tsx
@@ -17,9 +17,10 @@ export default function DAY6Main() {
             <img src="" alt="aespa 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="DAY6 꾸미기" />DAY6<img src={MainH2Deco} alt="DAY6 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                #####<br />
-                하루에 하루만 더<br />
-                &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
+                혹시 꿈을 찾고 있나요?<br />
+                아니면 접고 있나요?<br />
+                &apos;DAY6&apos;와 함께 꿈을 꿔봐요!
+            </p>
             <img src="" className='tri_m_img txt_m_img' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full aes_m_mem_tr_full'>
