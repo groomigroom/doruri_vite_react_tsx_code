96b908c71c6bd1d4d44cdac10f4691a19e05b576
groomigroom <googjohn0143@gmail.com>
2026-07-08 07:03:15 +0900

    Update StrayMemFilix.tsx

--- src/modules/Stray_Kids/members/StrayMemFilix.tsx
+++ src/modules/Stray_Kids/members/StrayMemFilix.tsx
@@ -3,7 +3,7 @@ import './StrayMemFilix.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Str_mm_filix_img from '../main/img/stray_m_mem_Filix.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -15,7 +15,7 @@ export default function StrayMemFilix() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src="" alt="필릭스 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Str_mm_filix_img} alt="필릭스 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
