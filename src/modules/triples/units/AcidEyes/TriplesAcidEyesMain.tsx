33ead4fe3b33d8616b99f9e7e2f985c7376eeab9
groomigroom <googjohn0143@gmail.com>
2026-07-07 12:25:51 +0900

    Update TriplesAcidEyesMain.tsx

--- src/modules/triples/units/AcidEyes/TriplesAcidEyesMain.tsx
+++ src/modules/triples/units/AcidEyes/TriplesAcidEyesMain.tsx
@@ -21,20 +21,26 @@ export default function TriplesAcidEyesMain() {
         <div className='tri_aci_m_full'>
             <Menu />
             <img src={TriplesAci_m_logo} alt="tri_aci 로고" className='tri_m_logo' />
-            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="tri_aaa 로고 꾸미기" />+&#40;KR&#41;ystal Eyes<img src={MainH2Deco} alt="tri_aaa 로고 꾸미기" /></h2>
+            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="tri_aci 로고 꾸미기" />Acid Eyes<img src={MainH2Deco} alt="tri_aci 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                &apos;+&#40;KR&#41;ystal Eyes&apos;와 함께<br />
-                더 빠른 하루로<br />
+                &apos;Acid Eyes&apos;와 함께<br />
+                여기에서 Together<br />
                 Boom cherry talk!
             </p>
-            <img src="" className='tri_m_img tri_kre_m_img' alt='tri_kre' />
+            <img src="" className='tri_m_img tri_aci_m_img' alt='tri_aci' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-            <section className='tri_m_mem_tr_full tri_kre_m_mem_tr_full'>
+            <section className='tri_m_mem_tr_full tri_aci_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/TriplesMemS1' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src="" alt="윤서연 이미지" />
                             <h4>윤서연</h4>
+                        </article>
+                    </Link>
+                    <Link to='/TriplesMemS2' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src="" alt="정혜린 이미지" />
+                            <h4>정혜린</h4>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS3' className='tri_m_mem_a'>
@@ -43,20 +49,38 @@ export default function TriplesAcidEyesMain() {
                             <h4>이지우</h4>
                         </article>
                     </Link>
-
-                </section>
-
-                <section className='tri_m_mem_tr'>
                     <Link to='/TriplesMemS4' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src="" alt="김채연 이미지" />
                             <h4>김채연</h4>
                         </article>
                     </Link>
+
+                </section>
+
+                <section className='tri_m_mem_tr'>
+                    <Link to='/TriplesMemS5' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src="" alt="김유연 이미지" />
+                            <h4>김유연</h4>
+                        </article>
+                    </Link>
                     <Link to='/TriplesMemS6' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src="" alt="김수민 이미지" />
                             <h4>김수민</h4>
+                        </article>
+                    </Link>
+                    <Link to='/TriplesMemS7' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src="" alt="김나경 이미지" />
+                            <h4>김나경</h4>
+                        </article>
+                    </Link>
+                    <Link to='/TriplesMemS8' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src="" alt="공유빈 이미지" />
+                            <h4>공유빈</h4>
                         </article>
                     </Link>
 
