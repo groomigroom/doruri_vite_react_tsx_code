dfda9793eac3abebc4eeff15c40622a7072f677a
groomigroom <googjohn0143@gmail.com>
2026-07-07 14:37:06 +0900

    Update TriplesLOVElutionMain.tsx

--- src/modules/triples/units/LOVElution/TriplesLOVElutionMain.tsx
+++ src/modules/triples/units/LOVElution/TriplesLOVElutionMain.tsx
@@ -7,12 +7,16 @@ import Menu from '../../../Menu';
 
 import TriplesLov_m_logo from './img/TriplesLov_m_logo.svg';
 import MainH2Deco from '../../../img/main_h2_deco.png';
-// import Aes_m_img from './img/aespa_m_aespa.jpg';
+import TriplesLov_m_img from './img/TriplesLov_m_img.jpg';
 
-// import Aes_m_MemKarina_img from './img/aespa_m_mem_Karina.jpg';
-// import Aes_m_MemJijell_img from './img/aespa_m_mem_Jijel.jpg';
-// import Aes_m_MemWinter_img from './img/aespa_m_mem_Winter.jpg';
-// import Aes_m_MemNingning_img from './img/aespa_m_mem_Ningning.jpg';
+import TriplesLov_m_S1_img from './img/TriplesLov_m_S1_img.jpg';
+import TriplesLov_m_S2_img from './img/TriplesLov_m_S2_img.jpg';
+import TriplesLov_m_S8_img from './img/TriplesLov_m_S8_img.jpg';
+import TriplesLov_m_S9_img from './img/TriplesLov_m_S9_img.jpg';
+import TriplesLov_m_S10_img from './img/TriplesLov_m_S10_img.jpg';
+import TriplesLov_m_S13_img from './img/TriplesLov_m_S13_img.jpg';
+import TriplesLov_m_S14_img from './img/TriplesLov_m_S14_img.jpg';
+import TriplesLov_m_S15_img from './img/TriplesLov_m_S15_img.jpg';
 
 //#000000
 
@@ -27,31 +31,31 @@ export default function TriplesLOVElutionMain() {
                 지금 내가 자신 있게<br />
                 반짝일 수 있게!
             </p>
-            <img src="" className='tri_m_img tri_lov_m_img' alt='tri_aci' />
+            <img src={TriplesLov_m_img} className='tri_m_img tri_lov_m_img' alt='tri_aci' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full tri_lov_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/TriplesMemS1' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="윤서연 이미지" />
+                            <img src={TriplesLov_m_S1_img} alt="윤서연 이미지" />
                             <h4>윤서연</h4>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS2' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="정혜린 이미지" />
+                            <img src={TriplesLov_m_S2_img} alt="정혜린 이미지" />
                             <h4>정혜린</h4>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS8' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="공유빈 이미지" />
+                            <img src={TriplesLov_m_S8_img} alt="공유빈 이미지" />
                             <h4>공유빈</h4>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS9' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="카에데 이미지" />
+                            <img src={TriplesLov_m_S9_img} alt="카에데 이미지" />
                             <h4>카에데</h4>
                         </article>
                     </Link>
@@ -61,25 +65,25 @@ export default function TriplesLOVElutionMain() {
                 <section className='tri_m_mem_tr'>
                     <Link to='/TriplesMemS10' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="서다현 이미지" />
+                            <img src={TriplesLov_m_S10_img} alt="서다현 이미지" />
                             <h4>서다현</h4>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS13' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="니엔 이미지" />
+                            <img src={TriplesLov_m_S13_img} alt="니엔 이미지" />
                             <h4>니엔</h4>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS14' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="박소현 이미지" />
+                            <img src={TriplesLov_m_S14_img} alt="박소현 이미지" />
                             <h4>박소현</h4>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS15' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="신위 이미지" />
+                            <img src={TriplesLov_m_S15_img} alt="신위 이미지" />
                             <h4>신위</h4>
                         </article>
                     </Link>
