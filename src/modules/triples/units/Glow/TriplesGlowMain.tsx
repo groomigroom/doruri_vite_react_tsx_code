c3190dc66f393b24d68eb03945c1b6fa15136582
groomigroom <googjohn0143@gmail.com>
2026-07-07 16:09:55 +0900

    Update TriplesGlowMain.tsx

--- src/modules/triples/units/Glow/TriplesGlowMain.tsx
+++ src/modules/triples/units/Glow/TriplesGlowMain.tsx
@@ -1,64 +1,59 @@
 import { Link } from 'react-router-dom';
 
 import '../../main/TriplesMain.css';
-import './TriplesAcidAngelFromAsiaMain.css';
+import './TriplesGlowMain.css';
 
 import Menu from '../../../Menu';
 
-import TriplesAAA_m_logo from './img/TriplesAcidAngelFromAsia_m_logo.svg';
+import TriplesGlow_m_logo from './img/TriplesGlow_m_logo.svg';
 import MainH2Deco from '../../../img/main_h2_deco.png';
-import TriplesAAA_m_img from './img/TriAaa_m_img.jpg';
-
-import TriplesAAA_m_S2_img from './img/TriAaa_m_S2_mem_img.png';
-import TriplesAAA_m_S5_img from './img/TriAaa_m_S5_mem_img.png';
-import TriplesAAA_m_S7_img from './img/TriAaa_m_S7_mem_img.png';
-import TriplesAAA_m_S8_img from './img/TriAaa_m_S8_mem_img.png';
+// import TriplesAAA_m_img from './img/TriAaa_m_img.jpg';
 
 
 
-//DB3475
+//000000
 
 export default function TriplesGlowMain() {
     return (
-        <div className='tri_aaa_m_full'>
+        <div className='tri_glo_m_full'>
             <Menu />
-            <img src={TriplesAAA_m_logo} alt="tri_aaa 로고" className='tri_m_logo' />
-            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="tri_aaa 로고 꾸미기" />Acid Angel from Asia<img src={MainH2Deco} alt="tri_aaa 로고 꾸미기" /></h2>
+            <img src={TriplesGlow_m_logo} alt="tri_glow 로고" className='tri_m_logo' />
+            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="tri_glow 로고 꾸미기" />Glow<img src={MainH2Deco} alt="tri_glow 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
                 &apos;Acid Angel from Asia&apos;와 함께<br />
-                여기에서 Together<br />
+                ########여기에서 Together<br />
                 무질서한 그대로
             </p>
-            <img src={TriplesAAA_m_img} className='tri_m_img tri_aaa_m_img' alt='tri_aaa' />
+            <img src="" className='tri_m_img tri_glo_m_img' alt='tri_glow' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-            <section className='tri_m_mem_tr_full tri_aaa_m_mem_tr_full'>
+            <section className='tri_m_mem_tr_full tri_glo_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
-                    <Link to='/TriplesMemS2' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS21' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={TriplesAAA_m_S2_img} alt="정혜린 이미지" />
-                            <h4>정혜린</h4>
+                            <img src="" alt="김채원 이미지" />
+                            <h4>김채원</h4>
                         </article>
                     </Link>
-                    <Link to='/TriplesMemS5' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS22' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={TriplesAAA_m_S5_img} alt="김유연 이미지" />
-                            <h4>김유연</h4>
+                            <img src="" alt="설린 이미지" />
+                            <h4>설린</h4>
                         </article>
                     </Link>
 
                 </section>
 
                 <section className='tri_m_mem_tr'>
-                    <Link to='/TriplesMemS7' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS23' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={TriplesAAA_m_S7_img} alt="김나경 이미지" />
-                            <h4>김나경</h4>
+                            <img src="" alt="서아 이미지" />
+                            <h4>서아</h4>
                         </article>
                     </Link>
-                    <Link to='/TriplesMemS8' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS24' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={TriplesAAA_m_S8_img} alt="공유빈 이미지" />
-                            <h4>공유빈</h4>
+                            <img src="" alt="지연 이미지" />
+                            <h4>지연</h4>
                         </article>
                     </Link>
 
@@ -74,6 +69,4 @@ export default function TriplesGlowMain() {
 };
 
 
-.tsx
 
-000000
