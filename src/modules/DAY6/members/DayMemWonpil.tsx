b7dc43c27e04ebb258b8d4ea904e387a6c630148
groomigroom <googjohn0143@gmail.com>
2026-07-07 20:56:15 +0900

    Update DayMemWonpil.tsx

--- src/modules/DAY6/members/DayMemWonpil.tsx
+++ src/modules/DAY6/members/DayMemWonpil.tsx
@@ -3,7 +3,7 @@ import './DayMemWonpil.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Day_mm_wonpil_img from '../main/img/day6_m_mem_Wonpil.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -15,7 +15,7 @@ export default function DayMemWonpil() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src="" alt="원필 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Day_mm_wonpil_img} alt="원필 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
