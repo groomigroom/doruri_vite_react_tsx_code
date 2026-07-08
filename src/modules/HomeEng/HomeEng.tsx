48223d436cf8d48c3260f93eeaf0173209497c00
groomigroom <googjohn0143@gmail.com>
2026-07-09 08:32:24 +0900

    Replace Menu component with MenuEng in HomeEng

--- src/modules/HomeEng/HomeEng.tsx
+++ src/modules/HomeEng/HomeEng.tsx
@@ -2,12 +2,14 @@ import { Link } from "react-router-dom";
 
 import "../home.css";
 
+import MenuEng from "./MenuEng";
+
 // import Menu from './Menu';
 
 export default function HomeEng() {
     return (
         <div className="home_full">
-            {/* <Menu /> */}
+            <MenuEng />
             <section className="home_translate">
                 <Link to='/'>한국어</Link>
                 <Link to='/Eng' className="on">English</Link>
