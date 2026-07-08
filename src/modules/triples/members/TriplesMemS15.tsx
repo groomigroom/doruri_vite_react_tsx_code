b4520576ef79a4d7c68ce2cefc819550ca6a939b
groomigroom <googjohn0143@gmail.com>
2026-07-08 10:14:54 +0900

    Update TriplesMemS15.tsx

--- src/modules/triples/members/TriplesMemS15.tsx
+++ src/modules/triples/members/TriplesMemS15.tsx
@@ -12,9 +12,10 @@ export default function TriplesMemS15() {
         <div className="tri_mm_s15_full">
             <Menu />
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="트리플에스 로고 꾸미기" />신위<img src={MainH2Deco} alt="트리플에스 로고 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>#####24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                적극적으로<br />
+                깊은 배려심과 함께<br />
+                여러분께 찾아 갈게요!</p>
             <img src={Tri_mm_s15_img} alt="신위 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
