e8c2977597001613002f8940623170b00284c2a5
groomigroom <googjohn0143@gmail.com>
2026-07-09 07:18:02 +0900

    Update Menu.tsx

--- src/modules/Menu.tsx
+++ src/modules/Menu.tsx
@@ -1,18 +1,35 @@
+import { useState } from 'react';
+
 import { Link } from 'react-router-dom';
 
 import './Menu.css';
 
 export default function Menu() {
+    const [MenuMOn, MenuMIsOn] = useState<boolean>(false);
+
+    const MenuOnToggle = () => {
+        MenuMIsOn(true);
+    };
+
+    const MenuOffToggle = () => {
+        MenuMIsOn(false);
+    };
+
+    // const [MenuLiOn, MenuLiIsOn] = useState<boolean>(false);
+
+
+
     return (
         <div className="Menu_full">
             <header>
                 <Link to='/' className='menu_logo' >
                     <img src="" alt="" />
                 </Link>
-
-                <nav className='menu_nav'>
+                <div className="menu_mobile_button" onClick={MenuOnToggle}>보기</div>
+                <nav className={`menu_nav ${MenuMOn ? 'on' : ''}`}>
+                    <div className="menu_mobile_off" onClick={MenuOffToggle}>닫기</div>
                     <ul>
-                        <li><a href="#">도루리는?</a>
+                        <li><a href="#">도루리는?</a><button className='menu_nav_li_btn1'>메뉴 버튼1</button>
                             <ul>
                                 <li><a href="#">소개</a></li>
                                 <li><a href="/DoruriMap">도루리 사이트 구조</a></li>
@@ -20,9 +37,10 @@ export default function Menu() {
                                 <li><a href="#">세부4</a></li>
                             </ul>
                         </li>
-                        <li><a href="#">아이돌 탐색</a>
+                        <li>
+                            <a href='#'>아이돌 탐색</a>
                             <ul>
-                                <li><Link to='/IdolList'>목록</Link></li>
+                                <li><Link to='/IdolList' onClick={MenuOffToggle}>목록</Link></li>
                                 <li><a href="#">세부2</a></li>
                                 <li><a href="#">세부3</a></li>
                                 <li><a href="#">세부4</a></li>
