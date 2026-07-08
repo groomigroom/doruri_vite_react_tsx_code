815d1d20498de8b1c21fdccd1aa82f0be18a7ab9
groomigroom <googjohn0143@gmail.com>
2026-07-09 08:32:02 +0900

    Implement MenuEng component with toggle functionality

--- src/modules/HomeEng/MenuEng.tsx
+++ src/modules/HomeEng/MenuEng.tsx
@@ -1,1 +1,71 @@
+import { useState } from 'react';
+
+import { Link } from 'react-router-dom';
+
+import '../Menu.css';
+
+export default function MenuEng() {
+    const [MenuEngMOn, MenuEngMIsOn] = useState<boolean>(false);
+
+    const MenuEngOnToggle = () => {
+        MenuEngMIsOn(true);
+    };
+
+    const MenuEngOffToggle = () => {
+        MenuEngMIsOn(false);
+    };
+
+    // const [MenuLiOn, MenuLiIsOn] = useState<boolean>(false);
+
+
+
+    return (
+        <div className="Menu_full">
+            <header>
+                <Link to='/' className='menu_logo' >
+                    <img src="" alt="" />
+                </Link>
+                <div className="menu_mobile_button" onClick={MenuEngOnToggle}>보기</div>
+                <nav className={`menu_nav ${MenuEngMOn ? 'on' : ''}`}>
+                    <div className="menu_mobile_off" onClick={MenuEngOffToggle}>닫기</div>
+                    <ul>
+                        <li><a href="#">도루리는?</a><button className='menu_nav_li_btn1'>메뉴 버튼1</button>
+                            <ul>
+                                <li><a href="#">소개</a></li>
+                                <li><a href="/DoruriMap">도루리 사이트 구조</a></li>
+                                <li><a href="#">세부3</a></li>
+                                <li><a href="#">세부4</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href='#'>아이돌 탐색</a>
+                            <ul>
+                                <li><Link to='/IdolList' onClick={MenuEngOffToggle}>목록</Link></li>
+                                <li><a href="#">세부2</a></li>
+                                <li><a href="#">세부3</a></li>
+                                <li><a href="#">세부4</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">메뉴1</a>
+                            <ul>
+                                <li><a href="#">세부1</a></li>
+                                <li><a href="#">세부2</a></li>
+                                <li><a href="#">세부3</a></li>
+                                <li><a href="#">세부4</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">메뉴1</a>
+                            <ul>
+                                <li><a href="#">세부1</a></li>
+                                <li><a href="#">세부2</a></li>
+                                <li><a href="#">세부3</a></li>
+                                <li><a href="#">세부4</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    );
+}
 
