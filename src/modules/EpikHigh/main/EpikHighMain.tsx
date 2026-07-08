4bc106320f9cb56f5226ee117131bf3c0ddd089d
groomigroom <googjohn0143@gmail.com>
2026-07-08 21:12:01 +0900

    Update EpikHighMain.tsx

--- src/modules/EpikHigh/main/EpikHighMain.tsx
+++ src/modules/EpikHigh/main/EpikHighMain.tsx
@@ -5,73 +5,45 @@ import './EpikHighMain.css';
 
 import Menu from '../../Menu';
 
-import Tws_m_logo from './img/Tws_m_logo.svg';
+import EpikHigh_m_logo from './img/EpikHigh_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-import Tws_m_img from './img/tws_m_tws.jpg';
+// import Tws_m_img from './img/tws_m_tws.jpg';
 
-import Tws_m_MemSinyu_img from './img/tws_m_mem_Sinyu.jpg';
-import Tws_m_MemDohun_img from './img/tws_m_mem_Dohun.jpg';
-import Tws_m_MemYoungjae_img from './img/tws_m_mem_Youngjae.jpg';
-import Tws_m_MemHanjin_img from './img/tws_m_mem_Hanjin.jpg';
-import Tws_m_MemJihun_img from './img/tws_m_mem_Jihun.jpg';
-import Tws_m_MemGyungmin_img from './img/tws_m_mem_Kyungmin.jpg';
 
 //#000000
 
 export default function EpikHighMain() {
     return (
-        <div className='tws_m_full'>
+        <div className='epi_m_full'>
             <Menu />
-            <img src={Tws_m_logo} alt="TWS 로고" className='tri_m_logo' />
-            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="TWS 로고 꾸미기" />TWS<img src={MainH2Deco} alt="TWS 로고 꾸미기" /></h2>
+            <img src={EpikHigh_m_logo} alt="EpikHigh 로고" className='tri_m_logo' />
+            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="EpikHigh 로고 꾸미기" />에픽하이<img src={MainH2Deco} alt="EpikHigh 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                Twenty four seven<br />
+                #####Twenty four seven<br />
                 &apos;TWS&apos;와 함께<br />
                 새로운 가능성을 찾아 봐요!</p>
-            <img src={Tws_m_img} className='tri_m_img' alt='tws' />
+            <img src="" className='tri_m_img epi_m_img' alt='EpikHigh' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-            <section className='tri_m_mem_tr_full tws_m_mem_tr_full'>
+            <section className='tri_m_mem_tr_full epi_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/TwsMemSinyu' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Tws_m_MemSinyu_img} alt="신유 이미지" />
-                            <h4>신유</h4>
+                            <img src="" alt="타블로 이미지" />
+                            <h4>타블로</h4>
                         </article>
                     </Link>
                     <Link to='/TwsMemDohun' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Tws_m_MemDohun_img} alt="도훈 이미지" />
-                            <h4>도훈</h4>
+                            <img src="" alt="투컷 이미지" />
+                            <h4>투컷</h4>
                         </article>
                     </Link>
                     <Link to='/TwsMemYoungjae' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Tws_m_MemYoungjae_img} alt="영재 이미지" />
-                            <h4>영재</h4>
+                            <img src="" alt="미쓰라 이미지" />
+                            <h4>미쓰라</h4>
                         </article>
                     </Link>
-                </section>
-
-                <section className='tri_m_mem_tr'>
-                    <Link to='/TwsMemHanjin' className='tri_m_mem_a'>
-                        <article className='tri_m_mem'>
-                            <img src={Tws_m_MemHanjin_img} alt="한진 이미지" />
-                            <h4>한진</h4>
-                        </article>
-                    </Link>
-                    <Link to='/TwsMemJihun' className='tri_m_mem_a'>
-                        <article className='tri_m_mem'>
-                            <img src={Tws_m_MemJihun_img} alt="지훈 이미지" />
-                            <h4>지훈</h4>
-                        </article>
-                    </Link>
-                    <Link to='/TwsMemGyungmin' className='tri_m_mem_a'>
-                        <article className='tri_m_mem'>
-                            <img src={Tws_m_MemGyungmin_img} alt="경민 이미지" />
-                            <h4>경민</h4>
-                        </article>
-                    </Link>
-
                 </section>
 
 
