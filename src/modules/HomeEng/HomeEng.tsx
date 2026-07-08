9bf2b5348b9914506d89e3f2ffb430865511cd23
groomigroom <googjohn0143@gmail.com>
2026-07-09 08:20:38 +0900

    Update HomeEng.tsx

--- src/modules/HomeEng/HomeEng.tsx
+++ src/modules/HomeEng/HomeEng.tsx
@@ -1,1 +1,17 @@
-HomeEng.tsx
+import { Link } from "react-router-dom";
+
+import "../home.css";
+
+// import Menu from './Menu';
+
+export default function HomeEng() {
+    return (
+        <div className="home_full">
+            {/* <Menu /> */}
+            <section className="home_translate">
+                <Link to='/'>한국어</Link>
+                <Link to='/Eng' className="on">English</Link>
+            </section>
+        </div>
+    )
+}
