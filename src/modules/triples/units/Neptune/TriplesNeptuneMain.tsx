de5a36eab8f45ede12ef6fbb4338e881aaa1182e
groomigroom <googjohn0143@gmail.com>
2026-07-08 15:28:24 +0900

    Update TriplesNeptuneMain.tsx

--- src/modules/triples/units/Neptune/TriplesNeptuneMain.tsx
+++ src/modules/triples/units/Neptune/TriplesNeptuneMain.tsx
@@ -7,8 +7,14 @@ import Menu from '../../../Menu';
 
 import TriplesNep_m_logo from './img/TriplesNeptune_m_logo.svg';
 import MainH2Deco from '../../../img/main_h2_deco.png';
-// import TriplesEvo_m_img from './img/TriplesEvolution_m_img.jpg';
+import TriplesNep_m_img from './img/TriplesNeptune_m_img.jpg';
 
+import TriplesNep_m_S1_img from '../../main/img/tri_m_memS1.jpg';
+import TriplesNep_m_S7_img from '../../main/img/tri_m_memS7.jpg';
+import TriplesNep_m_S10_img from '../../main/img/tri_m_memS10.jpg';
+import TriplesNep_m_S11_img from '../../main/img/tri_m_memS11.jpg';
+import TriplesNep_m_S13_img from '../../main/img/tri_m_memS13.jpg';
+import TriplesNep_m_S23_img from '../../main/img/tri_m_memS23.jpg';
 
 //#99ACCC
 
@@ -23,26 +29,26 @@ export default function TriplesNeptuneMain() {
                 지금 내 기분을 묻는다면<br />
                 구름 위를 걷는 기분!
             </p>
-            <img src="" className='tri_m_img tri_nep_m_img' alt='tri_neptune' />
+            <img src={TriplesNep_m_img} className='tri_m_img tri_nep_m_img' alt='tri_neptune' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-            <section className='tri_m_mem_tr_full tri_hat_m_mem_tr_full'>
+            <section className='tri_m_mem_tr_full tri_nep_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
-                    <Link to='/TriplesMemS3' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS1' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="이지우 이미지" />
-                            <h4>이지우</h4>
+                            <img src={TriplesNep_m_S1_img} alt="윤서연 이미지" />
+                            <h4>윤서연</h4>
                         </article>
                     </Link>
-                    <Link to='/TriplesMemS4' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS7' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="김채연 이미지" />
-                            <h4>김채연</h4>
+                            <img src={TriplesNep_m_S7_img} alt="김나경 이미지" />
+                            <h4>김나경</h4>
                         </article>
                     </Link>
-                    <Link to='/TriplesMemS5' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS10' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="김유연 이미지" />
-                            <h4>김유연</h4>
+                            <img src={TriplesNep_m_S10_img} alt="서다현 이미지" />
+                            <h4>서다현</h4>
                         </article>
                     </Link>
 
@@ -51,20 +57,20 @@ export default function TriplesNeptuneMain() {
                 <section className='tri_m_mem_tr'>
                     <Link to='/TriplesMemS11' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="코토네 이미지" />
+                            <img src={TriplesNep_m_S11_img} alt="코토네 이미지" />
                             <h4>코토네</h4>
                         </article>
                     </Link>
-                    <Link to='/TriplesMemS16' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS13' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="마유 이미지" />
-                            <h4>마유</h4>
+                            <img src={TriplesNep_m_S13_img} alt="니엔 이미지" />
+                            <h4>니엔</h4>
                         </article>
                     </Link>
-                    <Link to='/TriplesMemS20' className='tri_m_mem_a'>
+                    <Link to='/TriplesMemS23' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="박시온 이미지" />
-                            <h4>박시온</h4>
+                            <img src={TriplesNep_m_S23_img} alt="서아 이미지" />
+                            <h4>서아</h4>
                         </article>
                     </Link>
 
