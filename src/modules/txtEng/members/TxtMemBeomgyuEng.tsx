c9ff9618b553ddefd0763d4bef9860ed64df0f12
groomigroom <googjohn0143@gmail.com>
2026-07-09 20:15:37 +0900

    Add files via upload

---
+++ src/modules/txtEng/members/TxtMemBeomgyuEng.tsx
@@ -0,0 +1,66 @@
+import '../../triples/members/TriplesMemS1.css';
+import './TxtMemBeomgyu.css';
+
+import Menu from '../../Menu';
+
+// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+
+import MainH2Deco from '../../img/main_h2_deco.png';
+
+export default function TxtMemBeomgyuEng() {
+    return (
+        <div className="txt_mm_beomgyu_full">
+            <Menu />
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="투모로우바이투게더 로고 꾸미기" />Beomgyu<img src={MainH2Deco} alt="투모로우바이투게더 로고 꾸미기" /></h2>
+            <p className='tri_mm_s1_intropp'>
+                밝고 활발한<br />
+                표정으로<br />
+                여러분을 찾아 갈게요!</p>
+            <img src="" alt="Beomgyu image" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <table className='tri_mm_s1_intropa_tb'>
+                <tbody>
+                    <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2003년 8월 6일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>161cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>B형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>햄햄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>취미</p></td>
+                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>특기</p></td>
+                        <td><p>셀카찍기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td><p>아이스크림</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>소속 유닛</p></td>
+                        <td>
+                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                LOVElution, neptune</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>좋음</p></td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    );
+}
+
