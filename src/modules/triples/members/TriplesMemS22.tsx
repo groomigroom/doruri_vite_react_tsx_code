32f74218f5600c605451968703542bd2ceaebe0d
groomigroom <googjohn0143@gmail.com>
2026-07-10 18:48:30 +0900

    Update TriplesMemS22.tsx

--- src/modules/triples/members/TriplesMemS22.tsx
+++ src/modules/triples/members/TriplesMemS22.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './TriplesMemS1.css';
 import './TriplesMemS22.css';
 
@@ -11,6 +13,10 @@ export default function TriplesMemS22() {
     return (
         <div className="tri_mm_s22_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/TriplesMemS22' className="on">한국어</Link>
+                <Link to='/TriplesMemS22Eng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="트리플에스 로고 꾸미기" />설린<img src={MainH2Deco} alt="트리플에스 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>
                 온화하고<br />
