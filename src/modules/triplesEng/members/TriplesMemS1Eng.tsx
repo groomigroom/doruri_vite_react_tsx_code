2719d36da04a41ac7a6ad820c1d2ebb0c759929e
groomigroom <googjohn0143@gmail.com>
2026-07-10 10:23:59 +0900

    Update TriplesMemS1Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS1Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS1Eng.tsx
@@ -1,1 +1,73 @@
-TriplesMemS1Eng.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/members/TriplesMemS1.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+
+// import Tri_mm_s1_img from '../main/img/tri_m_memS1.jpg';
+
+import MainH2Deco from '../../img/main_h2_deco.png';
+
+export default function TriplesMemS1Eng() {
+    return (
+        <div className="tri_mm_s1_full">
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/TriplesMemS1'>한국어</Link>
+                <Link to='/TriplesMemS1Eng' className="on">English</Link>
+            </section>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="tripleS deco" />Yoon SeoYeon<img src={MainH2Deco} alt="tripleS deco" /></h2>
+            <p className='tri_mm_s1_intropp'>
+                I think 24 is a lot<br />
+                I'm trying to keep the members in order<br />
+                do everything responsibly!
+            </p>
+            <img src='' alt="Yoon SeoYeon img" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb'>
+                <tbody>
+                    <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2003년 8월 6일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>161cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>B형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>햄햄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>취미</p></td>
+                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>특기</p></td>
+                        <td><p>셀카찍기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td><p>아이스크림</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>소속 유닛</p></td>
+                        <td>
+                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                LOVElution, neptune</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>좋음</p></td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </div>
+    );
+};
+
