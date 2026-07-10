3c3e3cc957f19b8e55c47470c3f722a64f6509ea
groomigroom <googjohn0143@gmail.com>
2026-07-10 12:16:07 +0900

    Update TriplesMemS3Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS3Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS3Eng.tsx
@@ -1,1 +1,75 @@
+import { Link } from 'react-router-dom';
+
+import '../../triples/members/TriplesMemS1.css';
+import '../../triples/members/TriplesMemS3.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+
+// import Tri_mm_s3_img from '../main/img/tri_m_memS3.jpg'
+
+import MainH2Deco from '../../img/main_h2_deco.png';
+
+export default function TriplesMemS3Eng() {
+    return (
+        <div className="tri_mm_s3_full">
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/TriplesMemS3'>한국어</Link>
+                <Link to='/TriplesMemS3Eng' className="on">English</Link>
+            </section>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="triplesS deco" />Lee JiWoo<img src={MainH2Deco} alt="triplesS deco" /></h2>
+            <p className='tri_mm_s1_intropp'>
+                It's not just one shape<br />
+                I'm going to show you a lot of my personalities<br />
+                Let me show you!</p>
+            <img src='' alt="Lee JiWoo img" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb'>
+                <tbody>
+                    <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2003년 8월 6일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>161cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>B형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>햄햄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>취미</p></td>
+                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>특기</p></td>
+                        <td><p>셀카찍기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td><p>아이스크림</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>소속 유닛</p></td>
+                        <td>
+                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                LOVElution, neptune</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>좋음</p></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+
+};
+
+
+
 
