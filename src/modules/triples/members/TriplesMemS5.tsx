45df90097f99210a42009287ad5afb3002dfa9db
groomigroom <googjohn0143@gmail.com>
2026-07-10 12:45:19 +0900

    Update TriplesMemS5.tsx

--- src/modules/triples/members/TriplesMemS5.tsx
+++ src/modules/triples/members/TriplesMemS5.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './TriplesMemS1.css';
 import './TriplesMemS5.css';
 
@@ -11,6 +13,10 @@ export default function TriplesMemS5() {
     return (
         <div className="tri_mm_s5_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/TriplesMemS5' className="on">한국어</Link>
+                <Link to='/TriplesMemS5Eng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="트리플에스 로고 꾸미기" />김유연<img src={MainH2Deco} alt="트리플에스 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>
                 다른 멤버들을<br />
