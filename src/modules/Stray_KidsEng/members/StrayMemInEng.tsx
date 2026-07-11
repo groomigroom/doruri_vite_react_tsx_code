6251e9c461eded3d76dff7084ec7600be2efef5f
groomigroom <googjohn0143@gmail.com>
2026-07-11 20:44:23 +0900

    Update StrayMemInEng.tsx

--- src/modules/Stray_KidsEng/members/StrayMemInEng.tsx
+++ src/modules/Stray_KidsEng/members/StrayMemInEng.tsx
@@ -17,11 +17,11 @@ export default function StrayMemInEng() {
                 <Link to='/StrayMemIn'>한국어</Link>
                 <Link to='/StrayMemInEng' className="on">English</Link>
             </section>
-            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="Stray Kids deco" />아이엔<img src={MainH2Deco} alt="Stray Kids 로고 꾸미기" /></h2>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="Stray Kids deco" />I.N<img src={MainH2Deco} alt="Stray Kids 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src='' alt="아이엔 이미지" className='tri_mm_s1_img' />
+            <img src='' alt="I.N" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
