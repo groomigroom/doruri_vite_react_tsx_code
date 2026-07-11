97678438acf7833bfb188adc537d15017c605d7c
groomigroom <googjohn0143@gmail.com>
2026-07-11 15:41:09 +0900

    Update StrayKidsMain.tsx

--- src/modules/Stray_Kids/main/StrayKidsMain.tsx
+++ src/modules/Stray_Kids/main/StrayKidsMain.tsx
@@ -26,6 +26,10 @@ export default function StrayKidsMain() {
     return (
         <div className='str_m_full'>
             <Menu />
+            <section className="home_translate">
+                <Link to='/StrayKidsMain' className="on">한국어</Link>
+                <Link to='/StrayKidsMainEng'>English</Link>
+            </section>
             <img src={Str_m_logo} alt="스트레이 키즈 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" />Stray Kids<img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
