33658be802591ac30d66d819644287e05f532baa
groomigroom <googjohn0143@gmail.com>
2026-07-11 10:57:09 +0900

    Update IoiMemKimsohye.tsx

--- src/modules/ioi/members/IoiMemKimsohye.tsx
+++ src/modules/ioi/members/IoiMemKimsohye.tsx
@@ -12,9 +12,10 @@ export default function IoiMemKimsohye() {
         <div className="ioi_mm_kimsohye_full">
             <Menu />
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />김소혜<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                다양한 생각과<br />
+                제 개성을 담아서<br />
+                여러분을 찾아 갈게요!</p>
             <img src="" alt="김소혜 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
