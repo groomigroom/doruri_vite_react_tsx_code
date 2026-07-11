6b04db71c483b8a8d645ea50dee9f6cbbf07658f
groomigroom <googjohn0143@gmail.com>
2026-07-11 21:14:47 +0900

    Update IllMemMinju.tsx

--- src/modules/illit/members/IllMemMinju.tsx
+++ src/modules/illit/members/IllMemMinju.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './IllMemMinju.css';
 
@@ -11,6 +13,10 @@ export default function IllMemMinju() {
     return (
         <div className="ill_mm_minju_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/IllMemMinju' className="on">한국어</Link>
+                <Link to='/IllMemMinjuEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="illit 로고 꾸미기" />민주<img src={MainH2Deco} alt="illit 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
