bb457e1b8663576726730518664611ca98a3c12f
groomigroom <googjohn0143@gmail.com>
2026-07-11 14:02:53 +0900

    Update IoiMemJeonsomi.tsx

--- src/modules/ioi/members/IoiMemJeonsomi.tsx
+++ src/modules/ioi/members/IoiMemJeonsomi.tsx
@@ -12,9 +12,10 @@ export default function IoiMemJeonsomi() {
         <div className="ioi_mm_jeonsomi_full">
             <Menu />
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />전소미<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                무너지지 않는<br />
+                멘탈을 지켜서<br />
+                여러분을 찾아 갈게요!</p>
             <img src="" alt="전소미 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
