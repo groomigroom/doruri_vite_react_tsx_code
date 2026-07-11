01ebf0f9e7f32c279fa14b3b4c669a59dc04bc14
groomigroom <googjohn0143@gmail.com>
2026-07-12 08:24:11 +0900

    Update StrayKidsMain.tsx

--- src/modules/Stray_Kids/main/StrayKidsMain.tsx
+++ src/modules/Stray_Kids/main/StrayKidsMain.tsx
@@ -33,9 +33,9 @@ export default function StrayKidsMain() {
             <img src={Str_m_logo} alt="스트레이 키즈 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" />Stray Kids<img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                #####<br />
-                하루에 하루만 더<br />
-                &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
+                &apos;Stray Kids&apos;와 함께<br />
+                시련 속에서도 계속<br />
+                running out!</p>
             <img src={Str_m_img} className='tri_m_img' alt='스트레이 키즈' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full str_m_mem_tr_full'>
