05360cf8ce3049fb7fafa597158bfb59065e1215
groomigroom <googjohn0143@gmail.com>
2026-07-11 11:06:09 +0900

    Update IoiMemYuyeonjungEng.tsx

--- src/modules/ioiEng/members/IoiMemYuyeonjungEng.tsx
+++ src/modules/ioiEng/members/IoiMemYuyeonjungEng.tsx
@@ -18,9 +18,10 @@ export default function IoiMemYuyeonjungEng() {
                 <Link to='/IoiMemYuyeonjungEng' className="on">English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="ioi deco" />YEONJUNG<img src={MainH2Deco} alt="ioi deco" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                I will come to you<br />
+                with fun<br />
+                and excitement!</p>
             <img src="" alt="YEONJUNG" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
