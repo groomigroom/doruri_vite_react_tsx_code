10c21f90c59612f752e360e47b6b445246bd7bce
groomigroom <googjohn0143@gmail.com>
2026-07-11 16:08:02 +0900

    Update StrayMemHan.tsx

--- src/modules/Stray_Kids/members/StrayMemHan.tsx
+++ src/modules/Stray_Kids/members/StrayMemHan.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './StrayMemHan.css';
 
@@ -11,6 +13,10 @@ export default function StrayMemHan() {
     return (
         <div className="str_mm_han_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/StrayMemHan' className="on">한국어</Link>
+                <Link to='/StrayMemHanEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="Stray Kids 로고 꾸미기" />한<img src={MainH2Deco} alt="Stray Kids 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
