85271a18429fb23fd7f7c40610a8dda197e39847
groomigroom <googjohn0143@gmail.com>
2026-07-11 10:49:33 +0900

    Change introduction text in IoiMemJungcheyunEng
    Updated the introduction paragraph to reflect a new message.

--- src/modules/ioiEng/members/IoiMemJungcheyunEng.tsx
+++ src/modules/ioiEng/members/IoiMemJungcheyunEng.tsx
@@ -18,9 +18,10 @@ export default function IoiMemJungcheyunEng() {
                 <Link to='/IoiMemJungcheyunEng' className="on">English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />Jung Chaeyeon<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                I'll work hard and<br />
+                come back to you<br />
+                no matter what!</p>
             <img src="" alt="Jung Chaeyeon" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
