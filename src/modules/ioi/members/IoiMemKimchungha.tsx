517f1594dfd21299ebbd450b69b98df2db654253
groomigroom <googjohn0143@gmail.com>
2026-07-11 10:35:59 +0900

    Update introduction paragraph in IoiMemKimchungha

--- src/modules/ioi/members/IoiMemKimchungha.tsx
+++ src/modules/ioi/members/IoiMemKimchungha.tsx
@@ -12,9 +12,10 @@ export default function IoiMemKimchungha() {
         <div className="ioi_mm_kimchungha_full">
             <Menu />
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />김청하<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                제가 그동안<br />
+                꿈꿔왔던 노래를<br />
+                들려 드릴게요!</p>
             <img src="" alt="김청하 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
