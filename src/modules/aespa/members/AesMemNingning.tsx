a1202084e10d3dcbbeee05ae78c21b7f97f3e5f6
groomigroom <googjohn0143@gmail.com>
2026-07-13 08:30:18 +0900

    Update AesMemNingning.tsx

--- src/modules/aespa/members/AesMemNingning.tsx
+++ src/modules/aespa/members/AesMemNingning.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './AesMemNingning.css';
 
@@ -11,6 +13,10 @@ export default function AesMemNingning() {
     return (
         <div className="aes_mm_ningning_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/AesMemNingning' className="on">한국어</Link>
+                <Link to='/AesMemNingningEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="aespa 꾸미기" />닝닝<img src={MainH2Deco} alt="aespa 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
