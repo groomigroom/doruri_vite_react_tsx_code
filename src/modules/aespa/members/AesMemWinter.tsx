04a8bfa75036450bdec310c51194be9e9a7240b8
groomigroom <googjohn0143@gmail.com>
2026-07-13 08:37:15 +0900

    Update AesMemWinter.tsx

--- src/modules/aespa/members/AesMemWinter.tsx
+++ src/modules/aespa/members/AesMemWinter.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './AesMemNingning.css';
 
@@ -11,6 +13,10 @@ export default function AesMemWinter() {
     return (
         <div className="aes_mm_winter_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/AesMemWinter' className="on">한국어</Link>
+                <Link to='/AesMemWinterEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="aespa 꾸미기" />윈터<img src={MainH2Deco} alt="aespa 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
