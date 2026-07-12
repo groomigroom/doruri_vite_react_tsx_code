11ede811a25a489b848dd4aff6803c837e257b24
groomigroom <googjohn0143@gmail.com>
2026-07-12 20:36:45 +0900

    Update TriplesMemS6.tsx

--- src/modules/triples/members/TriplesMemS6.tsx
+++ src/modules/triples/members/TriplesMemS6.tsx
@@ -27,7 +27,7 @@ export default function TriplesMemS6() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2007년 10월 3일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
@@ -35,33 +35,34 @@ export default function TriplesMemS6() {
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>AB형</p></td>
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>애깅</p></td>
                     </tr>
                     <tr>
                         <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>영화 관람</p></td>
                     </tr>
                     <tr>
                         <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>말하기</p></td>
                     </tr>
                     <tr>
                         <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>떡볶이</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>
+                                +&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                EVOLution, ∞!, zenith</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>활발함</p></td>
                     </tr>
                 </tbody>
             </table>
