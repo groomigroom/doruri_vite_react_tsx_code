9f65fa592da4782166a28713e3c50e2c2c8574d0
groomigroom <googjohn0143@gmail.com>
2026-07-12 10:22:53 +0900

    Update StrayMemFilix.tsx

--- src/modules/Stray_Kids/members/StrayMemFilix.tsx
+++ src/modules/Stray_Kids/members/StrayMemFilix.tsx
@@ -18,9 +18,10 @@ export default function StrayMemFilix() {
                 <Link to='/StrayMemFilixEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="Stray Kids 로고 꾸미기" />필릭스<img src={MainH2Deco} alt="Stray Kids 로고 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                긍정적으로<br />
+                많은 생각을 해보며<br />
+                &apos;Stray Kids&apos; 활동을 준비 할게요!</p>
             <img src={Str_mm_filix_img} alt="필릭스 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
