56fbe323e070ca2704e4ae7b4466ea69232ed064
groomigroom <googjohn0143@gmail.com>
2026-07-12 13:29:33 +0900

    Update TwsMemGyungmin.tsx

--- src/modules/tws/members/TwsMemGyungmin.tsx
+++ src/modules/tws/members/TwsMemGyungmin.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './TwsMemGyungmin.css';
 
@@ -11,6 +13,10 @@ export default function TwsMemGyungmin() {
     return (
         <div className="tws_mm_gyungmin_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/TwsMemGyungmin' className="on">한국어</Link>
+                <Link to='/TwsMemGyungminEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="TWS 꾸미기" />경민<img src={MainH2Deco} alt="TWS 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
