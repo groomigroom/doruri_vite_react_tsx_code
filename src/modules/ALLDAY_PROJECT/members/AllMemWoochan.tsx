1e4fb0d6d6a8d5d3ac753e78772200818d2bc5d8
groomigroom <googjohn0143@gmail.com>
2026-07-13 12:48:36 +0900

    Update AllMemWoochan.tsx

--- src/modules/ALLDAY_PROJECT/members/AllMemWoochan.tsx
+++ src/modules/ALLDAY_PROJECT/members/AllMemWoochan.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './AllMemWoochan.css';
 
@@ -11,6 +13,10 @@ export default function AllMemWoochan() {
     return (
         <div className="all_mm_woochan_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/AllMemWoochan' className="on">한국어</Link>
+                <Link to='/AllMemWoochanEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="allday project 꾸미기" />우찬<img src={MainH2Deco} alt="allday project 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
