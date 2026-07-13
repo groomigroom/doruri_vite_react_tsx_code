fd65e3a491c4b54e09bc7eb84b4d2ddd2bf0f2fb
groomigroom <googjohn0143@gmail.com>
2026-07-13 14:48:45 +0900

    Update IoiMemJugyulgyoungEng.tsx

--- src/modules/ioiEng/members/IoiMemJugyulgyoungEng.tsx
+++ src/modules/ioiEng/members/IoiMemJugyulgyoungEng.tsx
@@ -5,7 +5,7 @@ import '../../ioi/members/IoiMemJugyulgyoung.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Ioi_m_MemJugyulgyoung_img from '../../ioi/main/img/ioi_m_mem_Jugyulgyung.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -22,7 +22,7 @@ export default function IoiMemJugyulgyoungEng() {
                 I'm going to work<br />
                 hard without<br />
                 giving up!</p>
-            <img src="" alt="Zhou Jieqiong" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Ioi_m_MemJugyulgyoung_img} alt="Zhou Jieqiong" className='tri_mm_s1_img ioi_mm_jugyulgyoung_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
