cf231e4b0605087ee9b1ff158381bcb2cef11c2f
groomigroom <googjohn0143@gmail.com>
2026-07-13 17:37:46 +0900

    Update IoiMemChoiyujung.tsx

--- src/modules/ioi/members/IoiMemChoiyujung.tsx
+++ src/modules/ioi/members/IoiMemChoiyujung.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './IoiMemChoiyujung.css';
 
@@ -11,6 +13,10 @@ export default function IoiMemChoiyujung() {
     return (
         <div className="ioi_mm_choiyujung_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/IoiMemChoiyujung' className="on">한국어</Link>
+                <Link to='/IoiMemChoiyujungEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />최유정<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>
                 제 개성있는<br />
