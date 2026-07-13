be59877edfa1d3403360c64808cbc9fbb87900f4
groomigroom <googjohn0143@gmail.com>
2026-07-14 08:41:48 +0900

    Update IllMemMoka.tsx

--- src/modules/illit/members/IllMemMoka.tsx
+++ src/modules/illit/members/IllMemMoka.tsx
@@ -5,7 +5,7 @@ import './IllMemMoka.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import ill_mm_moka_img from '../main/img/illit_m_mem_Moka.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -21,7 +21,7 @@ export default function IllMemMoka() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src="" alt="모카 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={ill_mm_moka_img} alt="모카 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
