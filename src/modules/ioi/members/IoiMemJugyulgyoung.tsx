43883a388d7fcc516c14bbd2999606f665a9f318
groomigroom <googjohn0143@gmail.com>
2026-07-13 17:38:56 +0900

    Update IoiMemJugyulgyoung.tsx

--- src/modules/ioi/members/IoiMemJugyulgyoung.tsx
+++ src/modules/ioi/members/IoiMemJugyulgyoung.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './IoiMemJugyulgyoung.css';
 
@@ -11,6 +13,10 @@ export default function IoiMemJugyulgyoung() {
     return (
         <div className="ioi_mm_jugyulgyoung_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/IoiMemJugyulgyoung' className="on">한국어</Link>
+                <Link to='/IoiMemJugyulgyoungEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />주결경<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>
                 포기하지 않고<br />
