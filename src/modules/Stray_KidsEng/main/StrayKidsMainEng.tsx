a2192080a48d781a6eb212680d82b113c012e7e0
groomigroom <googjohn0143@gmail.com>
2026-07-13 15:20:19 +0900

    Update StrayKidsMainEng.tsx

--- src/modules/Stray_KidsEng/main/StrayKidsMainEng.tsx
+++ src/modules/Stray_KidsEng/main/StrayKidsMainEng.tsx
@@ -7,16 +7,16 @@ import MenuEng from '../../HomeEng/MenuEng';
 
 import Str_m_logo from '../../Stray_Kids/main/img/Str_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-// import Str_m_img from './img/stray_m_stray_kids.jpg';
+import Str_m_img from '../../Stray_Kids/main/img/stray_m_stray_kids.jpg';
 
-// import Str_m_MemBangchan_img from './img/stray_m_mem_Bangchan.jpg';
-// import Str_m_MemLino_img from './img/stray_m_mem_Lino.jpg';
-// import Str_m_MemChangbin_img from './img/stray_m_mem_Changbin.jpg';
-// import Str_m_MemHyunjin_img from './img/stray_m_mem_Hyunjin.jpg';
-// import Str_m_MemHan_img from './img/stray_m_mem_Han.jpg';
-// import Str_m_MemFilix_img from './img/stray_m_mem_Filix.jpg';
-// import Str_m_MemSeungmin_img from './img/stray_m_mem_Seungmin.jpg';
-// import Str_m_MemIn_img from './img/stray_m_mem_In.jpg';
+import Str_m_MemBangchan_img from '../../Stray_Kids/main/img/stray_m_mem_Bangchan.jpg';
+import Str_m_MemLino_img from '../../Stray_Kids/main/img/stray_m_mem_Lino.jpg';
+import Str_m_MemChangbin_img from '../../Stray_Kids/main/img/stray_m_mem_Changbin.jpg';
+import Str_m_MemHyunjin_img from '../../Stray_Kids/main/img/stray_m_mem_Hyunjin.jpg';
+import Str_m_MemHan_img from '../../Stray_Kids/main/img/stray_m_mem_Han.jpg';
+import Str_m_MemFilix_img from '../../Stray_Kids/main/img/stray_m_mem_Filix.jpg';
+import Str_m_MemSeungmin_img from '../../Stray_Kids/main/img/stray_m_mem_Seungmin.jpg';
+import Str_m_MemIn_img from '../../Stray_Kids/main/img/stray_m_mem_In.jpg';
 
 //#191919
 //#ffffff
@@ -36,34 +36,34 @@ export default function StrayKidsMainEng() {
                 With<br />
                 &apos;Stray Kids&apos;,<br />
                 running out!</p>
-            <img src='' className='tri_m_img' alt='Stray Kids' />
+            <img src={Str_m_img} className='tri_m_img' alt='Stray Kids' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ Members ▪</h2>
             <section className='tri_m_mem_tr_full str_m_mem_tr_full'>
 
                 <section className='tri_m_mem_tr str_m_mem_tr'>
                     <Link to='/StrayMemBangchanEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Bangchan" />
+                            <img src={Str_m_MemBangchan_img} alt="Bangchan" />
                             <h4>Bangchan</h4>
                         </article>
                     </Link>
                     <Link to='/StrayMemLinoEng'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Lee Know" />
+                            <img src={Str_m_MemLino_img} alt="Lee Know" />
                             <h4>Lee Know</h4>
                         </article>
                     </Link>
 
                     <Link to='/StrayMemChangbinEng'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Changbin" />
+                            <img src={Str_m_MemChangbin_img} alt="Changbin" />
                             <h4>Changbin</h4>
                         </article>
                     </Link>
 
                     <Link to='/StrayMemHyunjinEng'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Hyunjin" />
+                            <img src={Str_m_MemHyunjin_img} alt="Hyunjin" />
                             <h4>Hyunjin</h4>
                         </article>
                     </Link>
@@ -72,28 +72,28 @@ export default function StrayKidsMainEng() {
                 <section className='tri_m_mem_tr'>
                     <Link to='/StrayMemHanEng'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Han" />
+                            <img src={Str_m_MemHan_img} alt="Han" />
                             <h4>Han</h4>
                         </article>
                     </Link>
 
                     <Link to='/StrayMemFilixEng'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Felix" />
+                            <img src={Str_m_MemFilix_img} alt="Felix" />
                             <h4>Felix</h4>
                         </article>
                     </Link>
 
                     <Link to='/StrayMemSeungminEng'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Seungmin" />
+                            <img src={Str_m_MemSeungmin_img} alt="Seungmin" />
                             <h4>Seungmin</h4>
                         </article>
                     </Link>
 
                     <Link to='/StrayMemInEng'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="I.N" />
+                            <img src={Str_m_MemIn_img} alt="I.N" />
                             <h4>I.N</h4>
                         </article>
                     </Link>
