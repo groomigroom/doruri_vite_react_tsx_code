22fb0983f82e4f14fffd240345e7be23ef721a5d
groomigroom <googjohn0143@gmail.com>
2026-07-14 08:25:14 +0900

    Fix formatting and clean up imports in ResMemLivEng

--- src/modules/RESCENEEng/members/ResMemLivEng.tsx
+++ src/modules/RESCENEEng/members/ResMemLivEng.tsx
@@ -21,7 +21,7 @@ export default function ResMemLivEng() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src={Res_mm_Liv_img} alt="Liv" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Res_mm_Liv_img} alt="Liv" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
