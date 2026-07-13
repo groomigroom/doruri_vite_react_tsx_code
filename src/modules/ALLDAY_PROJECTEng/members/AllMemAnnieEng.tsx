fbc7773ebb0a397af2e77ae1ce9443b0ed59d77a
groomigroom <googjohn0143@gmail.com>
2026-07-13 18:28:55 +0900

    Update AllMemAnnieEng.tsx

--- src/modules/ALLDAY_PROJECTEng/members/AllMemAnnieEng.tsx
+++ src/modules/ALLDAY_PROJECTEng/members/AllMemAnnieEng.tsx
@@ -5,7 +5,7 @@ import '../../ALLDAY_PROJECT/members/AllMemAnnie.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import all_mm_annie_img from '../../ALLDAY_PROJECT/main/img/allday_m_mem_Annie.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -21,7 +21,7 @@ export default function AllMemAnnieEng() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src="" alt="Annie" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={all_mm_annie_img} alt="Annie" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
