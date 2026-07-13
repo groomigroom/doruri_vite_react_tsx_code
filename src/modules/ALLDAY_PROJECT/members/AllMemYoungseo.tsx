f2b68d3d93aab5a86985130818e0acea69643e52
groomigroom <googjohn0143@gmail.com>
2026-07-13 12:48:53 +0900

    Update AllMemYoungseo.tsx

--- src/modules/ALLDAY_PROJECT/members/AllMemYoungseo.tsx
+++ src/modules/ALLDAY_PROJECT/members/AllMemYoungseo.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
 import './AllMemYoungseo.css';
 
@@ -11,6 +13,10 @@ export default function AllMemYoungseo() {
     return (
         <div className="all_mm_youngseo_full">
             <Menu />
+            <section className="home_translate">
+                <Link to='/AllMemYoungseo' className="on">한국어</Link>
+                <Link to='/AllMemYoungseoEng'>English</Link>
+            </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="allday project 꾸미기" />영서<img src={MainH2Deco} alt="allday project 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
