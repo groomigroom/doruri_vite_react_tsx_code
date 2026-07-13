d5c06c6b390ed17172faa7788657b6fd030c5070
groomigroom <googjohn0143@gmail.com>
2026-07-13 16:22:47 +0900

    Update IllMemMinjuEng.tsx

--- src/modules/illitEng/members/IllMemMinjuEng.tsx
+++ src/modules/illitEng/members/IllMemMinjuEng.tsx
@@ -5,7 +5,7 @@ import '../../illit/members/IllMemMinju.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Ill_m_MemMinju_img from '../../illit/main/img/illit_m_mem_Minju.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -21,7 +21,7 @@ export default function IllMemMinjuEng() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src="" alt="Minju" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Ill_m_MemMinju_img} alt="Minju" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
