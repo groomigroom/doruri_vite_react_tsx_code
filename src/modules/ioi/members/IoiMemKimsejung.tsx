f9235b22492b713ca5775ac3e9c58ad8aed38439
groomigroom <googjohn0143@gmail.com>
2026-07-13 15:06:14 +0900

    Update IoiMemKimsejung.tsx

--- src/modules/ioi/members/IoiMemKimsejung.tsx
+++ src/modules/ioi/members/IoiMemKimsejung.tsx
@@ -3,7 +3,7 @@ import './IoiMemKimsejung.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Ioi_m_MemKimsejung_img from '../main/img/ioi_m_mem_Kimsejung.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -16,7 +16,7 @@ export default function IoiMemKimsejung() {
                 감성적인 생각과<br />
                 다양한 생각을 가지고<br />
                 여러분을 찾아 갈게요!</p>
-            <img src="" alt="김세정 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Ioi_m_MemKimsejung_img} alt="김세정 이미지" className='tri_mm_s1_img ioi_mm_kimsejung_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
