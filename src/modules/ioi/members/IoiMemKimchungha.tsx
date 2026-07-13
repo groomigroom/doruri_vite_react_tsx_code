d601b4c7dbb5e15569231d69b62f7be1534d5c25
groomigroom <googjohn0143@gmail.com>
2026-07-13 14:58:53 +0900

    Update IoiMemKimchungha.tsx

--- src/modules/ioi/members/IoiMemKimchungha.tsx
+++ src/modules/ioi/members/IoiMemKimchungha.tsx
@@ -3,7 +3,7 @@ import './IoiMemKimchungha.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Ioi_m_MemKimchungha_img from '../main/img/ioi_m_mem_Kimchungha.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -16,7 +16,7 @@ export default function IoiMemKimchungha() {
                 제가 그동안<br />
                 꿈꿔왔던 노래를<br />
                 들려 드릴게요!</p>
-            <img src="" alt="김청하 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Ioi_m_MemKimchungha_img} alt="김청하 이미지" className='tri_mm_s1_img ioi_mm_kimchungha_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
