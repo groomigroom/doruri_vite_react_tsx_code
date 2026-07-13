1f3f937102ac694583f972d1d638419aef06d867
groomigroom <googjohn0143@gmail.com>
2026-07-14 08:35:44 +0900

    Update ResMemWoniEng.tsx

--- src/modules/RESCENEEng/members/ResMemWoniEng.tsx
+++ src/modules/RESCENEEng/members/ResMemWoniEng.tsx
@@ -18,9 +18,10 @@ export default function ResMemWoniEng() {
                 <Link to='/ResMemWoniEng' className="on">English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="RESCENE deco" />Woni<img src={MainH2Deco} alt="RESCENE deco" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                I'll take good care of<br />
+                the members in<br />
+                a lively but mature way!</p>
             <img src={Res_mm_Woni_img} alt="Woni" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
