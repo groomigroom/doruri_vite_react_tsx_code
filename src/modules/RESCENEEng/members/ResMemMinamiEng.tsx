08f89f3841007b9806a5fcdef161d9e074e59f3a
groomigroom <googjohn0143@gmail.com>
2026-07-13 18:05:21 +0900

    Update ResMemMinamiEng.tsx

--- src/modules/RESCENEEng/members/ResMemMinamiEng.tsx
+++ src/modules/RESCENEEng/members/ResMemMinamiEng.tsx
@@ -5,7 +5,7 @@ import '../../RESCENE/members/ResMemMinami.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Res_mm_Minami_img from '../../RESCENE/main/img/rescene_m_mem_Minami.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -21,7 +21,7 @@ export default function ResMemMinamiEng() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src="" alt="Minami" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Res_mm_Minami_img} alt="Minami" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
