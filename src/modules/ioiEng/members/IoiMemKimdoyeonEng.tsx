583c662d4791e5a5455bb689f4925f0d01516c1f
groomigroom <googjohn0143@gmail.com>
2026-07-13 15:02:19 +0900

    Update IoiMemKimdoyeonEng.tsx

--- src/modules/ioiEng/members/IoiMemKimdoyeonEng.tsx
+++ src/modules/ioiEng/members/IoiMemKimdoyeonEng.tsx
@@ -5,7 +5,7 @@ import '../../ioi/members/IoiMemKimdoyeon.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Ioi_m_MemKimdoyeon_img from '../../ioi/main/img/ioi_m_mem_Kimdoyeon.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -22,7 +22,7 @@ export default function IoiMemKimdoyeonEng() {
                 I'm crying,<br />
                 but I'm going<br />
                 to find you!</p>
-            <img src="" alt="Kim Doyeon" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Ioi_m_MemKimdoyeon_img} alt="Kim Doyeon" className='tri_mm_s1_img ioi_mm_kimdoyeon_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
