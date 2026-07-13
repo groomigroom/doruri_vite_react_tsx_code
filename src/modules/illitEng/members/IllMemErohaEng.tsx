252a90eb54d25f77e46463506964136081917337
groomigroom <googjohn0143@gmail.com>
2026-07-14 08:48:47 +0900

    Update IllMemErohaEng.tsx

--- src/modules/illitEng/members/IllMemErohaEng.tsx
+++ src/modules/illitEng/members/IllMemErohaEng.tsx
@@ -18,9 +18,10 @@ export default function IllMemErohaEng() {
                 <Link to='/IllMemErohaEng' className="on">English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="illit deco" />Iroha<img src={MainH2Deco} alt="illit deco" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                I'll prepare<br />
+                perfectly and<br />
+                go meet you!</p>
             <img src={Ill_m_MemEroha_img} alt="Iroha" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
