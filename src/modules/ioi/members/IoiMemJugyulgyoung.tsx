68491fa6b8cad49a9fa56cf33573b48b13be6426
groomigroom <googjohn0143@gmail.com>
2026-07-13 14:47:42 +0900

    Update IoiMemJugyulgyoung.tsx

--- src/modules/ioi/members/IoiMemJugyulgyoung.tsx
+++ src/modules/ioi/members/IoiMemJugyulgyoung.tsx
@@ -3,7 +3,7 @@ import './IoiMemJugyulgyoung.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Ioi_m_MemJugyulgyoung_img from '../main/img/ioi_m_mem_Jugyulgyung.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -16,7 +16,7 @@ export default function IoiMemJugyulgyoung() {
                 포기하지 않고<br />
                 열심히 노력하며<br />
                 여러분을 찾아 갈게요!</p>
-            <img src="" alt="주결경 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Ioi_m_MemJugyulgyoung_img} alt="주결경 이미지" className='tri_mm_s1_img ioi_mm_jugyulgyoung_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
