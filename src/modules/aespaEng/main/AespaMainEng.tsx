9c914ace473531cf0c0f6192029128261f5bea55
groomigroom <googjohn0143@gmail.com>
2026-07-13 18:18:44 +0900

    Update AespaMainEng.tsx

--- src/modules/aespaEng/main/AespaMainEng.tsx
+++ src/modules/aespaEng/main/AespaMainEng.tsx
@@ -7,12 +7,12 @@ import MenuEng from '../../HomeEng/MenuEng';
 
 import Aes_m_logo from '../../aespa/main/img/Aes_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-// import Aes_m_img from './img/aespa_m_aespa.jpg';
+import Aes_m_img from '../../aespa/main/img/aespa_m_aespa.jpg';
 
-// import Aes_m_MemKarina_img from './img/aespa_m_mem_Karina.jpg';
-// import Aes_m_MemJijell_img from './img/aespa_m_mem_Jijel.jpg';
-// import Aes_m_MemWinter_img from './img/aespa_m_mem_Winter.jpg';
-// import Aes_m_MemNingning_img from './img/aespa_m_mem_Ningning.jpg';
+import Aes_m_MemKarina_img from '../../aespa/main/img/aespa_m_mem_Karina.jpg';
+import Aes_m_MemJijell_img from '../../aespa/main/img/aespa_m_mem_Jijel.jpg';
+import Aes_m_MemWinter_img from '../../aespa/main/img/aespa_m_mem_Winter.jpg';
+import Aes_m_MemNingning_img from '../../aespa/main/img/aespa_m_mem_Ningning.jpg';
 
 //191919
 //ffffff
@@ -32,19 +32,19 @@ export default function AespaMainEng() {
                 in a different<br />
                 world with &apos;aespa&apos;!
             </p>
-            <img src='' className='tri_m_img' alt='aespa' />
+            <img src={Aes_m_img} className='tri_m_img' alt='aespa' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ Members ▪</h2>
             <section className='tri_m_mem_tr_full aes_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/AesMemKarinaEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="카리나 이미지" />
+                            <img src={Aes_m_MemKarina_img} alt="카리나 이미지" />
                             <h4>카리나</h4>
                         </article>
                     </Link>
                     <Link to='/AesMemJijellEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="지젤 이미지" />
+                            <img src={Aes_m_MemJijell_img} alt="지젤 이미지" />
                             <h4>지젤</h4>
                         </article>
                     </Link>
@@ -54,13 +54,13 @@ export default function AespaMainEng() {
                 <section className='tri_m_mem_tr'>
                     <Link to='/AesMemWinterEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="윈터 이미지" />
+                            <img src={Aes_m_MemWinter_img} alt="윈터 이미지" />
                             <h4>윈터</h4>
                         </article>
                     </Link>
                     <Link to='/AesMemNingningEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="닝닝 이미지" />
+                            <img src={Aes_m_MemNingning_img} alt="닝닝 이미지" />
                             <h4>닝닝</h4>
                         </article>
                     </Link>
