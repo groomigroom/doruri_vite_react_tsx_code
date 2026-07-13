57833213c4aa3a300fb92687cd0ed2f152f8668c
groomigroom <googjohn0143@gmail.com>
2026-07-14 08:23:17 +0900

    Update ResMemLiv.tsx

--- src/modules/RESCENE/members/ResMemLiv.tsx
+++ src/modules/RESCENE/members/ResMemLiv.tsx
@@ -5,7 +5,7 @@ import './ResMemLiv.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import res_mm_liv_img from '../main/img/rescene_m_mem_Liv.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -21,7 +21,7 @@ export default function ResMemLiv() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src="" alt="리브 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={res_mm_liv_img} alt="리브 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
