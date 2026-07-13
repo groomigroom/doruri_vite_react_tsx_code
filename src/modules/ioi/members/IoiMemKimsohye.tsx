56846d137d2da811957386f2801c02d4f85df2d5
groomigroom <googjohn0143@gmail.com>
2026-07-13 15:09:26 +0900

    Update IoiMemKimsohye.tsx

--- src/modules/ioi/members/IoiMemKimsohye.tsx
+++ src/modules/ioi/members/IoiMemKimsohye.tsx
@@ -3,7 +3,7 @@ import './IoiMemKimsohye.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Ioi_m_MemKimsohye_img from '../main/img/ioi_m_mem_Kimsohye.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -16,7 +16,7 @@ export default function IoiMemKimsohye() {
                 다양한 생각과<br />
                 제 개성을 담아서<br />
                 여러분을 찾아 갈게요!</p>
-            <img src="" alt="김소혜 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Ioi_m_MemKimsohye_img} alt="김소혜 이미지" className='tri_mm_s1_img ioi_mm_kimsohye_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
