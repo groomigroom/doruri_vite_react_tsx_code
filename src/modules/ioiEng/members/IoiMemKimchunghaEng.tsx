0b5cb26a8985beb259096c69cf620f7149632ddc
groomigroom <googjohn0143@gmail.com>
2026-07-13 14:59:20 +0900

    Add image import for Kimchungha member component

--- src/modules/ioiEng/members/IoiMemKimchunghaEng.tsx
+++ src/modules/ioiEng/members/IoiMemKimchunghaEng.tsx
@@ -5,7 +5,7 @@ import '../../ioi/members/IoiMemKimchungha.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Ioi_m_MemKimchungha_img from '../../ioi/main/img/ioi_m_mem_Kimchungha.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -22,7 +22,7 @@ export default function IoiMemKimchunghaEng() {
                 I'll play the song<br />
                 I've been<br />
                 dreaming of!</p>
-            <img src="" alt="CHUNG HA 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <img src={Ioi_m_MemKimchungha_img} alt="CHUNG HA 이미지" className='tri_mm_s1_img ioi_mm_kimchungha_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
