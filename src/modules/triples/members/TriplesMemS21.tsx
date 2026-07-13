48fd3eca03dfdaa8c1e2008ef38fe34b50bbd9d8
groomigroom <googjohn0143@gmail.com>
2026-07-13 12:02:14 +0900

    Update TriplesMemS21.tsx

--- src/modules/triples/members/TriplesMemS21.tsx
+++ src/modules/triples/members/TriplesMemS21.tsx
@@ -27,41 +27,42 @@ export default function TriplesMemS21() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2007년 5월 2일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>163cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>AB형</p></td>
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>꼬북이</p></td>
                     </tr>
                     <tr>
                         <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>독서</p></td>
                     </tr>
                     <tr>
                         <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>특공 무술</p></td>
                     </tr>
                     <tr>
                         <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>마라탕</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>
+                                Glow, ∞!, <br />
+                                Alphie, sun</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>독보적임</p></td>
                     </tr>
                 </tbody>
             </table>
