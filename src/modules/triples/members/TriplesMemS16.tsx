e56cc49561d408700f4afb62ad57ea18537050d5
groomigroom <googjohn0143@gmail.com>
2026-07-13 10:21:29 +0900

    Update TriplesMemS16.tsx

--- src/modules/triples/members/TriplesMemS16.tsx
+++ src/modules/triples/members/TriplesMemS16.tsx
@@ -21,11 +21,11 @@ export default function TriplesMemS16() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2002년 5월 12일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>160cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
@@ -33,29 +33,29 @@ export default function TriplesMemS16() {
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>고예린</p></td>
                     </tr>
                     <tr>
                         <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>드라마 보기</p></td>
                     </tr>
                     <tr>
                         <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>사진 찍기</p></td>
                     </tr>
                     <tr>
                         <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>삼겹살</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>
+                                EVOLution, ∞!, sun</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>온화함</p></td>
                     </tr>
                 </tbody>
             </table>
