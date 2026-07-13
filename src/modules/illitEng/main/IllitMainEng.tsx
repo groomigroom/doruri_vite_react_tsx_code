92416edc38b436ccfe18e158885c9f238a36851b
groomigroom <googjohn0143@gmail.com>
2026-07-13 16:16:56 +0900

    Update IllitMainEng.tsx

--- src/modules/illitEng/main/IllitMainEng.tsx
+++ src/modules/illitEng/main/IllitMainEng.tsx
@@ -7,13 +7,13 @@ import MenuEng from '../../HomeEng/MenuEng';
 
 import Ill_m_logo from '../../illit/main/img/Ill_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-// import Ill_m_img from './img/illit_m_illit.jpg';
+import Ill_m_img from '../../illit/main/img/illit_m_illit.jpg';
 
-// import Ill_m_MemYunah_img from './img/illit_m_mem_Yunah.jpg';
-// import Ill_m_MemMinju_img from './img/illit_m_mem_Minju.jpg';
-// import Ill_m_MemMoka_img from './img/illit_m_mem_Moka.jpg';
-// import Ill_m_MemWonhee_img from './img/illit_m_mem_Wonhee.jpg';
-// import Ill_m_MemEroha_img from './img/illit_m_mem_Iroha.jpg';
+import Ill_m_MemYunah_img from '../../illit/main/img/illit_m_mem_Yunah.jpg';
+import Ill_m_MemMinju_img from '../../illit/main/img/illit_m_mem_Minju.jpg';
+import Ill_m_MemMoka_img from '../../illit/main/img/illit_m_mem_Moka.jpg';
+import Ill_m_MemWonhee_img from '../../illit/main/img/illit_m_mem_Wonhee.jpg';
+import Ill_m_MemEroha_img from '../../illit/main/img/illit_m_mem_Iroha.jpg';
 
 //191919
 //FFFFFF
@@ -33,25 +33,25 @@ export default function IllitMainEng() {
                 Who's your bias?<br />
                 I'm your bias!
             </p>
-            <img src='' className='tri_m_img' alt='illit' />
+            <img src={Ill_m_img} className='tri_m_img' alt='illit' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ Members ▪</h2>
             <section className='tri_m_mem_tr_full ill_m_mem_tr_full'>
                 <section className='ill_m_mem_tr3'>
                     <Link to='/IllMemYunahEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Yunah" className='ill_m_mem_img' />
+                            <img src={Ill_m_MemYunah_img} alt="Yunah" className='ill_m_mem_img' />
                             <h4>Yunah</h4>
                         </article>
                     </Link>
                     <Link to='/IllMemMinjuEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Minju" />
+                            <img src={Ill_m_MemMinju_img} alt="Minju" />
                             <h4>Minju</h4>
                         </article>
                     </Link>
                     <Link to='/IllMemMokaEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Moka" />
+                            <img src={Ill_m_MemMoka_img} alt="Moka" />
                             <h4>Moka</h4>
                         </article>
                     </Link>
@@ -60,13 +60,13 @@ export default function IllitMainEng() {
                 <section className='ill_m_mem_tr2'>
                     <Link to='/IllMemWonheeEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Wonhee" />
+                            <img src={Ill_m_MemWonhee_img} alt="Wonhee" />
                             <h4>Wonhee</h4>
                         </article>
                     </Link>
                     <Link to='/IllMemErohaEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="Iroha" />
+                            <img src={Ill_m_MemEroha_img} alt="Iroha" />
                             <h4>Iroha</h4>
                         </article>
                     </Link>
