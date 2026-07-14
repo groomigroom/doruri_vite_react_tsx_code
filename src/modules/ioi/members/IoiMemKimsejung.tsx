7ba38a88a36c52f5ea674d697ebae9616aa973fe
groomigroom <googjohn0143@gmail.com>
2026-07-14 15:47:21 +0900

    Update IoiMemKimsejung.tsx

--- src/modules/ioi/members/IoiMemKimsejung.tsx
+++ src/modules/ioi/members/IoiMemKimsejung.tsx
@@ -23,45 +23,31 @@ export default function IoiMemKimsejung() {
                 다양한 생각을 가지고<br />
                 여러분을 찾아 갈게요!</p>
             <img src={Ioi_m_MemKimsejung_img} alt="김세정 이미지" className='tri_mm_s1_img ioi_mm_kimsejung_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <table className='tri_mm_s1_intropa_tb ioi_mm_kimsejung_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>1996년 8월 28일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>164cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>AB형</p></td>
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>갓세정</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>포지션</p></td>
+                        <td><p>메인보컬</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>사교적임?</p></td>
                     </tr>
                 </tbody>
             </table>
