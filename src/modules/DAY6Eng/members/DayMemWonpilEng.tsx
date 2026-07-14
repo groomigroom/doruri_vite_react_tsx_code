1f280df9d0a9328437acde57c1a375c6292b9acb
groomigroom <googjohn0143@gmail.com>
2026-07-14 20:35:18 +0900

    Update DayMemWonpilEng.tsx

--- src/modules/DAY6Eng/members/DayMemWonpilEng.tsx
+++ src/modules/DAY6Eng/members/DayMemWonpilEng.tsx
@@ -1,23 +1,29 @@
+import { Link } from 'react-router-dom';
+
 import '../../triples/members/TriplesMemS1.css';
-import './DayMemWonpil.css';
+import '../../DAY6/members/DayMemWonpil.css';
 
-import Menu from '../../Menu';
+import MenuEng from '../../HomeEng/MenuEng';
 
-import Day_mm_wonpil_img from '../main/img/day6_m_mem_Wonpil.jpg';
+import Day_mm_wonpil_img from '../../DAY6/main/img/day6_m_mem_Wonpil.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
-export default function DayMemWonpil() {
+export default function DayMemWonpilEng() {
     return (
         <div className="day_mm_wonpil_full">
-            <Menu />
-            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="DAY6 꾸미기" />원필<img src={MainH2Deco} alt="DAY6 꾸미기" /></h2>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/DayMemWonpil'>한국어</Link>
+                <Link to='/DayMemWonpilEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="DAY6 deco" />Wonpil<img src={MainH2Deco} alt="DAY6 deco" /></h2>
             <p className='tri_mm_s1_intropp'>
-                다정하고 밝게<br />
-                천사같이<br />
-                여러분께 찾아 갈게요!
+                I will come to you<br />
+                like an angel in a kind<br />
+                and bright way!
             </p>
-            <img src={Day_mm_wonpil_img} alt="원필 이미지" className='tri_mm_s1_img' />
+            <img src={Day_mm_wonpil_img} alt="Wonpil" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
@@ -66,6 +72,3 @@ export default function DayMemWonpil() {
 
 };
 
-
-
-DayMemWonpilEng.tsx
