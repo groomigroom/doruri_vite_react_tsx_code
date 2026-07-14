413feacb4795a5ea8d2d0f0fdfe9b3b9d6ed3bf7
groomigroom <googjohn0143@gmail.com>
2026-07-14 17:15:43 +0900

    Update DayMemYoungKEng.tsx

--- src/modules/DAY6Eng/members/DayMemYoungKEng.tsx
+++ src/modules/DAY6Eng/members/DayMemYoungKEng.tsx
@@ -1,1 +1,69 @@
+import '../../triples/members/TriplesMemS1.css';
+import './DayMemYoungK.css';
+
+import Menu from '../../Menu';
+
+import Day_mm_youngk_img from '../main/img/day6_m_mem_Youngk.jpg';
+
+import MainH2Deco from '../../img/main_h2_deco.png';
+
+export default function DayMemYoungK() {
+    return (
+        <div className="day_mm_youngk_full">
+            <Menu />
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="DAY6 꾸미기" />Young K<img src={MainH2Deco} alt="DAY6 꾸미기" /></h2>
+            <p className='tri_mm_s1_intropp'>
+                성실하게 노력하며<br />
+                음악으로<br />
+                여러분께 찾아 갈게요!
+            </p>
+            <img src={Day_mm_youngk_img} alt="Young K 이미지" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb'>
+                <tbody>
+                    <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2003년 8월 6일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>161cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>B형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>햄햄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>취미</p></td>
+                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>특기</p></td>
+                        <td><p>셀카찍기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td><p>아이스크림</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>소속 유닛</p></td>
+                        <td>
+                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                LOVElution, neptune</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>좋음</p></td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </div>
+    );
+
+};
+
 DayMemYoungKEng.tsx
