1ae75be293022c2a8b2ea72feb7ec2e17471f504
groomigroom <googjohn0143@gmail.com>
2026-07-14 14:04:10 +0900

    Update AkmuMemChanhyuk.tsx

--- src/modules/akmu/members/AkmuMemChanhyuk.tsx
+++ src/modules/akmu/members/AkmuMemChanhyuk.tsx
@@ -23,15 +23,15 @@ export default function AkmuMemChanhyuk() {
                 잘 준비해서<br />
                 여러분을 찾아 갈게요!</p>
             <img src={akmu_mm_chanhyuk_img} alt="이찬혁 이미지" className='tri_mm_s1_img akmu_mm_chanhyuk_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <table className='tri_mm_s1_intropa_tb akm_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>1996년 9월 12일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>168cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
@@ -39,29 +39,16 @@ export default function AkmuMemChanhyuk() {
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>이PD</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>MBTI</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>ENTJ</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>준비성이 뛰어남</p></td>
                     </tr>
                 </tbody>
             </table>
