34d50a0d25a8bdc1795092c9365fdda60e9867d2
groomigroom <googjohn0143@gmail.com>
2026-07-14 09:56:40 +0900

    Update IllMemMinjuEng.tsx

--- src/modules/illitEng/members/IllMemMinjuEng.tsx
+++ src/modules/illitEng/members/IllMemMinjuEng.tsx
@@ -18,9 +18,10 @@ export default function IllMemMinjuEng() {
                 <Link to='/IllMemMinjuEng' className="on">English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="illit deco" />Minju<img src={MainH2Deco} alt="illit deco" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                I'll be considerate of<br />
+                the members and<br />
+                get along well with them!</p>
             <img src={Ill_m_MemMinju_img} alt="Minju" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
