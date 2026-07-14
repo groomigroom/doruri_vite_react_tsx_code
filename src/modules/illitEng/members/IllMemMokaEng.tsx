1f4e93f6105dff06aa42e6f2a1eed432d841d406
groomigroom <googjohn0143@gmail.com>
2026-07-14 09:48:13 +0900

    Update IllMemMokaEng.tsx

--- src/modules/illitEng/members/IllMemMokaEng.tsx
+++ src/modules/illitEng/members/IllMemMokaEng.tsx
@@ -18,9 +18,10 @@ export default function IllMemMokaEng() {
                 <Link to='/IllMemMokaEng' className="on">English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="illit deco" />Moka<img src={MainH2Deco} alt="illit deco" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                I'll go and meet you<br />
+                with a smile with<br />
+                a positive heart!</p>
             <img src={Ill_m_MemMoka_img} alt="Moka" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
