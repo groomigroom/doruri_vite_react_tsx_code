1aad0dbf8141ff8ecc831501f218ea0bb4a3b570
groomigroom <googjohn0143@gmail.com>
2026-07-14 16:51:35 +0900

    Update AespaMainEng.tsx

--- src/modules/aespaEng/main/AespaMainEng.tsx
+++ src/modules/aespaEng/main/AespaMainEng.tsx
@@ -39,13 +39,17 @@ export default function AespaMainEng() {
                     <Link to='/AesMemKarinaEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={Aes_m_MemKarina_img} alt="카리나 이미지" />
-                            <h4>카리나</h4>
+                            <div>
+                                <h4>Karina</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/AesMemJijellEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={Aes_m_MemJijell_img} alt="지젤 이미지" />
-                            <h4>지젤</h4>
+                            <div>
+                                <h4>Giselle</h4>
+                            </div>
                         </article>
                     </Link>
 
@@ -55,13 +59,17 @@ export default function AespaMainEng() {
                     <Link to='/AesMemWinterEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={Aes_m_MemWinter_img} alt="윈터 이미지" />
-                            <h4>윈터</h4>
+                            <div>
+                                <h4>Winter</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/AesMemNingningEng' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={Aes_m_MemNingning_img} alt="닝닝 이미지" />
-                            <h4>닝닝</h4>
+                            <div>
+                                <h4>Ningning</h4>
+                            </div>
                         </article>
                     </Link>
 
