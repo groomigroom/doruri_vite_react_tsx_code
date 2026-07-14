79ee9748b6fc05d6c7b5150eef18db15d68426d3
groomigroom <googjohn0143@gmail.com>
2026-07-14 10:36:11 +0900

    Update Bol4MainEng.tsx

--- src/modules/bol4Eng/main/Bol4MainEng.tsx
+++ src/modules/bol4Eng/main/Bol4MainEng.tsx
@@ -24,7 +24,7 @@ export default function Bol4MainEng() {
                 하루에 하루만 더<br />
                 &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
             <img src={Bol_m_img} className='tri_m_img txt_m_img' alt='BOL4' />
-            <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
+            <h2 className='tri_m_h2 tri_m_h2_2'>▪ Members ▪</h2>
             <section className='tri_m_mem_tr_full ten_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/BolMemAnjiyoungEng' className='tri_m_mem_a'>
