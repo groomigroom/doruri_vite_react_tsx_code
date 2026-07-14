6a840138b45886f738971e1b1cc10066208bd6ec
groomigroom <googjohn0143@gmail.com>
2026-07-14 15:14:43 +0900

    Update IoiMemGangmina.tsx

--- src/modules/ioi/members/IoiMemGangmina.tsx
+++ src/modules/ioi/members/IoiMemGangmina.tsx
@@ -23,45 +23,31 @@ export default function IoiMemGangmina() {
                 긍정적인 마음을 가지고<br />
                 여러분을 찾아 갈게요!</p>
             <img src={Ioi_m_MemGangmina_img} alt="강미나 이미지" className='tri_mm_s1_img ioi_mm_gangmina_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <table className='tri_mm_s1_intropa_tb ioi_mm_gangmina_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>1999년 12월 4일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>162cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>O형</p></td>
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>깡미나</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>포지션</p></td>
+                        <td><p>보컬</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>활발함?</p></td>
                     </tr>
                 </tbody>
             </table>
