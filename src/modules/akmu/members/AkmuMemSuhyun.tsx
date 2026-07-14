77795a286f70c661a2c274e1bc287801f6f90c34
groomigroom <googjohn0143@gmail.com>
2026-07-14 14:09:39 +0900

    Update AkmuMemSuhyun.tsx

--- src/modules/akmu/members/AkmuMemSuhyun.tsx
+++ src/modules/akmu/members/AkmuMemSuhyun.tsx
@@ -23,15 +23,15 @@ export default function AkmuMemSuhyun() {
                 잘 이겨내며<br />
                 여러분을 찾아 갈게요!</p>
             <img src={akmu_mm_suhyun_img} alt="이수현 이미지" className='tri_mm_s1_img akmu_mm_suhyun_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <table className='tri_mm_s1_intropa_tb akm_mm_suhyun_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>1999년 5월 4일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>165cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
@@ -39,29 +39,16 @@ export default function AkmuMemSuhyun() {
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>이수</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>MBTI</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>ISTP</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>차분함</p></td>
                     </tr>
                 </tbody>
             </table>
