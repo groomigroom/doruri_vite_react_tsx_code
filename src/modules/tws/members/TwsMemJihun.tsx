1ce157a585499913da3677c4b1c21ec7e0ff6e94
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:35:57 +0900

    Update TwsMemJihun.tsx

--- src/modules/tws/members/TwsMemJihun.tsx
+++ src/modules/tws/members/TwsMemJihun.tsx
@@ -18,9 +18,10 @@ export default function TwsMemJihun() {
                 <Link to='/TwsMemJihunEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="TWS 꾸미기" />지훈<img src={MainH2Deco} alt="TWS 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                밝게 웃으며<br />
+                즐거운 마음으로<br />
+                여러분을 찾아 갈게요!</p>
             <img src={Tws_mm_jihun_img} alt="지훈 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb tws_mm_jihun_intropa_tb'>
                 <tbody>
