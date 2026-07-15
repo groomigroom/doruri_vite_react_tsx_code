5bb2e193d317575af0b46bfe87e9f234278ddf76
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:37:33 +0900

    Update TwsMemGyungmin.tsx

--- src/modules/tws/members/TwsMemGyungmin.tsx
+++ src/modules/tws/members/TwsMemGyungmin.tsx
@@ -18,9 +18,10 @@ export default function TwsMemGyungmin() {
                 <Link to='/TwsMemGyungminEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="TWS 꾸미기" />경민<img src={MainH2Deco} alt="TWS 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                다양한 아이디어를<br />
+                제시하며 멤버들과<br />
+                잘 활동해 볼게요!</p>
             <img src={Tws_mm_gyungmin_img} alt="경민 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb tws_mm_gyungmin_intropa_tb'>
                 <tbody>
