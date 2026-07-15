59572f097a9587466e7dfb7f4bc7384b9069fc1c
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:56:57 +0900

    Update AllMemYoungseo.tsx

--- src/modules/ALLDAY_PROJECT/members/AllMemYoungseo.tsx
+++ src/modules/ALLDAY_PROJECT/members/AllMemYoungseo.tsx
@@ -18,9 +18,10 @@ export default function AllMemYoungseo() {
                 <Link to='/AllMemYoungseoEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="allday project 꾸미기" />영서<img src={MainH2Deco} alt="allday project 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                차분하고<br />
+                성실하게 잘 준비해서<br />
+                여러분을 만나러 갈게요!</p>
             <img src={All_mm_youngseo_img} alt="영서 이미지" className='tri_mm_s1_img all_mm_youngseo_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
