da213247c8710e7b1f810b69ffa9de74ae8928eb
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:08:16 +0900

    Update TwsMemDohun.tsx

--- src/modules/tws/members/TwsMemDohun.tsx
+++ src/modules/tws/members/TwsMemDohun.tsx
@@ -5,7 +5,7 @@ import './TwsMemDohun.css';
 
 import Menu from '../../Menu';
 
-// import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
+import Tws_mm_dohun_img from '../main/img/tws_m_mem_Dohun.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -21,8 +21,8 @@ export default function TwsMemDohun() {
             <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
                 그리고... 그리고... 그리고...<br />
                 더 열심히 더 열심히</p>
-            <img src="" alt="도훈 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <img src={Tws_mm_dohun_img} alt="도훈 이미지" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb tws_mm_dohun_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
