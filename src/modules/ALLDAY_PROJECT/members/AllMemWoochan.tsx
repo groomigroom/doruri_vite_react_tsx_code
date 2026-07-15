1f7cb411f659b7494dcb8adda58d2133ee32d6d8
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:54:41 +0900

    Update AllMemWoochan.tsx

--- src/modules/ALLDAY_PROJECT/members/AllMemWoochan.tsx
+++ src/modules/ALLDAY_PROJECT/members/AllMemWoochan.tsx
@@ -18,9 +18,10 @@ export default function AllMemWoochan() {
                 <Link to='/AllMemWoochanEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="allday project 꾸미기" />우찬<img src={MainH2Deco} alt="allday project 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                활발하고<br />
+                밝은 모습으로<br />
+                여러분을 만나러 갈게요!</p>
             <img src="" alt="우찬 이미지" className='tri_mm_s1_img all_mm_woochan_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
