344d76c8e6f7241d20b6b2a6eb1a21e05fd68a24
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:46:14 +0900

    Update AesMemNingning.tsx

--- src/modules/aespa/members/AesMemNingning.tsx
+++ src/modules/aespa/members/AesMemNingning.tsx
@@ -18,9 +18,10 @@ export default function AesMemNingning() {
                 <Link to='/AesMemNingningEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="aespa 꾸미기" />닝닝<img src={MainH2Deco} alt="aespa 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                솔직하게<br />
+                제 개성을 여러분께<br />
+                보여 드릴게요!</p>
             <img src="" alt="닝닝 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
