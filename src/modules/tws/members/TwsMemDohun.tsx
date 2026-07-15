3e50e63a8d81b1e4dafd8ffd9bcc4af4c65fd7d6
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:27:42 +0900

    Update TwsMemDohun.tsx

--- src/modules/tws/members/TwsMemDohun.tsx
+++ src/modules/tws/members/TwsMemDohun.tsx
@@ -18,9 +18,10 @@ export default function TwsMemDohun() {
                 <Link to='/TwsMemDohunEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="TWS 꾸미기" />도훈<img src={MainH2Deco} alt="TWS 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                다정하게<br />
+                멤버들을 잘 챙기며<br />
+                여러분을 만나러 갈게요!</p>
             <img src={Tws_mm_dohun_img} alt="도훈 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb tws_mm_dohun_intropa_tb'>
                 <tbody>
