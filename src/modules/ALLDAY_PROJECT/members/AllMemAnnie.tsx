a43a31635d2ec442c9284ee4cd0ab2ec55824574
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:48:48 +0900

    Update AllMemAnnie.tsx

--- src/modules/ALLDAY_PROJECT/members/AllMemAnnie.tsx
+++ src/modules/ALLDAY_PROJECT/members/AllMemAnnie.tsx
@@ -18,9 +18,10 @@ export default function AllMemAnnie() {
                 <Link to='/AllMemAnnieEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="allday project 꾸미기" />애니<img src={MainH2Deco} alt="allday project 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                멤버들의 다양한<br />
+                의견을 존중하며<br />
+                잘 활동해 볼게요!</p>
             <img src="" alt="애니 이미지" className='tri_mm_s1_img all_mm_annie_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
