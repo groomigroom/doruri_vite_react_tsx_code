3316dc7cf836abaed52a0ef871a5ef23a77aaa06
groomigroom <googjohn0143@gmail.com>
2026-07-15 15:34:29 +0900

    Update TwsMemHanjin.tsx

--- src/modules/tws/members/TwsMemHanjin.tsx
+++ src/modules/tws/members/TwsMemHanjin.tsx
@@ -18,9 +18,10 @@ export default function TwsMemHanjin() {
                 <Link to='/TwsMemHanjinEng'>English</Link>
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="TWS 꾸미기" />한진<img src={MainH2Deco} alt="TWS 꾸미기" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
+            <p className='tri_mm_s1_intropp'>
+                풍부한 감수성과<br />
+                진중한 마음을 가지고<br />
+                여러분을 찾아 갈게요!</p>
             <img src={Tws_mm_hanjin_img} alt="한진 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb tws_mm_hanjin_intropa_tb'>
                 <tbody>
