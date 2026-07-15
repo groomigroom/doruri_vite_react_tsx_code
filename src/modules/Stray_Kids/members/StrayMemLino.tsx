8d06b35285cf1926c505cdfbbec0c1d6b6132266
groomigroom <googjohn0143@gmail.com>
2026-07-15 10:45:24 +0900

    Update StrayMemLino.tsx

--- src/modules/Stray_Kids/members/StrayMemLino.tsx
+++ src/modules/Stray_Kids/members/StrayMemLino.tsx
@@ -27,41 +27,40 @@ export default function StrayMemLino() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>1998년 10월 25일</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>O형</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
+                        <td><p>DANCERACHA</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ISFP</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>이리노</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>SKZOO</p></td>
+                        <td><p>리빗</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>상징 동물</p></td>
+                        <td><p>토끼</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>포지션</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>메인 댄서</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>사교적임</p></td>
                     </tr>
                 </tbody>
             </table>
