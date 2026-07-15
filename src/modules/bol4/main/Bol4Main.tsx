6109aa9cd72855b39b2447ea6c0ac68343630881
groomigroom <googjohn0143@gmail.com>
2026-07-15 16:02:01 +0900

    Update Bol4Main.tsx

--- src/modules/bol4/main/Bol4Main.tsx
+++ src/modules/bol4/main/Bol4Main.tsx
@@ -20,10 +20,10 @@ export default function Bol4Main() {
             <img src={Bol_m_logo} alt="볼빨간사춘기 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="볼빨간사춘기 꾸미기" />볼빨간사춘기<img src={MainH2Deco} alt="볼빨간사춘기 꾸미기" /></h2>
             <p className='tri_m_intropp'>
-                #####<br />
-                하루에 하루만 더<br />
-                &apos;투모로우바이투게더&apos;와 함께 힘내 보아요.</p>
-            <img src={Bol_m_img} className='tri_m_img txt_m_img' alt='볼빨간사춘기' />
+                &apos;볼빨간사춘기&apos;와 함께<br />
+                작고 단단한 마음으로<br />
+                어딘가를 찾아 가봐요!</p>
+            <img src={Bol_m_img} className='tri_m_img' alt='볼빨간사춘기' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full ten_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
