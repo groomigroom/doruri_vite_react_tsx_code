e9101a5947515bcc12f34a4201ede181df70ad8f
groomigroom <googjohn0143@gmail.com>
2026-07-15 21:32:55 +0900

    Add files via upload

---
+++ src/modules/idntt/members/Idn_m_memid14.tsx
@@ -0,0 +1,89 @@
+import { Link } from 'react-router-dom';
+
+import './Idn_m_memid14.css';
+
+import Menu from '../../Menu';
+
+// import Idn_mm_memid14_img from '../main/img/tri_m_memS1.jpg';
+
+import MainH2Deco from '../../img/main_h2_deco.png';
+
+export default function Idn_m_memid14() {
+    return (
+        <div className="idn_mm_id14_full">
+            <Menu />
+            <section className="home_translate">
+                <Link to='/Idn_m_memid14' className="on">한국어</Link>
+                <Link to='/Idn_m_memid14Eng'>English</Link>
+            </section>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="idntt 로고 꾸미기" />한예준<img src={MainH2Deco} alt="idntt 로고 꾸미기" /></h2>
+            <p className='tri_mm_s1_intropp'>
+                #####24명은 많은 것 같아도<br />
+                멤버들 질서 유지를<br />
+                책임감 있게 다 해볼게요!
+            </p>
+            <img src='' alt="한예준 이미지" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb'>
+                <tbody>
+                    <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2003년 8월 6일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>161cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>B형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>햄햄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>취미</p></td>
+                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>특기</p></td>
+                        <td><p>셀카찍기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td><p>아이스크림</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>소속 유닛</p></td>
+                        <td>
+                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                LOVElution, neptune</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>좋음</p></td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </div>
+    );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
