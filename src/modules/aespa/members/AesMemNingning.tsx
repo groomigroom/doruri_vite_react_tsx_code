d6e02edd6c2c758c8ba24210d3525a1beef6dd36
groomigroom <googjohn0143@gmail.com>
2026-07-16 09:50:25 +0900

    Update AesMemNingning.tsx

--- src/modules/aespa/members/AesMemNingning.tsx
+++ src/modules/aespa/members/AesMemNingning.tsx
@@ -27,7 +27,7 @@ export default function AesMemNingning() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2002년 10월 23일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
@@ -35,33 +35,32 @@ export default function AesMemNingning() {
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>O형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>포지션</p></td>
+                        <td><p>보컬</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>전투 스킬</p></td>
+                        <td><p>E.D Hacker</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>캐릭터</p></td>
+                        <td><p>뤠</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>INFP</p></td>
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>닝</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>솔직함</p></td>
                     </tr>
                 </tbody>
             </table>
