8ad7e1c82c68ba622a8d0459623d8ab92395bf64
groomigroom <googjohn0143@gmail.com>
2026-07-16 15:45:41 +0900

    Update ArtMemHeejin.tsx

--- src/modules/ARTMS/members/ArtMemHeejin.tsx
+++ src/modules/ARTMS/members/ArtMemHeejin.tsx
@@ -1,1 +1,74 @@
-ArtMemHeejin.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/members/TriplesMemS1.css';
+import './ArtMemHeejin.css';
+
+import Menu from '../../Menu';
+
+// import Art_mm_Heejin_img from '../main/img/tri_m_memS1.jpg';
+
+import MainH2Deco from '../../img/main_h2_deco.png';
+
+export default function ArtMemHeejin() {
+    return (
+        <div className="art_mm_heejin_full">
+            <Menu />
+            <section className="home_translate">
+                <Link to='/ArtMemHeejin' className="on">한국어</Link>
+                <Link to='/ArtMemHeejinEng'>English</Link>
+            </section>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="ARTMS deco" />희진<img src={MainH2Deco} alt="ARTMS deco" /></h2>
+            <p className='tri_mm_s1_intropp'>
+                &apos;희진&apos;과 함께<br />
+                Don&apos;t Wanna<br />
+                Breakdown!
+            </p>
+            <img src='' alt="희진" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb'>
+                <tbody>
+                    <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2003년 8월 6일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>161cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>B형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>햄햄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>취미</p></td>
+                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>특기</p></td>
+                        <td><p>셀카찍기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td><p>아이스크림</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>소속 유닛</p></td>
+                        <td>
+                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                LOVElution, neptune</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>좋음</p></td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </div>
+    );
+};
+
