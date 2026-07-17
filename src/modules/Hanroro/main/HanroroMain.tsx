f9e7416deada6a4117ad27d71b4afb4e62bbef1a
groomigroom <googjohn0143@gmail.com>
2026-07-17 13:55:20 +0900

    Update HanroroMain.tsx

--- src/modules/Hanroro/main/HanroroMain.tsx
+++ src/modules/Hanroro/main/HanroroMain.tsx
@@ -1,25 +1,25 @@
 import { Link } from 'react-router-dom';
 
 import '../../triples/main/TriplesMain.css'
-import './TenCMMain.css';
+import './HanroroMain.css';
 
 import Menu from '../../Menu';
 
-import Ten_m_logo from './img/TenCM_m_logo.svg';
+//import Han_m_logo from './img/TenCM_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-import Ten_m_img from './img/10cm_m_mem_Gwonjungyeol.jpg';
+//import Ten_m_img from './img/10cm_m_mem_Gwonjungyeol.jpg';
 
 //000000
 
 export default function HanroroMain() {
     return (
-        <div className='ten_m_full'>
+        <div className='han_m_full'>
             <Menu />
             <section className="home_translate">
                 <Link to='/HanroroMain' className="on">한국어</Link>
                 <Link to='/HanroroMainEng'>English</Link>
             </section>
-            <img src={Ten_m_logo} alt="10CM 로고" className='tri_m_logo' />
+            <img src='' alt="한로로 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="한로로 꾸미기" />한로로<img src={MainH2Deco} alt="한로로 꾸미기" /></h2>
             <p className='tri_m_intropp'>
                 일말의 가능성이 없더라도<br />
