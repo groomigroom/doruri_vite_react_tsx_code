90278684a68e3611f7b58202ba89bc95c67229b7
groomigroom <googjohn0143@gmail.com>
2026-07-17 13:51:15 +0900

    Update HanroroMain.tsx

--- src/modules/Hanroro/main/HanroroMain.tsx
+++ src/modules/Hanroro/main/HanroroMain.tsx
@@ -1,1 +1,51 @@
-HanroroMain.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/main/TriplesMain.css'
+import './TenCMMain.css';
+
+import Menu from '../../Menu';
+
+import Ten_m_logo from './img/TenCM_m_logo.svg';
+import MainH2Deco from '../../img/main_h2_deco.png';
+import Ten_m_img from './img/10cm_m_mem_Gwonjungyeol.jpg';
+
+//000000
+
+export default function HanroroMain() {
+    return (
+        <div className='ten_m_full'>
+            <Menu />
+            <section className="home_translate">
+                <Link to='/HanroroMain' className="on">한국어</Link>
+                <Link to='/HanroroMainEng'>English</Link>
+            </section>
+            <img src={Ten_m_logo} alt="10CM 로고" className='tri_m_logo' />
+            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="한로로 꾸미기" />한로로<img src={MainH2Deco} alt="한로로 꾸미기" /></h2>
+            <p className='tri_m_intropp'>
+                일말의 가능성이 없더라도<br />
+                &apos;10CM&apos;와 함께<br />
+                오늘도 힘내 보아요!
+            </p>
+            <img src={Ten_m_img} className='tri_m_img ten_m_img' alt='한로로' />
+            <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
+            <section className='tri_m_mem_tr_full ten_m_mem_tr_full'>
+                <section className='tri_m_mem_tr'>
+                    <Link to='/TenMemGwonjungyeol' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={Ten_m_img} alt="권정열 이미지" />
+                            <div>
+                                <h4>권정열</h4>
+                            </div>
+                        </article>
+                    </Link>
+                </section>
+
+
+            </section>
+
+
+        </div>
+    );
+};
+
+
