fdd8262336f7fe225d20fae644b96611cf4d5b9a
groomigroom <googjohn0143@gmail.com>
2026-07-17 18:43:14 +0900

    Update BtoMemLeeMinhyuk.tsx

--- src/modules/BTOB/members/BtoMemLeeMinhyuk.tsx
+++ src/modules/BTOB/members/BtoMemLeeMinhyuk.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import '../../triples/members/TriplesMemS1.css';
 import './BtoMemLeeMinhyuk.css';
 
 import Menu from '../../Menu';
@@ -71,6 +72,5 @@ export default function BtoMemLeeMinhyuk() {
         </div>
     );
 };
-
 
 
