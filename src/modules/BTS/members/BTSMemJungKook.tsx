a8cb949a1717e9cc48756c4f8d9a40b25d2e3052
groomigroom <googjohn0143@gmail.com>
2026-07-17 16:38:11 +0900

    Update BTSMemJungKook.tsx

--- src/modules/BTS/members/BTSMemJungKook.tsx
+++ src/modules/BTS/members/BTSMemJungKook.tsx
@@ -1,2 +1,74 @@
-BTSMemJungKook.tsx
-정국
+import { Link } from 'react-router-dom';
+
+import './BTSMemJungKook.css';
+
+import Menu from '../../Menu';
+
+//import Tri_mm_s1_img from '../main/img/tri_m_memS1.jpg';
+
+import MainH2Deco from '../../img/main_h2_deco.png';
+
+export default function BTSMemJungKook() {
+    return (
+        <div className="bts_mm_jin_full">
+            <Menu />
+            <section className="home_translate">
+                <Link to='/BTSMemJungKook' className="on">한국어</Link>
+                <Link to='/BTSMemJungKookEng'>English</Link>
+            </section>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="방탄소년단 꾸미기" />정국<img src={MainH2Deco} alt="방탄소년단 꾸미기" /></h2>
+            <p className='tri_mm_s1_intropp'>
+                &apos;정국&apos;과 함께<br />
+                어딘가로 열심히<br />
+                달려 가봐요!
+            </p>
+            <img src='' alt="정국 이미지" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb'>
+                <tbody>
+                    <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2003년 8월 6일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>161cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>B형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>햄햄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>취미</p></td>
+                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>특기</p></td>
+                        <td><p>셀카찍기</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td><p>아이스크림</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>소속 유닛</p></td>
+                        <td>
+                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
+                                LOVElution, neptune</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>좋음</p></td>
+                    </tr>
+                </tbody>
+            </table>
+
+
+        </div>
+    );
+};
+
+
