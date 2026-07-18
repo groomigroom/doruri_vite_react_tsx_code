99bae6ebbbfb33ad4609111e3799734a8f4f7abc
groomigroom <googjohn0143@gmail.com>
2026-07-18 13:45:05 +0900

    Idn_m_memid18.tsx 업데이트

--- src/modules/idntt/members/Idn_m_memid18.tsx
+++ src/modules/idntt/members/Idn_m_memid18.tsx
@@ -73,21 +73,21 @@ export default function Idn_m_memid18() {
 
 
 1 출생 
-2008년 1월 1일
+2008년 10월 11일
 2 키
 ??
 3 혈액형
-B형
+A형
 4 소속 유닛
 itsnotover
 5 MBTI
-ISTJ
+ISFP
 6 상징색
-#0050a4
+#62d3d1
 7 성격
 ??
 8 별명
-아기 리더
+밤돌이
 
 
 
