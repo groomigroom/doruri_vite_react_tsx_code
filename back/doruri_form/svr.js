84e4a275babe69d665d581cb3f84925f7152cd0d
groomigroom <googjohn0143@gmail.com>
2026-07-19 13:52:08 +0900

    Update svr.js

--- back/doruri_form/svr.js
+++ back/doruri_form/svr.js
@@ -2,12 +2,19 @@ const express = require('express');
 const mysql = require('mysql');
 const path = require('path');
 const static = require('serve_static');
+const dbconfig = require('./config/dbconfig.json');
 
+//Database connection pool
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: '122.32.33.22',
-    user: '---',
-    password: 'veryimport',
-    database: 'FirstDabase',
+    host: dbconfig.host,
+    user: dbconfig.user,
+    password: dbconfig.password,
+    database: dbconfig.database,
     debug: false
 });
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/public', static(path.join(__dirname, 'public')));
