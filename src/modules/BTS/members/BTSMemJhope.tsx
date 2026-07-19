a71e6210288e3c496b671052063b223eaf0974b0
groomigroom <googjohn0143@gmail.com>
2026-07-19 13:22:34 +0900

    Update BTSMemJhope.tsx

--- src/modules/BTS/members/BTSMemJhope.tsx
+++ src/modules/BTS/members/BTSMemJhope.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import '../../triples/members/TriplesMemS1.css';
 import './BTSMemJhope.css';
 
 import Menu from '../../Menu';
@@ -71,6 +72,3 @@ export default function BTSMemJhope() {
         </div>
     );
 };
-
-
-
