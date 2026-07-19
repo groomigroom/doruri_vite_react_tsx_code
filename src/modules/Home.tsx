62cd9d92d4fee6641ddcaadd40a7a0022c0f4144
groomigroom <googjohn0143@gmail.com>
2026-07-19 18:32:36 +0900

    Add FooterPart component to Home

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import "./home.css";
 
 import Menu from './Menu';
+import FooterPart from "./FooterPart";
 
 export default function Home() {
     return (
@@ -12,6 +13,7 @@ export default function Home() {
                 <Link to='/' className="on">한국어</Link>
                 <Link to='/Eng'>English</Link>
             </section>
+            <FooterPart />
         </div>
     )
 }
