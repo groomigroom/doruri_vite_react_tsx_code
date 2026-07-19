ec9f867a94a9fa30826b01e903649cdaf54b466f
groomigroom <googjohn0143@gmail.com>
2026-07-19 13:30:02 +0900

    Update BTSMemJungKook.tsx

--- src/modules/BTS/members/BTSMemJungKook.tsx
+++ src/modules/BTS/members/BTSMemJungKook.tsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import '../../triples/members/TriplesMemS1.css';
 import './BTSMemJungKook.css';
 
 import Menu from '../../Menu';
@@ -70,5 +71,3 @@ export default function BTSMemJungKook() {
         </div>
     );
 };
-
-
