f099913e0086244c6e9f84bee47a83ac9064c5cb
groomigroom <googjohn0143@gmail.com>
2026-07-19 18:34:18 +0900

    Update FooterPart.tsx

--- src/modules/FooterPart.tsx
+++ src/modules/FooterPart.tsx
@@ -2,6 +2,9 @@ import './FooterPart.css';
 
 export default function FooterPart() {
     return (
-        <div className="footerpart_full"></div>
+        <div className="footerpart_full">
+            <p>부분별 회사용</p>
+            <p>c 도두리</p>
+        </div>
     );
 };
