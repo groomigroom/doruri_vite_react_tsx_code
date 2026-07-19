2ce320700ae6d4086b48bedf6c41e54a93545a12
groomigroom <googjohn0143@gmail.com>
2026-07-20 06:40:26 +0900

    Update member details in Idn_m_memid4 component

--- src/modules/idntt/members/Idn_m_memid4.tsx
+++ src/modules/idntt/members/Idn_m_memid4.tsx
@@ -18,50 +18,44 @@ export default function Idn_m_memid4() {
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="idntt 로고 꾸미기" />최태인<img src={MainH2Deco} alt="idntt 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>
-                #####24명은 많은 것 같아도<br />
-                멤버들 질서 유지를<br />
-                책임감 있게 다 해볼게요!
+                신중하게<br />
+                잘 준비해서<br />
+                여러분을 만나러 갈게요!
             </p>
             <img src='' alt="최태인 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2005년 11월 17일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>185cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>??</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>unevermet</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>INTP</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>상징색</p></td>
+                        <td><p>#564fed</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>성격</p></td>
+                        <td><p>차분함</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>별명</p></td>
+                        <td><p>쿼카</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -73,59 +67,5 @@ export default function Idn_m_memid4() {
 
 
 
-1 출생
-2005년 11월 17일
-2 키
-185cm
-3 혈액형
-??
-4 소속 유닛
-unevermet
-5 MBTI
-INTP
-6 상징색
-#564fed
-7 성격
-차분함
-8 별명
-쿼카
-
-신중하게
-잘 준비해서
-여러분을 만나러 갈게요!
 
 
-idntt 4번
-
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>2005년 11월 17일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>185cm</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>??</p></td>
-</tr>
-<tr>
-<td><p>소속 유닛</p></td>
-<td><p>unevermet</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>INTP</p></td>
-</tr>
-<tr>
-<td><p>상징색</p></td>
-<td><p>#564fed</p></td>
-</tr>
-<tr>
-<td><p>성격</p></td>
-<td><p>차분함</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>별명</p></td>
-<td><p>쿼카</p></td>
-</tr>
