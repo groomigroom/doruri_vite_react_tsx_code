46522efc0d1fda0cd8933c99b2db5f0d88253563
groomigroom <googjohn0143@gmail.com>
2026-07-20 08:28:24 +0900

    Update TriplesMemS14.tsx

--- src/modules/triples/members/TriplesMemS14.tsx
+++ src/modules/triples/members/TriplesMemS14.tsx
@@ -4,6 +4,7 @@ import './TriplesMemS1.css';
 import './TriplesMemS14.css';
 
 import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
 
 import Tri_mm_s14_img from '../main/img/tri_m_memS14.jpg';
 
@@ -66,6 +67,7 @@ export default function TriplesMemS14() {
                     </tr>
                 </tbody>
             </table>
+            <FooterPart />
         </div>
     );
 
