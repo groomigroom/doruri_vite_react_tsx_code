0eec75586b36ce89f32c0aef499739b73ffb27c3
groomigroom <googjohn0143@gmail.com>
2026-07-20 08:23:53 +0900

    Update TriplesMemS10.tsx

--- src/modules/triples/members/TriplesMemS10.tsx
+++ src/modules/triples/members/TriplesMemS10.tsx
@@ -4,6 +4,7 @@ import './TriplesMemS1.css';
 import './TriplesMemS10.css';
 
 import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
 
 import Tri_mm_s10_img from '../main/img/tri_m_memS10.jpg';
 
@@ -65,6 +66,7 @@ export default function TriplesMemS10() {
                     </tr>
                 </tbody>
             </table>
+            <FooterPart />
         </div>
     );
 
