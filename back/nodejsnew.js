7212c47581231a788df065cb3a7e4896c0d28848
groomigroom <googjohn0143@gmail.com>
2026-07-20 07:15:23 +0900

    Update nodejsnew.js

--- back/nodejsnew.js
+++ back/nodejsnew.js
@@ -1,8 +1,6 @@
-터미널에 npm init -y
+npm init -y로 package.json 기본 설정으로 만들기
 
-npm install express --save
+npm install express --save 로 express 설치하기
+https://www.youtube.com/watch?v=cTJ8QKyLD6A&list=PLwsSnOYjEpei9J0zWjwvSWCnHyqxuPLTW&index=5
 
-
-
-
-
+4분 34초까지봄
