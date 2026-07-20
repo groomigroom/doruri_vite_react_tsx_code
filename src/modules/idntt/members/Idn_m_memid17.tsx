eda27794690f3c7e75c275b424bc5ff6fa50540d
groomigroom <googjohn0143@gmail.com>
2026-07-20 15:33:53 +0900

    Update Idn_m_memid17.tsx

--- src/modules/idntt/members/Idn_m_memid17.tsx
+++ src/modules/idntt/members/Idn_m_memid17.tsx
@@ -27,41 +27,35 @@ export default function Idn_m_memid17() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2008년 1월 1일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>??</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
                         <td><p>B형</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
                         <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>itsnotover</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ISTJ</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>상징색</p></td>
+                        <td><p>#0050a4</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>성격</p></td>
+                        <td><p>??</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>별명</p></td>
+                        <td><p>아기 리더</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -73,58 +67,10 @@ export default function Idn_m_memid17() {
 
 
 
-1 출생 
-2008년 1월 1일
-2 키
-??
-3 혈액형
-B형
-4 소속 유닛
-itsnotover
-5 MBTI
-ISTJ
-6 상징색
-#0050a4
-7 성격
-??
-8 별명
-아기 리더
 
 
-idntt 17번
 
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>2008년 1월 1일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>??</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>B형</p></td>
-</tr>
-<tr>
-<td><p>소속 유닛</p></td>
-<td><p>itsnotover</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ISTJ</p></td>
-</tr>
-<tr>
-<td><p>상징색</p></td>
-<td><p>#0050a4</p></td>
-</tr>
-<tr>
-<td><p>성격</p></td>
-<td><p>??</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>별명</p></td>
-<td><p>아기 리더</p></td>
-</tr>
+
 
 
 
