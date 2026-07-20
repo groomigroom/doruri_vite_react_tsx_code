3a826f3a24a5bda30a8e160b5b1dfc03ab43d2a9
groomigroom <googjohn0143@gmail.com>
2026-07-20 13:07:42 +0900

    Update Idn_m_memid9.tsx

--- src/modules/idntt/members/Idn_m_memid9.tsx
+++ src/modules/idntt/members/Idn_m_memid9.tsx
@@ -27,41 +27,35 @@ export default function Idn_m_memid9() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2003년 6월 4일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>184cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>A형</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>yesweare</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ISFP</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>상징색</p></td>
+                        <td><p>#f99bbb</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>성격</p></td>
+                        <td><p>??</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>별명</p></td>
+                        <td><p>??</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -71,57 +65,9 @@ export default function Idn_m_memid9() {
     );
 };
 
-1 출생 
-2003년 6월 4일
-2 키 ㅇㅇ
-184cm
-3 혈액형
-A형
-4 소속 유닛
-yesweare
-5 MBTI
-ISFP
-6 상징색
-#f99bbb
-7 성격
-??
-8 별명
-??
 
 
-idntt 8번
 
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>2003년 6월 4일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>184cm</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>A형</p></td>
-</tr>
-<tr>
-<td><p>소속 유닛</p></td>
-<td><p>yesweare</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ISFP</p></td>
-</tr>
-<tr>
-<td><p>상징색</p></td>
-<td><p>#f99bbb</p></td>
-</tr>
-<tr>
-<td><p>성격</p></td>
-<td><p>??</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>별명</p></td>
-<td><p>??</p></td>
-</tr>
+
 
 
