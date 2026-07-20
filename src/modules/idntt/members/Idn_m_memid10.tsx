33dc7607ac58c6828d7e4f4852a034d8c5d5859c
groomigroom <googjohn0143@gmail.com>
2026-07-20 13:08:57 +0900

    Update Idn_m_memid10.tsx

--- src/modules/idntt/members/Idn_m_memid10.tsx
+++ src/modules/idntt/members/Idn_m_memid10.tsx
@@ -27,41 +27,35 @@ export default function Idn_m_memid10() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2006년 4월 9일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>179cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>A형</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>yesweare</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>INFP</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>상징색</p></td>
+                        <td><p>#338ce5</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>성격</p></td>
+                        <td><p>거짓말을 못함</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>별명</p></td>
+                        <td><p>게로네코</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -71,58 +65,10 @@ export default function Idn_m_memid10() {
     );
 };
 
-1 출생 
-2006년 4월 9일
-2 키
-179cm
-3 혈액형
-A형
-4 소속 유닛
-yesweare
-5 MBTI
-INFP
-6 상징색
-#338ce5
-7 성격
-거짓말을 못함
-8 별명
-게로네코
 
 
-idntt 10번
 
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>2006년 4월 9일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>179cm</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>A형</p></td>
-</tr>
-<tr>
-<td><p>소속 유닛</p></td>
-<td><p>yesweare</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>INFP</p></td>
-</tr>
-<tr>
-<td><p>상징색</p></td>
-<td><p>#338ce5</p></td>
-</tr>
-<tr>
-<td><p>성격</p></td>
-<td><p>거짓말을 못함</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>별명</p></td>
-<td><p>게로네코</p></td>
-</tr>
+
 
 
 
