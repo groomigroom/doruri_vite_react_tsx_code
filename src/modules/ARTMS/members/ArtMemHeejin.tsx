c82be674054cfa46f30430c4b5a201226549a891
groomigroom <googjohn0143@gmail.com>
2026-07-21 08:45:58 +0900

    ArtMemHeejin.tsx 업데이트

--- src/modules/ARTMS/members/ArtMemHeejin.tsx
+++ src/modules/ARTMS/members/ArtMemHeejin.tsx
@@ -72,3 +72,46 @@ export default function ArtMemHeejin() {
     );
 };
 
+artms 희진
+
+<tr className='tri_mm_s1_intropa_tb_tr1'>
+<td><p>출생</p></td>
+<td><p>2000년 10월 19일</p></td>
+</tr>
+<tr>
+<td><p>키</p></td>
+<td><p>161.2cm</p></td>
+</tr>
+<tr>
+<td><p>혈액형</p></td>
+<td><p>A형</p></td>
+</tr>
+<tr>
+<td><p>상징</p></td>
+<td><p>토끼</p></td>
+</tr>
+<tr>
+<td><p>별명</p></td>
+<td><p>희끼</p></td>
+</tr>
+<tr>
+<td><p>MBTI</p></td>
+<td><p>ENTJ</p></td>
+</tr>
+<tr>
+<td><p>소속 유닛</p></td>
+<td><p>x</p></td>
+</tr>
+<tr className='tri_mm_s1_intropa_tb_tr9'>
+<td><p>성격</p></td>
+<td><p>자존감 높음</p></td>
+</tr>
+
+1 출생v
+2 키
+3 혈액형f
+4 상징ㅎ
+5 별명ㅎ
+6 MBTI
+7 소속 유닛
+8 성격 희진 자존감 높음 하슬 계획적임 김립 부지런함 진솔 활발함 최리 발랄함
