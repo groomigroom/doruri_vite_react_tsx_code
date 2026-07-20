2570ac82c10c8fd2b7bb892240e2e8a4d0d0b168
groomigroom <googjohn0143@gmail.com>
2026-07-20 09:16:52 +0900

    Idn_m_memid10.tsx 업데이트

--- src/modules/idntt/members/Idn_m_memid10.tsx
+++ src/modules/idntt/members/Idn_m_memid10.tsx
@@ -89,7 +89,40 @@ INFP
 게로네코
 
 
+idntt 10번
 
+<tr className='tri_mm_s1_intropa_tb_tr1'>
+<td><p>출생</p></td>
+<td><p>2006년 4월 9일</p></td>
+</tr>
+<tr>
+<td><p>키</p></td>
+<td><p>179cm</p></td>
+</tr>
+<tr>
+<td><p>혈액형</p></td>
+<td><p>A형</p></td>
+</tr>
+<tr>
+<td><p>소속 유닛</p></td>
+<td><p>yesweare</p></td>
+</tr>
+<tr>
+<td><p>MBTI</p></td>
+<td><p>INFP</p></td>
+</tr>
+<tr>
+<td><p>상징색</p></td>
+<td><p>#338ce5</p></td>
+</tr>
+<tr>
+<td><p>성격</p></td>
+<td><p>거짓말을 못함</p></td>
+</tr>
+<tr className='tri_mm_s1_intropa_tb_tr9'>
+<td><p>별명</p></td>
+<td><p>게로네코</p></td>
+</tr>
 
 
 
