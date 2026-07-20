ab448bb7dd18be4df3d6f457e17af1dc3e5bcf3f
groomigroom <googjohn0143@gmail.com>
2026-07-20 09:29:32 +0900

    Idn_m_memid12.tsx 업데이트

--- src/modules/idntt/members/Idn_m_memid12.tsx
+++ src/modules/idntt/members/Idn_m_memid12.tsx
@@ -91,7 +91,40 @@ ESTJ
 
 
 
+idntt 12번
 
+<tr className='tri_mm_s1_intropa_tb_tr1'>
+<td><p>출생</p></td>
+<td><p>2008년 12월 4일</p></td>
+</tr>
+<tr>
+<td><p>키</p></td>
+<td><p>182cm</p></td>
+</tr>
+<tr>
+<td><p>혈액형</p></td>
+<td><p>B형</p></td>
+</tr>
+<tr>
+<td><p>소속 유닛</p></td>
+<td><p>yesweare</p></td>
+</tr>
+<tr>
+<td><p>MBTI</p></td>
+<td><p>ESTJ</p></td>
+</tr>
+<tr>
+<td><p>상징색</p></td>
+<td><p>#fdd82e</p></td>
+</tr>
+<tr>
+<td><p>성격</p></td>
+<td><p>순함</p></td>
+</tr>
+<tr className='tri_mm_s1_intropa_tb_tr9'>
+<td><p>별명</p></td>
+<td><p>??</p></td>
+</tr>
 
 
 
