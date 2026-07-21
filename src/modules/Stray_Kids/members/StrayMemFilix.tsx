2a74c965d9f1ee28f5aa2c7fd4b360cc78e06e9d
groomigroom <googjohn0143@gmail.com>
2026-07-22 08:14:37 +0900

    Update StrayMemFilix.tsx

--- src/modules/Stray_Kids/members/StrayMemFilix.tsx
+++ src/modules/Stray_Kids/members/StrayMemFilix.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import './StrayMemFilix.css';
 
 import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
 
 import Str_mm_filix_img from '../main/img/stray_m_mem_Filix.jpg';
 
@@ -64,7 +65,7 @@ export default function StrayMemFilix() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPart />
         </div>
     );
 }
