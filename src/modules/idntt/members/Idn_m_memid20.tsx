2462313d148666f032d0730140ae3def2e4e69f8
groomigroom <googjohn0143@gmail.com>
2026-07-21 12:14:55 +0900

    Update Idn_m_memid20.tsx

--- src/modules/idntt/members/Idn_m_memid20.tsx
+++ src/modules/idntt/members/Idn_m_memid20.tsx
@@ -18,50 +18,44 @@ export default function Idn_m_memid20() {
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="idntt 로고 꾸미기" />조은찬<img src={MainH2Deco} alt="idntt 로고 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>
-                #####24명은 많은 것 같아도<br />
-                멤버들 질서 유지를<br />
-                책임감 있게 다 해볼게요!
+                개성있는<br />
+                제 음악과 춤을<br />
+                전해 드릴게요!
             </p>
             <img src='' alt="조은찬 이미지" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2007년 9월 7일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>180cm 후반</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>A형</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>itsnotover</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ENFP</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>상징색</p></td>
+                        <td><p>#846eea</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>성격</p></td>
+                        <td><p>4차원</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>별명</p></td>
+                        <td><p>모드하우스 1호 알파카</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -72,63 +66,13 @@ export default function Idn_m_memid20() {
 };
 
 
-1 출생 
-2007년 9월 7일
-2 키
-180cm 후반
-3 혈액형
-A형
-4 소속 유닛
-itsnotover
-5 MBTI
-ENFP
-6 상징색
-#846eea
-7 성격
-4차원
-8 별명
-모드하우스 1호 알파카
 
 
-idntt 20번
-
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>2007년 9월 7일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>180cm 후반</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>A형</p></td>
-</tr>
-<tr>
-<td><p>소속 유닛</p></td>
-<td><p>itsnotover</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ENFP</p></td>
-</tr>
-<tr>
-<td><p>상징색</p></td>
-<td><p>#846eea</p></td>
-</tr>
-<tr>
-<td><p>성격</p></td>
-<td><p>4차원</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>별명</p></td>
-<td><p>모드하우스 1호 알파카</p></td>
-</tr>
 
 
-개성있는
-제 음악과 춤을
-전해 드릴게요!
+
+
+
 
 
 
