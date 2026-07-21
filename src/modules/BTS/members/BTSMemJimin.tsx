dfae8910a5fca32252563a830a73d5c278e85ae9
groomigroom <googjohn0143@gmail.com>
2026-07-21 21:50:20 +0900

    BTSMemJimin.tsx 업데이트

--- src/modules/BTS/members/BTSMemJimin.tsx
+++ src/modules/BTS/members/BTSMemJimin.tsx
@@ -72,3 +72,33 @@ export default function BTSMemJimin() {
         </div>
     );
 };
+방탄 지민
+
+<tr className='tri_mm_s1_intropa_tb_tr1'>
+<td><p>출생</p></td>
+<td><p>1995년 10월 13일</p></td>
+</tr>
+<tr>
+<td><p>키</p></td>
+<td><p>174cm</p></td>
+</tr>
+<tr>
+<td><p>혈액형</p></td>
+<td><p>A형</p></td>
+</tr>
+<tr>
+<td><p>포지션</p></td>
+<td><p>메인댄서, 리드보컬</p></td>
+</tr>
+<tr>
+<td><p>MBTI</p></td>
+<td><p>ESTJ</p></td>
+</tr>
+<tr>
+<td><p>별명</p></td>
+<td><p>빡지민이</p></td>
+</tr>
+<tr className='tri_mm_s1_intropa_tb_tr9'>
+<td><p>성격</p></td>
+<td><p>??</p></td>
+</tr>
