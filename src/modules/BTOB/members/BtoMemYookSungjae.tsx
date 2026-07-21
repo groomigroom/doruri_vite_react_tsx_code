5fb2bb1fad8e7f5105250a330ac840e88bbeec2d
groomigroom <googjohn0143@gmail.com>
2026-07-21 19:41:43 +0900

    BtoMemYookSungjae.tsx 업데이트

--- src/modules/BTOB/members/BtoMemYookSungjae.tsx
+++ src/modules/BTOB/members/BtoMemYookSungjae.tsx
@@ -73,3 +73,37 @@ export default function BtoMemYookSungjae() {
     );
 };
 
+비투비 육성재
+
+<tr className='tri_mm_s1_intropa_tb_tr1'>
+<td><p>출생</p></td>
+<td><p>1995년 5월 2일</p></td>
+</tr>
+<tr>
+<td><p>키</p></td>
+<td><p>180cm</p></td>
+</tr>
+<tr>
+<td><p>혈액형</p></td>
+<td><p>A형</p></td>
+</tr>
+<tr>
+<td><p>소속 유닛</p></td>
+<td><p>비투비-블루</p></td>
+</tr>
+<tr>
+<td><p>포지션</p></td>
+<td><p>서브보컬</p></td>
+</tr>
+<tr>
+<td><p>MBTI</p></td>
+<td><p>ENTP</p></td>
+</tr>
+<tr>
+<td><p>별명</p></td>
+<td><p>육잘또</p></td>
+</tr>
+<tr className='tri_mm_s1_intropa_tb_tr9'>
+<td><p>성격</p></td>
+<td><p>착함</p></td>
+</tr>
