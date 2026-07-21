c886b6fbb210ccdbb6bb276d5e409520d1608e57
groomigroom <googjohn0143@gmail.com>
2026-07-21 14:00:55 +0900

    BtoMemPeniel.tsx 업데이트

--- src/modules/BTOB/members/BtoMemPeniel.tsx
+++ src/modules/BTOB/members/BtoMemPeniel.tsx
@@ -72,3 +72,38 @@ export default function BtoMemPeniel() {
         </div>
     );
 };
+
+비투비 프니엘
+
+<tr className='tri_mm_s1_intropa_tb_tr1'>
+<td><p>출생</p></td>
+<td><p>1993년 3월 10일</p></td>
+</tr>
+<tr>
+<td><p>키</p></td>
+<td><p>174cm</p></td>
+</tr>
+<tr>
+<td><p>혈액형</p></td>
+<td><p>B형</p></td>
+</tr>
+<tr>
+<td><p>소속 유닛</p></td>
+<td><p>비투비 포유</p></td>
+</tr>
+<tr>
+<td><p>포지션</p></td>
+<td><p>리드래퍼, 메인댄서</p></td>
+</tr>
+<tr>
+<td><p>MBTI</p></td>
+<td><p>ESTP</p></td>
+</tr>
+<tr>
+<td><p>별명</p></td>
+<td><p>동그니</p></td>
+</tr>
+<tr className='tri_mm_s1_intropa_tb_tr9'>
+<td><p>성격</p></td>
+<td><p>착함</p></td>
+</tr>
