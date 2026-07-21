916855b02f5cac7cc2059647c1c51e1610a7cf62
groomigroom <googjohn0143@gmail.com>
2026-07-22 08:18:55 +0900

    Update StrayMemHyunjin.tsx

--- src/modules/Stray_Kids/members/StrayMemHyunjin.tsx
+++ src/modules/Stray_Kids/members/StrayMemHyunjin.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import './StrayMemHyunjin.css';
 
 import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
 
 import Str_mm_hyunjin_img from '../main/img/stray_m_mem_Hyunjin.jpg';
 
@@ -64,7 +65,7 @@ export default function StrayMemHyunjin() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPart />
         </div>
     );
 }
