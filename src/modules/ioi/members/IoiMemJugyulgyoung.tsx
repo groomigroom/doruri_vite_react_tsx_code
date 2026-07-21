a21fec18454bbb864c5d57fb6c07fa4d589d5692
groomigroom <googjohn0143@gmail.com>
2026-07-22 08:03:43 +0900

    Update IoiMemJugyulgyoung.tsx

--- src/modules/ioi/members/IoiMemJugyulgyoung.tsx
+++ src/modules/ioi/members/IoiMemJugyulgyoung.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import './IoiMemJugyulgyoung.css';
 
 import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
 
 import Ioi_m_MemJugyulgyoung_img from '../main/img/ioi_m_mem_Jugyulgyung.jpg';
 
@@ -51,7 +52,7 @@ export default function IoiMemJugyulgyoung() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPart />
         </div>
     );
 };
