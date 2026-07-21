ba84196d6dc02e7405d2bf284e6f2b1040606f83
groomigroom <googjohn0143@gmail.com>
2026-07-22 08:35:37 +0900

    Update TwsMemSinyu.tsx

--- src/modules/tws/members/TwsMemSinyu.tsx
+++ src/modules/tws/members/TwsMemSinyu.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import './TwsMemSinyu.css';
 
 import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
 
 import Tws_mm_sinyu_img from '../main/img/tws_m_mem_Sinyu.jpg';
 
@@ -55,7 +56,7 @@ export default function TwsMemSinyu() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPart />
         </div>
     );
 };
