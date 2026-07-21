a799c1b25175729105aaefdf6b41e681e24f4e64
groomigroom <googjohn0143@gmail.com>
2026-07-22 08:04:50 +0900

    Update IoiMemKimchungha.tsx

--- src/modules/ioi/members/IoiMemKimchungha.tsx
+++ src/modules/ioi/members/IoiMemKimchungha.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import './IoiMemKimchungha.css';
 
 import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
 
 import Ioi_m_MemKimchungha_img from '../main/img/ioi_m_mem_Kimchungha.jpg';
 
@@ -51,7 +52,7 @@ export default function IoiMemKimchungha() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPart />
         </div>
     );
 };
