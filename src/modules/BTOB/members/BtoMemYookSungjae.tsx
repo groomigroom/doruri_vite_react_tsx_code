5b5563a7c937f73990953577c19ff7fcf5d355ca
groomigroom <googjohn0143@gmail.com>
2026-07-22 12:32:03 +0900

    Update BtoMemYookSungjae.tsx

--- src/modules/BTOB/members/BtoMemYookSungjae.tsx
+++ src/modules/BTOB/members/BtoMemYookSungjae.tsx
@@ -30,42 +30,37 @@ export default function BtoMemYookSungjae() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>1995년 5월 2일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>180cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>A형</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>비투비-블루</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>포지션</p></td>
+                        <td><p>서브보컬</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ENTP</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>육잘또</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>착함</p></td>
                     </tr>
+
                 </tbody>
             </table>
             <FooterPart />
@@ -74,39 +69,3 @@ export default function BtoMemYookSungjae() {
     );
 };
 
-
-
-비투비 육성재
-
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>1995년 5월 2일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>180cm</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>A형</p></td>
-</tr>
-<tr>
-<td><p>소속 유닛</p></td>
-<td><p>비투비-블루</p></td>
-</tr>
-<tr>
-<td><p>포지션</p></td>
-<td><p>서브보컬</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ENTP</p></td>
-</tr>
-<tr>
-<td><p>별명</p></td>
-<td><p>육잘또</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>성격</p></td>
-<td><p>착함</p></td>
-</tr>
