c4a7a0de6e984b132b88cdd649557f05a899b2f7
groomigroom <googjohn0143@gmail.com>
2026-07-22 09:38:28 +0900

    Update ResMemMinami.tsx

--- src/modules/RESCENE/members/ResMemMinami.tsx
+++ src/modules/RESCENE/members/ResMemMinami.tsx
@@ -28,41 +28,40 @@ export default function ResMemMinami() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2006년 11월 29일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>162cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>O형</p></td>
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>미남이</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>remini</p></td>
+                        <td><p>밍</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>상징 동물</p></td>
+                        <td><p>바다표범</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ENFP</p></td>
                     </tr>
                     <tr>
                         <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>??</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>정이 많음</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -72,42 +71,3 @@ export default function ResMemMinami() {
 
 };
 
-미나미
-
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>2006년 11월 29일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>162cm</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>O형</p></td>
-</tr>
-<tr>
-<td><p>별명</p></td>
-<td><p>미남이</p></td>
-</tr>
-<tr>
-<td><p>remini</p></td>
-<td><p>밍</p></td>
-</tr>
-<tr>
-<td><p>상징 동물</p></td>
-<td><p>바다표범</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ENFP</p></td>
-</tr>
-<tr>
-<td><p>특기</p></td>
-<td>
-<p>??</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>성격</p></td>
-<td><p>정이 많음</p></td>
-</tr>
