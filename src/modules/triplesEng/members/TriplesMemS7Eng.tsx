951e7b929a00872152171996c398fc73a08cb400
groomigroom <googjohn0143@gmail.com>
2026-07-22 20:17:43 +0900

    Update TriplesMemS7Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS7Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS7Eng.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../triples/members/TriplesMemS7.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Tri_mm_s7_img from '../../triples/main/img/tri_m_memS7.jpg';
 
@@ -65,6 +66,7 @@ export default function TriplesMemS7Eng() {
                     </tr>
                 </tbody>
             </table>
+            <FooterPartEng />
         </div>
     );
 
