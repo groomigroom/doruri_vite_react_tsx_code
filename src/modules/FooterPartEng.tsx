ecae67b447cfedab1326610228088359c741436e
groomigroom <googjohn0143@gmail.com>
2026-07-22 19:57:27 +0900

    Create FooterPartEng.tsx

---
+++ src/modules/FooterPartEng.tsx
@@ -0,0 +1,12 @@
+import './FooterPart.css';
+
+export default function FooterPartEng() {
+    return (
+        <div className="footerpart_full">
+            <p>part companies</p>
+            <p>c Doruri</p>
+        </div>
+    );
+};
+
+
