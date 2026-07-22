5f0092fef9068d122b61714daa4675ec00e4186a
groomigroom <googjohn0143@gmail.com>
2026-07-23 07:02:48 +0900

    Update TriplesMemS12.tsx

--- src/modules/triples/members/TriplesMemS12.tsx
+++ src/modules/triples/members/TriplesMemS12.tsx
@@ -67,8 +67,7 @@ export default function TriplesMemS12() {
                     </tr>
                 </tbody>
             </table>
-
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ ㈜모드하우스' />
         </div>
     );
 
