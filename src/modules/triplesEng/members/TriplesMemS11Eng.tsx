66393b70608fe709a8659041f44e43e7164a57c1
groomigroom <googjohn0143@gmail.com>
2026-07-22 20:26:27 +0900

    Update TriplesMemS11Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS11Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS11Eng.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../triples/members/TriplesMemS11.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Tri_mm_s11_img from '../../triples/main/img/tri_m_memS11.jpg';
 
@@ -65,6 +66,7 @@ export default function TriplesMemS11Eng() {
                     </tr>
                 </tbody>
             </table>
+            <FooterPartEng />
         </div>
     );
 
