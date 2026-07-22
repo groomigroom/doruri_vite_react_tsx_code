8016b1e2cc9ebf9bf07ee5c7e696626f89db3c63
groomigroom <googjohn0143@gmail.com>
2026-07-22 09:15:00 +0900

    ResMemMinami.tsx 업데이트

--- src/modules/RESCENE/members/ResMemMinami.tsx
+++ src/modules/RESCENE/members/ResMemMinami.tsx
@@ -72,3 +72,42 @@ export default function ResMemMinami() {
 
 };
 
+미나미
+
+<tr className='tri_mm_s1_intropa_tb_tr1'>
+<td><p>출생</p></td>
+<td><p>2006년 11월 29일</p></td>
+</tr>
+<tr>
+<td><p>키</p></td>
+<td><p>162cm</p></td>
+</tr>
+<tr>
+<td><p>혈액형</p></td>
+<td><p>O형</p></td>
+</tr>
+<tr>
+<td><p>별명</p></td>
+<td><p>미남이</p></td>
+</tr>
+<tr>
+<td><p>remini</p></td>
+<td><p>밍</p></td>
+</tr>
+<tr>
+<td><p>상징 동물</p></td>
+<td><p>바다표범</p></td>
+</tr>
+<tr>
+<td><p>MBTI</p></td>
+<td><p>ENFP</p></td>
+</tr>
+<tr>
+<td><p>특기</p></td>
+<td>
+<p>??</p></td>
+</tr>
+<tr className='tri_mm_s1_intropa_tb_tr9'>
+<td><p>성격</p></td>
+<td><p>정이 많음</p></td>
+</tr>
