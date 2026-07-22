1b8602e9de5d68c9a5f9096ef193cf7fe3063210
groomigroom <googjohn0143@gmail.com>
2026-07-22 12:34:14 +0900

    Update ArtMemHeejin.tsx

--- src/modules/ARTMS/members/ArtMemHeejin.tsx
+++ src/modules/ARTMS/members/ArtMemHeejin.tsx
@@ -29,41 +29,35 @@ export default function ArtMemHeejin() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2000년 10월 19일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>161.2cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>A형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>상징</p></td>
+                        <td><p>토끼</p></td>
                     </tr>
                     <tr>
                         <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>희끼</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>ENTJ</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>x</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>자존감 높음</p></td>
                     </tr>
                 </tbody>
             </table>
