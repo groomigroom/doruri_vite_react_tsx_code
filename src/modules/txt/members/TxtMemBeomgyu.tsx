43b98003c3c628dfaadef012db3b10085164adbf
groomigroom <googjohn0143@gmail.com>
2026-07-23 08:54:13 +0900

    Update TxtMemBeomgyu.tsx

--- src/modules/txt/members/TxtMemBeomgyu.tsx
+++ src/modules/txt/members/TxtMemBeomgyu.tsx
@@ -65,7 +65,7 @@ export default function TxtMemBeomgyu() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ 빅히트 뮤직' />
         </div>
     );
 }
