6e539af767c8e8dbbc06c700d2fd5fa66f0a8edf
groomigroom <googjohn0143@gmail.com>
2026-07-22 20:23:49 +0900

    Include FooterPartEng in TriplesMemS10Eng
    Added FooterPartEng component to the TriplesMemS10Eng component.

--- src/modules/triplesEng/members/TriplesMemS10Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS10Eng.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../triples/members/TriplesMemS10.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Tri_mm_s10_img from '../../triples/main/img/tri_m_memS10.jpg';
 
@@ -65,6 +66,7 @@ export default function TriplesMemS10Eng() {
                     </tr>
                 </tbody>
             </table>
+            <FooterPartEng />
         </div>
     );
 
