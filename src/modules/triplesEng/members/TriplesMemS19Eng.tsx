8ef5b33af7ec230ede61767baf403fd2b306a269
groomigroom <googjohn0143@gmail.com>
2026-07-22 20:41:40 +0900

    Update TriplesMemS19Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS19Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS19Eng.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../triples/members/TriplesMemS19.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Tri_mm_s19_img from '../../triples/main/img/tri_m_memS19.jpg';
 
@@ -65,6 +66,7 @@ export default function TriplesMemS19Eng() {
                     </tr>
                 </tbody>
             </table>
+            <FooterPartEng />
         </div>
     );
 };
