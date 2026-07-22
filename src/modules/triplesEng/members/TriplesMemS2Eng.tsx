a8113e5dbc758be65b2c5b505ab2fa231c39172d
groomigroom <googjohn0143@gmail.com>
2026-07-22 20:08:33 +0900

    Update TriplesMemS2Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS2Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS2Eng.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../triples/members/TriplesMemS2.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Tri_mm_s2_img from '../../triples/main/img/tri_m_memS2.jpg'
 
@@ -66,6 +67,7 @@ export default function TriplesMemS2Eng() {
                     </tr>
                 </tbody>
             </table>
+            <FooterPartEng />
         </div>
     );
 
