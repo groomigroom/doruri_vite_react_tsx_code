04c653afef56bd46fc77a0a4e56d1b366907ec06
groomigroom <googjohn0143@gmail.com>
2026-07-22 12:31:36 +0900

    Update BtoMemSeoEunkwang.tsx

--- src/modules/BTOB/members/BtoMemSeoEunkwang.tsx
+++ src/modules/BTOB/members/BtoMemSeoEunkwang.tsx
@@ -30,41 +30,35 @@ export default function BtoMemSeoEunkwang() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>1990년 11월 22일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>171cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>A형</p></td>
                     </tr>
                     <tr>
                         <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>비투비-블루, 비투비 포유, 구공탄</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>포지션</p></td>
+                        <td><p>리더, 메인보컬</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ESFJ</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>실버 라이트</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>밝음</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -73,39 +67,3 @@ export default function BtoMemSeoEunkwang() {
         </div>
     );
 };
-
-
-비투비 서은광
-
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>1990년 11월 22일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>171cm</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>A형</p></td>
-</tr>
-<tr>
-<td><p>소속 유닛</p></td>
-<td><p>비투비-블루, 비투비 포유, 구공탄</p></td>
-</tr>
-<tr>
-<td><p>포지션</p></td>
-<td><p>리더, 메인보컬</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ESFJ</p></td>
-</tr>
-<tr>
-<td><p>별명</p></td>
-<td><p>실버 라이트</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>성격</p></td>
-<td><p>밝음</p></td>
-</tr>
