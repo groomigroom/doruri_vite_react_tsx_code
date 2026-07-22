b280cfe028a6847be10e11f991a787dec01b811e
groomigroom <googjohn0143@gmail.com>
2026-07-22 20:42:45 +0900

    Update TriplesMemS21Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS21Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS21Eng.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../triples/members/TriplesMemS21.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Tri_mm_s21_img from '../../triples/main/img/tri_m_memS21.jpg';
 
@@ -65,6 +66,7 @@ export default function TriplesMemS21Eng() {
                     </tr>
                 </tbody>
             </table>
+            <FooterPartEng />
         </div>
     );
 };
