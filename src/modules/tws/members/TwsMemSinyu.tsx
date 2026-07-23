78e11557f95b09ca02eba6bd4b30deb8ea1b20df
groomigroom <googjohn0143@gmail.com>
2026-07-23 12:01:58 +0900

    Update TwsMemSinyu.tsx

--- src/modules/tws/members/TwsMemSinyu.tsx
+++ src/modules/tws/members/TwsMemSinyu.tsx
@@ -56,7 +56,7 @@ export default function TwsMemSinyu() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ 플레디스 엔터테인먼트' />
         </div>
     );
 };
