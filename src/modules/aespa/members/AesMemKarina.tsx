18bd9dd28e73124e7bbdd0d146412ba522a70713
groomigroom <googjohn0143@gmail.com>
2026-07-23 12:18:30 +0900

    Update AesMemKarina.tsx

--- src/modules/aespa/members/AesMemKarina.tsx
+++ src/modules/aespa/members/AesMemKarina.tsx
@@ -65,7 +65,7 @@ export default function AesMemKarina() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ SM엔터테인먼트' />
         </div>
     );
 
