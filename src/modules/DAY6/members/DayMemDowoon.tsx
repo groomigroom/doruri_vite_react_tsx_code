fc9230348b098efd19b8a5b0c6161e0eb112c1ea
groomigroom <googjohn0143@gmail.com>
2026-07-23 12:28:21 +0900

    Update DayMemDowoon.tsx

--- src/modules/DAY6/members/DayMemDowoon.tsx
+++ src/modules/DAY6/members/DayMemDowoon.tsx
@@ -59,7 +59,7 @@ export default function DayMemDowoon() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ JYP엔터테인먼트' />
         </div>
     );
 
