8c2ac225c432d82de0916ac32dbd3fc21bda8915
groomigroom <googjohn0143@gmail.com>
2026-07-23 15:31:17 +0900

    Update TriplesMemS8Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS8Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS8Eng.tsx
@@ -27,42 +27,44 @@ export default function TriplesMemS8Eng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>February 3, 2005</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>165cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>B</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Gongyubam</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>hobby</p></td>
+                        <td><p>Wool felt</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>speciality</p></td>
+                        <td><p>Cooking</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>favorite food</p></td>
+                        <td><p>Meat</p></td>
                     </tr>
                     <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>units</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>
+                                Acid Angel from Asia, Acid Eyes, <br />
+                                LOVElution, Visionary Vision, zenith
+                            </p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>cool-headedness</p></td>
                     </tr>
                 </tbody>
             </table>
