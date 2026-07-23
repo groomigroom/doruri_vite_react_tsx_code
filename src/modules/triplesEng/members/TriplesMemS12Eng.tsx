6e93f2986d39df00cebf5568b234ac02f38a81a2
groomigroom <googjohn0143@gmail.com>
2026-07-23 15:53:34 +0900

    Translate Korean text to English in TriplesMemS12Eng

--- src/modules/triplesEng/members/TriplesMemS12Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS12Eng.tsx
@@ -27,42 +27,44 @@ export default function TriplesMemS12Eng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>January 8, 2008</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>163cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>A</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Kwakkwakkwakyeonji</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>hobby</p></td>
+                        <td><p>Game</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>speciality</p></td>
+                        <td><p>Piano</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>favorite food</p></td>
+                        <td><p>Rolled Omelet</p></td>
                     </tr>
                     <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>units</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>
+                                EVOLution, Visionary Vision, <br />
+                                zenith
+                            </p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>bright</p></td>
                     </tr>
                 </tbody>
             </table>
