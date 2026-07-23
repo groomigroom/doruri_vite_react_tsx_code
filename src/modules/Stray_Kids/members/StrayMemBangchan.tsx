cc61eca581a0219f4cb291a33d44fc65d1afc3d7
groomigroom <googjohn0143@gmail.com>
2026-07-23 10:14:55 +0900

    Update StrayMemBangchan.tsx

--- src/modules/Stray_Kids/members/StrayMemBangchan.tsx
+++ src/modules/Stray_Kids/members/StrayMemBangchan.tsx
@@ -65,7 +65,7 @@ export default function StrayMemBangchan() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ JYP엔터테인먼트' />
         </div>
     );
 }
