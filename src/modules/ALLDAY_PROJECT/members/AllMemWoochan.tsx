fd43c778268188ad9bcf39064b28a3003ad4b7f7
groomigroom <googjohn0143@gmail.com>
2026-07-23 12:25:56 +0900

    Update AllMemWoochan.tsx

--- src/modules/ALLDAY_PROJECT/members/AllMemWoochan.tsx
+++ src/modules/ALLDAY_PROJECT/members/AllMemWoochan.tsx
@@ -56,7 +56,7 @@ export default function AllMemWoochan() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ THEBLACKLABEL' />
         </div>
     );
 }
