fe991a1e60fb4cd7e4083a97567f6c8007f91fec
groomigroom <googjohn0143@gmail.com>
2026-07-23 10:17:12 +0900

    Update StrayMemFilix.tsx

--- src/modules/Stray_Kids/members/StrayMemFilix.tsx
+++ src/modules/Stray_Kids/members/StrayMemFilix.tsx
@@ -65,7 +65,7 @@ export default function StrayMemFilix() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ JYP엔터테인먼트' />
         </div>
     );
 }
