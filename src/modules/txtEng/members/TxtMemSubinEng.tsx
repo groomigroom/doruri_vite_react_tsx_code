9e90b187f04f6d1771055f59f2db318d0fbe1b4a
groomigroom <googjohn0143@gmail.com>
2026-07-23 13:46:39 +0900

    Update TxtMemSubinEng.tsx

--- src/modules/txtEng/members/TxtMemSubinEng.tsx
+++ src/modules/txtEng/members/TxtMemSubinEng.tsx
@@ -4,6 +4,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../txt/members/TxtMemSubin.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Txt_mm_Subin_img from '../../txt/main/img/txt_m_mem_subin.jpg';
 
@@ -65,7 +66,7 @@ export default function TxtMemSubinEng() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPartEng />
         </div>
     );
 }
