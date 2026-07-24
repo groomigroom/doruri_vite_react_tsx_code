e8f016980125a3d5d338fc923776c31e13ca1d95
groomigroom <googjohn0143@gmail.com>
2026-07-24 16:14:01 +0900

    Update TxtMemBeomgyuEng.tsx

--- src/modules/txtEng/members/TxtMemBeomgyuEng.tsx
+++ src/modules/txtEng/members/TxtMemBeomgyuEng.tsx
@@ -27,42 +27,41 @@ export default function TxtMemBeomgyuEng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>March 13, 2001</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>179cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>AB</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Bamgyu</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>hobby</p></td>
+                        <td><p>Guitar</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>speciality</p></td>
+                        <td><p>Talking</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>position</p></td>
+                        <td><p>x</p></td>
                     </tr>
                     <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>MBTI</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>INFP</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>Bright</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -71,42 +70,3 @@ export default function TxtMemBeomgyuEng() {
     );
 }
 
-
-투바투 범규
-
-<td><p>birth</p></td>
-<td><p>March 13, 2001</p></td>
-</tr>
-<tr>
-<td><p>height</p></td>
-<td><p>179cm</p></td>
-</tr>
-<tr>
-<td><p>blood type</p></td>
-<td><p>AB</p></td>
-</tr>
-<tr>
-<td><p>nickname</p></td>
-<td><p>Bamgyu</p></td>
-</tr>
-<tr>
-<td><p>hobby</p></td>
-<td><p>Guitar</p></td>
-</tr>
-<tr>
-<td><p>speciality</p></td>
-<td><p>Talking</p></td>
-</tr>
-<tr>
-<td><p>position</p></td>
-<td><p>x</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td>
-<p>INFP</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>character</p></td>
-<td><p>Bright</p></td>
-</tr>
