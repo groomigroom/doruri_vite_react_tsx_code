045d64d7bdcfa4b05bfb3bbacefeba987d9b1dcd
groomigroom <googjohn0143@gmail.com>
2026-07-25 08:21:45 +0900

    Update AllMemAnnieEng.tsx

--- src/modules/ALLDAY_PROJECTEng/members/AllMemAnnieEng.tsx
+++ src/modules/ALLDAY_PROJECTEng/members/AllMemAnnieEng.tsx
@@ -68,3 +68,32 @@ export default function AllMemAnnieEng() {
         </div>
     );
 }
+
+ <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>2002년 1월 23일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>168cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>O형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>포지션</p></td>
+                        <td><p>래퍼</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ESTJ</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>애니언니</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>성실함</p></td>
+                    </tr>
