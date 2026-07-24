4d590a827db23167aeddf7819297052f68f78776
groomigroom <googjohn0143@gmail.com>
2026-07-25 08:16:32 +0900

    Update TenMemGwonjungyeolEng.tsx

--- src/modules/10CMEng/members/TenMemGwonjungyeolEng.tsx
+++ src/modules/10CMEng/members/TenMemGwonjungyeolEng.tsx
@@ -72,3 +72,40 @@ export default function TenMemGwonjungyeolEng() {
 
 
 
+ <tr className='tri_mm_s1_intropa_tb_tr1'>
+                        <td><p>출생</p></td>
+                        <td><p>1983년 3월 1일</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>키</p></td>
+                        <td><p>172cm</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>혈액형</p></td>
+                        <td><p>O형</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>별명</p></td>
+                        <td><p>요정열</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>포지션 1</p></td>
+                        <td><p>보컬</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>포지션 2</p></td>
+                        <td><p>기타</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>MBTI</p></td>
+                        <td><p>ISFP-T</p></td>
+                    </tr>
+                    <tr>
+                        <td><p>좋아하는 음식</p></td>
+                        <td>
+                            <p>라면</p></td>
+                    </tr>
+                    <tr className='tri_mm_s1_intropa_tb_tr9'>
+                        <td><p>성격</p></td>
+                        <td><p>??</p></td>
+                    </tr>
