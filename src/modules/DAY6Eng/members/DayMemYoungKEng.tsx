c53fc9bb267f8d8a585dc736440354a72dd76a0e
groomigroom <googjohn0143@gmail.com>
2026-07-26 08:59:49 +0900

    Update DayMemYoungKEng.tsx

--- src/modules/DAY6Eng/members/DayMemYoungKEng.tsx
+++ src/modules/DAY6Eng/members/DayMemYoungKEng.tsx
@@ -27,42 +27,41 @@ export default function DayMemYoungKEng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>December 19, 1993</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>180.2cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blooe type</p></td>
+                        <td><p>B</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>position</p></td>
+                        <td><p>bass, vocal, rap, electric guitar</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>animal character</p></td>
+                        <td><p>KE</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>symbolic animal</p></td>
+                        <td><p>fox</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Toranow</p></td>
                     </tr>
                     <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>units</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>DAY6 &#40;Even of Day&#41;</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>diligence</p></td>
                     </tr>
                 </tbody>
             </table>
