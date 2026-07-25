933e87aa4ccbc9fe1f45acdf486b0b2270558cd4
groomigroom <googjohn0143@gmail.com>
2026-07-25 20:11:21 +0900

    Update IllMemErohaEng.tsx

--- src/modules/illitEng/members/IllMemErohaEng.tsx
+++ src/modules/illitEng/members/IllMemErohaEng.tsx
@@ -26,55 +26,8 @@ export default function IllMemErohaEng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
-                    </tr>
-                    <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
-                    </tr>
-                </tbody>
-            </table>
-
-        </div>
-    );
-};
-
-
-
- <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>birth</p></td>
-                        <td><p>2008년 2월 4일</p></td>
+                        <td><p>Februry 4, 2008</p></td>
                     </tr>
                     <tr>
                         <td><p>height</p></td>
@@ -82,7 +35,7 @@ export default function IllMemErohaEng() {
                     </tr>
                     <tr>
                         <td><p>blood type</p></td>
-                        <td><p>A형</p></td>
+                        <td><p>A</p></td>
                     </tr>
                     <tr>
                         <td><p>animal character</p></td>
@@ -94,18 +47,25 @@ export default function IllMemErohaEng() {
                     </tr>
                     <tr>
                         <td><p>nickname</p></td>
-                        <td><p>로하</p></td>
+                        <td><p>Roha</p></td>
                     </tr>
                     <tr>
                         <td><p>hobby</p></td>
-                        <td><p>음악 듣기</p></td>
+                        <td><p>listening music</p></td>
                     </tr>
                     <tr>
                         <td><p>speciality</p></td>
                         <td>
-                            <p>다도</p></td>
+                            <p>tea ceremony</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>character</p></td>
-                        <td><p>감성적임</p></td>
+                        <td><p>emotional</p></td>
                     </tr>
+                </tbody>
+            </table>
+
+        </div>
+    );
+};
+
