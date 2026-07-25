4776d8fce6f0599ed1a589fdd2df38bca419e0ba
groomigroom <googjohn0143@gmail.com>
2026-07-25 18:37:14 +0900

    Update StrayMemHyunjinEng.tsx

--- src/modules/Stray_KidsEng/members/StrayMemHyunjinEng.tsx
+++ src/modules/Stray_KidsEng/members/StrayMemHyunjinEng.tsx
@@ -26,42 +26,41 @@ export default function StrayMemHyunjinEng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>March 20, 2000</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>B</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>units</p></td>
+                        <td><p>x</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>INFP</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Hyunee</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>SKZOO</p></td>
+                        <td><p>Jiniret</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>symbolic animal</p></td>
+                        <td><p>white weasel</p></td>
                     </tr>
                     <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>position</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>main dancer</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>kindness</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -70,43 +69,3 @@ export default function StrayMemHyunjinEng() {
     );
 }
 
-Jiniret
-
-현진잉그하기
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>birth</p></td>
-<td><p>March 20, 2000</p></td>
-</tr>
-<tr>
-<td><p>blood type</p></td>
-<td><p>B</p></td>
-</tr>
-<tr>
-<td><p>units</p></td>
-<td><p>x</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>INFP</p></td>
-</tr>
-<tr>
-<td><p>nickname</p></td>
-<td><p>Hyunee</p></td>
-</tr>
-<tr>
-<td><p>SKZOO</p></td>
-<td><p>Jiniret</p></td>
-</tr>
-<tr>
-<td><p>symbolic animal</p></td>
-<td><p>white weasel</p></td>
-</tr>
-<tr>
-<td><p>position</p></td>
-<td>
-<p>main dancer</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>character</p></td>
-<td><p>kindness</p></td>
-</tr>
