de9a0d5a688134cd70621b960a08a1d294e701a2
groomigroom <googjohn0143@gmail.com>
2026-07-25 11:08:51 +0900

    Update StrayMemFilixEng.tsx

--- src/modules/Stray_KidsEng/members/StrayMemFilixEng.tsx
+++ src/modules/Stray_KidsEng/members/StrayMemFilixEng.tsx
@@ -26,42 +26,41 @@ export default function StrayMemFilixEng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>September 15, 2000</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>AB</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>units</p></td>
+                        <td><p>x</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>ENFJ</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Yongbokee</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>SKZOO</p></td>
+                        <td><p>BbokAri</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>symbolic animal</p></td>
+                        <td><p>chick</p></td>
                     </tr>
                     <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>position</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>dancer??</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>positive</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -70,43 +69,3 @@ export default function StrayMemFilixEng() {
     );
 }
 
-BbokAri
-
-필릭스잉그하기
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>birth</p></td>
-<td><p>September 15, 2000</p></td>
-</tr>
-<tr>
-<td><p>blood type</p></td>
-<td><p>AB</p></td>
-</tr>
-<tr>
-<td><p>units</p></td>
-<td><p>x</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ENFJ</p></td>
-</tr>
-<tr>
-<td><p>nickname</p></td>
-<td><p>Yongbokee</p></td>
-</tr>
-<tr>
-<td><p>SKZOO</p></td>
-<td><p>BbokAri</p></td>
-</tr>
-<tr>
-<td><p>symbolic animal</p></td>
-<td><p>chick</p></td>
-</tr>
-<tr>
-<td><p>position</p></td>
-<td>
-<p>dancer??</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>character</p></td>
-<td><p>positive</p></td>
-</tr>
