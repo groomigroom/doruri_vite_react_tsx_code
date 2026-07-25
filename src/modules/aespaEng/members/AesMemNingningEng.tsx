903be80a642a1e62c362fea5cb43ec6ce0777e8d
groomigroom <googjohn0143@gmail.com>
2026-07-26 07:32:03 +0900

    Update AesMemNingningEng.tsx

--- src/modules/aespaEng/members/AesMemNingningEng.tsx
+++ src/modules/aespaEng/members/AesMemNingningEng.tsx
@@ -17,50 +17,50 @@ export default function AesMemNingningEng() {
                 <Link to='/AesMemNingning'>한국어</Link>
                 <Link to='/AesMemNingningEng' className="on">English</Link>
             </section>
-            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="aespa deco" />닝닝<img src={MainH2Deco} alt="aespa deco" /></h2>
-            <p className='tri_mm_s1_intropp'>24명은 너무 많지 않아요?<br />
-                그리고... 그리고... 그리고...<br />
-                더 열심히 더 열심히</p>
-            <img src={aes_mm_ningning_img} alt="닝닝 이미지" className='tri_mm_s1_img txt_mm_beomgyu_img' />
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="aespa deco" />Ningning<img src={MainH2Deco} alt="aespa deco" /></h2>
+            <p className='tri_mm_s1_intropp'>
+                I'll show you<br />
+                my personality<br />
+                honestly!</p>
+            <img src={aes_mm_ningning_img} alt="Ningning" className='tri_mm_s1_img txt_mm_beomgyu_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>October 23, 2002</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
+                        <td><p>height</p></td>
                         <td><p>161cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>O</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>position</p></td>
+                        <td><p>vocal</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>combat skill</p></td>
+                        <td><p>E.D Hacker</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>animal character</p></td>
+                        <td><p>Rue</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>INFP</p></td>
                     </tr>
                     <tr>
-                        <td><p>소속 유닛</p></td>
+                        <td><p>nickname</p></td>
                         <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                            <p>Ning</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>frankness</p></td>
                     </tr>
                 </tbody>
             </table>
