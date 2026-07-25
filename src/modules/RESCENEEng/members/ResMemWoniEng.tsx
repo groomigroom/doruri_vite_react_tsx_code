585244021f3d7407b35512306b597eb4078443e5
groomigroom <googjohn0143@gmail.com>
2026-07-26 07:15:13 +0900

    Update ResMemWoniEng.tsx

--- src/modules/RESCENEEng/members/ResMemWoniEng.tsx
+++ src/modules/RESCENEEng/members/ResMemWoniEng.tsx
@@ -26,51 +26,6 @@ export default function ResMemWoniEng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
-                    </tr>
-                    <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
-    );
-
-};
-
-<tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>birth</p></td>
                         <td><p>May 25, 2004</p></td>
                     </tr>
@@ -107,5 +62,10 @@ export default function ResMemWoniEng() {
                         <td><p>character</p></td>
                         <td><p>decency</p></td>
                     </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 
+};
 
