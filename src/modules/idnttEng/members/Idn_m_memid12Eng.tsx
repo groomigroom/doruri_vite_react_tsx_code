70f0563a284c692eac95093453380dee199b0c21
groomigroom <googjohn0143@gmail.com>
2026-07-26 13:06:38 +0900

    Update Idn_m_memid12Eng.tsx

--- src/modules/idnttEng/members/Idn_m_memid12Eng.tsx
+++ src/modules/idnttEng/members/Idn_m_memid12Eng.tsx
@@ -26,42 +26,36 @@ export default function Idn_m_memid12Eng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>December 4, 2008</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>182cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>B</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>units</p></td>
+                        <td><p>yesweare</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>ESTJ</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>symbolic color</p></td>
+                        <td><p>#fdd82e</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>character</p></td>
+                        <td><p>mildness</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>??</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -72,39 +66,7 @@ export default function Idn_m_memid12Eng() {
 };
 
 
-아이덴티티12멤잉그
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>birth</p></td>
-<td><p>December 4, 2008</p></td>
-</tr>
-<tr>
-<td><p>height</p></td>
-<td><p>182cm</p></td>
-</tr>
-<tr>
-<td><p>blood type</p></td>
-<td><p>B</p></td>
-</tr>
-<tr>
-<td><p>units</p></td>
-<td><p>yesweare</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ESTJ</p></td>
-</tr>
-<tr>
-<td><p>symbolic color</p></td>
-<td><p>#fdd82e</p></td>
-</tr>
-<tr>
-<td><p>character</p></td>
-<td><p>mildness</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>nickname</p></td>
-<td><p>??</p></td>
-</tr>
+
 
 
 
