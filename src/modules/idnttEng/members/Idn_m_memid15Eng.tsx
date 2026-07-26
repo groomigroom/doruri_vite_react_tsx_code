06bc8d326fa6ec7a2099fe3f412abcb44b5527a1
groomigroom <googjohn0143@gmail.com>
2026-07-26 15:16:51 +0900

    Update Idn_m_memid15Eng.tsx

--- src/modules/idnttEng/members/Idn_m_memid15Eng.tsx
+++ src/modules/idnttEng/members/Idn_m_memid15Eng.tsx
@@ -18,50 +18,44 @@ export default function Idn_m_memid15Eng() {
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="idntt deco" />choigyeongbeen<img src={MainH2Deco} alt="idntt deco" /></h2>
             <p className='tri_mm_s1_intropp'>
-                #####24명은 많은 것 같아도<br />
-                멤버들 질서 유지를<br />
-                책임감 있게 다 해볼게요!
+                I'll take good<br />
+                care of the members<br />
+                and do my best!
             </p>
             <img src='' alt="choigyeongbeen" className='tri_mm_s1_img' />
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>September 4, 2008</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>180cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>A</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>units</p></td>
+                        <td><p>yesweare</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>ISFP</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>symbolic color</p></td>
+                        <td><p>#4edd9c</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>character</p></td>
+                        <td><p>meticulousness</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>??</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -72,39 +66,7 @@ export default function Idn_m_memid15Eng() {
 };
 
 
-아이덴티티 15멤잉그
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>birth</p></td>
-<td><p>September 4, 2008</p></td>
-</tr>
-<tr>
-<td><p>height</p></td>
-<td><p>180cm</p></td>
-</tr>
-<tr>
-<td><p>blood type</p></td>
-<td><p>A</p></td>
-</tr>
-<tr>
-<td><p>units</p></td>
-<td><p>yesweare</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ISFP</p></td>
-</tr>
-<tr>
-<td><p>symbolic color</p></td>
-<td><p>#4edd9c</p></td>
-</tr>
-<tr>
-<td><p>character</p></td>
-<td><p>meticulousness</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>nickname</p></td>
-<td><p>??</p></td>
-</tr>
+
 
 
 
