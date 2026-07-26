e41adfba67dc9662b3c7fd0bc9eddcfe9583075e
groomigroom <googjohn0143@gmail.com>
2026-07-26 10:14:01 +0900

    Idn_m_memid12Eng.tsx 업데이트

--- src/modules/idnttEng/members/Idn_m_memid12Eng.tsx
+++ src/modules/idnttEng/members/Idn_m_memid12Eng.tsx
@@ -72,7 +72,39 @@ export default function Idn_m_memid12Eng() {
 };
 
 
-
+아이덴티티12멤잉그
+<tr className='tri_mm_s1_intropa_tb_tr1'>
+<td><p>birth</p></td>
+<td><p>December 4, 2008</p></td>
+</tr>
+<tr>
+<td><p>height</p></td>
+<td><p>182cm</p></td>
+</tr>
+<tr>
+<td><p>blood type</p></td>
+<td><p>B</p></td>
+</tr>
+<tr>
+<td><p>units</p></td>
+<td><p>yesweare</p></td>
+</tr>
+<tr>
+<td><p>MBTI</p></td>
+<td><p>ESTJ</p></td>
+</tr>
+<tr>
+<td><p>symbolic color</p></td>
+<td><p>#fdd82e</p></td>
+</tr>
+<tr>
+<td><p>character</p></td>
+<td><p>mildness</p></td>
+</tr>
+<tr className='tri_mm_s1_intropa_tb_tr9'>
+<td><p>nickname</p></td>
+<td><p>??</p></td>
+</tr>
 
 
 
