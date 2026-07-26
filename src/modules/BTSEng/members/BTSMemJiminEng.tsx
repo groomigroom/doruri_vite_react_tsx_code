389e77ef7002635caa5290f99f647784c9fb3dec
groomigroom <googjohn0143@gmail.com>
2026-07-27 06:49:27 +0900

    Update BTSMemJiminEng.tsx

--- src/modules/BTSEng/members/BTSMemJiminEng.tsx
+++ src/modules/BTSEng/members/BTSMemJiminEng.tsx
@@ -24,46 +24,35 @@ export default function BTSMemJiminEng() {
                 suits me with &apos;Jimin&apos;!
             </p>
             <img src='' alt="Jimin" className='tri_mm_s1_img' />
-            #####
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>October 13, 1995</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>174cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>A</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>position</p></td>
+                        <td><p>main dancer, lead vocal</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>ESTJ</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Pparkjimini</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>??</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -72,33 +61,3 @@ export default function BTSMemJiminEng() {
         </div>
     );
 };
-
-지민잉그맵
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>birth</p></td>
-<td><p>October 13, 1995</p></td>
-</tr>
-<tr>
-<td><p>height</p></td>
-<td><p>174cm</p></td>
-</tr>
-<tr>
-<td><p>blood type</p></td>
-<td><p>A</p></td>
-</tr>
-<tr>
-<td><p>position</p></td>
-<td><p>main dancer, lead vocal</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ESTJ</p></td>
-</tr>
-<tr>
-<td><p>nickname</p></td>
-<td><p>Pparkjimini</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>character</p></td>
-<td><p>??</p></td>
-</tr>
