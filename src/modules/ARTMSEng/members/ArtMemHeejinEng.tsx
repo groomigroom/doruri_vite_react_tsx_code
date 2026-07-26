834fad553bb7ae9744bb1cbe80769c2fcd90830a
groomigroom <googjohn0143@gmail.com>
2026-07-26 15:38:36 +0900

    Update ArtMemHeejinEng.tsx

--- src/modules/ARTMSEng/members/ArtMemHeejinEng.tsx
+++ src/modules/ARTMSEng/members/ArtMemHeejinEng.tsx
@@ -27,42 +27,36 @@ export default function ArtMemHeejinEng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>October 19, 2000</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>161.2cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>A</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>symbol</p></td>
+                        <td><p>rabbit</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Heekki</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>ENTJ</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>units</p></td>
+                        <td><p>x</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>high self-esteem</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -72,38 +66,5 @@ export default function ArtMemHeejinEng() {
     );
 };
 
-아르테멤희진잉그
 
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>birth</p></td>
-<td><p>October 19, 2000</p></td>
-</tr>
-<tr>
-<td><p>height</p></td>
-<td><p>161.2cm</p></td>
-</tr>
-<tr>
-<td><p>blood type</p></td>
-<td><p>A</p></td>
-</tr>
-<tr>
-<td><p>symbol</p></td>
-<td><p>rabbit</p></td>
-</tr>
-<tr>
-<td><p>nickname</p></td>
-<td><p>Heekki</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>ENTJ</p></td>
-</tr>
-<tr>
-<td><p>units</p></td>
-<td><p>x</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>character</p></td>
-<td><p>high self-esteem</p></td>
-</tr>
 
