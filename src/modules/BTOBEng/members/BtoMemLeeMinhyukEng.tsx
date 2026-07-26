686e5d955435de85582aaaced60f2d1da9fce864
groomigroom <googjohn0143@gmail.com>
2026-07-26 16:38:51 +0900

    Update BtoMemLeeMinhyukEng.tsx

--- src/modules/BTOBEng/members/BtoMemLeeMinhyukEng.tsx
+++ src/modules/BTOBEng/members/BtoMemLeeMinhyukEng.tsx
@@ -28,42 +28,36 @@ export default function BtoMemLeeMinhyukEng() {
             <table className='tri_mm_s1_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
-                        <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>birth</p></td>
+                        <td><p>November 29, 1990</p></td>
                     </tr>
                     <tr>
-                        <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>height</p></td>
+                        <td><p>171cm</p></td>
                     </tr>
                     <tr>
-                        <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>blood type</p></td>
+                        <td><p>A</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>unit</p></td>
+                        <td><p>BTOB 4U, 90TAN</p></td>
                     </tr>
                     <tr>
-                        <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>position</p></td>
+                        <td><p>main rapper</p></td>
                     </tr>
                     <tr>
-                        <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>ISTJ</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>nickname</p></td>
+                        <td><p>Heotaramzzi</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
-                        <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>character</p></td>
+                        <td><p>meticulousness</p></td>
                     </tr>
                 </tbody>
             </table>
