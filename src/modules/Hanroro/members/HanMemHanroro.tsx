8f0153473a4e19ade0b641deaf8bd512777b46cc
groomigroom <googjohn0143@gmail.com>
2026-07-26 20:52:37 +0900

    Update HanMemHanroro.tsx

--- src/modules/Hanroro/members/HanMemHanroro.tsx
+++ src/modules/Hanroro/members/HanMemHanroro.tsx
@@ -20,10 +20,9 @@ export default function HanMemHanroro() {
             </section>
             <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="한로로 꾸미기" />한로로<img src={MainH2Deco} alt="한로로 꾸미기" /></h2>
             <p className='tri_mm_s1_intropp'>
-                #####
-                24명은 많은 것 같아도<br />
-                멤버들 질서 유지를<br />
-                책임감 있게 다 해볼게요!
+                여러분에게<br />
+                좋은 영향을 줄 수<br />
+                있도록 노력해 볼게요!
             </p>
             <img src='' alt="한로로 이미지" className='tri_mm_s1_img' />
             #####
@@ -31,41 +30,35 @@ export default function HanMemHanroro() {
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
-                        <td><p>2003년 8월 6일</p></td>
+                        <td><p>2000년 11월 11일</p></td>
                     </tr>
                     <tr>
                         <td><p>키</p></td>
-                        <td><p>161cm</p></td>
+                        <td><p>158cm</p></td>
                     </tr>
                     <tr>
                         <td><p>혈액형</p></td>
-                        <td><p>B형</p></td>
+                        <td><p>A형</p></td>
                     </tr>
                     <tr>
-                        <td><p>별명</p></td>
-                        <td><p>햄햄</p></td>
+                        <td><p>MBTI</p></td>
+                        <td><p>INFJ</p></td>
                     </tr>
                     <tr>
                         <td><p>취미</p></td>
-                        <td><p>좋아하는 영화 여러번 보기</p></td>
+                        <td><p>글쓰기</p></td>
                     </tr>
                     <tr>
                         <td><p>특기</p></td>
-                        <td><p>셀카찍기</p></td>
+                        <td><p>잘 웃기</p></td>
                     </tr>
                     <tr>
-                        <td><p>좋아하는 음식</p></td>
-                        <td><p>아이스크림</p></td>
-                    </tr>
-                    <tr>
-                        <td><p>소속 유닛</p></td>
-                        <td>
-                            <p>+&#40;KR&#41;ystal Eyes, Acid Eyes, <br />
-                                LOVElution, neptune</p></td>
+                        <td><p>별명</p></td>
+                        <td><p>~~로로</p></td>
                     </tr>
                     <tr className='tri_mm_s1_intropa_tb_tr9'>
                         <td><p>성격</p></td>
-                        <td><p>좋음</p></td>
+                        <td><p>밝음</p></td>
                     </tr>
                 </tbody>
             </table>
@@ -74,78 +67,3 @@ export default function HanMemHanroro() {
         </div>
     );
 };
-
-
-여러분에게
-좋은 영향을 줄 수
-있도록 노력해 볼게요!
-
-한로로eng 냐용도 있음 
-
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>출생</p></td>
-<td><p>2000년 11월 11일</p></td>
-</tr>
-<tr>
-<td><p>키</p></td>
-<td><p>158cm</p></td>
-</tr>
-<tr>
-<td><p>혈액형</p></td>
-<td><p>A형</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>INFJ</p></td>
-</tr>
-<tr>
-<td><p>취미</p></td>
-<td><p>글쓰기</p></td>
-</tr>
-<tr>
-<td><p>특기</p></td>
-<td><p>잘 웃기</p></td>
-</tr>
-<tr>
-<td><p>별명</p></td>
-<td><p>~~로로</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>성격</p></td>
-<td><p>밝음</p></td>
-</tr>
-
-한로로eng
-
-<tr className='tri_mm_s1_intropa_tb_tr1'>
-<td><p>birth</p></td>
-<td><p>Novemmer 11, 2000</p></td>
-</tr>
-<tr>
-<td><p>height</p></td>
-<td><p>158cm</p></td>
-</tr>
-<tr>
-<td><p>blood type</p></td>
-<td><p>A</p></td>
-</tr>
-<tr>
-<td><p>MBTI</p></td>
-<td><p>INFJ</p></td>
-</tr>
-<tr>
-<td><p>hobby</p></td>
-<td><p>writing</p></td>
-</tr>
-<tr>
-<td><p>speciality</p></td>
-<td><p>smile well</p></td>
-</tr>
-<tr>
-<td><p>nickname</p></td>
-<td><p>~~roro</p></td>
-</tr>
-<tr className='tri_mm_s1_intropa_tb_tr9'>
-<td><p>character</p></td>
-<td><p>bright</p></td>
-</tr>
