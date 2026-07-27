32a2a6d992d93a789811d4be78323e26391d61b1
groomigroom <googjohn0143@gmail.com>
2026-07-27 09:17:54 +0900

    Update HomeEng.tsx

--- src/modules/HomeEng/HomeEng.tsx
+++ src/modules/HomeEng/HomeEng.tsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import { EngPage } from "./EngPage";
 
 import "../home.css";
 
 import MenuEng from "./MenuEng";
 
+
 // import Menu from './Menu';
 
 export default function HomeEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="home_full">
             <MenuEng />
