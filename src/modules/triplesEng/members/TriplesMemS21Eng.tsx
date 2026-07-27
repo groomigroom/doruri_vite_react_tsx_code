c9ed94f8bee30168d081c73fe8ad10cc594ebecd
groomigroom <googjohn0143@gmail.com>
2026-07-27 09:58:16 +0900

    Update TriplesMemS21Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS21Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS21Eng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../triples/members/TriplesMemS21.css';
@@ -11,6 +12,8 @@ import Tri_mm_s21_img from '../../triples/main/img/tri_m_memS21.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS21Eng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="tri_mm_s21_full">
             <MenuEng />
