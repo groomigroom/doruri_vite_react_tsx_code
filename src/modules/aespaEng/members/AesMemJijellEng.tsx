7a0715f4078528779b5dd73b5f78b3cb686063ac
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:31:16 +0900

    Update AesMemJijellEng.tsx

--- src/modules/aespaEng/members/AesMemJijellEng.tsx
+++ src/modules/aespaEng/members/AesMemJijellEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../aespa/members/AesMemJijell.css';
@@ -10,6 +11,8 @@ import aes_mm_jijell_img from '../../aespa/main/img/aespa_m_mem_Jijel.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function AesMemJijellEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="aes_mm_jijell_full">
             <MenuEng />
