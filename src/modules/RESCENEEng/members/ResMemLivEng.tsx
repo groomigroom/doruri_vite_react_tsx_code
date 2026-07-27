9ace6db887fb5282e257a60eea686fea185b15a9
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:26:26 +0900

    Update ResMemLivEng.tsx

--- src/modules/RESCENEEng/members/ResMemLivEng.tsx
+++ src/modules/RESCENEEng/members/ResMemLivEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../RESCENE/members/ResMemLiv.css';
@@ -10,6 +11,8 @@ import Res_mm_Liv_img from '../../RESCENE/main/img/rescene_m_mem_Liv.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function ResMemLivEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="res_mm_liv_full">
             <MenuEng />
