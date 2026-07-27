3f43888b95e5e05c2245cbebc72b1bf960dd1816
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:16:17 +0900

    Update StrayMemBangchanEng.tsx

--- src/modules/Stray_KidsEng/members/StrayMemBangchanEng.tsx
+++ src/modules/Stray_KidsEng/members/StrayMemBangchanEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../Stray_Kids/members/StrayMemBangchan.css';
@@ -10,6 +11,8 @@ import Str_mm_bangchan_img from '../../Stray_Kids/main/img/stray_m_mem_Bangchan.
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function StrayMemBangchanEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="str_mm_bangchan_full">
             <MenuEng />
