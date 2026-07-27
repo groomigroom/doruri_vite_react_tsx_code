0dc6387b7ba6bce3cc3d27e19338fb1bb4cccf3a
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:43:06 +0900

    Update BolMemAnjiyoungEng.tsx

--- src/modules/bol4Eng/members/BolMemAnjiyoungEng.tsx
+++ src/modules/bol4Eng/members/BolMemAnjiyoungEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../bol4/members/BolMemAnjiyoung.css';
@@ -10,6 +11,8 @@ import MenuEng from '../../HomeEng/MenuEng';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function BolMemAnjiyoungEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="bol_mm_anjiyoung_full">
             <MenuEng />
