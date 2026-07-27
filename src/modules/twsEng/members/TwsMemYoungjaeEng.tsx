7c6b13bc8b7c8c85de8ff04b13a4cea2d89acd02
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:24:21 +0900

    Update TwsMemYoungjaeEng.tsx

--- src/modules/twsEng/members/TwsMemYoungjaeEng.tsx
+++ src/modules/twsEng/members/TwsMemYoungjaeEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../tws/members/TwsMemYoungjae.css';
@@ -10,6 +11,8 @@ import Tws_mm_youngjae_img from '../../tws/main/img/tws_m_mem_Jihun.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TwsMemYoungjaeEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="tws_mm_youngjae_full">
             <MenuEng />
