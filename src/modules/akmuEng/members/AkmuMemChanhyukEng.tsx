816b6a4d346b74104037615c82cfabb7a2ca5641
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:08:16 +0900

    Update AkmuMemChanhyukEng.tsx

--- src/modules/akmuEng/members/AkmuMemChanhyukEng.tsx
+++ src/modules/akmuEng/members/AkmuMemChanhyukEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../akmu/members/AkmuMemChanhyuk.css';
@@ -10,6 +11,8 @@ import akmu_mm_chanhyuk_img from '../../akmu/main/img/aknu_m_chanhyuk.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function AkmuMemChanhyukEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="akmu_mm_chanhyuk_full">
             <MenuEng />
