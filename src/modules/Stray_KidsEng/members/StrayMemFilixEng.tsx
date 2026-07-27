caa6a3df2d85e0e41adf8f727730b7e457c17746
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:16:45 +0900

    Update StrayMemFilixEng.tsx

--- src/modules/Stray_KidsEng/members/StrayMemFilixEng.tsx
+++ src/modules/Stray_KidsEng/members/StrayMemFilixEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../Stray_Kids/members/StrayMemFilix.css';
@@ -10,6 +11,8 @@ import Str_mm_filix_img from '../../Stray_Kids/main/img/stray_m_mem_Filix.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function StrayMemFilixEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="str_mm_filix_full">
             <MenuEng />
