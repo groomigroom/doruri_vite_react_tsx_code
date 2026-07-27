31705b748a4eb44dbfc354608695578e437a80c4
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:13:49 +0900

    Update IoiMemKimsohyeEng.tsx

--- src/modules/ioiEng/members/IoiMemKimsohyeEng.tsx
+++ src/modules/ioiEng/members/IoiMemKimsohyeEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../ioi/members/IoiMemKimsohye.css';
@@ -10,6 +11,8 @@ import Ioi_m_MemKimsohye_img from '../../ioi/main/img/ioi_m_mem_Kimsohye.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function IoiMemKimsohyeEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="ioi_mm_kimsohye_full">
             <MenuEng />
