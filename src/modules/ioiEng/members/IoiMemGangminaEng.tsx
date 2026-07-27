b92cee403091e30d41b7babaa9fc3bbfe59cd404
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:10:59 +0900

    Update IoiMemGangminaEng.tsx

--- src/modules/ioiEng/members/IoiMemGangminaEng.tsx
+++ src/modules/ioiEng/members/IoiMemGangminaEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../ioi/members/IoiMemGangmina.css';
@@ -10,6 +11,8 @@ import Ioi_m_MemGangmina_img from '../../ioi/main/img/ioi_m_mem_Gangmina.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function IoiMemGangminaEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="ioi_mm_gangmina_full">
             <MenuEng />
