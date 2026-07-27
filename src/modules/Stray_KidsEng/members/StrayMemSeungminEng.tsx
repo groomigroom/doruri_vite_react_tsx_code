04f9039f4062e8eadd6fac54602e4354c251fcd7
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:18:32 +0900

    Update StrayMemSeungminEng.tsx

--- src/modules/Stray_KidsEng/members/StrayMemSeungminEng.tsx
+++ src/modules/Stray_KidsEng/members/StrayMemSeungminEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../Stray_Kids/members/StrayMemSeungmin.css';
@@ -11,6 +12,8 @@ import MainH2Deco from '../../img/main_h2_deco.png';
 
 
 export default function StrayMemSeungminEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="str_mm_seungmin_full">
             <MenuEng />
