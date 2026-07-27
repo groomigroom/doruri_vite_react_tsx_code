bcf0571bbc42320d3a7534e9357dee3f1e472797
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:23:19 +0900

    Update TwsMemHanjinEng.tsx

--- src/modules/twsEng/members/TwsMemHanjinEng.tsx
+++ src/modules/twsEng/members/TwsMemHanjinEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../tws/members/TwsMemHanjin.css';
@@ -10,6 +11,8 @@ import Tws_m_MemHanjin_img from '../../tws/main/img/tws_m_mem_Hanjin.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TwsMemHanjinEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="tws_mm_hanjin_full">
             <MenuEng />
