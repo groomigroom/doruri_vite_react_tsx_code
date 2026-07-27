aabea1d91f1336a6d474db903fa7a5868e1f9dff
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:32:10 +0900

    Update AesMemWinterEng.tsx

--- src/modules/aespaEng/members/AesMemWinterEng.tsx
+++ src/modules/aespaEng/members/AesMemWinterEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../aespa/members/AesMemWinter.css';
@@ -10,6 +11,8 @@ import aes_mm_winter_img from '../../aespa/main/img/aespa_m_mem_Winter.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function AesMemWinterEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="aes_mm_winter_full">
             <MenuEng />
