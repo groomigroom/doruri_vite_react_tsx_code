8d6e4841ffdd55a02ceb251e2b8471cfa0fc84b2
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:34:33 +0900

    Update AllMemBaileyEng.tsx

--- src/modules/ALLDAY_PROJECTEng/members/AllMemBaileyEng.tsx
+++ src/modules/ALLDAY_PROJECTEng/members/AllMemBaileyEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../ALLDAY_PROJECT/members/AllMemBailey.css';
@@ -10,6 +11,8 @@ import all_mm_bailey_img from '../../ALLDAY_PROJECT/main/img/allday_m_mem_Bailey
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function AllMemBaileyEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="all_mm_bailey_full">
             <MenuEng />
