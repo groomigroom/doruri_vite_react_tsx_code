3a80a547bc5580772cc03877cc59a948aa38387a
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:11:51 +0900

    Update IoiMemJugyulgyoungEng.tsx

--- src/modules/ioiEng/members/IoiMemJugyulgyoungEng.tsx
+++ src/modules/ioiEng/members/IoiMemJugyulgyoungEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../ioi/members/IoiMemJugyulgyoung.css';
@@ -10,6 +11,8 @@ import Ioi_m_MemJugyulgyoung_img from '../../ioi/main/img/ioi_m_mem_Jugyulgyung.
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function IoiMemJugyulgyoungEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="ioi_mm_jugyulgyoung_full">
             <MenuEng />
