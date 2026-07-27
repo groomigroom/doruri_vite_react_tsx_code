4f49e4106af0fa317d13d6b9a5e16a9734e4c8b9
groomigroom <googjohn0143@gmail.com>
2026-07-27 10:11:32 +0900

    Update IoiMemJeonsomiEng.tsx

--- src/modules/ioiEng/members/IoiMemJeonsomiEng.tsx
+++ src/modules/ioiEng/members/IoiMemJeonsomiEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../ioi/members/IoiMemJeonsomi.css';
@@ -10,6 +11,8 @@ import Ioi_m_MemJeonsomi_img from '../../ioi/main/img/ioi_m_mem_Jeonsomi.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function IoiMemJeonsomiEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="ioi_mm_jeonsomi_full">
             <MenuEng />
