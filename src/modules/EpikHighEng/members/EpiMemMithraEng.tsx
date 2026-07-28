8991049069cb7ddaa340eb63589846e311e1c08e
groomigroom <googjohn0143@gmail.com>
2026-07-29 07:31:20 +0900

    Update EpiMemMithraEng.tsx

--- src/modules/EpikHighEng/members/EpiMemMithraEng.tsx
+++ src/modules/EpikHighEng/members/EpiMemMithraEng.tsx
@@ -5,6 +5,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../EpikHigh/members/EpiMemMithra.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 // import Txt_mm_Beomgyu_img from '../main/img/txt_m_mem_beomgyu.jpg';
 
@@ -50,8 +51,7 @@ export default function EpiMemMithraEng() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPartEng FooterPartEngText='ⓒ Ours' />
         </div>
     );
 };
-
