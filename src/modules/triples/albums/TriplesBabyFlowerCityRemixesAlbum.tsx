21a5ae906b3d5f136b28721bfd8539f9c4ace54b
groomigroom <googjohn0143@gmail.com>
2026-07-28 12:56:11 +0900

    Update TriplesBabyFlowerCityRemixesAlbum.tsx

--- src/modules/triples/albums/TriplesBabyFlowerCityRemixesAlbum.tsx
+++ src/modules/triples/albums/TriplesBabyFlowerCityRemixesAlbum.tsx
@@ -1,3 +1,5 @@
+import './TriplesBabyFlowerCityRemixesAlbum.css';
+
 export default function TriplesBabyFlowerCityRemixesAlbum() {
     return (
         <div className="tri_BabyFlowerCityRemixesAlbum_full"></div>
