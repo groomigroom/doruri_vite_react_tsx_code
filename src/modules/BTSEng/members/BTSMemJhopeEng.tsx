2b5dded51ebf14926bc1e580a7a0769e9e68cee9
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:27:09 +0900

    Update BTSMemJhopeEng.tsx

--- src/modules/BTSEng/members/BTSMemJhopeEng.tsx
+++ src/modules/BTSEng/members/BTSMemJhopeEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../BTS/members/BTSMemJhope.css';
@@ -10,6 +11,8 @@ import MenuEng from '../../HomeEng/MenuEng';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function BTSMemJhopeEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="bts_mm_jhope_full">
             <MenuEng />
