b33bf165540e52b5e9161d26f3b5258f3838d010
groomigroom <googjohn0143@gmail.com>
2026-07-29 07:40:45 +0900

    Update Idn_m_memid2Eng.tsx

--- src/modules/idnttEng/members/Idn_m_memid2Eng.tsx
+++ src/modules/idnttEng/members/Idn_m_memid2Eng.tsx
@@ -4,6 +4,7 @@ import { EngPage } from '../../HomeEng/EngPage';
 import '../../idntt/members/Idn_m_memid2.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 // import Idn_mm_memid2_img from '../main/img/tri_m_memS1.jpg';
 
@@ -63,12 +64,7 @@ export default function Idn_m_memid2Eng() {
                 </tbody>
             </table>
 
-
+            <FooterPartEng FooterPartEngText='ⓒ Modhaus' />
         </div>
     );
 };
-
-
-
-
-
