387c6e4a08e18fcb9ce8a09b00cea46be94a1eb5
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:35:21 +0900

    Update HanroroMain.tsx

--- src/modules/Hanroro/main/HanroroMain.tsx
+++ src/modules/Hanroro/main/HanroroMain.tsx
@@ -54,7 +54,7 @@ export default function HanroroMain() {
                 </section>
             </section>
 
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ authentic' />
         </div>
     );
 };
