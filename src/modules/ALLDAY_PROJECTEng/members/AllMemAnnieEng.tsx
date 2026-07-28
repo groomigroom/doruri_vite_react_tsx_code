22410c5af104b1636af22eb288b556a384c4c3d0
groomigroom <googjohn0143@gmail.com>
2026-07-28 20:44:54 +0900

    Update AllMemAnnieEng.tsx

--- src/modules/ALLDAY_PROJECTEng/members/AllMemAnnieEng.tsx
+++ src/modules/ALLDAY_PROJECTEng/members/AllMemAnnieEng.tsx
@@ -5,6 +5,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../ALLDAY_PROJECT/members/AllMemAnnie.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import all_mm_annie_img from '../../ALLDAY_PROJECT/main/img/allday_m_mem_Annie.jpg';
 
@@ -58,7 +59,7 @@ export default function AllMemAnnieEng() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPartEng FooterPartEngText='ⓒ THEBLACKLABEL' />
         </div>
     );
 }
