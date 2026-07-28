27e655895a8f80170698b6ce696d834694319ad1
groomigroom <googjohn0143@gmail.com>
2026-07-29 08:48:30 +0900

    Update Bol4FindYouAlbum.tsx

--- src/modules/bol4/albums/Bol4FindYouAlbum.tsx
+++ src/modules/bol4/albums/Bol4FindYouAlbum.tsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import './Bol4FindYouAlbum.css';
+
+import Menu from '../../Menu';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
 export default function Bol4FindYouAlbum() {
     return (
         <div className='bol_Bol4FindYouAlbum_full'>
-
+            <Menu />
+            <section className="home_translate">
+                <Link to='/Bol4FindYouAlbum' className="on">한국어</Link>
+                <Link to='/Bol4FindYouAlbumEng'>English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'><img src={MainH2deco} alt="볼빨간 사춘기 앨범 꾸미기" />Find You<img src={MainH2deco} alt="볼빨간 사춘기 앨범 꾸미기" /></h2>
         </div>
     );
 }
