1fffc82e21d3a26f4a8db5cb55d9e8e6a8deb524
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:54:12 +0900

    Update EpikHighMain.tsx

--- src/modules/EpikHigh/main/EpikHighMain.tsx
+++ src/modules/EpikHigh/main/EpikHighMain.tsx
@@ -77,9 +77,7 @@ export default function EpikHighMain() {
                 </section>
 
             </section>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ 아워즈' />
         </div >
     );
 };
-
-
