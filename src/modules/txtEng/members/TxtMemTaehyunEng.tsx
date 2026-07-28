751f411870d771a2edfeba46268d1da52ea4d0a5
groomigroom <googjohn0143@gmail.com>
2026-07-28 18:13:19 +0900

    Update TxtMemTaehyunEng.tsx

--- src/modules/txtEng/members/TxtMemTaehyunEng.tsx
+++ src/modules/txtEng/members/TxtMemTaehyunEng.tsx
@@ -68,7 +68,7 @@ export default function TxtMemTaehyunEng() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPartEng />
+            <FooterPartEng FooterPartEngText='ⓒ BIGHIT MUSIC' />
         </div>
     );
 }
