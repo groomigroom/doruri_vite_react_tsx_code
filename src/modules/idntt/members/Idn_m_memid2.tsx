99ae8f181efda20ed833058ae477269acf1c8289
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:42:59 +0900

    Update Idn_m_memid2.tsx

--- src/modules/idntt/members/Idn_m_memid2.tsx
+++ src/modules/idntt/members/Idn_m_memid2.tsx
@@ -60,7 +60,7 @@ export default function Idn_m_memid2() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ ㈜모드하우스' />
 
         </div>
     );
