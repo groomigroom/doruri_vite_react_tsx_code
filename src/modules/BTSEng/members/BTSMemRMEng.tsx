f820c21770d1b7462c4a2ce6556b41ff09384d5c
groomigroom <googjohn0143@gmail.com>
2026-07-29 08:39:49 +0900

    Update BTSMemRMEng.tsx

--- src/modules/BTSEng/members/BTSMemRMEng.tsx
+++ src/modules/BTSEng/members/BTSMemRMEng.tsx
@@ -5,6 +5,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../BTS/members/BTSMemRM.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 //import Tri_mm_s1_img from '../main/img/tri_m_memS1.jpg';
 
@@ -61,7 +62,7 @@ export default function BTSMemRMEng() {
                 </tbody>
             </table>
 
-
+            <FooterPartEng FooterPartEngText='ⓒ BIGHIT MUSIC' />
         </div>
     );
 };
