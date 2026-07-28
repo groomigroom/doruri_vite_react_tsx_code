270dab4458b86b6619c7d268e38b250bcecf6cc9
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:58:34 +0900

    Remove extraneous text from FooterPart
    Removed unnecessary text from FooterPart component.

--- src/modules/FooterPart.tsx
+++ src/modules/FooterPart.tsx
@@ -8,7 +8,6 @@ export default function FooterPart({ FooterPartText }: FooterPartProps) {
     return (
         <div className="footerpart_full">
             <p className='FooterPart_company_pp'>{FooterPartText}</p>
-            60px
         </div>
     );
 };
