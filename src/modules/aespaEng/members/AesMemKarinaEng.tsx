ce147e22ed3fd75cf0545f4d578c4367bc2bc52c
groomigroom <googjohn0143@gmail.com>
2026-07-28 20:42:22 +0900

    Update AesMemKarinaEng.tsx

--- src/modules/aespaEng/members/AesMemKarinaEng.tsx
+++ src/modules/aespaEng/members/AesMemKarinaEng.tsx
@@ -5,6 +5,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../aespa/members/AesMemKarina.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import aes_mm_karina_img from '../../aespa/main/img/aespa_m_mem_Karina.jpg';
 
@@ -67,9 +68,8 @@ export default function AesMemKarinaEng() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPartEng FooterPartEngText='ⓒ SM ENTERTAINMENT' />
         </div>
     );
 
 };
-
