8e70c624ae8353aca09eb0e352012cd6d93732a5
groomigroom <googjohn0143@gmail.com>
2026-07-28 18:22:40 +0900

    Update StrayMemLinoEng.tsx

--- src/modules/Stray_KidsEng/members/StrayMemLinoEng.tsx
+++ src/modules/Stray_KidsEng/members/StrayMemLinoEng.tsx
@@ -5,6 +5,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../Stray_Kids/members/StrayMemLino.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Str_mm_lino_img from '../../Stray_Kids/main/img/stray_m_mem_Lino.jpg';
 
@@ -67,8 +68,7 @@ export default function StrayMemLinoEng() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPartEng FooterPartEngText='ⓒ JYP ENTERTAINMENT' />
         </div>
     );
 }
-
