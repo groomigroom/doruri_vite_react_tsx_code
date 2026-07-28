bc6c2db02fa53aa7072986249fba55c7d197d9fc
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:28:21 +0900

    Update BTSMemJungKookEng.tsx

--- src/modules/BTSEng/members/BTSMemJungKookEng.tsx
+++ src/modules/BTSEng/members/BTSMemJungKookEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../BTS/members/BTSMemJungKook.css';
@@ -10,6 +11,8 @@ import MenuEng from '../../HomeEng/MenuEng';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function BTSMemJungKookEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="bts_mm_jungkook_full">
             <MenuEng />
