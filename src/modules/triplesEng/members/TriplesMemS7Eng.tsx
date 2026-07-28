a27b46bc4ed337a649a2123c92bbf16732f0c4b3
groomigroom <googjohn0143@gmail.com>
2026-07-28 18:03:27 +0900

    Update TriplesMemS7Eng.tsx

--- src/modules/triplesEng/members/TriplesMemS7Eng.tsx
+++ src/modules/triplesEng/members/TriplesMemS7Eng.tsx
@@ -71,7 +71,7 @@ export default function TriplesMemS7Eng() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPartEng />
+            <FooterPartEng FooterPartEngText='ⓒ Modhaus' />
         </div>
     );
 
