1c136e7094c9cc054557d3e47946606a60b4d6a3
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:27:37 +0900

    Update BTSMemJinEng.tsx

--- src/modules/BTSEng/members/BTSMemJinEng.tsx
+++ src/modules/BTSEng/members/BTSMemJinEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../BTS/members/BTSMemJin.css';
@@ -10,6 +11,8 @@ import MenuEng from '../../HomeEng/MenuEng';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function BTSMemJinEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="bts_mm_jin_full">
             <MenuEng />
