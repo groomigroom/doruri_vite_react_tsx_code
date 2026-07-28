622245123e90549ad409cbd372ebfe893b340261
groomigroom <googjohn0143@gmail.com>
2026-07-28 15:41:19 +0900

    Update ArtMemKimlipEng.tsx

--- src/modules/ARTMSEng/members/ArtMemKimlipEng.tsx
+++ src/modules/ARTMSEng/members/ArtMemKimlipEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../ARTMS/members/ArtMemKimlip.css';
@@ -10,6 +11,8 @@ import MenuEng from '../../HomeEng/MenuEng';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function ArtMemKimlipEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="art_mm_kimlip_full">
             <MenuEng />
