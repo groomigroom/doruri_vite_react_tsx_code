1b002e70ff381613ffd1f531cc44f5875a11535f
groomigroom <googjohn0143@gmail.com>
2026-07-28 18:20:47 +0900

    Update StrayMemBangchanEng.tsx

--- src/modules/Stray_KidsEng/members/StrayMemBangchanEng.tsx
+++ src/modules/Stray_KidsEng/members/StrayMemBangchanEng.tsx
@@ -5,6 +5,7 @@ import '../../triples/members/TriplesMemS1.css';
 import '../../Stray_Kids/members/StrayMemBangchan.css';
 
 import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
 
 import Str_mm_bangchan_img from '../../Stray_Kids/main/img/stray_m_mem_Bangchan.jpg';
 
@@ -67,8 +68,7 @@ export default function StrayMemBangchanEng() {
                     </tr>
                 </tbody>
             </table>
-
+            <FooterPartEng FooterPartEngText='ⓒ JYP ENTERTAINMENT' />
         </div>
     );
 }
-
