eabcf2fa992ccab72d3aeb0c35dbb66d24ef0db3
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:54:33 +0900

    Update EpiMemMithra.tsx

--- src/modules/EpikHigh/members/EpiMemMithra.tsx
+++ src/modules/EpikHigh/members/EpiMemMithra.tsx
@@ -42,7 +42,7 @@ export default function EpiMemMithra() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ 아워즈' />
         </div>
     );
 };
