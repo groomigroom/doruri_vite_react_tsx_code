de91236d1433bd3be70c82d46d49ed1c794c06c2
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:23:53 +0900

    Update BtoMemPenielEng.tsx

--- src/modules/BTOBEng/members/BtoMemPenielEng.tsx
+++ src/modules/BTOBEng/members/BtoMemPenielEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../BTOB/members/BtoMemPeniel.css';
@@ -10,6 +11,8 @@ import Menu from '../../Menu';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function BtoMemPenielEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="bto_mm_peniel_full">
             <Menu />
