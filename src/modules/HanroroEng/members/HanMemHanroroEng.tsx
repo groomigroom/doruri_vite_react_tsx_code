a56c959f5b8b889834419b8042433d81a83e74e8
groomigroom <googjohn0143@gmail.com>
2026-07-28 17:25:50 +0900

    Update HanMemHanroroEng.tsx

--- src/modules/HanroroEng/members/HanMemHanroroEng.tsx
+++ src/modules/HanroroEng/members/HanMemHanroroEng.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
 
 import '../../triples/members/TriplesMemS1.css';
 import '../../Hanroro/members/HanMemHanroro.css';
@@ -10,6 +11,8 @@ import MenuEng from '../../HomeEng/MenuEng';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function HanMemHanroroEng() {
+    EngPage('en', 'ko');
+
     return (
         <div className="han_mm_hanroro_full">
             <MenuEng />
