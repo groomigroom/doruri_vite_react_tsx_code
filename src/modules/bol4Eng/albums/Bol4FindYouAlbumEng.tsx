0ce0e63e7074d4c2fb8490ec9e06945604925d8b
groomigroom <googjohn0143@gmail.com>
2026-07-29 09:51:32 +0900

    Update Bol4FindYouAlbumEng.tsx

--- src/modules/bol4Eng/albums/Bol4FindYouAlbumEng.tsx
+++ src/modules/bol4Eng/albums/Bol4FindYouAlbumEng.tsx
@@ -1,1 +1,125 @@
-Bol4FindYouAlbumEng.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import './Bol4FindYouAlbumEng.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function Bol4FindYouAlbumEng() {
+    return (
+        <div className='bol_Bol4FindYouAlbumEng_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/Bol4FindYouAlbum'>한국어</Link>
+                <Link to='/Bol4FindYouAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'><img src={MainH2deco} alt="bol4 deco" />Find You<img src={MainH2deco} alt="bol4 deco" /></h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                This is an album prepared with<br />
+                the thought that even the flower<br />
+                petals are all spring!
+            </p>
+            <img src="" alt="Find You" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                2026.04.22.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                indie music, folk/blues, rock/metal
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Kanverse
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 Bol4FindYouAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                번호
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                제목
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Find You
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                2
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                아름다운 안녕
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                3
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                나를 봄으로 데려가!
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ Kanverse' />
+        </div>
+    );
+}
+
+
+
