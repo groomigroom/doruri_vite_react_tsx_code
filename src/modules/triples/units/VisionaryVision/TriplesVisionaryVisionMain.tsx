b1a7e866039d3f3d6d50e16db746534948efe8ba
groomigroom <googjohn0143@gmail.com>
2026-07-29 14:08:04 +0900

    Update TriplesVisionaryVisionMain.tsx

--- src/modules/triples/units/VisionaryVision/TriplesVisionaryVisionMain.tsx
+++ src/modules/triples/units/VisionaryVision/TriplesVisionaryVisionMain.tsx
@@ -4,6 +4,7 @@ import '../../main/TriplesMain.css';
 import './TriplesVisionaryVisionMain.css';
 
 import Menu from '../../../Menu';
+import FooterPart from '../../../FooterPart';
 
 import TriplesVis_m_logo from './img/TriplesVisionaryVision_m_logo.svg';
 import MainH2Deco from '../../../img/main_h2_deco.png';
@@ -42,25 +43,33 @@ export default function TriplesVisionaryVisionMain() {
                     <Link to='/TriplesMemS2' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S2_img} alt="정혜린 이미지" />
-                            <h4>정혜린</h4>
+                            <div>
+                                <h4>정혜린</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS5' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S5_img} alt="김유연 이미지" />
-                            <h4>김유연</h4>
+                            <div>
+                                <h4>김유연</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS7' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S7_img} alt="김나경 이미지" />
-                            <h4>김나경</h4>
+                            <div>
+                                <h4>김나경</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS8' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S8_img} alt="공유빈 이미지" />
-                            <h4>공유빈</h4>
+                            <div>
+                                <h4>공유빈</h4>
+                            </div>
                         </article>
                     </Link>
 
@@ -70,28 +79,35 @@ export default function TriplesVisionaryVisionMain() {
                     <Link to='/TriplesMemS9' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S9_img} alt="카에데 이미지" />
-                            <h4>카에데</h4>
+                            <div>
+                                <h4>카에데</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS11' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S11_img} alt="코토네 이미지" />
-                            <h4>코토네</h4>
+                            <div>
+                                <h4>코토네</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS12' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S12_img} alt="곽연지 이미지" />
-                            <h4>곽연지</h4>
+                            <div>
+                                <h4>곽연지</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS13' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S13_img} alt="니엔 이미지" />
-                            <h4>니엔</h4>
+                            <div>
+                                <h4>니엔</h4>
+                            </div>
                         </article>
                     </Link>
-
 
                 </section>
 
@@ -99,35 +115,40 @@ export default function TriplesVisionaryVisionMain() {
                     <Link to='/TriplesMemS14' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S14_img} alt="박소현 이미지" />
-                            <h4>박소현</h4>
+                            <div>
+                                <h4>박소현</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS15' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S15_img} alt="신위 이미지" />
-                            <h4>신위</h4>
+                            <div>
+                                <h4>신위</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS17' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S17_img} alt="린 이미지" />
-                            <h4>린</h4>
+                            <div>
+                                <h4>린</h4>
+                            </div>
                         </article>
                     </Link>
                     <Link to='/TriplesMemS24' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
                             <img src={TriplesVis_m_S24_img} alt="지연 이미지" />
-                            <h4>지연</h4>
+                            <div>
+                                <h4>지연</h4>
+                            </div>
                         </article>
                     </Link>
 
-
                 </section>
 
-
             </section>
-
-
+            <FooterPart FooterPartText='ⓒ ㈜모드하우스' />
         </div>
     );
 };
