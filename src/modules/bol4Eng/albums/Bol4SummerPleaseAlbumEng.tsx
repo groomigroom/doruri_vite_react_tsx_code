6137e5a5556393d83382258d975d0016cd3a8826
groomigroom <googjohn0143@gmail.com>
2026-07-29 10:11:28 +0900

    Update Bol4SummerPleaseAlbumEng.tsx

--- src/modules/bol4Eng/albums/Bol4SummerPleaseAlbumEng.tsx
+++ src/modules/bol4Eng/albums/Bol4SummerPleaseAlbumEng.tsx
@@ -1,1 +1,106 @@
-Bol4SummerPleaseAlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../bol4/albums/Bol4SummerPleaseAlbum.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function Bol4SummerPleaseAlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='bol_Bol4SummerPleaseAlbum_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/Bol4SummerPleaseAlbum'>한국어</Link>
+                <Link to='/Bol4SummerPleaseAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'><img src={MainH2deco} alt="bol4 deco" />Summer, please<img src={MainH2deco} alt="bol4 deco" /></h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                I’ll take you to<br />
+                the season that was the bluest,<br />
+                hoping summer will sparkle!
+            </p>
+            <img src="" alt="Summer, please" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                2026.07.22.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                ballad, indie music
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Kanverse
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 Bol4SummerPleaseAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Summer, please
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ Kanverse' />
+        </div>
+    );
+}
+
+
+
+
+
