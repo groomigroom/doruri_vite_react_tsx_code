4de3cf6f6ce46e9e51ca4785bfa01db4ee05a41d
groomigroom <googjohn0143@gmail.com>
2026-07-30 08:15:58 +0900

    Update RoutesSetup.tsx

--- src/modules/RoutesSetup.tsx
+++ src/modules/RoutesSetup.tsx
@@ -113,6 +113,7 @@ import TxtMemYeonjun_7_22_end from './txt/members/yeonjun/TxtMemYeonjun_7_22_end
 
 import TxtMemYeonjunNOLABELSPART01Album from './txt/members/yeonjun/albums/TxtMemYeonjunNOLABELSPART01Album';
 import TxtMemYeonjunNOLABELSPART01AlbumEng from './txtEng/members/yeonjun/albums/TxtMemYeonjunNOLABELSPART01AlbumEng';
+import TxtMemYeonjunNOLABELSPART02Album from './txt/members/yeonjun/albums/TxtMemYeonjunNOLABELSPART02Album';
 
 import TxtMemYeonjunEng from './txtEng/members/TxtMemYeonjunEng';
 import TxtMemYeonjun_7_22_endEng from './txtEng/members/yeonjun/TxtMemYeonjun_7_22_endEng';
@@ -554,6 +555,7 @@ export default function RoutesSetup() {
 
             <Route path='/TxtMemYeonjunNOLABELSPART01Album' element={<TxtMemYeonjunNOLABELSPART01Album />}></Route>
             <Route path='/TxtMemYeonjunNOLABELSPART01AlbumEng' element={<TxtMemYeonjunNOLABELSPART01AlbumEng />}></Route>
+            <Route path='/TxtMemYeonjunNOLABELSPART02Album' element={<TxtMemYeonjunNOLABELSPART02Album />}></Route>
 
             <Route path='/TxtMemYeonjunEng' element={<TxtMemYeonjunEng />}></Route>
             <Route path='/TxtMemYeonjun_7_22_endEng' element={<TxtMemYeonjun_7_22_endEng />}></Route>
