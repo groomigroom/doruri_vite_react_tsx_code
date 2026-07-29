2381c07e493cea22236b41fec450ce5e7f666132
groomigroom <googjohn0143@gmail.com>
2026-07-29 17:26:14 +0900

    Update DayMemYoungKYOUNGESTAlbumEng.tsx

--- src/modules/DAY6Eng/members/YoungK/albums/DayMemYoungKYOUNGESTAlbumEng.tsx
+++ src/modules/DAY6Eng/members/YoungK/albums/DayMemYoungKYOUNGESTAlbumEng.tsx
@@ -1,1 +1,275 @@
-DayMemYoungKYOUNGESTAlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../../../HomeEng/EngPage';
+
+import '../../../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../../../DAY6/members/YoungK/albums/DayMemYoungKYOUNGESTAlbum.css';
+
+import Menu from '../../../../Menu';
+import FooterPart from '../../../../FooterPart';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../../../img/main_h2_deco.png';
+
+export default function DayMemYoungKYOUNGESTAlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='day_DayMemYoungKYOUNGESTAlbum_full'>
+            <Menu />
+            <section className="home_translate">
+                <Link to='/DayMemYoungKYOUNGESTAlbum'>한국어</Link>
+                <Link to='/DayMemYoungKYOUNGESTAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="Young K deco" />YOUNGEST<img src={MainH2deco} alt="Young K deco" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                It&apos;s an album filled<br />
+                with the wish that good<br />
+                things happen to everyone!
+            </p>
+            <img src='' alt="YOUNGEST" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                July, 27, 2026
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                rock/metal, R&B/Soul, folk/blues, ballad, electronica
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                JYP ENTERTAINMENT
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 day_DayMemYoungKYOUNGESTAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Marionette
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                2
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Shut The Door
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                3
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                F world
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                4
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Hey Honey
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                5
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                응원가
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                6
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                SPIKE
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                7
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Yonge St.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                8
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                million reasons
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                9
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                우리가 헤어질 100가지 이유 &#40;with JINJOO of DNCE&#41;
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                10
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Drivin&apos; into Hell
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                11
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                whatever
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                12
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Goodbye, Love
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                13
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                집으로 향한다
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                14
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                안개꽃
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                15
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                작업실에서 커피를
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPart FooterPartText='ⓒ JYP엔터테인먼트' />
+        </div>
+    );
+}
+
+
