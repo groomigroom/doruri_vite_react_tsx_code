728883ea42ac340d15ded65b48ffada3df72c949
groomigroom <googjohn0143@gmail.com>
2026-07-29 10:31:25 +0900

    Update TriplesBabyFlowerCityRemixesAlbumEng.tsx

--- src/modules/triplesEng/albums/TriplesBabyFlowerCityRemixesAlbumEng.tsx
+++ src/modules/triplesEng/albums/TriplesBabyFlowerCityRemixesAlbumEng.tsx
@@ -1,1 +1,136 @@
-TriplesBabyFlowerCityRemixesAlbumEng.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function TriplesBabyFlowerCityRemixesAlbumEng() {
+    return (
+        <div className="tri_BabyFlowerCityRemixesAlbum_full">
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/TriplesBabyFlowerCityRemixesAlbum'>한국어</Link>
+                <Link to='/TriplesBabyFlowerCityRemixesAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'><img src={MainH2deco} alt="tripleS deco" />&lt;Baby Flower City Remixes&gt;<img src={MainH2deco} alt="tripleS deco" /></h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                This album was prepared<br />
+                with the hope that we can<br />
+                now bloom like flowers too!
+            </p>
+            <img src="" alt="Baby Flower City Remixes" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                발매일
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                2026.06.08.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                장르
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                댄스
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                기획사
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                모드하우스
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                번호
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                제목
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Baby Flower &#40;Seoul Remix : Vendors&#41;
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                2
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Baby Flower &#40;Bangkok Remix : Kurtz&#41;
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                3
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Baby Flower &#40;Taipei Remix : ntrophy&#41;
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                4
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Baby Flower &#40;Tokyo Remix : Full8loom&#41;
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPart FooterPartText='ⓒ 켄버스' />
+        </div>
+    );
+}
+
+
+
