9d46e461f546366ba42c184e2b4405c4f64bf707
groomigroom <googjohn0143@gmail.com>
2026-07-29 20:50:45 +0900

    Update TxtStarsChapterTogetherAlbumEng.tsx

--- src/modules/txtEng/albums/TxtStarsChapterTogetherAlbumEng.tsx
+++ src/modules/txtEng/albums/TxtStarsChapterTogetherAlbumEng.tsx
@@ -1,1 +1,190 @@
-TxtStarsChapterTogetherAlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../txt/albums/TxtStarsChapterTogetherAlbum.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function TxtStarsChapterTogetherAlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='txt_TxtStarsChapterTogetherAlbum_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/TxtStarsChapterTogetherAlbum'>한국어</Link>
+                <Link to='/TxtStarsChapterTogetherAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="txt deco" />Star&apos;s Chapter: TOGETHER<img src={MainH2deco} alt="txt deco" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                We prepared this album<br />
+                while reflecting on<br />
+                the meaning of togetherness!
+            </p>
+            <img src='' alt="Star's Chapter: TOGETHER" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                July, 21, 2025
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                rock/metal, dance, R&B/Soul, rap/hiphop
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                BIGHIT MUSIC
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 txt_TxtStarsChapterTogetherAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Upside Down Kiss
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                2
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Beautiful Strangers
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                3
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Ghost Girl
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                4
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Sunday Driver
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                5
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Dance With You
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                6
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Take My Half
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                7
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Bird of Night
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                8
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Star&apos;s song
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ BIGHIT MUSIC' />
+        </div>
+    );
+}
