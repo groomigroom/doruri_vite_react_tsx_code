69531b55ced2be46f47b46f5aa554cb64c71f970
groomigroom <googjohn0143@gmail.com>
2026-07-30 16:16:39 +0900

    Update BtoMemSeoEunkwang.tsx

--- src/modules/BTOB/members/BtoMemSeoEunkwang.tsx
+++ src/modules/BTOB/members/BtoMemSeoEunkwang.tsx
@@ -62,7 +62,7 @@ export default function BtoMemSeoEunkwang() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ 비투비 컴퍼니' />
 
         </div>
     );
