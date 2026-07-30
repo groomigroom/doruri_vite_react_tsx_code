866d55ba706fa91ab57fb436e0cebae1b6b57d88
groomigroom <googjohn0143@gmail.com>
2026-07-30 15:36:47 +0900

    Update Day6EvenOfDayMain.tsx

--- src/modules/DAY6/units/EvenOfDay/Day6EvenOfDayMain.tsx
+++ src/modules/DAY6/units/EvenOfDay/Day6EvenOfDayMain.tsx
@@ -1,1 +1,89 @@
-Day6EvenOfDayMain.tsx
+import { Link } from 'react-router-dom';
+
+import '../../../triples/main/TriplesMain.css'
+import './Day6EvenOfDayMain.css';
+
+import Menu from '../../../Menu';
+import FooterPart from '../../../FooterPart';
+
+// import StrayKids3Racha_m_logo from './img/StrayKids3Racha_m_logo.png';
+import MainH2Deco from '../../../img/main_h2_deco.png';
+// import StrayKids3Racha_m_img from './img/StrayKids3Racha_m_img.jpg';
+
+
+//#191919
+
+export default function Day6EvenOfDayMain() {
+    return (
+        <div className='day_EvenOfDay_m_full'>
+            <Menu />
+            <img src='' alt="DAY6 &#40;Even of Day&#41; 로고" className='tri_m_logo' />
+            <h2 className='tri_m_h2 tri_m_h2_1'>
+                <img src={MainH2Deco} alt="DAY6 &#40;Even of Day&#41; 꾸미기" />DAY6 &#40;Even of Day&#41;<img src={MainH2Deco} alt="DAY6 &#40;Even of Day&#41; 꾸미기" />
+            </h2>
+            <p className='tri_m_intropp'>
+                #####
+                &apos;에픽하이&apos;와 함께<br />
+                아무도 내 맘을 몰라도<br />
+                Can't stop!</p>
+            <img src='' className='tri_m_img' alt='3RACHA' />
+            <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
+            <section className='tri_m_mem_tr_full day_EvenOfDay_m_mem_tr_full'>
+                <section className='tri_m_mem_tr'>
+                    <Link to='/DayMemWonpil' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src='' alt="원필 이미지" />
+                            <div>
+                                <h4>원필</h4>
+                            </div>
+                        </article>
+                    </Link>
+                    <Link to='/DayMemDowoon' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src='' alt="도운 이미지" />
+                            <div>
+                                <h4>도운</h4>
+                            </div>
+                        </article>
+                    </Link>
+                    <Link to='/DayMemYoungK' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src='' alt="Young K 이미지" />
+                            <div>
+                                <h4>Young K</h4>
+                            </div>
+                        </article>
+                    </Link>
+                </section>
+            </section>
+
+            {/* 모바일 보기용 */}
+            {/* <section className="tri_m_mem_tr_full_mobile epi_m_mem_tr_full_mobile">
+                <section className="tri_m_mem_trtr">
+                    <section className='tri_m_mem_tr'>
+                        <Link to='/EpiMemTablo' className='tri_m_mem_a'>
+                            <img src="" alt="타블로 이미지" />
+                            <h4>타블로</h4>
+                        </Link>
+                        <Link to='/EpiMemTwokutz' className='tri_m_mem_a'>
+                            <img src="" alt="투컷 이미지" />
+                            <h4>투컷</h4>
+                        </Link>
+                    </section>
+                </section>
+                <section className="tri_m_mem_trtr">
+                    <section className='tri_m_mem_tr1'>
+                        <Link to='/EpiMemMithra' className='tri_m_mem_a'>
+                            <img src="" alt="미쓰라 이미지" />
+                            <h4>미쓰라</h4>
+                        </Link>
+                    </section>
+                </section>
+
+            </section> */}
+            <FooterPart FooterPartText='ⓒ JYP엔터테인먼트' />
+        </div >
+    );
+};
+
+
