849fc9e865869de7bf7e01a9a582ff86d922167f
groomigroom <googjohn0143@gmail.com>
2026-07-31 07:12:01 +0900

    Update IllitBubeeKoreanVerAlbumEng.tsx

--- src/modules/illitEng/albums/IllitBubeeKoreanVerAlbumEng.tsx
+++ src/modules/illitEng/albums/IllitBubeeKoreanVerAlbumEng.tsx
@@ -1,1 +1,106 @@
-IllitBubeeKoreanVerAlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../illit/albums/IllitBubeeKoreanVerAlbum.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function IllitBubeeKoreanVerAlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='ill_IllitBubeeKoreanVerAlbum_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/IllitBubeeKoreanVerAlbum'>한국어</Link>
+                <Link to='/IllitBubeeKoreanVerAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="illit deco" />Bubee &#40;Korean Ver.&#41;<img src={MainH2deco} alt="illit deco" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                This is an album<br />
+                prepared with a<br />
+                refreshing and lively feel!
+            </p>
+            <img src='' alt="Bubee &#40;Korean Ver.&#41;" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                April, 13, 2026
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                dance
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                BE:LIFT LAB
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 ill_IllitBubeeKoreanVerAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Bubee &#40;Korean Ver.&#41;
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ BE:LIFT LAB' />
+        </div>
+    );
+}
