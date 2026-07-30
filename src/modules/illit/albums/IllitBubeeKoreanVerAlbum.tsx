77b488d641503000218f6ea151c12056d474df76
groomigroom <googjohn0143@gmail.com>
2026-07-31 06:53:55 +0900

    Update IllitBubeeKoreanVerAlbum.tsx

--- src/modules/illit/albums/IllitBubeeKoreanVerAlbum.tsx
+++ src/modules/illit/albums/IllitBubeeKoreanVerAlbum.tsx
@@ -1,1 +1,103 @@
-IllitBubeeKoreanVerAlbum.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import './IllitBubeeKoreanVerAlbum.css';
+
+import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function IllitBubeeKoreanVerAlbum() {
+    return (
+        <div className='ill_IllitBubeeKoreanVerAlbum_full'>
+            <Menu />
+            <section className="home_translate">
+                <Link to='/IllitBubeeKoreanVerAlbum' className="on">한국어</Link>
+                <Link to='/IllitBubeeKoreanVerAlbumEng'>English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="illit 앨범 꾸미기" />Bubee &#40;Korean Ver.&#41;<img src={MainH2deco} alt="illit 앨범 꾸미기" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                청량하고<br />
+                통통튀는 느낌으로<br />
+                준비한 앨범이에요!
+            </p>
+            <img src='' alt="Bubee &#40;Korean Ver.&#41;" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                발매일
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                2026.04.13.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                장르
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                댄스
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                기획사
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                빌리프랩
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 ill_IllitBubeeKoreanVerAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                번호
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                제목
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Bubee &#40;Korean Ver.&#41;
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPart FooterPartText='ⓒ 빌리프랩' />
+        </div>
+    );
+}
