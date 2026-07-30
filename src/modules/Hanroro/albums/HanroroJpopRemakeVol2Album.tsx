0a071877891e9d69e347dee34eefea8ea621a86b
groomigroom <googjohn0143@gmail.com>
2026-07-31 08:56:07 +0900

    Update HanroroJpopRemakeVol2Album.tsx

--- src/modules/Hanroro/albums/HanroroJpopRemakeVol2Album.tsx
+++ src/modules/Hanroro/albums/HanroroJpopRemakeVol2Album.tsx
@@ -1,1 +1,105 @@
-HanroroJpopRemakeVol2Album.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import './HanroroJpopRemakeVol2Album.css';
+
+import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function HanroroJpopRemakeVol2Album() {
+    return (
+        <div className='han_HanroroJpopRemakeVol2Album_full'>
+            <Menu />
+            <section className="home_translate">
+                <Link to='/HanroroJpopRemakeVol2Album' className="on">한국어</Link>
+                <Link to='/HanroroJpopRemakeVol2AlbumEng'>English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="한로로 앨범 꾸미기" />J-POP REMAKE Vol.2<img src={MainH2deco} alt="한로로 앨범 꾸미기" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                저만의 새로운<br />
+                목소리를 담아서<br />
+                앨범을 준비했어요!
+            </p>
+            <img src='' alt="J-POP REMAKE Vol.2" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                발매일
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                2026.07.30.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                장르
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                록/메탈
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                기획사
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                SHGOLD NETWORKS
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 tws_TwsTodayMeetingAgainAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                번호
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                제목
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                잔혹한 천사의 테제
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPart FooterPartText='ⓒ authentic' />
+        </div>
+    );
+}
+
+
