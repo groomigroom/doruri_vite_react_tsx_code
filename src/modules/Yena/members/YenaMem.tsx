7a625b96b7a52d57ec45a231c20be85d4b4192d0
groomigroom <googjohn0143@gmail.com>
2026-07-30 16:06:29 +0900

    Update YenaMem.tsx

--- src/modules/Yena/members/YenaMem.tsx
+++ src/modules/Yena/members/YenaMem.tsx
@@ -65,7 +65,7 @@ export default function YenaMem() {
                     </tr>
                 </tbody>
             </table>
-            <FooterPart />
+            <FooterPart FooterPartText='ⓒ YH엔터테인먼트' />
         </div>
     );
 
