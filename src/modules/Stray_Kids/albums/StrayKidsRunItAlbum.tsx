6a79e0a352f43aa39519c68e1203b93927159cbf
groomigroom <googjohn0143@gmail.com>
2026-07-30 10:17:57 +0900

    Update StrayKidsRunItAlbum.tsx

--- src/modules/Stray_Kids/albums/StrayKidsRunItAlbum.tsx
+++ src/modules/Stray_Kids/albums/StrayKidsRunItAlbum.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
-import './IoiIoiLoopAlbum.css';
+import './StrayKidsRunItAlbum.css';
 
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
@@ -12,21 +12,21 @@ import MainH2deco from '../../img/main_h2_deco.png';
 
 export default function StrayKidsRunItAlbum() {
     return (
-        <div className='ioi_IoiIoiLoopAlbum_full'>
+        <div className='str_StrayKidsRunItAlbum_full'>
             <Menu />
             <section className="home_translate">
-                <Link to='/IoiIoiLoopAlbum' className="on">한국어</Link>
-                <Link to='/IoiIoiLoopAlbumEng'>English</Link>
+                <Link to='/StrayKidsRunItAlbum' className="on">한국어</Link>
+                <Link to='/StrayKidsRunItAlbumEng'>English</Link>
             </section>
             <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
-                <img src={MainH2deco} alt="ioi 앨범 꾸미기" />I.O.I 3rd MINI ALBUM &#91;I.O.I : LOOP&#93;<img src={MainH2deco} alt="ioi 앨범 꾸미기" />
+                <img src={MainH2deco} alt="Stray Kids 앨범 꾸미기" />RUN IT<img src={MainH2deco} alt="Stray Kids 앨범 꾸미기" />
             </h2>
             <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
                 dddd10년전의 기억을<br />
                 담아서 앨범을<br />
                 준비했어요!
             </p>
-            <img src='' alt="I.O.I 3rd MINI ALBUM &lsqb;I.O.I : LOOP&rsqb;" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <img src='' alt="RUN IT" className='tri_BabyFlowerCityRemixesAlbum_img' />
             <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
                 <tbody>
                     <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
@@ -37,7 +37,7 @@ export default function StrayKidsRunItAlbum() {
                         </td>
                         <td>
                             <p>
-                                2026.05.19.
+                                2026.06.24.
                             </p>
                         </td>
                     </tr>
@@ -49,7 +49,7 @@ export default function StrayKidsRunItAlbum() {
                         </td>
                         <td>
                             <p>
-                                발라드, 댄스
+                                랩/힙합
                             </p>
                         </td>
                     </tr>
@@ -61,14 +61,14 @@ export default function StrayKidsRunItAlbum() {
                         </td>
                         <td>
                             <p>
-                                스윙엔터테인먼트
+                                JYP엔터테인먼트
                             </p>
                         </td>
                     </tr>
                 </tbody>
             </table>
             <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
-            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 ioi_IoiIoiLoopAlbum_tb2'>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 str_StrayKidsRunItAlbum_tb2'>
                 <tbody>
                     <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
                         <td>
@@ -82,7 +82,8 @@ export default function StrayKidsRunItAlbum() {
                             </p>
                         </td>
                     </tr>
-                    <tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
                         <td>
                             <p>
                                 1
@@ -90,74 +91,13 @@ export default function StrayKidsRunItAlbum() {
                         </td>
                         <td>
                             <p>
-                                IOI &#40;Where My Girls At&#41;
-                            </p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p>
-                                2
-                            </p>
-                        </td>
-                        <td>
-                            <p>
-                                갑자기
-                            </p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p>
-                                3
-                            </p>
-                        </td>
-                        <td>
-                            <p>
-                                SPF 100+ &#40;Summer Pop Fantasy&#41;
-                            </p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p>
-                                4
-                            </p>
-                        </td>
-                        <td>
-                            <p>
-                                IF I
-                            </p>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <p>
-                                5
-                            </p>
-                        </td>
-                        <td>
-                            <p>
-                                그때 우리 지금
-                            </p>
-                        </td>
-                    </tr>
-
-                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
-                        <td>
-                            <p>
-                                6
-                            </p>
-                        </td>
-                        <td>
-                            <p>
-                                웃으며 안녕 &#40;Recorded in 2016&#41; &#40;Prod.진영&#41;
+                                RUN IT
                             </p>
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <FooterPart FooterPartText='ⓒ 스윙엔터테인먼트' />
+            <FooterPart FooterPartText='ⓒ JYP엔터테인먼트' />
         </div>
     );
 }
