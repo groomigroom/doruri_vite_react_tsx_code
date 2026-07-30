0635405898ec7ae2fd700538cb4c3df151c46822
groomigroom <googjohn0143@gmail.com>
2026-07-30 09:03:35 +0900

    Update IoiSmileByeAlbumEng.tsx

--- src/modules/ioiEng/albums/IoiSmileByeAlbumEng.tsx
+++ src/modules/ioiEng/albums/IoiSmileByeAlbumEng.tsx
@@ -1,1 +1,106 @@
-IoiSmileByeAlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../ioi/albums/IoiSmileByeAlbum.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function IoiSmileByeAlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='ioi_IoiSmileByeAlbum_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/IoiSmileByeAlbum'>한국어</Link>
+                <Link to='/IoiSmileByeAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="ioi deco" />Goodbye with a smile &#40;Recorded in 2016&#41; &#40;Prod. JINYOUNG&#41;<img src={MainH2deco} alt="ioi deco" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                dddddd밤새 내린 비에 젖은<br />
+                꽃잎들까지도 다 봄이라는<br />
+                생각을 해보며 준비한 앨범이에요!
+            </p>
+            <img src='' alt="Goodbye with a smile &#40;Recorded in 2016&#41; &#40;Prod. JINYOUNG&#41;" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                May, 4, 2026
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                ballad
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                SWING ENTERTAINMENT
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 ioi_IoiSmileByeAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Goodbye with a smile &#40;Recorded in 2016&#41; &#40;Prod. JINYOUNG&#41;
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ SWING ENTERTAINMENT' />
+        </div>
+    );
+}
