9f58aa6bafcfafc188acf0cee408256784a00153
groomigroom <googjohn0143@gmail.com>
2026-07-31 08:01:48 +0900

    Update TwsNoTragedyAlbum.tsx

--- src/modules/tws/albums/TwsNoTragedyAlbum.tsx
+++ src/modules/tws/albums/TwsNoTragedyAlbum.tsx
@@ -1,1 +1,162 @@
-TwsNoTragedyAlbum.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import './TwsNoTragedyAlbum.css';
+
+import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function TwsNoTragedyAlbum() {
+    return (
+        <div className='tws_TwsNoTragedyAlbum_full'>
+            <Menu />
+            <section className="home_translate">
+                <Link to='/TwsNoTragedyAlbum' className="on">한국어</Link>
+                <Link to='/TwsNoTragedyAlbumEng'>English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="tws 앨범 꾸미기" />NO TRAGEDY<img src={MainH2deco} alt="tws 앨범 꾸미기" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                주체적인 마음을<br />
+                담아서 준비한<br />
+                앨범이에요!
+            </p>
+            <img src='' alt="NO TRAGEDY" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                발매일
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                2026.04.27.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                장르
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                댄스, 랩/힙합, POP
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                기획사
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                플레디스 엔터테인먼트
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 tws_TwsNoTragedyAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                번호
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                제목
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                너의 모든 가능성이 되어 줄게
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                2
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                널 따라가 &#40;You, You&#41;
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                3
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Why You So Bad?
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                4
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Get It Now
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                5
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Fire Escape
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                6
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Back To Strangers
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPart FooterPartText='ⓒ 플레디스 엔터테인먼트' />
+        </div>
+    );
+}
