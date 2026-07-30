9c167d99ed88eb36480cd38a8b8c31b7b78c14ae
groomigroom <googjohn0143@gmail.com>
2026-07-30 10:46:17 +0900

    Update StrayKidsStarLightAlbumEng.tsx

--- src/modules/Stray_KidsEng/albums/StrayKidsStarLightAlbumEng.tsx
+++ src/modules/Stray_KidsEng/albums/StrayKidsStarLightAlbumEng.tsx
@@ -1,1 +1,106 @@
-StrayKidsStarLightAlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../Stray_Kids/albums/StrayKidsStarLightAlbum.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function StrayKidsStarLightAlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='str_StrayKidsStarLightAlbum_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/StrayKidsStarLightAlbum'>한국어</Link>
+                <Link to='/StrayKidsStarLightAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="Stray Kids deco" />Star, light &#40;STAY&#41;<img src={MainH2deco} alt="Stray Kids deco" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                dddd10년전의 기억을<br />
+                담아서 앨범을<br />
+                준비했어요!
+            </p>
+            <img src='' alt="Star, light &#40;STAY&#41;" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                March, 25, 2026
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                ballad
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                JYP ENTERTAINMENT
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 str_StrayKidsStarLightAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Star, light &#40;STAY&#41;
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ JYP ENTERTAINMENT' />
+        </div>
+    );
+}
