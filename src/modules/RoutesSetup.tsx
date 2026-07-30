f7b7ba9315a6e12d5bd30cea8504bb3e51df2323
groomigroom <googjohn0143@gmail.com>
2026-07-30 10:25:23 +0900

    Update RoutesSetup.tsx

--- src/modules/RoutesSetup.tsx
+++ src/modules/RoutesSetup.tsx
@@ -177,6 +177,7 @@ import StrayKidsMain from './Stray_Kids/main/StrayKidsMain';
 import StrayKidsMainEng from './Stray_KidsEng/main/StrayKidsMainEng';
 
 import StrayKidsRunItAlbum from './Stray_Kids/albums/StrayKidsRunItAlbum';
+import StrayKidsRunItAlbumEng from './Stray_KidsEng/albums/StrayKidsRunItAlbumEng';
 
 import StrayMemBangchan from './Stray_Kids/members/StrayMemBangchan';
 import StrayMemBangchanEng from './Stray_KidsEng/members/StrayMemBangchanEng';
@@ -627,6 +628,7 @@ export default function RoutesSetup() {
             <Route path='/StrayKidsMainEng' element={<StrayKidsMainEng />}></Route>
 
             <Route path='/StrayKidsRunItAlbum' element={<StrayKidsRunItAlbum />}></Route>
+            <Route path='/StrayKidsRunItAlbumEng' element={<StrayKidsRunItAlbumEng />}></Route>
 
             <Route path='/StrayMemBangchan' element={<StrayMemBangchan />}></Route>
             <Route path='/StrayMemBangchanEng' element={<StrayMemBangchanEng />}></Route>
