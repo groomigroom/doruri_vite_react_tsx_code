b6e885aeed15556eebee01788ffdd4970df510ad
groomigroom <googjohn0143@gmail.com>
2026-07-31 15:53:30 +0900

    Update YenaMain.tsx

--- src/modules/Yena/main/YenaMain.tsx
+++ src/modules/Yena/main/YenaMain.tsx
@@ -6,11 +6,11 @@ import './YenaMain.css';
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
 
-// import Ten_m_logo from './img/TenCM_m_logo.svg';
+import Yena_m_logo from './img/Yena_m_logo_191919.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-// import Ten_m_img from './img/10cm_m_mem_Gwonjungyeol.jpg';
+import Yena_m_img from './img/Yen_m_img.jpg';
 
-//000000
+//#191919
 
 export default function YenaMain() {
     return (
@@ -20,20 +20,20 @@ export default function YenaMain() {
                 <Link to='/YenaMain' className="on">한국어</Link>
                 <Link to='/YenaMainEng'>English</Link>
             </section>
-            <img src='' alt="최예나 로고" className='tri_m_logo' />
+            <img src={Yena_m_logo} alt="최예나 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="최예나 꾸미기" />최예나<img src={MainH2Deco} alt="최예나 꾸미기" /></h2>
             <p className='tri_m_intropp'>
                 &apos;최예나&apos;와 함께<br />
                 ca-ca-catch<br />
                 my heart!
             </p>
-            <img src='' className='tri_m_img yen_m_img' alt='10CM' />
+            <img src={Yena_m_img} className='tri_m_img yen_m_img' alt='최예나' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 소개 ▪</h2>
             <section className='tri_m_mem_tr_full yen_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/YenaMem' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="최예나" />
+                            <img src={Yena_m_img} alt="최예나" />
                             <div>
                                 <h4>최예나</h4>
                             </div>
