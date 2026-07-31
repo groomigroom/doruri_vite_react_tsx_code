32b9c49df7b12a2661bee3ac0c6c1c079a81d31c
groomigroom <googjohn0143@gmail.com>
2026-07-31 16:30:30 +0900

    Update Idn_m_memid4.tsx

--- src/modules/idntt/members/Idn_m_memid4.tsx
+++ src/modules/idntt/members/Idn_m_memid4.tsx
@@ -1,11 +1,12 @@
 import { Link } from 'react-router-dom';
 
+import '../../triples/members/TriplesMemS1.css';
 import './Idn_m_memid4.css';
 
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
 
-// import Idn_mm_memid4_img from '../main/img/tri_m_memS1.jpg';
+import Idn_mm_memid4_img from '../main/img/Idn_mm_id4_img.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -23,8 +24,8 @@ export default function Idn_m_memid4() {
                 잘 준비해서<br />
                 여러분을 만나러 갈게요!
             </p>
-            <img src='' alt="최태인 이미지" className='tri_mm_s1_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <img src={Idn_mm_memid4_img} alt="최태인 이미지" className='tri_mm_s1_img idn_mm_id4_img' />
+            <table className='tri_mm_s1_intropa_tb idn_mm_id4_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
