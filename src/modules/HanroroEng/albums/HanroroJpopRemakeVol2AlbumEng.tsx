40a7151d24bf8bfa360c112460ebff62ed8fcba8
groomigroom <googjohn0143@gmail.com>
2026-07-31 09:04:48 +0900

    Update HanroroJpopRemakeVol2AlbumEng.tsx

--- src/modules/HanroroEng/albums/HanroroJpopRemakeVol2AlbumEng.tsx
+++ src/modules/HanroroEng/albums/HanroroJpopRemakeVol2AlbumEng.tsx
@@ -1,1 +1,106 @@
-HanroroJpopRemakeVol2AlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../Hanroro/albums/HanroroJpopRemakeVol2Album.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function HanroroJpopRemakeVol2AlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='han_HanroroJpopRemakeVol2Album_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/HanroroJpopRemakeVol2Album'>한국어</Link>
+                <Link to='/HanroroJpopRemakeVol2AlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="hanroro deco" />J-POP REMAKE Vol.2<img src={MainH2deco} alt="hanroro deco" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                I prepared this album<br />
+                with my own new<br />
+                voice in it!
+            </p>
+            <img src='' alt="J-POP REMAKE Vol.2" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                July, 30, 2026
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                rock/metal
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                SHGOLD NETWORKS
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 han_HanroroJpopRemakeVol2Album_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                The Thesis of the Cruel Angel
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ authentic' />
+        </div>
+    );
+}
