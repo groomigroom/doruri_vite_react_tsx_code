dc5eae708667d02d8d6c2a0c70a8f53bdc73c0da
groomigroom <googjohn0143@gmail.com>
2026-07-31 15:42:12 +0900

    Update EpikHighMain.tsx

--- src/modules/EpikHigh/main/EpikHighMain.tsx
+++ src/modules/EpikHigh/main/EpikHighMain.tsx
@@ -8,8 +8,11 @@ import FooterPart from '../../FooterPart';
 
 import EpikHigh_m_logo from './img/EpikHigh_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-// import Tws_m_img from './img/tws_m_tws.jpg';
+import EpikHigh_m_img from './img/Epi_m_img.jpg';
 
+import EpikHighTablo_m_img from './img/Epi_mm_Tablo_img.jpg';
+import EpikHighMithra_m_img from './img/Epi_mm_Mithra_img.jpg';
+import EpikHighTukutz_m_img from './img/Epi_mm_Tukutz_img.jpg';
 
 //#000000
 
@@ -22,14 +25,14 @@ export default function EpikHighMain() {
             <p className='tri_m_intropp'>
                 &apos;에픽하이&apos;와 함께<br />
                 아무도 내 맘을 몰라도<br />
-                Can't stop!</p>
-            <img src="" className='tri_m_img epi_m_img' alt='EpikHigh' />
+                Can&apos;t stop!</p>
+            <img src={EpikHigh_m_img} className='tri_m_img epi_m_img' alt='EpikHigh' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full epi_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/EpiMemTablo' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="타블로 이미지" />
+                            <img src={EpikHighTablo_m_img} alt="타블로 이미지" />
                             <div>
                                 <h4>타블로</h4>
                             </div>
@@ -37,7 +40,7 @@ export default function EpikHighMain() {
                     </Link>
                     <Link to='/EpiMemTwokutz' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="투컷 이미지" />
+                            <img src={EpikHighTukutz_m_img} alt="투컷 이미지" />
                             <div>
                                 <h4>투컷</h4>
                             </div>
@@ -45,7 +48,7 @@ export default function EpikHighMain() {
                     </Link>
                     <Link to='/EpiMemMithra' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src="" alt="미쓰라 이미지" />
+                            <img src={EpikHighMithra_m_img} alt="미쓰라 이미지" />
                             <div>
                                 <h4>미쓰라</h4>
                             </div>
