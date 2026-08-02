f7aa84f5fde57a43f0c6df1df6159f20fd253fb5
groomigroom <googjohn0143@gmail.com>
2026-08-03 08:51:56 +0900

    Update Idn_m_memid17.tsx

--- src/modules/idntt/members/Idn_m_memid17.tsx
+++ src/modules/idntt/members/Idn_m_memid17.tsx
@@ -5,7 +5,7 @@ import './Idn_m_memid17.css';
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
 
-// import Idn_mm_memid17_img from '../main/img/tri_m_memS1.jpg';
+import Idn_mm_memid17_img from '../main/img/Idn_mm_id17_img.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -23,8 +23,8 @@ export default function Idn_m_memid17() {
                 열심히<br />
                 활동해 볼게요!
             </p>
-            <img src='' alt="곽기웅 이미지" className='tri_mm_s1_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <img src={Idn_mm_memid17_img} alt="곽기웅 이미지" className='tri_mm_s1_img idn_mm_id17_img' />
+            <table className='tri_mm_s1_intropa_tb idn_mm_id17_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
