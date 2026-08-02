948bf8f817e1f464e76448b9422004a8f747635c
groomigroom <googjohn0143@gmail.com>
2026-08-02 15:55:28 +0900

    Update StrayKidsSKZReplay2026Pt1AlbumEng.tsx

--- src/modules/Stray_KidsEng/albums/StrayKidsSKZReplay2026Pt1AlbumEng.tsx
+++ src/modules/Stray_KidsEng/albums/StrayKidsSKZReplay2026Pt1AlbumEng.tsx
@@ -1,1 +1,388 @@
-StrayKidsSKZReplay2026Pt1AlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../Stray_Kids/albums/StrayKidsSKZReplay2026Pt1Album.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+// import Bol4FindYouAlbum_img from './img/Bol4FindYouAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function StrayKidsSKZReplay2026Pt1AlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='str_StrayKidsSKZReplay2026Pt1Album_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/StrayKidsSKZReplay2026Pt1Album'>한국어</Link>
+                <Link to='/StrayKidsSKZReplay2026Pt1AlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="Stray Kids deco" />SKZ-REPLAY 2026 Pt.1<img src={MainH2deco} alt="Stray Kids deco" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                We put together an album<br />
+                with a variety of songs<br />
+                and prepared it!
+            </p>
+            <img src='' alt="StrayKidsSKZReplay2026Pt1Album" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                August, 1, 2026
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                ballad, rap/hiphop, dance, R&B/Soul, jazz
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                JYP ENTERTAINMENT
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='str_StrayKidsSKZReplay2026Pt1Album_tb2'>
+                <tbody>
+                    <tr className='str_StrayKidsSKZReplay2026Pt1Album_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                singer
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Battle Ground &#40;Korean Version&#41;
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                2
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Roman Empire
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Bangchan
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                3
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                LOVER
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Hyunjin
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                4
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Raining stars
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Han
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                5
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                There&apos;s nothing special about happiness
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, I.N
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                6
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Goodbye
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Seungmin
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                7
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Still here
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Seungmin
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                8
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                rapid stream
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Seungmin
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                9
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                MIC&BRUSH
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Hyunjin
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                10
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                PERFUME
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Seungmin
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                11
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                BABY
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Bangchan
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                12
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Don&apos;t Say
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Han
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                13
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Command
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Changbin
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                14
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                REV IT UP
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Felix
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                15
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Quietly &#40;Live&#41;
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, Lee Know
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                16
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                I&apos;ll hug you &#40;Live&#41;
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids, I.N
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='str_StrayKidsSKZReplay2026Pt1Album_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                17
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Star, light &#40;STAY&#41;
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Stray Kids
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ JYP ENTERTAINMENT' />
+        </div>
+    );
+}
