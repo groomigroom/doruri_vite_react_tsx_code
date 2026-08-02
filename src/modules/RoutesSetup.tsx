20c65e811136b1b8bea64bd9d66717f558624806
groomigroom <googjohn0143@gmail.com>
2026-08-02 16:03:37 +0900

    Update RoutesSetup.tsx

--- src/modules/RoutesSetup.tsx
+++ src/modules/RoutesSetup.tsx
@@ -22,6 +22,7 @@ import TriplesBabyFlowerCityRemixesAlbum from './triples/albums/TriplesBabyFlowe
 import TriplesBabyFlowerCityRemixesAlbumEng from './triplesEng/albums/TriplesBabyFlowerCityRemixesAlbumEng';
 import TriplesLoveAndPopPt1Album from './triples/albums/TriplesLoveAndPopPt1Album';
 import TriplesLoveAndPopPt1AlbumEng from './triplesEng/albums/TriplesLoveAndPopPt1AlbumEng';
+import TriplesDreamDressAlbum from './triples/albums/TriplesDreamDressAlbum';
 
 import TriplesMemS1 from './triples/members/TriplesMemS1';
 import TriplesMemS1Eng from './triplesEng/members/TriplesMemS1Eng';
@@ -528,6 +529,7 @@ export default function RoutesSetup() {
             <Route path='/TriplesBabyFlowerCityRemixesAlbumEng' element={<TriplesBabyFlowerCityRemixesAlbumEng />}></Route>
             <Route path='/TriplesLoveAndPopPt1Album' element={<TriplesLoveAndPopPt1Album />}></Route>
             <Route path='/TriplesLoveAndPopPt1AlbumEng' element={<TriplesLoveAndPopPt1AlbumEng />}></Route>
+            <Route path='/TriplesDreamDressAlbum' element={<TriplesDreamDressAlbum />}></Route>
 
             <Route path='/TriplesMemS1' element={<TriplesMemS1 />}></Route>
             <Route path='/TriplesMemS1Eng' element={<TriplesMemS1Eng />}></Route>
