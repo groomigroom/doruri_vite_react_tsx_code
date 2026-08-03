f3fe9a8812d4103f987e950180566cf02fc18d93
groomigroom <googjohn0143@gmail.com>
2026-08-03 16:38:13 +0900

    Update ResMain.tsx

--- src/modules/RESCENE/main/ResMain.tsx
+++ src/modules/RESCENE/main/ResMain.tsx
@@ -121,6 +121,41 @@ export default function ResMain() {
                 </section>
 
             </section >
+
+            <h2 className='tri_m_h2 rescene_m_h2_4'>▪ 이벤트 ▪</h2>
+            <section className='tri_m_event_tr_full rescene_m_event_tr_full'>
+                <section className='tri_m_event_tr'>
+                    <Link to='/ResceneEvent' className='tri_m_event_a'>
+                        <article className='tri_m_events'>
+                            <img src='' alt="rescene event" />
+                            <div>
+                                <h4>
+                                    album<br />
+                                    이벤트
+                                </h4>
+                            </div>
+                        </article>
+                    </Link>
+                </section>
+            </section>
+
+            <h2 className='tri_m_h2 rescene_m_h2_5'>▪ 최신 앨범 ▪</h2>
+            <section className='tri_m_album_tr_full rescene_m_album_tr_full'>
+                <section className='tri_m_album_tr'>
+                    <Link to='/Album' className='tri_m_album_a'>
+                        <article className='tri_m_albums'>
+                            <img src='' alt="Album" />
+                            <div>
+                                <h4>
+                                    album<br />
+                                    TRAGEDY
+                                </h4>
+                            </div>
+                        </article>
+                    </Link>
+                </section>
+            </section>
+
             <FooterPart FooterPartText='ⓒ 더뮤즈엔터테인먼트' />
         </div >
     );
