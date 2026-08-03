56f112575bd9327576ab8b1dcc65a77586c14766
groomigroom <googjohn0143@gmail.com>
2026-08-03 10:25:13 +0900

    Update BTOBMain.tsx

--- src/modules/BTOB/main/BTOBMain.tsx
+++ src/modules/BTOB/main/BTOBMain.tsx
@@ -6,16 +6,17 @@ import './BTOBMain.css';
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
 
-// import Tws_m_logo from './img/Tws_m_logo.svg';
+import Btob_m_logo from './img/BTOB_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-// import Tws_m_img from './img/tws_m_tws.jpg';
+import Btob_m_img from './img/Bto_m_img.jpg';
 
-// import Tws_m_MemSinyu_img from './img/tws_m_mem_Sinyu.jpg';
-// import Tws_m_MemDohun_img from './img/tws_m_mem_Dohun.jpg';
-// import Tws_m_MemYoungjae_img from './img/tws_m_mem_Youngjae.jpg';
-// import Tws_m_MemHanjin_img from './img/tws_m_mem_Hanjin.jpg';
-// import Tws_m_MemJihun_img from './img/tws_m_mem_Jihun.jpg';
-// import Tws_m_MemGyungmin_img from './img/tws_m_mem_Kyungmin.jpg';
+import Btob_m_SeoEunkwang_img from './img/Bto_mm_Seoeungwang_img.jpg';
+import Btob_m_LeeMinhyuk_img from './img/Bto_mm_Leeminhyuk_img.jpg';
+import Btob_m_LeeChangsub_img from './img/Bto_mm_Leechangsub_img.jpg';
+import Btob_m_LimHyunsik_img from './img/Bto_mm_Limhyunsik_img.jpg';
+import Btob_m_Peniel_img from './img/Bto_mm_Peniel_img.jpg';
+import Btob_m_YookSungjae_img from './img/Bto_mm_Yooksungjae_img.jpg';
+
 
 //#191919
 
@@ -27,19 +28,19 @@ export default function BTOBMain() {
                 <Link to='/BTOBMain' className="on">한국어</Link>
                 <Link to='/BTOBMainEng'>English</Link>
             </section>
-            <img src='' alt="비투비 로고" className='tri_m_logo' />
+            <img src={Btob_m_logo} alt="비투비 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="비투비 로고 꾸미기" />비투비<img src={MainH2Deco} alt="비투비 로고 꾸미기" /></h2>
             <p className='tri_m_intropp'>
                 &apos;비투비&apos;와 함께<br />
                 우리 다시<br />
                 꿈을 꿔요 같이!</p>
-            <img src='' className='tri_m_img' alt='비투비' />
+            <img src={Btob_m_img} className='tri_m_img' alt='비투비' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-            <section className='tri_m_mem_tr_full'>
+            <section className='tri_m_mem_tr_full bto_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/BtoMemSeoEunkwang' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="서은광 이미지" />
+                            <img src={Btob_m_SeoEunkwang_img} alt="서은광 이미지" />
                             <div>
                                 <h4>서은광</h4>
                             </div>
@@ -47,7 +48,7 @@ export default function BTOBMain() {
                     </Link>
                     <Link to='/BtoMemLeeMinhyuk' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="이민혁 이미지" />
+                            <img src={Btob_m_LeeMinhyuk_img} alt="이민혁 이미지" />
                             <div>
                                 <h4>이민혁</h4>
                             </div>
@@ -55,7 +56,7 @@ export default function BTOBMain() {
                     </Link>
                     <Link to='/BtoMemLeeChangsub' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="이창섭 이미지" />
+                            <img src={Btob_m_LeeChangsub_img} alt="이창섭 이미지" />
                             <div>
                                 <h4>이창섭</h4>
                             </div>
@@ -66,7 +67,7 @@ export default function BTOBMain() {
                 <section className='tri_m_mem_tr'>
                     <Link to='/BtoMemLimHyunsik' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="임현식 이미지" />
+                            <img src={Btob_m_LimHyunsik_img} alt="임현식 이미지" />
                             <div>
                                 <h4>임현식</h4>
                             </div>
@@ -74,7 +75,7 @@ export default function BTOBMain() {
                     </Link>
                     <Link to='/BtoMemPeniel' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="프니엘 이미지" />
+                            <img src={Btob_m_Peniel_img} alt="프니엘 이미지" />
                             <div>
                                 <h4>프니엘</h4>
                             </div>
@@ -82,7 +83,7 @@ export default function BTOBMain() {
                     </Link>
                     <Link to='/BtoMemYookSungjae' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="육성재 이미지" />
+                            <img src={Btob_m_YookSungjae_img} alt="육성재 이미지" />
                             <div>
                                 <h4>육성재</h4>
                             </div>
