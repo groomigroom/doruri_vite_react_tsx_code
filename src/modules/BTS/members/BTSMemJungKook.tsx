b5e9a221454aeb14e9ce609b6fdaa70cee652168
groomigroom <googjohn0143@gmail.com>
2026-08-03 12:16:47 +0900

    Update BTSMemJungKook.tsx

--- src/modules/BTS/members/BTSMemJungKook.tsx
+++ src/modules/BTS/members/BTSMemJungKook.tsx
@@ -6,7 +6,7 @@ import './BTSMemJungKook.css';
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
 
-//import Tri_mm_s1_img from '../main/img/tri_m_memS1.jpg';
+import Bts_Jungkook_m_img from '../main/img/Bts_mm_Jungkook_img.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -24,8 +24,8 @@ export default function BTSMemJungKook() {
                 더 나은 방향으로<br />
                 달려 가봐요!
             </p>
-            <img src='' alt="정국 이미지" className='tri_mm_s1_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <img src={Bts_Jungkook_m_img} alt="정국 이미지" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb bts_mm_jungkook_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
