1ad7c3e531cd023d18bce34c6dad57ec73309742
groomigroom <googjohn0143@gmail.com>
2026-08-03 10:40:31 +0900

    Update BtoMemSeoEunkwang.tsx

--- src/modules/BTOB/members/BtoMemSeoEunkwang.tsx
+++ src/modules/BTOB/members/BtoMemSeoEunkwang.tsx
@@ -6,7 +6,7 @@ import './BtoMemSeoEunkwang.css';
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
 
-// import Tri_mm_s1_img from '../main/img/tri_m_memS1.jpg';
+import Btob_m_SeoEunkwang_img from '../main/img/Bto_mm_Seoeungwang_img.jpg';
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 
@@ -24,9 +24,8 @@ export default function BtoMemSeoEunkwang() {
                 Greatest moment를<br />
                 찾아 봐요!
             </p>
-            {/* 이미지랑 테이블하기 */}
-            <img src='' alt="서은광 이미지" className='tri_mm_s1_img' />
-            <table className='tri_mm_s1_intropa_tb'>
+            <img src={Btob_m_SeoEunkwang_img} alt="서은광 이미지" className='tri_mm_s1_img' />
+            <table className='tri_mm_s1_intropa_tb bto_mm_seoeunkwang_intropa_tb'>
                 <tbody>
                     <tr className='tri_mm_s1_intropa_tb_tr1'>
                         <td><p>출생</p></td>
