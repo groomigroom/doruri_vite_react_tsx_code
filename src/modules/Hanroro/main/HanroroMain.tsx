094e187ea47c1db6d8e81e32efdcebde180682fd
groomigroom <googjohn0143@gmail.com>
2026-08-03 10:50:28 +0900

    Update HanroroMain.tsx

--- src/modules/Hanroro/main/HanroroMain.tsx
+++ src/modules/Hanroro/main/HanroroMain.tsx
@@ -6,9 +6,9 @@ import './HanroroMain.css';
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
 
-//import Han_m_logo from './img/TenCM_m_logo.svg';
+import Han_m_logo from './img/Hanroro_m_logo_191919.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-//import Ten_m_img from './img/10cm_m_mem_Gwonjungyeol.jpg';
+import Hanroro_m_img from './img/Hanroro_m_img.jpg';
 
 //color code
 
@@ -20,20 +20,20 @@ export default function HanroroMain() {
                 <Link to='/HanroroMain' className="on">한국어</Link>
                 <Link to='/HanroroMainEng'>English</Link>
             </section>
-            <img src='' alt="한로로 로고" className='tri_m_logo' />
+            <img src={Han_m_logo} alt="한로로 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="한로로 꾸미기" />한로로<img src={MainH2Deco} alt="한로로 꾸미기" /></h2>
             <p className='tri_m_intropp'>
                 &apos;한로로&apos;와 함께<br />
                 돌고도는 세계에서<br />
                 좋은 생각을 해보아요!
             </p>
-            <img src='' className='tri_m_img ten_m_img' alt='한로로' />
+            <img src={Hanroro_m_img} className='tri_m_img ten_m_img' alt='한로로' />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 소개 ▪</h2>
-            <section className='tri_m_mem_tr_full'>
+            <section className='tri_m_mem_tr_full han_m_mem_tr_full'>
                 <section className='tri_m_mem_tr'>
                     <Link to='/HanMemHanroro' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="한로로 이미지" />
+                            <img src={Hanroro_m_img} alt="한로로 이미지" />
                             <div>
                                 <h4>한로로</h4>
                             </div>
