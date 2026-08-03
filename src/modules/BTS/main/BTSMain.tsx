6c55dda03d1344418d4e3e8945e57f428d0285a7
groomigroom <googjohn0143@gmail.com>
2026-08-04 08:18:45 +0900

    Update BTSMain.tsx

--- src/modules/BTS/main/BTSMain.tsx
+++ src/modules/BTS/main/BTSMain.tsx
@@ -160,6 +160,42 @@ export default function BTSMain() {
                 </section>
 
             </section>
+
+            <h2 className='tri_m_h2 bts_m_h2_4'>▪ 이벤트 ▪</h2>
+            <section className='tri_m_event_tr_full bts_m_event_tr_full'>
+                <section className='tri_m_event_tr'>
+                    <Link to='/HanroroEvent' className='tri_m_event_a'>
+                        <article className='tri_m_events'>
+                            <img src='' alt="bts event" />
+                            <div>
+                                <h4>
+                                    bts<br />
+                                    이벤트
+                                </h4>
+                            </div>
+                        </article>
+                    </Link>
+                </section>
+            </section>
+
+            <h2 className='tri_m_h2 bts_m_h2_5'>▪ 최신 앨범 ▪</h2>
+            <section className='tri_m_album_tr_full bts_m_album_tr_full'>
+                <section className='tri_m_album_tr'>
+                    <Link to='/HanroroJpopRemakeVol2Album' className='tri_m_album_a'>
+                        <article className='tri_m_albums'>
+                            <img src='' alt="bts" />
+                            <div>
+                                <h4>
+                                    bts<br />
+                                    bts<br />
+                                    Vol2
+                                </h4>
+                            </div>
+                        </article>
+                    </Link>
+                </section>
+            </section>
+
             <FooterPart FooterPartText='ⓒ 빅히트 뮤직' />
         </div>
     );
