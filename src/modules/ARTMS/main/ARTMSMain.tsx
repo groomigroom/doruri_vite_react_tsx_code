412919e00adf215c5683c7291440a53317d5ffb1
groomigroom <googjohn0143@gmail.com>
2026-08-03 09:13:05 +0900

    Update ARTMSMain.tsx

--- src/modules/ARTMS/main/ARTMSMain.tsx
+++ src/modules/ARTMS/main/ARTMSMain.tsx
@@ -6,15 +6,16 @@ import './ARTMSMain.css';
 import Menu from '../../Menu';
 import FooterPart from '../../FooterPart';
 
-// import Res_m_logo from './img/Res_m_logo.svg';
+import Artms_m_logo from './img/ARTMS_m_logo.svg';
 import MainH2Deco from '../../img/main_h2_deco.png';
-// import Res_m_img from './img/rescene_m_rescene.jpg';
+import Artms_m_img from './img/Art_m_img.jpg';
 
-// import Res_m_MemWoni_img from './img/rescene_m_mem_Woni.jpg';
-// import Res_m_MemLiv_img from './img/rescene_m_mem_Liv.jpg';
-// import Res_m_MemMinami_img from './img/rescene_m_mem_Minami.jpg';
-// import Res_m_MemMay_img from './img/rescene_m_mem_May.jpg';
-// import Res_m_MemJena_img from './img/rescene_m_mem_Jena.jpg';
+import Artms_Heejin_m_img from './img/Art_mm_Heejin_img.jpg';
+import Artms_Haseul_m_img from './img/Art_mm_Haseul_img.jpg';
+import Artms_Kimlip_m_img from './img/Art_mm_Kimlip_img.jpg';
+import Artms_Jinsoul_m_img from './img/Art_mm_Jinsoul_img.jpg';
+import Artms_Choerry_m_img from './img/Art_mm_Choerry_img.jpg';
+
 
 //191919
 
@@ -26,20 +27,20 @@ export default function ARTMSMain() {
                 <Link to='/ARTMSMain' className="on">한국어</Link>
                 <Link to='/ARTMSMainEng'>English</Link>
             </section>
-            <img src='' alt="ARTMS 로고" className='tri_m_logo' />
+            <img src={Artms_m_logo} alt="ARTMS 로고" className='tri_m_logo' />
             <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="ARTMS 꾸미기" />ARTMS<img src={MainH2Deco} alt="ARTMS 꾸미기" /></h2>
             <p className='tri_m_intropp'>
                 &apos;ARTMS&apos;와 함께<br />
                 다시 한번 더<br />
                 Fly Away!
             </p>
-            <img src='' className='tri_m_img' alt="rescene" />
+            <img src={Artms_m_img} className='tri_m_img' alt="ARTMS" />
             <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
             <section className='tri_m_mem_tr_full art_m_mem_tr_full'>
                 <section className='art_m_mem_tr3'>
                     <Link to='/ArtMemHeejin' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="희진 이미지" />
+                            <img src={Artms_Heejin_m_img} alt="희진 이미지" />
                             <div>
                                 <h4>희진</h4>
                             </div>
@@ -47,7 +48,7 @@ export default function ARTMSMain() {
                     </Link>
                     <Link to='/ArtMemHaseul' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="하슬 이미지" />
+                            <img src={Artms_Haseul_m_img} alt="하슬 이미지" />
                             <div>
                                 <h4>하슬</h4>
                             </div>
@@ -55,7 +56,7 @@ export default function ARTMSMain() {
                     </Link>
                     <Link to='/ArtMemKimlip' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="김립 이미지" />
+                            <img src={Artms_Kimlip_m_img} alt="김립 이미지" />
                             <div>
                                 <h4>김립</h4>
                             </div>
@@ -66,7 +67,7 @@ export default function ARTMSMain() {
                 <section className='art_m_mem_tr2'>
                     <Link to='/ArtMemJinsoul' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="진솔 이미지" />
+                            <img src={Artms_Jinsoul_m_img} alt="진솔 이미지" />
                             <div>
                                 <h4>진솔</h4>
                             </div>
@@ -74,7 +75,7 @@ export default function ARTMSMain() {
                     </Link>
                     <Link to='/ArtMemChoerry' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src='' alt="최리 이미지" />
+                            <img src={Artms_Choerry_m_img} alt="최리 이미지" />
                             <div>
                                 <h4>최리</h4>
                             </div>
