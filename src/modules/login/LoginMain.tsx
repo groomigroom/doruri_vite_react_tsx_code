b4924a2488bff27c7ba5bb15aee4d662b693cca5
groomigroom <googjohn0143@gmail.com>
2026-08-04 17:24:20 +0900

    Update LoginMain.tsx

--- src/modules/login/LoginMain.tsx
+++ src/modules/login/LoginMain.tsx
@@ -1,11 +1,13 @@
 export default function LoginMain() {
-    <div className="login_main_full">
-        <form action="./login_anne" method="post" encType="multipart/form-data">
-            <input type="text" name="title" placeholder="텍스트 입력" />
-            <input type="text" name="content" />
-            <input type="text" name="id" />
-            <input type="password" name="password" />
-            <input type="submit" />
-        </form>
-    </div>
+    return (
+        <div className="login_main_full">
+            <form action="./login_anne" method="post" encType="multipart/form-data">
+                <input type="text" name="title" placeholder="텍스트 입력" />
+                <input type="text" name="content" />
+                <input type="text" name="id" />
+                <input type="password" name="password" />
+                <input type="submit" />
+            </form>
+        </div>
+    );
 }
