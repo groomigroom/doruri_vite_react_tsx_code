6d62ed5bf79e5dd36771c632ec342c665d86f02c
groomigroom <googjohn0143@gmail.com>
2026-08-06 07:16:48 +0900

    Update DoruriUpdateAugust5th2026.tsx

--- src/modules/updates/2026August/DoruriUpdateAugust5th2026.tsx
+++ src/modules/updates/2026August/DoruriUpdateAugust5th2026.tsx
@@ -21,14 +21,18 @@ export default function DoruriUpdateAugust5th2026() {
                     </tr>
                     <tr>
                         <td><p>1</p></td>
-                        <td><Link to='/ResMain'>RESCENE 메인 페이지에 최신 앨범 등록</Link></td>
+                        <td><Link to='/ResMain'>에픽하이 - PUMP COLLECTOR&apos;S EDITION 앨범 등록</Link></td>
                     </tr>
                     <tr>
                         <td><p>2</p></td>
-                        <td><Link to='/RescenePrettyGirlSpecialSingleAlbum'>Pretty Girl - Special Single 앨범 등록</Link></td>
+                        <td><Link to='/ResMain'>RESCENE 메인 페이지에 최신 앨범 등록</Link></td>
+                    </tr>
+                    <tr>
+                        <td><p>3</p></td>
+                        <td><Link to='/RescenePrettyGirlSpecialSingleAlbum'>RESCENE - Pretty Girl - Special Single 앨범 등록</Link></td>
                     </tr>
                     <tr className='doruri_update_updatePart_intropa_tb_tr9'>
-                        <td><p>3</p></td>
+                        <td><p>4</p></td>
                         <td><Link to='/DoruriUpdate'>업데이트 내역 등록 시작</Link></td>
                     </tr>
                 </tbody>
