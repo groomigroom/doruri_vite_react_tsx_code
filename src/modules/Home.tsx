5e26e6d09965715956bcc40c4efb3ad0f896f5da
groomigroom <googjohn0143@gmail.com>
2026-08-05 12:50:27 +0900

    Update Home.tsx

--- src/modules/Home.tsx
+++ src/modules/Home.tsx
@@ -16,7 +16,63 @@ export default function Home() {
             <img src="" alt="도루리 로고" className="home_logo" />
             <section className="home_img_slide_full"></section>
             <h2 className='home_h2 home_h2_1'>▪ 2026.08.05. 업데이트 내역 ▪</h2>
-
+            <table className='home_main_intropa_tb'>
+                <tbody>
+                    <tr className='home_main_intropa_tb_tr1'>
+                        <td className='home_main_intropa_tb_td1'>
+                            <p>번호</p>
+                        </td>
+                        <td className='home_main_intropa_tb_td2'>
+                            <p>내용</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='home_main_intropa_tb_td1'>
+                            <p>1</p>
+                        </td>
+                        <td className='home_main_intropa_tb_td2'>
+                            <Link to='/'>앨범</Link>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='home_main_intropa_tb_td1'>
+                            <p>2</p>
+                        </td>
+                        <td className='home_main_intropa_tb_td2'>
+                            <Link to='/'>x</Link>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='home_main_intropa_tb_td1'>
+                            <p>3</p>
+                        </td>
+                        <td className='home_main_intropa_tb_td2'>
+                            <Link to='/'>x</Link>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='home_main_intropa_tb_td1'>
+                            <p>4</p>
+                        </td>
+                        <td className='home_main_intropa_tb_td2'>
+                            <Link to='/'>x</Link>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className='home_main_intropa_tb_td1'>
+                            <p>5</p>
+                        </td>
+                        <td className='home_main_intropa_tb_td2'>
+                            <Link to='/'>x</Link>
+                        </td>
+                    </tr>
+                    <tr className='home_main_intropa_tb_tr6'>
+                        <td>
+                            <Link to='/'>더 보기</Link>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
             <FooterPart FooterPartText='ⓒ 도루리' />
         </div>
     )
