99c4ec43ee77977835e7bf8f429cb56264db857f
groomigroom <googjohn0143@gmail.com>
2026-08-05 12:08:12 +0900

    Update DoruriUpdate.tsx

--- src/modules/updates/DoruriUpdate.tsx
+++ src/modules/updates/DoruriUpdate.tsx
@@ -1,7 +1,14 @@
+import './DoruriUpdate.css';
+
+import Menu from "../Menu";
+
+import MainH2Deco from '../img/main_h2_deco.png';
+
 export default function DoruriUpdate() {
     return (
         <div className="doruri_update_full">
-            업데이트
+            <Menu />
+            <h2 className='doruri_update_m_h2'><img src={MainH2Deco} alt="로그인 꾸미기" />업데이트 내역<img src={MainH2Deco} alt="로그인 꾸미기" /></h2>
         </div>
     );
 }
