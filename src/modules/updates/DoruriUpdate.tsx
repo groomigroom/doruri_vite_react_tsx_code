df7072d19e669ca76b1aebf1321036d5c47a7be7
groomigroom <googjohn0143@gmail.com>
2026-08-05 12:26:02 +0900

    Update DoruriUpdate.tsx

--- src/modules/updates/DoruriUpdate.tsx
+++ src/modules/updates/DoruriUpdate.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import './DoruriUpdate.css';
 
 import Menu from "../Menu";
@@ -9,6 +11,112 @@ export default function DoruriUpdate() {
         <div className="doruri_update_full">
             <Menu />
             <h2 className='doruri_update_m_h2'><img src={MainH2Deco} alt="로그인 꾸미기" />업데이트 내역<img src={MainH2Deco} alt="로그인 꾸미기" /></h2>
+            <section className='doruri_update_train_full'>
+                <section className='doruri_update_train'>
+                    <Link to='/DoruriUpdate'>
+                        <h4>2026.<br />
+                            08.<br />
+                            05.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                </section>
+                <section className='doruri_update_train'>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                </section>
+                <section className='doruri_update_train'>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                </section>
+                <section className='doruri_update_train'>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                    <Link to='/DoruriUpdate'>
+                        <h4>xxxx.<br />
+                            xx.<br />
+                            xx.
+                        </h4>
+                    </Link>
+                </section>
+            </section>
         </div>
     );
 }
