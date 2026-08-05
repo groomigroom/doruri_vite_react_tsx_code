9e4138dcf8f55cbc9cd09f3127a82a036fa86ca5
groomigroom <googjohn0143@gmail.com>
2026-08-05 12:56:16 +0900

    Update DoruriUpdateAugust8th2026.tsx

--- src/modules/updates/2026August/DoruriUpdateAugust8th2026.tsx
+++ src/modules/updates/2026August/DoruriUpdateAugust8th2026.tsx
@@ -1,1 +1,10 @@
-DoruriUpdateAugust8th2026.tsx
+import Menu from "../../Menu";
+
+export default function DoruriUpdateAugust8th2026() {
+    return (
+        <div className="doruri_update_DoruriUpdateAugust8th2026_full">
+            <Menu />
+            <h2 className='doruri_update_m_h2'><img src={MainH2Deco} alt="로그인 꾸미기" />업데이트 내역<img src={MainH2Deco} alt="로그인 꾸미기" /></h2>
+        </div>
+    );
+}
