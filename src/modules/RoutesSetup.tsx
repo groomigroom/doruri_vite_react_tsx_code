793f232ff425d5eae2ddc2df458b0c8c41c7c5e5
groomigroom <googjohn0143@gmail.com>
2026-08-05 21:16:01 +0900

    Update RoutesSetup.tsx

--- src/modules/RoutesSetup.tsx
+++ src/modules/RoutesSetup.tsx
@@ -372,6 +372,7 @@ import EpikHighMainEng from './EpikHighEng/main/EpikHighMainEng';
 
 import EpikHighBruzHalipaOursVersionAlbum from './EpikHigh/albums/EpikHighBruzHalipaOursVersionAlbum';
 import EpikHighBruzHalipaOursVersionAlbumEng from './EpikHighEng/albums/EpikHighBruzHalipaOursVersionAlbumEng';
+import EpikHighPumpCollectorsEditionAlbum from './EpikHigh/albums/EpikHighPumpCollectorsEditionAlbum';
 
 import EpiMemMithra from './EpikHigh/members/EpiMemMithra';
 import EpiMemMithraEng from './EpikHighEng/members/EpiMemMithraEng';
@@ -893,6 +894,7 @@ export default function RoutesSetup() {
 
             <Route path='/EpikHighBruzHalipaOursVersionAlbum' element={<EpikHighBruzHalipaOursVersionAlbum />}></Route>
             <Route path='/EpikHighBruzHalipaOursVersionAlbumEng' element={<EpikHighBruzHalipaOursVersionAlbumEng />}></Route>
+            <Route path='/EpikHighPumpCollectorsEditionAlbum' element={<EpikHighPumpCollectorsEditionAlbum />}></Route>
 
             <Route path='/EpiMemMithra' element={<EpiMemMithra />}></Route>
             <Route path='/EpiMemMithraEng' element={<EpiMemMithraEng />}></Route>
