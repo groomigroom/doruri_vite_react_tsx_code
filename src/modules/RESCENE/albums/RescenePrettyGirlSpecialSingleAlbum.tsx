bd50b55ddbd011c5f36677ab25b22d960d189809
groomigroom <googjohn0143@gmail.com>
2026-08-05 10:50:54 +0900

    Update RescenePrettyGirlSpecialSingleAlbum.tsx

--- src/modules/RESCENE/albums/RescenePrettyGirlSpecialSingleAlbum.tsx
+++ src/modules/RESCENE/albums/RescenePrettyGirlSpecialSingleAlbum.tsx
@@ -68,7 +68,7 @@ export default function RescenePrettyGirlSpecialSingleAlbum() {
                 </tbody>
             </table>
             <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
-            <table className='tri_BabyFlowerCityRemixesAlbum_tb2'>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 res_RescenePrettyGirlSpecialSingleAlbum_tb2'>
                 <tbody>
                     <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
                         <td>
