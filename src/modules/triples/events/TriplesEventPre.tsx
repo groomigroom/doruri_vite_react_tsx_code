ca6eff30ef389646249eb544d0debca6aae2701c
groomigroom <googjohn0143@gmail.com>
2026-08-06 15:25:25 +0900

    Update TriplesEventPre.tsx

--- src/modules/triples/events/TriplesEventPre.tsx
+++ src/modules/triples/events/TriplesEventPre.tsx
@@ -12,19 +12,23 @@ interface Tri_e_popProp {
 export default function TriplesEventPre({ tri_e_OnCl, tri_e_result }: Tri_e_popProp) {
     return (
         <div className="tri_e_p_full">
-            <section className="tri_e_p_ticket">
-                <img src={BabyFlowerAllImg} alt="트리플에스" className="tri_e_p_members_img" />
-                <img src={TriplesEventPre_m_logo} alt="트리플에스 로고" className="tri_e_p_logo_img" />
-                {/* #####0층 0구역 0번 랜덤으로 되게 수정하기 */}
-                <p className="tri_e_p_pp"><span>tripleS Baby Flower World Tour</span><br />
-                    0층 0구역 0번<br />
-                    일시 : 0000년 00월 00일<br />
-                    장소 : 0000</p>
-            </section>
+            {tri_e_result === 24 ?
+                (< section className="tri_e_p_ticket">
+                    <img src={BabyFlowerAllImg} alt="트리플에스" className="tri_e_p_members_img" />
+                    <img src={TriplesEventPre_m_logo} alt="트리플에스 로고" className="tri_e_p_logo_img" />
+                    {/* #####0층 0구역 0번 랜덤으로 되게 수정하기 */}
+                    <p className="tri_e_p_pp"><span>tripleS Baby Flower World Tour</span><br />
+                        0층 0구역 0번<br />
+                        일시 : 0000년 00월 00일<br />
+                        장소 : 0000</p>
+                </section>) :
+                (<section className="tri_e_p_lose">
+                    아쉽지만 모두 정답이 아니에요.
+                </section>)}
             <section className="tri_e_p_pp">
                 <h2>정답 : {tri_e_result}개</h2>
             </section>
             <button onClick={tri_e_OnCl}>닫기</button>
-        </div>
+        </div >
     );
 };
