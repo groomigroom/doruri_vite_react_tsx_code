13aec8e30a9650a1016ed69fa1e21cf4dc16250d
groomigroom <googjohn0143@gmail.com>
2026-08-07 07:07:17 +0900

    Update ResceneRunawayAlbumEng.tsx

--- src/modules/RESCENEEng/albums/ResceneRunawayAlbumEng.tsx
+++ src/modules/RESCENEEng/albums/ResceneRunawayAlbumEng.tsx
@@ -1,1 +1,103 @@
-ResceneRunawayAlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../RESCENE/albums/ResceneRunawayAlbum.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+import ResceneRunawayAlbumEng_img from '../../RESCENE/albums/img/ResceneRunawayAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function ResceneRunawayAlbumEngEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className="res_ResceneRunawayAlbum_full">
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/ResceneRunawayAlbum'>한국어</Link>
+                <Link to='/ResceneRunawayAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'><img src={MainH2deco} alt="RESCENE deco" />Runaway<img src={MainH2deco} alt="RESCENE deco" /></h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                We prepared this album<br />
+                with the idea of moving<br />
+                forward on my own terms in mind!</p>
+            <img src={ResceneRunawayAlbumEng_img} alt="Runaway" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                April, 8, 2026
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                dance
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                THE MUZE ENTERTAINMENT
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ song list ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 res_ResceneRunawayAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Runaway
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ THE MUZE ENTERTAINMENT' />
+        </div>
+    );
+}
