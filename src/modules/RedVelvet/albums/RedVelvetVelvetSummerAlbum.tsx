699946b058718df5989be791d6602afeb39d8d70
groomigroom <googjohn0143@gmail.com>
2026-08-09 16:10:52 +0900

    Update RedVelvetVelvetSummerAlbum.tsx

--- src/modules/RedVelvet/albums/RedVelvetVelvetSummerAlbum.tsx
+++ src/modules/RedVelvet/albums/RedVelvetVelvetSummerAlbum.tsx
@@ -1,1 +1,150 @@
-RedVelvetVelvetSummerAlbum.tsxRedVelvetVelvetSummerAlbum.tsx
+import { Link } from 'react-router-dom';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import './RedVelvetVelvetSummerAlbum.css';
+
+import Menu from '../../Menu';
+import FooterPart from '../../FooterPart';
+
+// import TriplesBabyFlowerCityRemixesAlbum_img from './img/TriplesBabyFlowerCityRemixesAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function RedVelvetVelvetSummerAlbum() {
+    return (
+        <div className="redvelvet_RedVelvetVelvetSummerAlbum_full">
+            <Menu />
+            <section className="home_translate">
+                <Link to='/RedVelvetVelvetSummerAlbum' className="on">한국어</Link>
+                <Link to='/RedVelvetVelvetSummerAlbumEng'>English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="Red Velvet" />Velvet Summer - Summer Mini Album<img src={MainH2deco} alt="Red Velvet" /></h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                다채로운<br />
+                곡들로 준비한<br />
+                앨범이에요!
+            </p>
+            <img src='' alt="RedVelvetVelvetSummerAlbum" className='tri_BabyFlowerCityRemixesAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                발매일
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                2026.08.03.
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                장르
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                댄스, R&B/Soul
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                기획사
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                SM엔터테인먼트
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ 곡 목록 ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Surfin&apos; Boy
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                2
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Hot Girls Cold Vibe
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                3
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Orchestra
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                4
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Hula Hoop
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                5
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                Hawaii
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPart FooterPartText='ⓒ SM엔터테인먼트' />
+        </div>
+    );
+}
