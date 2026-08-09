a8c09be639be5c0d682d7d841d13e2d467ff551b
groomigroom <googjohn0143@gmail.com>
2026-08-09 10:07:04 +0900

    Update BtobWeAgainAlbumEng.tsx

--- src/modules/BTOBEng/albums/BtobWeAgainAlbumEng.tsx
+++ src/modules/BTOBEng/albums/BtobWeAgainAlbumEng.tsx
@@ -1,1 +1,118 @@
-BtobWeAgainAlbumEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
+import '../../BTOB/albums/BtobWeAgainAlbum.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+import BtobWeAgainAlbum_img from '../../BTOB/albums/img/BtobWeAgainAlbum_img.jpg';
+
+import MainH2deco from '../../img/main_h2_deco.png';
+
+export default function BtobWeAgainAlbumEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='btob_BtobWeAgainAlbum_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/BtobWeAgainAlbum'>한국어</Link>
+                <Link to='/BtobWeAgainAlbumEng' className="on">English</Link>
+            </section>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_1'>
+                <img src={MainH2deco} alt="btob" />We again<img src={MainH2deco} alt="btob" />
+            </h2>
+            <p className='tri_BabyFlowerCityRemixesAlbum_intropp'>
+                This is an album prepared<br />
+                with all my heartfelt<br />
+                sincerity in it!
+            </p>
+            <img src={BtobWeAgainAlbum_img} alt="BtobWeAgainAlbum" className='tri_BabyFlowerCityRemixesAlbum_img ten_TenCMSliverOfPossibilityAlbum_img' />
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb1'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr1'>
+                        <td>
+                            <p>
+                                release date
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                March, 21, 2026
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                genre
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                ballad
+                            </p>
+                        </td>
+                    </tr>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb1_tr3'>
+                        <td>
+                            <p>
+                                agency
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                BTOB Company
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <h2 className='tri_BabyFlowerCityRemixesAlbum_h2 tri_BabyFlowerCityRemixesAlbum_h2_2'>▪ song list ▪</h2>
+            <table className='tri_BabyFlowerCityRemixesAlbum_tb2 btob_BtobWeAgainAlbum_tb2'>
+                <tbody>
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr1'>
+                        <td>
+                            <p>
+                                number
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                title
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>
+                                1
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                We again
+                            </p>
+                        </td>
+                    </tr>
+
+                    <tr className='tri_BabyFlowerCityRemixesAlbum_intropa_tb2_tr9'>
+                        <td>
+                            <p>
+                                2
+                            </p>
+                        </td>
+                        <td>
+                            <p>
+                                We again &#40;Inst.&#41;
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <FooterPartEng FooterPartEngText='ⓒ BTOB Company' />
+        </div>
+    );
+}
