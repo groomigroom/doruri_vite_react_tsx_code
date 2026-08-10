1c85e7c6d5ce5c69be0069dda78b0469f3ca85fa
groomigroom <googjohn0143@gmail.com>
2026-08-11 08:29:41 +0900

    Update AespaSYNKaeXISLINE2026SpecialDigitalSingleAlbum.tsx

--- src/modules/aespa/albums/AespaSYNKaeXISLINE2026SpecialDigitalSingleAlbum.tsx
+++ src/modules/aespa/albums/AespaSYNKaeXISLINE2026SpecialDigitalSingleAlbum.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
 import './AespaSynkAexisLineAlbum.css';
@@ -11,6 +12,8 @@ import FooterPart from '../../FooterPart';
 import MainH2deco from '../../img/main_h2_deco.png';
 
 export default function AespaSYNKaeXISLINE2026SpecialDigitalSingleAlbum() {
+    DoruriDocumentTitle('SYNK : aeXIS LINE');
+
     return (
         <div className="aes_AespaSYNKaeXISLINE2026SpecialDigitalSingleAlbum_full">
             <Menu />
