e29fa9557638fad4daf4d2d600cbcf2a18e574fb
groomigroom <googjohn0143@gmail.com>
2026-08-10 15:54:31 +0900

    Update IoiMemJugyulgyoung.tsx

--- src/modules/ioi/members/IoiMemJugyulgyoung.tsx
+++ src/modules/ioi/members/IoiMemJugyulgyoung.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './IoiMemJugyulgyoung.css';
@@ -11,6 +12,8 @@ import Ioi_m_MemJugyulgyoung_img from '../main/img/ioi_m_mem_Jugyulgyung.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function IoiMemJugyulgyoung() {
+    DoruriDocumentTitle('주결경');
+
     return (
         <div className="ioi_mm_jugyulgyoung_full">
             <Menu />
@@ -18,7 +21,9 @@ export default function IoiMemJugyulgyoung() {
                 <Link to='/IoiMemJugyulgyoung' className="on">한국어</Link>
                 <Link to='/IoiMemJugyulgyoungEng'>English</Link>
             </section>
-            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />주결경<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" /></h2>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'>
+                <img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />주결경<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />
+            </h2>
             <p className='tri_mm_s1_intropp'>
                 포기하지 않고<br />
                 열심히 노력하며<br />
