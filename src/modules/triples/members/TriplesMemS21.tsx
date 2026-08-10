d1122e6dbab946107a437b4ba41f3dfb672588ca
groomigroom <googjohn0143@gmail.com>
2026-08-10 10:38:23 +0900

    Add document title to TriplesMemS21 component
    Added document title setting for TriplesMemS21 component.

--- src/modules/triples/members/TriplesMemS21.tsx
+++ src/modules/triples/members/TriplesMemS21.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './TriplesMemS1.css';
 import './TriplesMemS21.css';
@@ -11,6 +12,8 @@ import Tri_mm_s21_img from '../main/img/tri_m_memS21.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS21() {
+    DoruriDocumentTitle('김채원');
+
     return (
         <div className="tri_mm_s21_full">
             <Menu />
