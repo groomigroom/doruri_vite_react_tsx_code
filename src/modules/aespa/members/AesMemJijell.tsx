8bf55158082fecf0959733f303d92fa7f7f38903
groomigroom <googjohn0143@gmail.com>
2026-08-11 08:35:04 +0900

    Update AesMemJijell.tsx

--- src/modules/aespa/members/AesMemJijell.tsx
+++ src/modules/aespa/members/AesMemJijell.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './AesMemJijell.css';
@@ -11,6 +12,8 @@ import Aes_mm_Jijell_img from '../main/img/aespa_m_mem_Jijel.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function AesMemJijell() {
+    DoruriDocumentTitle('지젤');
+
     return (
         <div className="aes_mm_jijell_full">
             <Menu />
