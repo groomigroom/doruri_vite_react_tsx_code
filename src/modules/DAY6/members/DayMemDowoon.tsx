560b9a0ddb360fd5f447cb281e19738f26450967
groomigroom <googjohn0143@gmail.com>
2026-08-11 08:55:12 +0900

    Update DayMemDowoon.tsx

--- src/modules/DAY6/members/DayMemDowoon.tsx
+++ src/modules/DAY6/members/DayMemDowoon.tsx
@@ -1,3 +1,5 @@
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
+
 import '../../triples/members/TriplesMemS1.css';
 import './DayMemDowoon.css';
 
@@ -9,6 +11,8 @@ import Day_mm_dowoon_img from '../main/img/day6_m_mem_Dowoon.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function DayMemDowoon() {
+    DoruriDocumentTitle('도운');
+
     return (
         <div className="day_mm_dowoon_full">
             <Menu />
