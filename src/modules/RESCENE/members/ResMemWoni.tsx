e5a68bda375e9134db91b162e5fd7588aabe98e5
groomigroom <googjohn0143@gmail.com>
2026-08-11 07:42:39 +0900

    Update ResMemWoni.tsx

--- src/modules/RESCENE/members/ResMemWoni.tsx
+++ src/modules/RESCENE/members/ResMemWoni.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './ResMemWoni.css';
@@ -11,6 +12,8 @@ import res_mm_woni_img from '../main/img/rescene_m_mem_Woni.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function ResMemWoni() {
+    DoruriDocumentTitle('원이');
+
     return (
         <div className="res_mm_woni_full">
             <Menu />
