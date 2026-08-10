0048472bd2d14085cab39630ddf3ea75ca6da12c
groomigroom <googjohn0143@gmail.com>
2026-08-10 10:27:11 +0900

    Update TriplesMemS14.tsx

--- src/modules/triples/members/TriplesMemS14.tsx
+++ src/modules/triples/members/TriplesMemS14.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './TriplesMemS1.css';
 import './TriplesMemS14.css';
@@ -11,6 +12,8 @@ import Tri_mm_s14_img from '../main/img/tri_m_memS14.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS14() {
+    DoruriDocumentTitle('박소현');
+
     return (
         <div className="tri_mm_s14_full">
             <Menu />
