8e96fffdd8f4ea59dcf385e2b02f299d031b2559
groomigroom <googjohn0143@gmail.com>
2026-08-10 15:53:32 +0900

    Update IoiMemJeonsomi.tsx

--- src/modules/ioi/members/IoiMemJeonsomi.tsx
+++ src/modules/ioi/members/IoiMemJeonsomi.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './IoiMemJeonsomi.css';
@@ -11,6 +12,8 @@ import Ioi_m_MemJeonsomi_img from '../main/img/ioi_m_mem_Jeonsomi.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function IoiMemJeonsomi() {
+    DoruriDocumentTitle('전소미');
+
     return (
         <div className="ioi_mm_jeonsomi_full">
             <Menu />
@@ -18,7 +21,9 @@ export default function IoiMemJeonsomi() {
                 <Link to='/IoiMemJeonsomi' className="on">한국어</Link>
                 <Link to='/IoiMemJeonsomiEng'>English</Link>
             </section>
-            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />전소미<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" /></h2>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'>
+                <img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />전소미<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />
+            </h2>
             <p className='tri_mm_s1_intropp'>
                 무너지지 않는<br />
                 멘탈을 지켜서<br />
