19b8f6677c434dfb217b43541a635570d6f1c38d
groomigroom <googjohn0143@gmail.com>
2026-08-11 07:27:31 +0900

    Update TwsMemSinyu.tsx

--- src/modules/tws/members/TwsMemSinyu.tsx
+++ src/modules/tws/members/TwsMemSinyu.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './TwsMemSinyu.css';
@@ -11,6 +12,8 @@ import Tws_mm_sinyu_img from '../main/img/tws_m_mem_Sinyu.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TwsMemSinyu() {
+    DoruriDocumentTitle('신유');
+
     return (
         <div className="tws_mm_sinyu_full">
             <Menu />
