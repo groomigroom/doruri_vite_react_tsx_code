a442d61fba74d07f11a66ff4afae825ad6d21121
groomigroom <googjohn0143@gmail.com>
2026-08-10 10:35:25 +0900

    Update TriplesMemS19.tsx

--- src/modules/triples/members/TriplesMemS19.tsx
+++ src/modules/triples/members/TriplesMemS19.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './TriplesMemS1.css';
 import './TriplesMemS19.css';
@@ -11,6 +12,8 @@ import Tri_mm_s19_img from '../main/img/tri_m_memS19.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS19() {
+    DoruriDocumentTitle('정하연');
+
     return (
         <div className="tri_mm_s19_full">
             <Menu />
@@ -70,20 +73,3 @@ export default function TriplesMemS19() {
         </div>
     );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
