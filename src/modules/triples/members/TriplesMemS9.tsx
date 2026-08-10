cbba06ca13a9f10f2537e4548ce1c1b2718dc643
groomigroom <googjohn0143@gmail.com>
2026-08-10 09:59:39 +0900

    Update TriplesMemS9.tsx

--- src/modules/triples/members/TriplesMemS9.tsx
+++ src/modules/triples/members/TriplesMemS9.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './TriplesMemS1.css';
 import './TriplesMemS9.css';
@@ -11,6 +12,8 @@ import Tri_mm_s9_img from '../main/img/tri_m_memS9.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS9() {
+    DoruriDocumentTitle('카에데');
+
     return (
         <div className="tri_mm_s9_full">
             <Menu />
