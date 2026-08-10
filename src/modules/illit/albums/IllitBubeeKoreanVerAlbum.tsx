67e9b9a4c4a063e1e99dae5dc65ddec53375de2c
groomigroom <googjohn0143@gmail.com>
2026-08-11 07:08:32 +0900

    Update IllitBubeeKoreanVerAlbum.tsx

--- src/modules/illit/albums/IllitBubeeKoreanVerAlbum.tsx
+++ src/modules/illit/albums/IllitBubeeKoreanVerAlbum.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
 import './IllitBubeeKoreanVerAlbum.css';
@@ -11,6 +12,8 @@ import FooterPart from '../../FooterPart';
 import MainH2deco from '../../img/main_h2_deco.png';
 
 export default function IllitBubeeKoreanVerAlbum() {
+    DoruriDocumentTitle('Bubee');
+
     return (
         <div className='ill_IllitBubeeKoreanVerAlbum_full'>
             <Menu />
