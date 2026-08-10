84f8be63dae07d673cef731cae1d7d915c92a8d7
groomigroom <googjohn0143@gmail.com>
2026-08-11 08:36:24 +0900

    Update AesMemWinter.tsx

--- src/modules/aespa/members/AesMemWinter.tsx
+++ src/modules/aespa/members/AesMemWinter.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './AesMemNingning.css';
@@ -11,6 +12,8 @@ import Aes_mm_Winter_img from '../main/img/aespa_m_mem_Winter.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function AesMemWinter() {
+    DoruriDocumentTitle('윈터');
+
     return (
         <div className="aes_mm_winter_full">
             <Menu />
