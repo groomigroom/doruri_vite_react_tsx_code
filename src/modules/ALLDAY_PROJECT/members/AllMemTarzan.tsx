148aea2fd29b3fc50b0fa8d9e27d37db999df169
groomigroom <googjohn0143@gmail.com>
2026-08-11 08:47:23 +0900

    Update AllMemTarzan.tsx

--- src/modules/ALLDAY_PROJECT/members/AllMemTarzan.tsx
+++ src/modules/ALLDAY_PROJECT/members/AllMemTarzan.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './AllMemTarzan.css';
@@ -11,6 +12,8 @@ import All_mm_Tarzan_img from '../main/img/allday_m_mem_Tarzan.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function AllMemTarzan() {
+    DoruriDocumentTitle('타잔');
+
     return (
         <div className="all_mm_tarzan_full">
             <Menu />
