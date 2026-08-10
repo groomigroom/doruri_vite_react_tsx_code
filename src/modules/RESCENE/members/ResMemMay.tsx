feed49e61d83b1a9ef27653b9d3c7a793b08eba0
groomigroom <googjohn0143@gmail.com>
2026-08-11 07:41:48 +0900

    Update ResMemMay.tsx

--- src/modules/RESCENE/members/ResMemMay.tsx
+++ src/modules/RESCENE/members/ResMemMay.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './ResMemMay.css';
@@ -11,6 +12,8 @@ import res_mm_may_img from '../main/img/rescene_m_mem_May.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function ResMemMay() {
+    DoruriDocumentTitle('메이');
+
     return (
         <div className="res_mm_may_full">
             <Menu />
