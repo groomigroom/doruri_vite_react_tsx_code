81b50999f1ded0efd3531ded29fab299c4517c41
groomigroom <googjohn0143@gmail.com>
2026-08-11 08:50:09 +0900

    Update AllMemYoungseo.tsx

--- src/modules/ALLDAY_PROJECT/members/AllMemYoungseo.tsx
+++ src/modules/ALLDAY_PROJECT/members/AllMemYoungseo.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './AllMemYoungseo.css';
@@ -11,6 +12,8 @@ import All_mm_youngseo_img from '../main/img/allday_m_mem_Youngseo.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function AllMemYoungseo() {
+    DoruriDocumentTitle('영서');
+
     return (
         <div className="all_mm_youngseo_full">
             <Menu />
