0e6a97e40c785f9fb8679eb215a0d840ce22a94e
groomigroom <googjohn0143@gmail.com>
2026-08-10 10:42:28 +0900

    Update TriplesMemS23.tsx

--- src/modules/triples/members/TriplesMemS23.tsx
+++ src/modules/triples/members/TriplesMemS23.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './TriplesMemS1.css';
 import './TriplesMemS23.css';
@@ -11,6 +12,8 @@ import Tri_mm_s23_img from '../main/img/tri_m_memS23.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS23() {
+    DoruriDocumentTitle('서아');
+
     return (
         <div className="tri_mm_s23_full">
             <Menu />
@@ -70,20 +73,3 @@ export default function TriplesMemS23() {
         </div>
     );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
