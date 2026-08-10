ffa1c6d6043aea57ade99ef0291e0ef812aeaf6f
groomigroom <googjohn0143@gmail.com>
2026-08-10 10:30:29 +0900

    Update TriplesMemS16.tsx

--- src/modules/triples/members/TriplesMemS16.tsx
+++ src/modules/triples/members/TriplesMemS16.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './TriplesMemS1.css';
 import './TriplesMemS16.css';
@@ -11,6 +12,8 @@ import Tri_mm_s16_img from '../main/img/tri_m_memS16.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function TriplesMemS16() {
+    DoruriDocumentTitle('마유');
+
     return (
         <div className="tri_mm_s16_full">
             <Menu />
@@ -70,20 +73,3 @@ export default function TriplesMemS16() {
         </div>
     );
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
