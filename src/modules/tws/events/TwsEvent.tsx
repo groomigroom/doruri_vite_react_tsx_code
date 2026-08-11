5f1d8ddf973362c8a49a20f43bcc30232c3a3185
groomigroom <googjohn0143@gmail.com>
2026-08-11 20:46:42 +0900

    Update TwsEvent.tsx

--- src/modules/tws/events/TwsEvent.tsx
+++ src/modules/tws/events/TwsEvent.tsx
@@ -1,1 +1,9 @@
-TwsEvent.tsx
+import './TwsEvent.css';
+
+export default function TwsEvent() {
+    return (
+        <div className="twsEvent_full">
+            <div className="tws_e_moving_object"></div>
+        </div>
+    );
+}
