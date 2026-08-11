3db2fe2112615d8f87bc305b88a8aca25db33992
groomigroom <googjohn0143@gmail.com>
2026-08-11 10:28:00 +0900

    Update EpikHighPumpCollectorsEditionAlbum.tsx

--- src/modules/EpikHigh/albums/EpikHighPumpCollectorsEditionAlbum.tsx
+++ src/modules/EpikHigh/albums/EpikHighPumpCollectorsEditionAlbum.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/albums/TriplesBabyFlowerCityRemixesAlbum.css';
 import './EpikHighPumpCollectorsEditionAlbum.css';
@@ -11,6 +12,8 @@ import EpikHighPumpCollectorsEditionAlbum_img from './img/EpikHighPumpCollectors
 import MainH2deco from '../../img/main_h2_deco.png';
 
 export default function EpikHighPumpCollectorsEditionAlbum() {
+    DoruriDocumentTitle('PUMP COLLECTOR\'S EDITION');
+
     return (
         <div className="epi_EpikHighPumpCollectorsEditionAlbum_full">
             <Menu />
