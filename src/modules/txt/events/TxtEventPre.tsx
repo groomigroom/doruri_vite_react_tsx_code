322dece36fb5b94dcc8a227b5621bcfdd3c0364c
groomigroom <googjohn0143@gmail.com>
2026-08-11 12:19:21 +0900

    Update TxtEventPre.tsx

--- src/modules/txt/events/TxtEventPre.tsx
+++ src/modules/txt/events/TxtEventPre.tsx
@@ -1,1 +1,18 @@
-TxtEventPre.tsx
+interface Txt_e_popProp {
+    txt_e_OnCl: () => void;
+    txt_e_result: string;
+}
+
+export default function TxtEventPre({ txt_e_OnCl, txt_e_result }: Txt_e_popProp) {
+    return (
+        <div className="txt_e_p_full">
+            {txt_e_result === "맞춘 개수: 4개" ?
+                (<section>당첨</section>) :
+                (<section>당첨 아님</section>)
+            }
+            <h2>{txt_e_result}</h2>
+            <button onClick={txt_e_OnCl}>닫기</button>
+        </div>
+    );
+
+}
