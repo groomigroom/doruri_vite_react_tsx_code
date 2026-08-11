ae0872bec557b110250a0a3c58232e58f47843cd
groomigroom <googjohn0143@gmail.com>
2026-08-11 10:49:58 +0900

    Update Idn_m_memid1.tsx

--- src/modules/idntt/members/Idn_m_memid1.tsx
+++ src/modules/idntt/members/Idn_m_memid1.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './Idn_m_memid1.css';
@@ -11,6 +12,8 @@ import Idn_mm_memid1_img from '../main/img/Idn_mm_id1_img.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function Idn_m_memid1() {
+    DoruriDocumentTitle('김도훈');
+
     return (
         <div className="idn_mm_id1_full">
             <Menu />
