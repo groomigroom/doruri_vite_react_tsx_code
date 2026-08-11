cb66dfcfeba61c588efe21f3ab67a5bc8e84c659
groomigroom <googjohn0143@gmail.com>
2026-08-12 08:57:20 +0900

    Update IllitEvent.tsx

--- src/modules/illit/events/IllitEvent.tsx
+++ src/modules/illit/events/IllitEvent.tsx
@@ -1,1 +1,30 @@
-IllitEvent.tsx
+export default function IllitEvent() {
+    return (
+        <div className="IllitEvent_full">
+            <button>a</button>
+            <button>b</button>
+            <button>c</button>
+            <button>d</button>
+            <button>i</button>
+
+            <button>g</button>
+            <button>h</button>
+            <button>t</button>
+            <button>j</button>
+            <button>k</button>
+
+            <button>l</button>
+            <button>m</button>
+            <button>n</button>
+            <button>s</button>
+            <button>o</button>
+
+            <button>x</button>
+            <button>m</button>
+            <button>y</button>
+            <button>e</button>
+            <button>z</button>
+
+        </div>
+    );
+}
