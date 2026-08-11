06040bee7e1424d27a91547bc84312c21d6f52a3
groomigroom <googjohn0143@gmail.com>
2026-08-11 13:32:08 +0900

    Update Idn_m_memid16.tsx

--- src/modules/idntt/members/Idn_m_memid16.tsx
+++ src/modules/idntt/members/Idn_m_memid16.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './Idn_m_memid16.css';
 
@@ -10,6 +11,8 @@ import Idn_mm_memid16_img from '../main/img/Idn_mm_id16_img.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function Idn_m_memid16() {
+    DoruriDocumentTitle('황은수');
+
     return (
         <div className="idn_mm_id16_full">
             <Menu />
