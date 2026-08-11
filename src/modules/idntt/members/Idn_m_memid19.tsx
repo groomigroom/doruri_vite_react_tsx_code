b4526546f3bf360c27445ae361fb502a87c98e56
groomigroom <googjohn0143@gmail.com>
2026-08-11 13:38:36 +0900

    Update Idn_m_memid19.tsx

--- src/modules/idntt/members/Idn_m_memid19.tsx
+++ src/modules/idntt/members/Idn_m_memid19.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './Idn_m_memid19.css';
 
@@ -10,6 +11,8 @@ import Idn_mm_memid19_img from '../main/img/Idn_mm_id19_img.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function Idn_m_memid19() {
+    DoruriDocumentTitle('양경호');
+
     return (
         <div className="idn_mm_id19_full">
             <Menu />
