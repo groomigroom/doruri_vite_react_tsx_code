1592cc14dd36080ec66c2c9f1942e4084bcbe0bd
groomigroom <googjohn0143@gmail.com>
2026-08-12 08:14:46 +0900

    Update TwsEvent.tsx

--- src/modules/tws/events/TwsEvent.tsx
+++ src/modules/tws/events/TwsEvent.tsx
@@ -1,9 +1,20 @@
+import { useState } from 'react';
+
 import './TwsEvent.css';
 
 export default function TwsEvent() {
+    const [tws_e_result, setTws_e_result] = useState<boolean>(false);
+
+    const OpenTws_e_result = () => setTws_e_result(true);
+
     return (
         <div className="twsEvent_full">
-            <div className="tws_e_moving_object"></div>
+            <div className="tws_e_moving_object" onClick={OpenTws_e_result}></div>
+            {tws_e_result && (
+                <section>
+                    <p>당첨</p>
+                </section>
+            )}
         </div>
     );
 }
