92a4fae719c44e1cb7f05ef25815abb58a368f7e
groomigroom <googjohn0143@gmail.com>
2026-08-11 16:02:01 +0900

    Set document title for BTS member Jin
    Added document title setting for BTS member Jin.

--- src/modules/BTS/members/BTSMemJin.tsx
+++ src/modules/BTS/members/BTSMemJin.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './BTSMemJin.css';
@@ -11,6 +12,8 @@ import Bts_Jin_m_img from '../main/img/Bts_mm_Jin_img.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function BTSMemJin() {
+    DoruriDocumentTitle('진');
+
     return (
         <div className="bts_mm_jin_full">
             <Menu />
@@ -18,7 +21,9 @@ export default function BTSMemJin() {
                 <Link to='/BTSMemJin' className="on">한국어</Link>
                 <Link to='/BTSMemJinEng'>English</Link>
             </section>
-            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'><img src={MainH2Deco} alt="방탄소년단 꾸미기" />진<img src={MainH2Deco} alt="방탄소년단 꾸미기" /></h2>
+            <h2 className='tri_mm_s1_h2 tri_mm_s1_h2_1'>
+                <img src={MainH2Deco} alt="방탄소년단 꾸미기" />진<img src={MainH2Deco} alt="방탄소년단 꾸미기" />
+            </h2>
             <p className='tri_mm_s1_intropp'>
                 &apos;진&apos;과 함께<br />
                 어딘가로 열심히<br />
