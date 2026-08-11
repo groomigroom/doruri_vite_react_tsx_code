cbb2f4646cc935a873f80710c641c9e303db88e2
groomigroom <googjohn0143@gmail.com>
2026-08-11 13:54:00 +0900

    Update ArtMemHeejin.tsx

--- src/modules/ARTMS/members/ArtMemHeejin.tsx
+++ src/modules/ARTMS/members/ArtMemHeejin.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './ArtMemHeejin.css';
@@ -11,6 +12,8 @@ import Art_mm_Heejin_img from '../main/img/Art_mm_Heejin_img.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function ArtMemHeejin() {
+    DoruriDocumentTitle('희진');
+
     return (
         <div className="art_mm_heejin_full">
             <Menu />
