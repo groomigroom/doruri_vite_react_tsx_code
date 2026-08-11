f643fc0298732e16ce8899c2cffef648371cfd27
groomigroom <googjohn0143@gmail.com>
2026-08-11 10:56:01 +0900

    Update Idn_m_memid8.tsx

--- src/modules/idntt/members/Idn_m_memid8.tsx
+++ src/modules/idntt/members/Idn_m_memid8.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import '../../triples/members/TriplesMemS1.css';
 import './Idn_m_memid8.css';
@@ -11,6 +12,8 @@ import Idn_mm_memid8_img from '../main/img/Idn_mm_id8_img.jpg';
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function Idn_m_memid8() {
+    DoruriDocumentTitle('이환희');
+
     return (
         <div className="idn_mm_id8_full">
             <Menu />
