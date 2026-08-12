cc8e343fd75782b319cb21f4da378065caab4e78
groomigroom <googjohn0143@gmail.com>
2026-08-12 10:13:09 +0900

    Update IllitEvent.tsx

--- src/modules/illit/events/IllitEvent.tsx
+++ src/modules/illit/events/IllitEvent.tsx
@@ -1,30 +1,87 @@
+import { useState } from 'react';
+import './IllitEvent.css';
+
 export default function IllitEvent() {
+    const [Illit_e_letter_i_result, setIllit_e_letter_i_result] = useState<boolean>(false);
+
+    const OpenIllit_e_letter_i_result = () => setIllit_e_letter_i_result(true);
+
+    const [Illit_e_letter_t_result, setIllit_e_letter_t_result] = useState<boolean>(false);
+
+    const OpenIllit_e_letter_t_result = () => setIllit_e_letter_t_result(true);
+
+    const [Illit_e_letter_s_result, setIllit_e_letter_s_result] = useState<boolean>(false);
+
+    const OpenIllit_e_letter_s_result = () => setIllit_e_letter_s_result(true);
+
+    const [Illit_e_letter_m_result, setIllit_e_letter_m_result] = useState<boolean>(false);
+
+    const OpenIllit_e_letter_m_result = () => setIllit_e_letter_m_result(true);
+
+    const [Illit_e_letter_e_result, setIllit_e_letter_e_result] = useState<boolean>(false);
+
+    const OpenIllit_e_letter_e_result = () => setIllit_e_letter_e_result(true);
+
+    const Illit_e_letter_all_checked = Illit_e_letter_i_result &&
+        Illit_e_letter_t_result &&
+        Illit_e_letter_s_result &&
+        Illit_e_letter_m_result &&
+        Illit_e_letter_e_result;
+
     return (
         <div className="IllitEvent_full">
             <button>a</button>
             <button>b</button>
             <button>c</button>
             <button>d</button>
-            <button>i</button>
+            <button onClick={OpenIllit_e_letter_i_result}>i</button>
 
             <button>g</button>
             <button>h</button>
-            <button>t</button>
+            <button onClick={OpenIllit_e_letter_t_result}>t</button>
             <button>j</button>
             <button>k</button>
 
             <button>l</button>
             <button>m</button>
             <button>n</button>
-            <button>s</button>
+            <button onClick={OpenIllit_e_letter_s_result}>s</button>
             <button>o</button>
 
             <button>x</button>
-            <button>m</button>
+            <button onClick={OpenIllit_e_letter_m_result}>m</button>
             <button>y</button>
-            <button>e</button>
+            <button onClick={OpenIllit_e_letter_e_result}>e</button>
             <button>z</button>
 
-        </div>
+            <p>
+                {Illit_e_letter_i_result && (
+                    <span>i</span>
+                )
+                }
+                {Illit_e_letter_t_result && (
+                    <span>t</span>
+                )
+                }
+                {Illit_e_letter_s_result && (
+                    <span>'s </span>
+                )
+                }
+                {Illit_e_letter_m_result && (
+                    <span>m</span>
+                )
+                }
+                {Illit_e_letter_e_result && (
+                    <span>e</span>
+                )
+                }
+                {Illit_e_letter_all_checked && (
+                    <section>
+                        <p>당첨!!!!!!</p>
+                    </section>
+                )
+                }
+            </p>
+        </div >
     );
 }
