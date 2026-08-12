400791321b97d47cf12cc8015fd59e34054bef66
groomigroom <googjohn0143@gmail.com>
2026-08-12 15:25:42 +0900

    Update TriplesMain20250512End.tsx

--- src/modules/triples/main/beforeMain/TriplesMain20250512End.tsx
+++ src/modules/triples/main/beforeMain/TriplesMain20250512End.tsx
@@ -1,1 +1,16 @@
-TriplesMain20250512End.tsx
+import { DoruriDocumentTitle } from "../../../DoruriDocumentTitle";
+import Menu from "../../../Menu";
+
+export default function TriplesMain20250512End() {
+    DoruriDocumentTitle('tripleS');
+
+    return (
+        <div className="tri_20250512_m_full">
+            <Menu />
+            <section className="home_translate">
+                <Link to='/TriplesMain20250512End' className="on">한국어</Link>
+                <Link to='/TriplesMain20250512EndEng'>English</Link>
+            </section>
+        </div>
+    );
+}
