df9b11942a13294f921dd6d91fc62928ee652e3d
groomigroom <googjohn0143@gmail.com>
2026-08-12 15:37:08 +0900

    Update TriplesMain20250512End.tsx

--- src/modules/triples/main/beforeMain/TriplesMain20250512End.tsx
+++ src/modules/triples/main/beforeMain/TriplesMain20250512End.tsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
 import { DoruriDocumentTitle } from "../../../DoruriDocumentTitle";
+
+import '../TriplesMain.css';
+import './TriplesMain20250512End.css';
+
 import Menu from "../../../Menu";
+
+import tri_20250512_m_logo from './img/Triples_2025_m_logo.svg';
 
 export default function TriplesMain20250512End() {
     DoruriDocumentTitle('tripleS');
@@ -11,6 +18,7 @@ export default function TriplesMain20250512End() {
                 <Link to='/TriplesMain20250512End' className="on">한국어</Link>
                 <Link to='/TriplesMain20250512EndEng'>English</Link>
             </section>
+            <img src={tri_20250512_m_logo} alt="트리플에스 로고" className='tri_m_logo' />
         </div>
     );
 }
