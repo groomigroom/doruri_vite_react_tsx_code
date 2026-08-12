b317a202e11e8d1aa9a7f6f8c1373bb9abbbf230
groomigroom <googjohn0143@gmail.com>
2026-08-12 10:17:29 +0900

    Update ResceneEvent.tsx

--- src/modules/RESCENE/events/ResceneEvent.tsx
+++ src/modules/RESCENE/events/ResceneEvent.tsx
@@ -1,1 +1,8 @@
-ResceneEvent.tsx
+export default function ResceneEvent() {
+    return (
+        <div className="ResceneEvent_full">
+            <section className="rescene_e_moving_car_section"></section>
+            <div className="rescene_e_moving_car"></div>
+        </div>
+    );
+}
