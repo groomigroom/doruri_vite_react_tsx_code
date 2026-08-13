2b55fa7c5ce3a5aa15a4a9b39c7f4241fa0a2f7c
groomigroom <googjohn0143@gmail.com>
2026-08-13 21:07:34 +0900

    Update Bol4Event.tsx

--- src/modules/bol4/events/Bol4Event.tsx
+++ src/modules/bol4/events/Bol4Event.tsx
@@ -1,7 +1,10 @@
 import { Link } from "react-router-dom";
 import { DoruriDocumentTitle } from "../../DoruriDocumentTitle";
 
+import './Bol4Event.css';
+
 import Menu from "../../Menu";
+import MainH2Deco from '../../img/main_h2_deco.png';
 
 
 export default function Bol4Event() {
@@ -14,6 +17,25 @@ export default function Bol4Event() {
                 <Link to='/Bol4Event' className="on">한국어</Link>
                 <Link to='/Bol4EventEng'>English</Link>
             </section>
+            <img src={""} alt="볼빨간사춘기 이벤트 로고" className="tri_e_logo" />
+            <h2 className="tri_e_h2"><img src={MainH2Deco} alt="TWS" />??? 이벤트<img src={MainH2Deco} alt="TWS" /></h2>
+            <p className='tri_e_intropp'>
+                3줄 소개글<br />
+                3줄 소개글<br />
+                3줄 소개글
+            </p>
+            <section className="bol4_e_event_real_part">
+                <div className="bol4_e_event_r_p_mbox bol4_e_event_r_p_mbox1"></div>
+                <div className="bol4_e_event_r_p_mbox bol4_e_event_r_p_mbox2"></div>
+                <div className="bol4_e_event_r_p_mbox bol4_e_event_r_p_mbox3"></div>
+                <div className="bol4_e_event_r_p_mbox bol4_e_event_r_p_mbox4"></div>
+                <div className="bol4_e_event_r_p_mbox bol4_e_event_r_p_mbox5"></div>
+                <div className="bol4_e_event_r_p_mbox bol4_e_event_r_p_mbox6"></div>
+                <div className="bol4_e_event_r_p_mbox bol4_e_event_r_p_mbox7"></div>
+                <div className="bol4_e_event_r_p_mbox bol4_e_event_r_p_mbox8"></div>
+                <p className="bol4_e_event_r_p_m_pp">글자</p>
+            </section>
         </div>
     );
 }
+
