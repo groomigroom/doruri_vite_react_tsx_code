96e0e0fab9781a97f16d8ddbedab4cbe1fdacb99
groomigroom <googjohn0143@gmail.com>
2026-08-13 12:52:38 +0900

    Update ResceneEvent.tsx

--- src/modules/RESCENE/events/ResceneEvent.tsx
+++ src/modules/RESCENE/events/ResceneEvent.tsx
@@ -1,8 +1,14 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { DoruriDocumentTitle } from '../../DoruriDocumentTitle';
 
 import './ResceneEvent.css';
+import Menu from '../../Menu';
+import MainH2Deco from '../../img/main_h2_deco.png';
 
 export default function ResceneEvent() {
+    DoruriDocumentTitle('RESCENE 이벤트');
+
     const [rescene_e_moving_count, setRescene_e_moving_count] = useState(0);
 
     const rescene_e_moving_clickChange = () => {
@@ -11,6 +17,18 @@ export default function ResceneEvent() {
 
     return (
         <div className="ResceneEvent_full">
+            <Menu />
+            <section className="home_translate">
+                <Link to='/ResceneEvent' className="on">한국어</Link>
+                <Link to='/ResceneEventEng'>English</Link>
+            </section>
+            <img src={""} alt="RESCENE 이벤트 로고" className="tri_e_logo" />
+            <h2 className="tri_e_h2"><img src={MainH2Deco} alt="트리플에스 로고 꾸미기" />자동차 클릭 이벤트<img src={MainH2Deco} alt="트리플에스 로고 꾸미기" /></h2>
+            <p className='tri_e_intropp'>
+                3줄 소개글<br />
+                3줄 소개글<br />
+                3줄 소개글
+            </p>
             <section className="rescene_e_moving_car_section">
                 <div className="rescene_e_moving_car" onClick={rescene_e_moving_clickChange}>
 
