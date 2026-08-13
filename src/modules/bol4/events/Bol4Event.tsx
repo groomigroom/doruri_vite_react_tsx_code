9fc834a290b97f1c1272c3079341a3c94811f5ab
groomigroom <googjohn0143@gmail.com>
2026-08-13 20:34:23 +0900

    Add Bol4Event component with routing links

--- src/modules/bol4/events/Bol4Event.tsx
+++ src/modules/bol4/events/Bol4Event.tsx
@@ -1,1 +1,19 @@
-Bol4Event.tsx
+import { Link } from "react-router-dom";
+import { DoruriDocumentTitle } from "../../DoruriDocumentTitle";
+
+import Menu from "../../Menu";
+
+
+export default function Bol4Event() {
+    DoruriDocumentTitle('볼빨간사춘기 이벤트');
+
+    return (
+        <div className="bol4Event_full">
+            <Menu />
+            <section className="home_translate">
+                <Link to='/Bol4Event' className="on">한국어</Link>
+                <Link to='/Bol4EventEng'>English</Link>
+            </section>
+        </div>
+    );
+}
