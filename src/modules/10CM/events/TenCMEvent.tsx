d82c84cbf43894ac9789ca69fe673b84ac4ea0fe
groomigroom <googjohn0143@gmail.com>
2026-08-15 14:37:32 +0900

    Update TenCMEvent.tsx

--- src/modules/10CM/events/TenCMEvent.tsx
+++ src/modules/10CM/events/TenCMEvent.tsx
@@ -1,1 +1,15 @@
-TenCMEvent.tsx
+export default function TenCMEvent() {
+    const [Tencm_e_number_input1, SetTencm_e_number_input1] = useState<string>("");
+
+    const Tencm_e_number_input1_change = (e: React.ChangeEvent<HTMLInputElement>) => {
+        SetTencm_e_number_input1(e.target.value);
+    };
+
+
+
+    return (
+        <div className="TenCMEvent_full">
+            <input type="text" name="Tencm_e_number_input1" value={Tencm_e_number_input1} onChange={Tencm_e_number_input1_change} />
+        </div>
+    );
+}
