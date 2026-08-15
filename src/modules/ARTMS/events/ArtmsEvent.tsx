8483b03f7697353196a341c9f2a9cd3c5435b1fc
groomigroom <googjohn0143@gmail.com>
2026-08-15 14:24:13 +0900

    Update ArtmsEvent.tsx

--- src/modules/ARTMS/events/ArtmsEvent.tsx
+++ src/modules/ARTMS/events/ArtmsEvent.tsx
@@ -19,18 +19,34 @@ export default function ArtmsEvent() {
     ]);
 
     const artms_e_correct_append = () => {
-        SetArtms_e_correct_arr([...artms_e_correct_arr, artms_e_before_arr[0]]);
-        SetArtms_e_before_arr(artms_e_before_arr.toSpliced(0, 1));
+        if (artms_e_correct_arr.length !== 5 || artms_e_before_arr.length !== 0) {
+            SetArtms_e_correct_arr([...artms_e_correct_arr, artms_e_before_arr[0]]);
+            SetArtms_e_before_arr(artms_e_before_arr.toSpliced(0, 1));
+        }
+    };
+
+    const [artms_e_correct_arr_ins_num, SetArtms_e_correct_arr_ins_num] = useState<string>("");
+
+    const artms_e_correct_arr_ins_num_change = (e: React.ChangeEvent<HTMLInputElement>) => {
+        SetArtms_e_correct_arr_ins_num(e.target.value);
     };
 
     const artms_e_correct_insert = () => {
-        SetArtms_e_correct_arr(artms_e_correct_arr.toSpliced(1, 0, artms_e_before_arr[1]));
+        if (artms_e_correct_arr.length !== 5 || artms_e_before_arr.length !== 0) {
+            SetArtms_e_correct_arr(artms_e_correct_arr.toSpliced(parseInt(artms_e_correct_arr_ins_num) - 1, 0, artms_e_before_arr[0]));
+            SetArtms_e_before_arr(artms_e_before_arr.toSpliced(0, 1));
+        }
     };
 
     const artms_e_correct_pop = () => {
-        SetArtms_e_before_arr([...artms_e_before_arr, artms_e_correct_arr[0]])
-        SetArtms_e_correct_arr(artms_e_correct_arr.toSpliced(0, 1));
+        if (artms_e_correct_arr.length !== 0) {
+            SetArtms_e_before_arr([...artms_e_before_arr, artms_e_correct_arr[artms_e_correct_arr.length - 1]]);
+            SetArtms_e_correct_arr(artms_e_correct_arr.toSpliced(artms_e_correct_arr.length - 1, 1));
+        }
     };
+
+    let artms_e_correct_all = artms_e_correct_arr[0] === '구름' && artms_e_correct_arr[1] === '구름구름' && artms_e_correct_arr[2] === '구름이'
+        && artms_e_correct_arr[3] === '멍멍이' && artms_e_correct_arr[4] === '비숑';
 
     return (
         <div className="ArtmsEvent_full">
@@ -57,6 +73,7 @@ export default function ArtmsEvent() {
                 <div>{artms_e_correct_arr[4]}</div>
             </section>
             <button className='artms_e_correct_arr_append' onClick={artms_e_correct_append}>뒤에 추가하기</button>
+            <input className='artms_e_correct_arr_ins_num' type="text" name="artms_e_correct_arr_ins_num" value={artms_e_correct_arr_ins_num} onChange={artms_e_correct_arr_ins_num_change} />
             <button className='artms_e_correct_arr_insert' onClick={artms_e_correct_insert}>사이에 삽입하기</button>
             <button className='artms_e_correct_arr_pop' onClick={artms_e_correct_pop}>사이에서 삭제하기</button>
             <h2 className="tri_e_h2 artms_e_h2_3">▪ 기존 배열칸?? ▪</h2>
@@ -67,6 +84,11 @@ export default function ArtmsEvent() {
                 <div>{artms_e_before_arr[3]}</div>
                 <div>{artms_e_before_arr[4]}</div>
             </section>
+            {artms_e_correct_all && (
+                <section className='artms_e_real_result'>
+                    <p>당첨</p>
+                </section>
+            )}
         </div>
     );
 }
