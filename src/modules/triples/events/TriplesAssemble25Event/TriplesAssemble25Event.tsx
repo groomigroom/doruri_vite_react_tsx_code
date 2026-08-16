f2c207b611c4d2caae73fc61a60a388e1e9c113e
groomigroom <googjohn0143@gmail.com>
2026-08-16 21:05:01 +0900

    Update TriplesAssemble25Event.tsx

--- src/modules/triples/events/TriplesAssemble25Event/TriplesAssemble25Event.tsx
+++ src/modules/triples/events/TriplesAssemble25Event/TriplesAssemble25Event.tsx
@@ -1,1 +1,74 @@
-TriplesAssemble25Event.tsx
+import './TriplesAssemble25Event.css';
+
+export default function TriplesAssemble25Event() {
+    return (
+        <div>
+            <section className="TriplesAssemble25Event_full_tree_part">
+                <div className="TriplesAssemble25Event_third_real_fulltr">
+                    <div className="TriplesAssemble25Event_one_3_1_row_stick"></div>
+                    <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_3_1_column_stick"></div>
+                    <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_3_1_to_one_2"></div>
+                    <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_3_2_to_one_2"></div>
+                    <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_3_1"></div>
+                </div>
+                <div className="TriplesAssemble25Event_second_real_fulltr">
+                    <div className="TriplesAssemble25Event_second_real_full">
+                        <div className="TriplesAssemble25Event_one_2_1_row_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_1_column_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_1_to_one_2"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_2_to_one_2"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_2_1"></div>
+                    </div>
+                    <div className="TriplesAssemble25Event_second_real_full">
+                        <div className="TriplesAssemble25Event_one_2_1_row_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_1_column_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_1_to_one_2"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_2_to_one_2"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_2_1"></div>
+                    </div>
+                </div>
+                <div className="TriplesAssemble25Event_first_real_fulltr">
+                    <div className="TriplesAssemble25Event_first_real_full">
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1"></div>
+                        <div className="TriplesAssemble25Event_one_1_1_row_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_column_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_to_one_1"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_2_to_one_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2"></div>
+                    </div>
+                    <div className="TriplesAssemble25Event_first_ghost_part"></div>
+                    <div className="TriplesAssemble25Event_first_real_full">
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1"></div>
+                        <div className="TriplesAssemble25Event_one_1_1_row_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_column_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_to_one_1"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_2_to_one_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2"></div>
+                    </div>
+                    <div className="TriplesAssemble25Event_first_ghost_part"></div>
+                    <div className="TriplesAssemble25Event_first_real_full">
+                        <div className="TriplesAssemble25Event_real_box one_1"></div>
+                        <div className="TriplesAssemble25Event_one_1_1_row_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_column_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_to_one_1"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_2_to_one_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2"></div>
+                    </div>
+                    <div className="TriplesAssemble25Event_first_ghost_part"></div>
+                    <div className="TriplesAssemble25Event_first_real_full">
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1"></div>
+                        <div className="TriplesAssemble25Event_one_1_1_row_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_column_stick"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_to_one_1"></div>
+                        <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_2_to_one_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2"></div>
+                    </div>
+                </div>
+            </section >
+        </div >
+    );
+}
