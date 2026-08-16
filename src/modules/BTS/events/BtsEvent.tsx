375475237df8a8d70b45be3b01572f5276285491
groomigroom <googjohn0143@gmail.com>
2026-08-16 18:28:34 +0900

    Update BtsEvent.tsx

--- src/modules/BTS/events/BtsEvent.tsx
+++ src/modules/BTS/events/BtsEvent.tsx
@@ -1,1 +1,21 @@
-BtsEvent.tsx
+import { useState } from 'react';
+import './BtsEvent.css';
+
+export default function BtsEvent() {
+    const [Bts_e_last_result, setBts_e_last_result] = useState<boolean>(false);
+
+    const Bts_e_last_result_win = () => {
+        setBts_e_last_result(true);
+    };
+
+    return (
+        <div>
+            <div className="bts_e_moving_div" onClick={Bts_e_last_result_win}></div>
+            {Bts_e_last_result && (
+                <section>
+                    <p>당첨</p>
+                </section>
+            )}
+        </div>
+    );
+}
