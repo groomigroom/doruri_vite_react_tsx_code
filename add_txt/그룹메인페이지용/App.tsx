0d4363b98ea3506a7d33fe8e3d7b7b5f836fecff
groomigroom <googjohn0143@gmail.com>
2026-08-17 16:20:05 +0900

    Create App.tsx

---
+++ add_txt/그룹메인페이지용/App.tsx
@@ -0,0 +1,257 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <div className='tri_m_full'>
+      <header>
+      </header>
+      <section className='tri_m_logo_section'>
+        <img src={""} alt="로고" className='tri_m_logo' />
+      </section>
+      <section className='tri_m_h2'>
+        <h2><img src={""} alt="메인 타이틀" />김구름<img src={""} alt="메인 타이틀" /></h2>
+        {/* MainH2Deco */}
+      </section>
+      <section className='tri_m_intropp'>
+        <p>
+          1번줄<br />
+          2번줄<br />
+          3번줄
+        </p>
+      </section>
+      <section className='tri_m_img'>
+        <img src={""} alt='' />
+      </section>
+      <section className='tri_m_h2'>
+        <h2>▪ 멤버 ▪</h2>
+      </section>
+      <section className='tri_m_mem_tr_full'>
+        <section className='tri_m_mem_tr4'>
+          <a href='/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="김구름 이미지" />
+              <div>
+                <h4>김구름1</h4>
+              </div>
+            </article>
+          </a>
+          <a href='/TriplesMemS2'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름2</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS3'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름3</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS4'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름4</h4>
+              </div>
+            </article>
+          </a >
+        </section>
+
+        <section className='tri_m_mem_tr4'>
+          <a href='/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="김구름 이미지" />
+              <div>
+                <h4>김구름5</h4>
+              </div>
+            </article>
+          </a>
+          <a href='/TriplesMemS2'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름6</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS3'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름7</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS4'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름8</h4>
+              </div>
+            </article>
+          </a >
+        </section>
+
+        <section className='tri_m_mem_tr4'>
+          <a href='/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="김구름 이미지" />
+              <div>
+                <h4>김구름9</h4>
+              </div>
+            </article>
+          </a>
+          <a href='/TriplesMemS2'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름10</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS3'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름11</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS4'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름12</h4>
+              </div>
+            </article>
+          </a >
+        </section>
+
+        <section className='tri_m_mem_tr4'>
+          <a href='/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="김구름 이미지" />
+              <div>
+                <h4>김구름13</h4>
+              </div>
+            </article>
+          </a>
+          <a href='/TriplesMemS2'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름14</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS3'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름15</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS4'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름16</h4>
+              </div>
+            </article>
+          </a >
+        </section>
+
+        <section className='tri_m_mem_tr4'>
+          <a href='/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="김구름 이미지" />
+              <div>
+                <h4>김구름17</h4>
+              </div>
+            </article>
+          </a>
+          <a href='/TriplesMemS2'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름18</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS3'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름19</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS4'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름20</h4>
+              </div>
+            </article>
+          </a >
+        </section>
+
+        <section className='tri_m_mem_tr4'>
+          <a href='/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="김구름 이미지" />
+              <div>
+                <h4>김구름21</h4>
+              </div>
+            </article>
+          </a>
+          <a href='/TriplesMemS2'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름22</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS3'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름23</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='/TriplesMemS4'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="이미지" />
+              <div>
+                <h4>김구름24</h4>
+              </div>
+            </article>
+          </a >
+        </section>
+
+      </section>
+      <section className='tri_m_h2'>
+        <h2>▪ 유닛 ▪</h2>
+      </section>
+    </div>
+  );
+}
