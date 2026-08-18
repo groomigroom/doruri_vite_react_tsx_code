faf1ef66e258d886778638d449f55b3c773b55fa
groomigroom <googjohn0143@gmail.com>
2026-08-18 21:15:49 +0900

    Update TriplesAssemble25Event.tsx

--- src/modules/triples/events/TriplesAssemble25Event/TriplesAssemble25Event.tsx
+++ src/modules/triples/events/TriplesAssemble25Event/TriplesAssemble25Event.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './TriplesAssemble25Event.css';
 
 interface TriplesAssemble25NodeType {
@@ -13,10 +14,22 @@ export default function TriplesAssemble25Event() {
         TriplesAssemble25_e_aft_node: null,
     };
 
+    const [TriplesAssemble25_e_node_1_1On, setTriplesAssemble25_e_node_1_1On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_1True = () => {
+        setTriplesAssemble25_e_node_1_1On(true);
+    };
+
     const TriplesAssemble25_e_node_1_2: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "1층2번자리",
         TriplesAssemble25_e_bef_node: null,
         TriplesAssemble25_e_aft_node: null,
+    };
+
+    const [TriplesAssemble25_e_node_1_2On, setTriplesAssemble25_e_node_1_2On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_2True = () => {
+        setTriplesAssemble25_e_node_1_2On(true);
     };
 
     const TriplesAssemble25_e_node_1_3: TriplesAssemble25NodeType = {
@@ -25,10 +38,22 @@ export default function TriplesAssemble25Event() {
         TriplesAssemble25_e_aft_node: null,
     };
 
+    const [TriplesAssemble25_e_node_1_3On, setTriplesAssemble25_e_node_1_3On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_3True = () => {
+        setTriplesAssemble25_e_node_1_3On(true);
+    };
+
     const TriplesAssemble25_e_node_1_4: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "1층4번자리",
         TriplesAssemble25_e_bef_node: null,
         TriplesAssemble25_e_aft_node: null,
+    };
+
+    const [TriplesAssemble25_e_node_1_4On, setTriplesAssemble25_e_node_1_4On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_4True = () => {
+        setTriplesAssemble25_e_node_1_4On(true);
     };
 
     const TriplesAssemble25_e_node_1_5: TriplesAssemble25NodeType = {
@@ -37,10 +62,22 @@ export default function TriplesAssemble25Event() {
         TriplesAssemble25_e_aft_node: null,
     };
 
+    const [TriplesAssemble25_e_node_1_5On, setTriplesAssemble25_e_node_1_5On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_5True = () => {
+        setTriplesAssemble25_e_node_1_5On(true);
+    };
+
     const TriplesAssemble25_e_node_1_6: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "1층6번자리",
         TriplesAssemble25_e_bef_node: null,
         TriplesAssemble25_e_aft_node: null,
+    };
+
+    const [TriplesAssemble25_e_node_1_6On, setTriplesAssemble25_e_node_1_6On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_6True = () => {
+        setTriplesAssemble25_e_node_1_6On(true);
     };
 
     const TriplesAssemble25_e_node_1_7: TriplesAssemble25NodeType = {
@@ -49,10 +86,22 @@ export default function TriplesAssemble25Event() {
         TriplesAssemble25_e_aft_node: null,
     };
 
+    const [TriplesAssemble25_e_node_1_7On, setTriplesAssemble25_e_node_1_7On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_7True = () => {
+        setTriplesAssemble25_e_node_1_7On(true);
+    };
+
     const TriplesAssemble25_e_node_1_8: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "1층8번자리",
         TriplesAssemble25_e_bef_node: null,
         TriplesAssemble25_e_aft_node: null,
+    };
+
+    const [TriplesAssemble25_e_node_1_8On, setTriplesAssemble25_e_node_1_8On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_8True = () => {
+        setTriplesAssemble25_e_node_1_8On(true);
     };
 
     const TriplesAssemble25_e_node_2_1: TriplesAssemble25NodeType = {
@@ -61,10 +110,22 @@ export default function TriplesAssemble25Event() {
         TriplesAssemble25_e_aft_node: TriplesAssemble25_e_node_1_2,
     };
 
+    const [TriplesAssemble25_e_node_2_1On, setTriplesAssemble25_e_node_2_1On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_2_1True = () => {
+        setTriplesAssemble25_e_node_2_1On(true);
+    };
+
     const TriplesAssemble25_e_node_2_2: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "2층2번자리",
         TriplesAssemble25_e_bef_node: TriplesAssemble25_e_node_1_3,
         TriplesAssemble25_e_aft_node: TriplesAssemble25_e_node_1_4,
+    };
+
+    const [TriplesAssemble25_e_node_2_2On, setTriplesAssemble25_e_node_2_2On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_2_2True = () => {
+        setTriplesAssemble25_e_node_2_2On(true);
     };
 
     const TriplesAssemble25_e_node_2_3: TriplesAssemble25NodeType = {
@@ -73,10 +134,22 @@ export default function TriplesAssemble25Event() {
         TriplesAssemble25_e_aft_node: TriplesAssemble25_e_node_1_6,
     };
 
+    const [TriplesAssemble25_e_node_2_3On, setTriplesAssemble25_e_node_2_3On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_2_3True = () => {
+        setTriplesAssemble25_e_node_2_3On(true);
+    };
+
     const TriplesAssemble25_e_node_2_4: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "2층4번자리",
         TriplesAssemble25_e_bef_node: TriplesAssemble25_e_node_1_7,
         TriplesAssemble25_e_aft_node: TriplesAssemble25_e_node_1_8,
+    };
+
+    const [TriplesAssemble25_e_node_2_4On, setTriplesAssemble25_e_node_2_4On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_2_4True = () => {
+        setTriplesAssemble25_e_node_2_4On(true);
     };
 
     const TriplesAssemble25_e_node_3_1: TriplesAssemble25NodeType = {
@@ -85,10 +158,22 @@ export default function TriplesAssemble25Event() {
         TriplesAssemble25_e_aft_node: TriplesAssemble25_e_node_2_2,
     };
 
+    const [TriplesAssemble25_e_node_3_1On, setTriplesAssemble25_e_node_3_1On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_3_1True = () => {
+        setTriplesAssemble25_e_node_3_1On(true);
+    };
+
     const TriplesAssemble25_e_node_3_2: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "3층2번자리",
         TriplesAssemble25_e_bef_node: TriplesAssemble25_e_node_2_3,
         TriplesAssemble25_e_aft_node: TriplesAssemble25_e_node_2_4,
+    };
+
+    const [TriplesAssemble25_e_node_3_2On, setTriplesAssemble25_e_node_3_2On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_3_2True = () => {
+        setTriplesAssemble25_e_node_3_2On(true);
     };
 
     const TriplesAssemble25_e_node_4_1: TriplesAssemble25NodeType = {
@@ -96,6 +181,13 @@ export default function TriplesAssemble25Event() {
         TriplesAssemble25_e_bef_node: TriplesAssemble25_e_node_3_1,
         TriplesAssemble25_e_aft_node: TriplesAssemble25_e_node_3_2,
     };
+
+    const [TriplesAssemble25_e_node_4_1On, setTriplesAssemble25_e_node_4_1On] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_4_1True = () => {
+        setTriplesAssemble25_e_node_4_1On(true);
+    };
+
 
     return (
         <div>
@@ -105,7 +197,7 @@ export default function TriplesAssemble25Event() {
                     <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_3_1_column_stick"></div>
                     <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_3_1_to_one_2"></div>
                     <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_3_2_to_one_2"></div>
-                    <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_3_1"></div>
+                    <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_3_1" onClick={TriplesAssemble25_e_node_4_1True}></div>
                 </div>
                 <div className="TriplesAssemble25Event_second_real_fulltr">
                     <div className="TriplesAssemble25Event_second_real_full">
@@ -113,57 +205,156 @@ export default function TriplesAssemble25Event() {
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_1_column_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_1_to_one_2"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_2_to_one_2"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_2_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_2_1" onClick={TriplesAssemble25_e_node_3_1True}></div>
                     </div>
                     <div className="TriplesAssemble25Event_second_real_full">
                         <div className="TriplesAssemble25Event_one_2_1_row_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_1_column_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_1_to_one_2"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_2_2_to_one_2"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_2_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_2_1" onClick={TriplesAssemble25_e_node_3_2True}></div>
                     </div>
                 </div>
                 <div className="TriplesAssemble25Event_first_real_fulltr">
                     <div className="TriplesAssemble25Event_first_real_full">
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1" onClick={TriplesAssemble25_e_node_2_1True}></div>
                         <div className="TriplesAssemble25Event_one_1_1_row_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_column_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_to_one_1"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_2_to_one_1"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1" onClick={TriplesAssemble25_e_node_1_1True}></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2" onClick={TriplesAssemble25_e_node_1_2True}></div>
                     </div>
                     <div className="TriplesAssemble25Event_first_ghost_part"></div>
                     <div className="TriplesAssemble25Event_first_real_full">
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1" onClick={TriplesAssemble25_e_node_2_2True}></div>
                         <div className="TriplesAssemble25Event_one_1_1_row_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_column_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_to_one_1"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_2_to_one_1"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1" onClick={TriplesAssemble25_e_node_1_3True}></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2" onClick={TriplesAssemble25_e_node_1_4True}></div>
                     </div>
                     <div className="TriplesAssemble25Event_first_ghost_part"></div>
                     <div className="TriplesAssemble25Event_first_real_full">
-                        <div className="TriplesAssemble25Event_real_box one_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1" onClick={TriplesAssemble25_e_node_2_3True}></div>
                         <div className="TriplesAssemble25Event_one_1_1_row_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_column_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_to_one_1"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_2_to_one_1"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1" onClick={TriplesAssemble25_e_node_1_5True}></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2" onClick={TriplesAssemble25_e_node_1_6True}></div>
                     </div>
                     <div className="TriplesAssemble25Event_first_ghost_part"></div>
                     <div className="TriplesAssemble25Event_first_real_full">
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1" onClick={TriplesAssemble25_e_node_2_4True}></div>
                         <div className="TriplesAssemble25Event_one_1_1_row_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_column_stick"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_1_to_one_1"></div>
                         <div className="TriplesAssemble25Event_box_225 TriplesAssemble25Event_one_1_2_to_one_1"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1"></div>
-                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2"></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_1" onClick={TriplesAssemble25_e_node_1_7True}></div>
+                        <div className="TriplesAssemble25Event_real_box TriplesAssemble25Event_one_1_2" onClick={TriplesAssemble25_e_node_1_8True}></div>
                     </div>
                 </div>
+
+                {TriplesAssemble25_e_node_1_1On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_1_1.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_1_2On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_1_2.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_1_3On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_1_3.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_1_4On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_1_4.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_1_5On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_1_5.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_1_6On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_1_6.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_1_7On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_1_7.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_1_8On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_1_8.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {/* 2층 내용 */}
+
+                {TriplesAssemble25_e_node_2_1On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_2_1.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_2_2On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_2_2.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_2_3On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_2_3.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_2_4On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_2_4.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {/* 3층 내용 */}
+
+                {TriplesAssemble25_e_node_3_1On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_3_1.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {TriplesAssemble25_e_node_3_2On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_3_2.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+                {/* 4층 내용 */}
+
+                {TriplesAssemble25_e_node_4_1On && (
+                    <section>
+                        <p>{TriplesAssemble25_e_node_4_1.TriplesAssemble25_e_content}</p>
+                    </section>
+                )}
+
+
+
             </section >
         </div >
     );
