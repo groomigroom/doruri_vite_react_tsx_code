810dc2ae2c1962c6489520de167dc08c98fdb82a
groomigroom <googjohn0143@gmail.com>
2026-08-18 16:03:44 +0900

    Update AkmuEvent.tsx

--- src/modules/akmu/events/AkmuEvent.tsx
+++ src/modules/akmu/events/AkmuEvent.tsx
@@ -6,6 +6,7 @@ import "./AkmuEvent.css";
 import Menu from "../../Menu";
 
 import MainH2Deco from '../../img/main_h2_deco.png';
+import Akmu_e_m_logo from './img/akmu_e_m_logo.png';
 
 export default function AkmuEvent() {
     const [akmu_e_check_value, setAkmu_e_check_value] = useState<string>('');
@@ -16,18 +17,26 @@ export default function AkmuEvent() {
 
     return (
         <div className="akmuevent_full">
-            <Menu />
-            <section className="home_translate">
-                <Link to='/AkmuEvent' className="on">한국어</Link>
-                <Link to='/AkmuEventEng'>English</Link>
+            <header className="triples_e_header">
+                <Menu />
+                <section className="home_translate">
+                    <Link to='/AkmuEvent' className="on">한국어</Link>
+                    <Link to='/AkmuEventEng'>English</Link>
+                </section>
+            </header>
+            <section className="tri_e_logo">
+                <img src={Akmu_e_m_logo} alt="악뮤 이벤트 로고" />
             </section>
-            <img src={""} alt="악뮤 이벤트 로고" className="tri_e_logo" />
-            <h2 className="tri_e_h2"><img src={MainH2Deco} alt="" />ㅇㅇ<img src={MainH2Deco} alt="" /></h2>
-            <p className='tri_e_intropp'>
-                3줄 소개글<br />
-                3줄 소개글<br />
-                3줄 소개글
-            </p>
+            <section className="tri_e_h2">
+                <h2><img src={MainH2Deco} alt="" />ㅇㅇ<img src={MainH2Deco} alt="" /></h2>
+            </section>
+            <section className='tri_e_intropp'>
+                <p>
+                    3줄 소개글<br />
+                    3줄 소개글<br />
+                    3줄 소개글
+                </p>
+            </section>
             <form className="akmu_e_form" action="" onSubmit={(e) => e.preventDefault()}>
                 <input type="radio" name="akmu_event" value="1" checked={akmu_e_check_value === '1'} onChange={akmu_e_handleChange} id="akmu_e_check1" />
                 <input type="radio" name="akmu_event" value="2" checked={akmu_e_check_value === '2'} onChange={akmu_e_handleChange} id="akmu_e_check2" />
@@ -44,8 +53,6 @@ export default function AkmuEvent() {
                 <div className="akmu_e_answer akmu_e_answer3">
                     <p>아름다운 마음, 슬픔, 기쁨</p>
                 </div>
-
-
             </form>
             <div>
                 {akmu_e_check_value === '1' && <p className="akmu_e_result">1번 선택함 당첨</p>}
