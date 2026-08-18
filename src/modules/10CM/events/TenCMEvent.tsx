5fd3bc18890deaae158ddb46af35edb6115487e5
groomigroom <googjohn0143@gmail.com>
2026-08-19 06:47:16 +0900

    Update TenCMEvent.tsx

--- src/modules/10CM/events/TenCMEvent.tsx
+++ src/modules/10CM/events/TenCMEvent.tsx
@@ -33,6 +33,14 @@ export default function TenCMEvent() {
 
     let Tencm_e_number_last_result: number = parseInt(Tencm_e_number_input1) * parseInt(Tencm_e_number_input2) + parseInt(Tencm_e_number_input3) - parseInt(Tencm_e_number_input4) - parseInt(Tencm_e_number_input5);
 
+    let Tencm_e_number_last_result_true: boolean = Tencm_e_number_last_result === 10 &&
+        parseInt(Tencm_e_number_input1) <= 5 && parseInt(Tencm_e_number_input1) >= 1 &&
+        parseInt(Tencm_e_number_input2) <= 5 && parseInt(Tencm_e_number_input2) >= 1 &&
+        parseInt(Tencm_e_number_input3) <= 5 && parseInt(Tencm_e_number_input3) >= 1 &&
+        parseInt(Tencm_e_number_input4) <= 5 && parseInt(Tencm_e_number_input4) >= 1 &&
+        parseInt(Tencm_e_number_input5) <= 5 && parseInt(Tencm_e_number_input5) >= 1;
+
+
     return (
         <div className="TenCMEvent_full">
             <p>1부터 5의 숫자를 넣어서 10이 되게 하세요</p>
@@ -52,7 +60,7 @@ export default function TenCMEvent() {
             <input type="text" name="Tencm_e_number_input5" value={Tencm_e_number_input5} onChange={Tencm_e_number_input5_change} />
             <p>= 10</p>
             <p>최종 결과 : {Tencm_e_number_last_result}</p>
-            {Tencm_e_number_last_result === 10 && (
+            {Tencm_e_number_last_result_true && (
                 <section>
                     <p>당첨입니다.</p>
                 </section>
