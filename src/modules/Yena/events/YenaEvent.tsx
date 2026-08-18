97bc1562991ccabace1174591b1ddc9adc160fb2
groomigroom <googjohn0143@gmail.com>
2026-08-19 06:59:50 +0900

    Update YenaEvent.tsx

--- src/modules/Yena/events/YenaEvent.tsx
+++ src/modules/Yena/events/YenaEvent.tsx
@@ -1,1 +1,40 @@
-YenaEvent.tsx
+import { useState } from "react";
+
+export default function YenaEvent() {
+
+    const [Yena_e_false_click, setYena_e_false_click] = useState<boolean>(false);
+    const Yena_e_false_cl_change = () => {
+        setYena_e_false_click(true);
+    };
+
+    const [Yena_e_true_click, setYena_e_true_click] = useState<boolean>(false);
+    const Yena_e_true_cl_change = () => {
+        setYena_e_true_click(true);
+    };
+
+
+
+    return (
+        <div>
+            <p onClick={Yena_e_true_cl_change}>나</p>
+            <section onClick={Yena_e_false_cl_change}>
+                <p>너</p>
+                <p>너</p>
+                <p>너</p>
+                <p>너</p>
+                <p>너</p>
+                <p>너</p>
+            </section>
+            {Yena_e_false_click && (
+                <section>
+                    <p>당첨 아님</p>
+                </section>
+            )}
+            {Yena_e_true_click && (
+                <section>
+                    <p>당첨!!!!!</p>
+                </section>
+            )}
+        </div>
+    );
+}
