87b429f5583ddd7ac4269b4ef9e1cf5cd293266b
groomigroom <googjohn0143@gmail.com>
2026-08-21 07:15:14 +0900

    Update TxtEvent.tsx

--- src/modules/txt/events/TxtEvent.tsx
+++ src/modules/txt/events/TxtEvent.tsx
@@ -5,12 +5,11 @@ import "../../triples/events/TriplesEvent.css";
 import "./TxtEvent.css";
 
 import TxtELogo from "./img/txt_e_logo.png";
-import TxtEventPre from "./TxtEventPre";
 import Menu from "../../Menu";
 import FooterPart from "../../FooterPart";
 
 import MainH2Deco from '../../img/main_h2_deco.png';
-
+import Txt_e_keyring_set_img from './img/txt_e_keyring_img.jpg';
 
 export default function TxtEvent() {
     const [txt_e_str, setTxt_e_str] = useState<string>("");
@@ -75,11 +74,28 @@ export default function TxtEvent() {
                     </div>
                 </form>
             </section>
-            <button className="txt_e_btn" onClick={txt_e_OpPopup}>제출하기</button>
-            {txt_e_PopopO && (
-                <TxtEventPre txt_e_OnCl={txt_e_ClPopup} txt_e_result={txt_e_last_str} />
-            )}
+            <section className="txt_e_btn">
+                <button onClick={txt_e_OpPopup}>제출하기</button>
+            </section>
+            {
+                txt_e_PopopO && (
+                    <section className="tri_e_last_win_part">
+                        <section className="tri_e_p_win_intropp">
+                            <p>
+                                당첨을 축하드려요!<br />
+                                상품은 키링입니다.<br />
+                                이벤트에 관심가져 주셔서 감사합니다.<br />
+                                이 이벤트는 가상의 이벤트입니다.<br />
+                                참고해 주시길 부탁드리겠습니다.<br />
+                            </p>
+                        </section>
+                        <section className="tri_e_p_win_present_150">
+                            <img src={Txt_e_keyring_set_img} alt="" className="TenCM_e_p_win_present_img" />
+                        </section>
+                    </section>
+                )
+            }
             <FooterPart FooterPartText='ⓒ 빅히트 뮤직' />
-        </div>
+        </div >
     );
 };
