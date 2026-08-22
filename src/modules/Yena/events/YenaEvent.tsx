b19421548e6dc8085eabfab1b319b9788031505a
groomigroom <googjohn0143@gmail.com>
2026-08-23 08:10:41 +0900

    Update YenaEvent.tsx

--- src/modules/Yena/events/YenaEvent.tsx
+++ src/modules/Yena/events/YenaEvent.tsx
@@ -3,13 +3,16 @@ import { useState } from "react";
 export default function YenaEvent() {
 
     const [Yena_e_false_click, setYena_e_false_click] = useState<boolean>(false);
+    const [Yena_e_true_click, setYena_e_true_click] = useState<boolean>(false);
     const Yena_e_false_cl_change = () => {
         setYena_e_false_click(true);
+        setYena_e_true_click(false);
     };
 
-    const [Yena_e_true_click, setYena_e_true_click] = useState<boolean>(false);
+
     const Yena_e_true_cl_change = () => {
         setYena_e_true_click(true);
+        setYena_e_false_click(false);
     };
 
 
