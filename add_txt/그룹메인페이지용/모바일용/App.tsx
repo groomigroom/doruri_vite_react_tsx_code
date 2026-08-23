5032841012a47b25b914835f7c3a695b514e6ce7
groomigroom <googjohn0143@gmail.com>
2026-08-23 15:15:51 +0900

    Create App.tsx

---
+++ add_txt/그룹메인페이지용/모바일용/App.tsx
@@ -0,0 +1,158 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <section className='tri_m_mem_tr_full'>
+        <section className='tri_m_mem_tr4'>
+          <a href='http://localhost:8080/members/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" className='tri_e_mem_imgborder' />
+              <div className='tri_e_mem_imgborder'>
+                <h4>1번</h4>
+              </div>
+            </article>
+          </a>
+          <a href='http://localhost:8080/members/TriplesMemS2' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>2번</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='http://localhost:8080/members/TriplesMemS3' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>3번</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='http://localhost:8080/members/TriplesMemS4' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt="d 이미지" />
+              <div>
+                <h4>4번</h4>
+              </div>
+            </article>
+          </a>
+        </section>
+        <section className='tri_m_mem_tr3_1'>
+          <a href='http://localhost:8080/members/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" className='tri_e_mem_imgborder' />
+              <div className='tri_e_mem_imgborder'>
+                <h4>1번</h4>
+              </div>
+            </article>
+          </a>
+          <a href='http://localhost:8080/members/TriplesMemS2' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>2번</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='http://localhost:8080/members/TriplesMemS3' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>3번</h4>
+              </div>
+            </article>
+          </a>
+        </section>
+        <section className='tri_m_mem_tr3_2'>
+          <a href='http://localhost:8080/members/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" className='tri_e_mem_imgborder' />
+              <div className='tri_e_mem_imgborder'>
+                <h4>1번</h4>
+              </div>
+            </article>
+          </a>
+          <a href='http://localhost:8080/members/TriplesMemS2' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>2번</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='http://localhost:8080/members/TriplesMemS3' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>3번</h4>
+              </div>
+            </article>
+          </a>
+        </section>
+        <section className='tri_m_mem_tr3_3'>
+          <a href='http://localhost:8080/members/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" className='tri_e_mem_imgborder' />
+              <div className='tri_e_mem_imgborder'>
+                <h4>1번</h4>
+              </div>
+            </article>
+          </a>
+          <a href='http://localhost:8080/members/TriplesMemS2' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>2번</h4>
+              </div>
+            </article>
+          </a>
+
+          <a href='http://localhost:8080/members/TriplesMemS3' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>3번</h4>
+              </div>
+            </article>
+          </a>
+        </section>
+        <section className='tri_m_mem_tr2'>
+          <a href='http://localhost:8080/members/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" className='tri_e_mem_imgborder' />
+              <div className='tri_e_mem_imgborder'>
+                <h4>1번</h4>
+              </div>
+            </article>
+          </a>
+          <a href='http://localhost:8080/members/TriplesMemS2' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" />
+              <div>
+                <h4>2번</h4>
+              </div>
+            </article>
+          </a>
+        </section>
+        <section className='tri_m_mem_tr1'>
+          <a href='http://localhost:8080/members/TriplesMemS1' className='tri_m_mem_a'>
+            <article className='tri_m_mem'>
+              <img src={""} alt=" 이미지" className='tri_e_mem_imgborder' />
+              <div className='tri_e_mem_imgborder'>
+                <h4>1번</h4>
+              </div>
+            </article>
+          </a>
+        </section>
+      </section>
+    </>
+  )
+}
+
+export default App
