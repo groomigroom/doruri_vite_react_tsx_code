2a7f71dbe6bf5d041d26439eced5f8a15bfa7c7c
groomigroom <googjohn0143@gmail.com>
2026-08-23 21:27:19 +0900

    Update TriplesAssemble25Event.tsx

--- src/modules/triples/events/TriplesAssemble25Event/TriplesAssemble25Event.tsx
+++ src/modules/triples/events/TriplesAssemble25Event/TriplesAssemble25Event.tsx
@@ -20,6 +20,10 @@ export default function TriplesAssemble25Event() {
         setTriplesAssemble25_e_node_1_1On(true);
     };
 
+    const TriplesAssemble25_e_node_1_1False = () => {
+        setTriplesAssemble25_e_node_1_1On(false);
+    };
+
     const TriplesAssemble25_e_node_1_2: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "1층2번자리",
         TriplesAssemble25_e_bef_node: null,
@@ -30,6 +34,10 @@ export default function TriplesAssemble25Event() {
 
     const TriplesAssemble25_e_node_1_2True = () => {
         setTriplesAssemble25_e_node_1_2On(true);
+    };
+
+    const TriplesAssemble25_e_node_1_2False = () => {
+        setTriplesAssemble25_e_node_1_2On(false);
     };
 
     const TriplesAssemble25_e_node_1_3: TriplesAssemble25NodeType = {
@@ -44,6 +52,11 @@ export default function TriplesAssemble25Event() {
         setTriplesAssemble25_e_node_1_3On(true);
     };
 
+    const TriplesAssemble25_e_node_1_3False = () => {
+        setTriplesAssemble25_e_node_1_3On(false);
+    };
+
+
     const TriplesAssemble25_e_node_1_4: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "1층4번자리",
         TriplesAssemble25_e_bef_node: null,
@@ -54,6 +67,10 @@ export default function TriplesAssemble25Event() {
 
     const TriplesAssemble25_e_node_1_4True = () => {
         setTriplesAssemble25_e_node_1_4On(true);
+    };
+
+    const TriplesAssemble25_e_node_1_4False = () => {
+        setTriplesAssemble25_e_node_1_4On(false);
     };
 
     const TriplesAssemble25_e_node_1_5: TriplesAssemble25NodeType = {
@@ -68,6 +85,10 @@ export default function TriplesAssemble25Event() {
         setTriplesAssemble25_e_node_1_5On(true);
     };
 
+    const TriplesAssemble25_e_node_1_5False = () => {
+        setTriplesAssemble25_e_node_1_5On(false);
+    };
+
     const TriplesAssemble25_e_node_1_6: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "1층6번자리",
         TriplesAssemble25_e_bef_node: null,
@@ -78,6 +99,10 @@ export default function TriplesAssemble25Event() {
 
     const TriplesAssemble25_e_node_1_6True = () => {
         setTriplesAssemble25_e_node_1_6On(true);
+    };
+
+    const TriplesAssemble25_e_node_1_6False = () => {
+        setTriplesAssemble25_e_node_1_6On(false);
     };
 
     const TriplesAssemble25_e_node_1_7: TriplesAssemble25NodeType = {
@@ -92,6 +117,10 @@ export default function TriplesAssemble25Event() {
         setTriplesAssemble25_e_node_1_7On(true);
     };
 
+    const TriplesAssemble25_e_node_1_7False = () => {
+        setTriplesAssemble25_e_node_1_7On(false);
+    };
+
     const TriplesAssemble25_e_node_1_8: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "1층8번자리",
         TriplesAssemble25_e_bef_node: null,
@@ -102,6 +131,10 @@ export default function TriplesAssemble25Event() {
 
     const TriplesAssemble25_e_node_1_8True = () => {
         setTriplesAssemble25_e_node_1_8On(true);
+    };
+
+    const TriplesAssemble25_e_node_1_8False = () => {
+        setTriplesAssemble25_e_node_1_8On(false);
     };
 
     const TriplesAssemble25_e_node_2_1: TriplesAssemble25NodeType = {
@@ -116,6 +149,10 @@ export default function TriplesAssemble25Event() {
         setTriplesAssemble25_e_node_2_1On(true);
     };
 
+    const TriplesAssemble25_e_node_2_1False = () => {
+        setTriplesAssemble25_e_node_2_1On(false);
+    };
+
     const TriplesAssemble25_e_node_2_2: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "2층2번자리",
         TriplesAssemble25_e_bef_node: TriplesAssemble25_e_node_1_3,
@@ -126,6 +163,10 @@ export default function TriplesAssemble25Event() {
 
     const TriplesAssemble25_e_node_2_2True = () => {
         setTriplesAssemble25_e_node_2_2On(true);
+    };
+
+    const TriplesAssemble25_e_node_2_2False = () => {
+        setTriplesAssemble25_e_node_2_2On(false);
     };
 
     const TriplesAssemble25_e_node_2_3: TriplesAssemble25NodeType = {
@@ -140,6 +181,10 @@ export default function TriplesAssemble25Event() {
         setTriplesAssemble25_e_node_2_3On(true);
     };
 
+    const TriplesAssemble25_e_node_2_3False = () => {
+        setTriplesAssemble25_e_node_2_3On(false);
+    };
+
     const TriplesAssemble25_e_node_2_4: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "2층4번자리",
         TriplesAssemble25_e_bef_node: TriplesAssemble25_e_node_1_7,
@@ -150,6 +195,10 @@ export default function TriplesAssemble25Event() {
 
     const TriplesAssemble25_e_node_2_4True = () => {
         setTriplesAssemble25_e_node_2_4On(true);
+    };
+
+    const TriplesAssemble25_e_node_2_4False = () => {
+        setTriplesAssemble25_e_node_2_4On(false);
     };
 
     const TriplesAssemble25_e_node_3_1: TriplesAssemble25NodeType = {
@@ -164,6 +213,10 @@ export default function TriplesAssemble25Event() {
         setTriplesAssemble25_e_node_3_1On(true);
     };
 
+    const TriplesAssemble25_e_node_3_1False = () => {
+        setTriplesAssemble25_e_node_3_1On(false);
+    };
+
     const TriplesAssemble25_e_node_3_2: TriplesAssemble25NodeType = {
         TriplesAssemble25_e_content: "3층2번자리",
         TriplesAssemble25_e_bef_node: TriplesAssemble25_e_node_2_3,
@@ -174,6 +227,10 @@ export default function TriplesAssemble25Event() {
 
     const TriplesAssemble25_e_node_3_2True = () => {
         setTriplesAssemble25_e_node_3_2On(true);
+    };
+
+    const TriplesAssemble25_e_node_3_2False = () => {
+        setTriplesAssemble25_e_node_3_2On(false);
     };
 
     const TriplesAssemble25_e_node_4_1: TriplesAssemble25NodeType = {
@@ -188,6 +245,70 @@ export default function TriplesAssemble25Event() {
         setTriplesAssemble25_e_node_4_1On(true);
     };
 
+    const TriplesAssemble25_e_node_4_1False = () => {
+        setTriplesAssemble25_e_node_4_1On(false);
+    };
+
+    const [TriplesAssemble25_e_last_result, setTriplesAssemble25_e_last_result] = useState(0);
+
+    const [TriplesAssemble25_e_node_1_1_v, setTriplesAssemble25_e_node_1_1_v] =
+        useState<string>('');
+    const [TriplesAssemble25_e_node_1_1_end, setTriplesAssemble25_e_node_1_1_end] = useState<boolean>(false);
+
+    const TriplesAssemble25_e_node_1_1_Chan = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const value = e.target.value;
+
+        setTriplesAssemble25_e_node_1_1_v(value);
+
+        if (value === '1_1_1' && TriplesAssemble25_e_node_1_1_end === false) {
+            setTriplesAssemble25_e_last_result(
+                (prev) => prev + 1
+            );
+            setTriplesAssemble25_e_node_1_1_end(true);
+        }
+    };
+
+    const [TriplesAssemble25_e_node_1_4_v, setTriplesAssemble25_e_node_1_4_v] =
+        useState<string>('');
+    const [TriplesAssemble25_e_node_1_4_end, setTriplesAssemble25_e_node_1_4_end] = useState<boolean>(false);
+
+
+    const TriplesAssemble25_e_node_1_4_Chan = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const value = e.target.value;
+
+        setTriplesAssemble25_e_node_1_4_v(value);
+
+        if (value === '1_4_3' && TriplesAssemble25_e_node_1_4_end === false) {
+            setTriplesAssemble25_e_last_result(
+                (prev) => prev + 1
+            );
+            setTriplesAssemble25_e_node_1_4_end(true);
+        }
+    };
+
+    const [TriplesAssemble25_e_node_1_7_v, setTriplesAssemble25_e_node_1_7_v] =
+        useState<string>('');
+    const [TriplesAssemble25_e_node_1_7_end, setTriplesAssemble25_e_node_1_7_end] = useState<boolean>(false);
+
+
+    const TriplesAssemble25_e_node_1_7_Chan = (
+        e: React.ChangeEvent<HTMLInputElement>
+    ) => {
+        const value = e.target.value;
+
+        setTriplesAssemble25_e_node_1_7_v(value);
+
+        if (value === '1_7_2' && TriplesAssemble25_e_node_1_7_end === false) {
+            setTriplesAssemble25_e_last_result(
+                (prev) => prev + 1
+            );
+            setTriplesAssemble25_e_node_1_7_end(true);
+        }
+    };
 
     return (
         <div>
@@ -260,48 +381,150 @@ export default function TriplesAssemble25Event() {
                 {TriplesAssemble25_e_node_1_1On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_1_1.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_1_1False}>닫기</button>
+                        <p>다음 중 맞는 건?</p>
+                        {/* 1번이 답 */}
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_1_Q"
+                            value="1_1_1"
+                            checked={TriplesAssemble25_e_node_1_1_v === '1_1_1'}
+                            onChange={TriplesAssemble25_e_node_1_1_Chan}
+                            id='TriplesAssemble25_e_node_1_1_v1'
+                        />
+
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_1_Q"
+                            value="1_1_2"
+                            checked={TriplesAssemble25_e_node_1_1_v === '1_1_2'}
+                            onChange={TriplesAssemble25_e_node_1_1_Chan}
+                            id='TriplesAssemble25_e_node_1_1_v2'
+                        />
+
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_1_Q"
+                            value="1_1_3"
+                            checked={TriplesAssemble25_e_node_1_1_v === '1_1_3'}
+                            onChange={TriplesAssemble25_e_node_1_1_Chan}
+                            id='TriplesAssemble25_e_node_1_1_v3'
+                        />
+                        <label htmlFor="TriplesAssemble25_e_node_1_1_v1"><h4>1</h4></label>
+                        <label htmlFor="TriplesAssemble25_e_node_1_1_v2"><h4>2</h4></label>
+                        <label htmlFor="TriplesAssemble25_e_node_1_1_v3"><h4>3</h4></label>
+
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_1_2On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_1_2.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_1_2False}>닫기</button>
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_1_3On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_1_3.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_1_3False}>닫기</button>
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_1_4On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_1_4.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_1_4False}>닫기</button>
+                        {/* 3번이 답 */}
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_4_Q"
+                            value="1_4_1"
+                            checked={TriplesAssemble25_e_node_1_4_v === '1_4_1'}
+                            onChange={TriplesAssemble25_e_node_1_4_Chan}
+                            id='TriplesAssemble25_e_node_1_4_v1'
+                        />
+
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_4_Q"
+                            value="1_4_2"
+                            checked={TriplesAssemble25_e_node_1_4_v === '1_4_2'}
+                            onChange={TriplesAssemble25_e_node_1_4_Chan}
+                            id='TriplesAssemble25_e_node_1_4_v2'
+                        />
+
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_4_Q"
+                            value="1_4_3"
+                            checked={TriplesAssemble25_e_node_1_4_v === '1_4_3'}
+                            onChange={TriplesAssemble25_e_node_1_4_Chan}
+                            id='TriplesAssemble25_e_node_1_4_v3'
+                        />
+                        <label htmlFor="TriplesAssemble25_e_node_1_4_v1"><h4>1</h4></label>
+                        <label htmlFor="TriplesAssemble25_e_node_1_4_v2"><h4>2</h4></label>
+                        <label htmlFor="TriplesAssemble25_e_node_1_4_v3"><h4>3</h4></label>
+
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_1_5On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_1_5.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_1_5False}>닫기</button>
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_1_6On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_1_6.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_1_6False}>닫기</button>
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_1_7On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_1_7.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_1_7False}>닫기</button>
+                        {/* 2번이 답 */}
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_7_Q"
+                            value="1_7_1"
+                            checked={TriplesAssemble25_e_node_1_7_v === '1_7_1'}
+                            onChange={TriplesAssemble25_e_node_1_7_Chan}
+                            id='TriplesAssemble25_e_node_1_7_v1'
+                        />
+
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_7_Q"
+                            value="1_7_2"
+                            checked={TriplesAssemble25_e_node_1_7_v === '1_7_2'}
+                            onChange={TriplesAssemble25_e_node_1_7_Chan}
+                            id='TriplesAssemble25_e_node_1_7_v2'
+                        />
+
+                        <input
+                            type="radio"
+                            name="TriplesAssemble25_e_node_1_7_Q"
+                            value="1_7_3"
+                            checked={TriplesAssemble25_e_node_1_7_v === '1_7_3'}
+                            onChange={TriplesAssemble25_e_node_1_7_Chan}
+                            id='TriplesAssemble25_e_node_1_7_v3'
+                        />
+                        <label htmlFor="TriplesAssemble25_e_node_1_7_v1"><h4>1</h4></label>
+                        <label htmlFor="TriplesAssemble25_e_node_1_7_v2"><h4>2</h4></label>
+                        <label htmlFor="TriplesAssemble25_e_node_1_7_v3"><h4>3</h4></label>
+
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_1_8On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_1_8.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_1_8False}>닫기</button>
                     </section>
                 )}
 
@@ -310,24 +533,28 @@ export default function TriplesAssemble25Event() {
                 {TriplesAssemble25_e_node_2_1On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_2_1.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_2_1False}>닫기</button>
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_2_2On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_2_2.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_2_2False}>닫기</button>
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_2_3On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_2_3.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_2_3False}>닫기</button>
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_2_4On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_2_4.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_2_4False}>닫기</button>
                     </section>
                 )}
 
@@ -336,12 +563,14 @@ export default function TriplesAssemble25Event() {
                 {TriplesAssemble25_e_node_3_1On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_3_1.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_3_1False}>닫기</button>
                     </section>
                 )}
 
                 {TriplesAssemble25_e_node_3_2On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_3_2.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_3_2False}>닫기</button>
                     </section>
                 )}
 
@@ -350,10 +579,11 @@ export default function TriplesAssemble25Event() {
                 {TriplesAssemble25_e_node_4_1On && (
                     <section>
                         <p>{TriplesAssemble25_e_node_4_1.TriplesAssemble25_e_content}</p>
+                        <button onClick={TriplesAssemble25_e_node_4_1False}>닫기</button>
                     </section>
                 )}
 
-
+                <p>점수 {TriplesAssemble25_e_last_result}점</p>
 
             </section >
         </div >
