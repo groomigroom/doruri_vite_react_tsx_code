9b37672dd1190f1c27c2fd9306f6bdd57b80bb51
groomigroom <googjohn0143@gmail.com>
2026-08-23 16:56:52 +0900

    Update YenaEventYouModule.tsx

--- src/modules/Yena/events/YenaEventYouModule.tsx
+++ src/modules/Yena/events/YenaEventYouModule.tsx
@@ -1,1 +1,72 @@
-YenaEventYouModule.tsx
+import './YenaEventYouModule.css';
+
+export default function YenaEventYouModule() {
+    return (
+        <div className="YenaEventYouModule_e_full">
+            <div className="yena_e_you_div1">
+                <p>
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너
+                </p>
+            </div>
+            <div className="yena_e_you_div2">
+                <p>
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너<br />
+                    너
+                </p>
+            </div>
+        </div>
+    );
+}
