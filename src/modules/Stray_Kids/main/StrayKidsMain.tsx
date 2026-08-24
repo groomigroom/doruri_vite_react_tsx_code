2ffd4e887088c7358967a42986f3381e66b7e268
groomigroom <googjohn0143@gmail.com>
2026-08-24 15:41:40 +0900

    Update StrayKidsMain.tsx

--- src/modules/Stray_Kids/main/StrayKidsMain.tsx
+++ src/modules/Stray_Kids/main/StrayKidsMain.tsx
@@ -36,92 +36,105 @@ export default function StrayKidsMain() {
 
     return (
         <div className='str_m_full'>
-            <Menu />
-            <section className="home_translate">
-                <Link to='/StrayKidsMain' className="on">한국어</Link>
-                <Link to='/StrayKidsMainEng'>English</Link>
+            <header className='tri_m_header'>
+                <Menu />
+                <section className="home_translate">
+                    <Link to='/StrayKidsMain' className="on">한국어</Link>
+                    <Link to='/StrayKidsMainEng'>English</Link>
+                </section>
+            </header>
+            <section className='tri_m_logo_section'>
+                <img src={Str_m_logo} alt="스트레이 키즈 로고" />
             </section>
-            <img src={Str_m_logo} alt="스트레이 키즈 로고" className='tri_m_logo' />
-            <h2 className='tri_m_h2 tri_m_h2_1'>
-                <img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" />Stray Kids<img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" />
-            </h2>
-            <p className='tri_m_intropp'>
-                &apos;Stray Kids&apos;와 함께<br />
-                시련 속에서도 계속<br />
-                running out!</p>
-            <img src={Str_m_img} className='tri_m_img' alt='스트레이 키즈' />
-            <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-            <section className='tri_m_mem_tr_full str_m_mem_tr_full'>
+            <section className='tri_m_h2'>
+                <h2>
+                    <img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" />Stray Kids<img src={MainH2Deco} alt="스트레이 키즈 로고 꾸미기" />
+                </h2>
+            </section>
+            <section className='tri_m_intropp'>
+                <p>
+                    &apos;Stray Kids&apos;와 함께<br />
+                    시련 속에서도 계속<br />
+                    running out!</p>
+            </section>
+            <section className='tri_m_img'>
+                <img src={Str_m_img} alt='스트레이 키즈' className='tri_m_mem_imgborder' />
+            </section>
+            <section className='tri_m_h2'>
+                <h2>▪ 멤버 ▪</h2>
+            </section>
 
-                <section className='tri_m_mem_tr str_m_mem_tr'>
+            <section className='tri_m_mem_tr_full'>
+
+                <section className='tri_m_mem_tr4'>
                     <Link to='/StrayMemBangchan' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Str_m_MemBangchan_img} alt="방찬 이미지" />
-                            <div>
+                            <img src={Str_m_MemBangchan_img} alt="방찬 이미지" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder'>
                                 <h4>방찬</h4>
                             </div>
                         </article>
                     </Link>
-                    <Link to='/StrayMemLino'>
+                    <Link to='/StrayMemLino' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Str_m_MemLino_img} alt="리노 이미지" />
-                            <div>
+                            <img src={Str_m_MemLino_img} alt="리노 이미지" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder'>
                                 <h4>리노</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/StrayMemChangbin'>
+                    <Link to='/StrayMemChangbin' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Str_m_MemChangbin_img} alt="창빈 이미지" />
-                            <div>
+                            <img src={Str_m_MemChangbin_img} alt="창빈 이미지" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder'>
                                 <h4>창빈</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/StrayMemHyunjin'>
+                    <Link to='/StrayMemHyunjin' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Str_m_MemHyunjin_img} alt="현진 이미지" />
-                            <div>
+                            <img src={Str_m_MemHyunjin_img} alt="현진 이미지" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder'>
                                 <h4>현진</h4>
                             </div>
                         </article>
                     </Link>
                 </section>
 
-                <section className='tri_m_mem_tr'>
-                    <Link to='/StrayMemHan'>
+                <section className='tri_m_mem_tr4'>
+                    <Link to='/StrayMemHan' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Str_m_MemHan_img} alt="한 이미지" />
-                            <div>
+                            <img src={Str_m_MemHan_img} alt="한 이미지" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder'>
                                 <h4>한</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/StrayMemFilix'>
+                    <Link to='/StrayMemFilix' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Str_m_MemFilix_img} alt="필릭스 이미지" />
-                            <div>
+                            <img src={Str_m_MemFilix_img} alt="필릭스 이미지" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder'>
                                 <h4>필릭스</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/StrayMemSeungmin'>
+                    <Link to='/StrayMemSeungmin' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Str_m_MemSeungmin_img} alt="승민 이미지" />
-                            <div>
+                            <img src={Str_m_MemSeungmin_img} alt="승민 이미지" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder' >
                                 <h4>승민</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/StrayMemIn'>
+                    <Link to='/StrayMemIn' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Str_m_MemIn_img} alt="아이엔 이미지" />
-                            <div>
+                            <img src={Str_m_MemIn_img} alt="아이엔 이미지" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder' >
                                 <h4>아이엔</h4>
                             </div>
                         </article>
@@ -184,13 +197,16 @@ export default function StrayKidsMain() {
                 </section>
 
             </section>
-            <h2 className='tri_m_h2 str_m_h2_3'>▪ 유닛 ▪</h2>
-            <section className='str_m_units_tr_full'>
-                <section className='tri_m_units_tr1'>
-                    <Link to='/StrayKidsThreeRachaMain' className='tri_m_units_a'>
-                        <article className='tri_m_units'>
-                            <img src={StrayKids3Racha_m_img} alt="3RACHA" />
-                            <div>
+
+            <section className='tri_m_h2'>
+                <h2>▪ 유닛 ▪</h2>
+            </section>
+            <section className='tri_m_mem_tr_full'>
+                <section className='tri_m_mem_tr1'>
+                    <Link to='/StrayKidsThreeRachaMain' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={StrayKids3Racha_m_img} alt="3RACHA" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder' >
                                 <h4>3RACHA</h4>
                             </div>
                         </article>
@@ -198,13 +214,15 @@ export default function StrayKidsMain() {
                 </section>
             </section>
 
-            <h2 className='tri_m_h2 stray_m_h2_4'>▪ 이벤트 ▪</h2>
-            <section className='tri_m_event_tr_full stray_m_event_tr_full'>
-                <section className='tri_m_event_tr'>
-                    <Link to='/StrayKidsEvent' className='tri_m_event_a'>
-                        <article className='tri_m_events'>
-                            <img src={Str_m_img} alt="stray kids event" />
-                            <div>
+            <section className='tri_m_h2'>
+                <h2>▪ 이벤트 ▪</h2>
+            </section>
+            <section className='tri_m_mem_tr_full'>
+                <section className='tri_m_mem_tr1'>
+                    <Link to='/StrayKidsEvent' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={Str_m_img} alt="stray kids event" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder' >
                                 <h4>Replay<br />
                                     이벤트</h4>
                             </div>
@@ -213,13 +231,15 @@ export default function StrayKidsMain() {
                 </section>
             </section>
 
-            <h2 className='tri_m_h2 stray_m_h2_5'>▪ 최신 앨범 ▪</h2>
-            <section className='tri_m_album_tr_full stray_m_album_tr_full'>
-                <section className='tri_m_album_tr'>
-                    <Link to='/StrayKidsSKZReplay2026Pt1Album' className='tri_m_album_a'>
-                        <article className='tri_m_albums'>
-                            <img src={StrayKidsSKZReplay2026Pt1Album_img} alt="StrayKidsSKZReplay2026Pt1Album" />
-                            <div>
+            <section className='tri_m_h2'>
+                <h2>▪ 최신 앨범 ▪</h2>
+            </section>
+            <section className='tri_m_mem_tr_full'>
+                <section className='tri_m_mem_tr1'>
+                    <Link to='/StrayKidsSKZReplay2026Pt1Album' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={StrayKidsSKZReplay2026Pt1Album_img} alt="StrayKidsSKZReplay2026Pt1Album" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder' >
                                 <h4>
                                     SKZ<br />
                                     Replay<br />
@@ -231,23 +251,25 @@ export default function StrayKidsMain() {
                 </section>
             </section>
 
-            <h2 className='tri_m_h2 stray_m_h2_6'>▪ sns ▪</h2>
-            <section className='tri_m_sns_tr_full str_m_sns_tr_full'>
-                <section className='tri_m_album_tr'>
-                    <a href="https://www.instagram.com/realstraykids" target='_blank' rel='noopener noreferrer' className='tri_m_sns_a'>
-                        <article className='tri_m_snss'>
-                            <img src={StrayKidsInstagram_img} alt="Stray Kids 인스타그램" />
-                            <div>
+            <section className='tri_m_h2'>
+                <h2>▪ sns ▪</h2>
+            </section>
+            <section className='tri_m_mem_tr_full'>
+                <section className='tri_m_mem_tr2'>
+                    <a href="https://www.instagram.com/realstraykids" target='_blank' rel='noopener noreferrer' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={StrayKidsInstagram_img} alt="Stray Kids 인스타그램" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder' >
                                 <h4>
                                     인스타그램
                                 </h4>
                             </div>
                         </article>
                     </a>
-                    <a href="https://www.youtube.com/@StrayKids" target='_blank' rel='noopener noreferrer' className='tri_m_sns_a'>
-                        <article className='tri_m_snss'>
-                            <img src={StrayKidsYoutube_img} alt="Stray Kids 유튜브" />
-                            <div>
+                    <a href="https://www.youtube.com/@StrayKids" target='_blank' rel='noopener noreferrer' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={StrayKidsYoutube_img} alt="Stray Kids 유튜브" className='tri_m_mem_imgborder' />
+                            <div className='tri_m_mem_imgborder' >
                                 <h4>
                                     유튜브
                                 </h4>
