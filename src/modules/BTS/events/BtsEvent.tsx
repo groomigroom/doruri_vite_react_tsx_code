c502a32ec7f21bb3337684babf1481a9bbe3e427
groomigroom <googjohn0143@gmail.com>
2026-08-24 20:51:13 +0900

    Update BtsEvent.tsx

--- src/modules/BTS/events/BtsEvent.tsx
+++ src/modules/BTS/events/BtsEvent.tsx
@@ -2,18 +2,23 @@ import { useState } from 'react';
 import './BtsEvent.css';
 
 export default function BtsEvent() {
-    const [Bts_e_last_result, setBts_e_last_result] = useState<boolean>(false);
+    const [Bts_e_last_result, setBts_e_last_result] = useState<number>(0);
 
     const Bts_e_last_result_win = () => {
-        setBts_e_last_result(true);
+        setBts_e_last_result((prev) => prev + 1);
     };
 
     return (
         <div>
             <div className="bts_e_moving_div" onClick={Bts_e_last_result_win}></div>
-            {Bts_e_last_result && (
+            <p>현재 점수 {Bts_e_last_result}점</p>
+            {Bts_e_last_result >= 3 ? (
                 <section>
                     <p>당첨</p>
+                </section>
+            ) : (
+                <section>
+                    <p>탈락</p>
                 </section>
             )}
         </div>
