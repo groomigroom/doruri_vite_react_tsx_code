82328bbdb3c148011c9d197ed9ea0c8cd71a107d
groomigroom <googjohn0143@gmail.com>
2026-08-24 15:05:04 +0900

    Update IoiMain.tsx

--- src/modules/ioi/main/IoiMain.tsx
+++ src/modules/ioi/main/IoiMain.tsx
@@ -36,121 +36,133 @@ export default function IoiMain() {
 
     return (
         <div className='ioi_m_full'>
-            <Menu />
-            <section className="home_translate">
-                <Link to='/IoiMain' className="on">한국어</Link>
-                <Link to='/IoiMainEng'>English</Link>
+            <header className='tri_m_header'>
+                <Menu />
+                <section className="home_translate">
+                    <Link to='/IoiMain' className="on">한국어</Link>
+                    <Link to='/IoiMainEng'>English</Link>
+                </section>
+            </header>
+            <section className='tri_m_logo_section'>
+                <img src={Ioi_m_logo} alt="아이오아이 로고" />
             </section>
-            <img src={Ioi_m_logo} alt="아이오아이 로고" className='tri_m_logo' />
-            <h2 className='tri_m_h2 tri_m_h2_1'>
-                <img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />아이오아이<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />
-            </h2>
-            <p className='tri_m_intropp'>갑자기 찾아온<br />
-                &apos;아이오아이&apos;와 함께<br />
-                오늘도 좋은 생각에 잠겨 보아요!
-            </p>
-            <img src={Ioi_m_img} className='tri_m_img ioi_m_img' alt='아이오아이 이미지' />
-            <h2 className='tri_m_h2 tri_m_h2_2'>▪ 멤버 ▪</h2>
-            <section className='ioi_m_mem_tr_full'>
+            <section className='tri_m_h2'>
+                <h2>
+                    <img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />아이오아이<img src={MainH2Deco} alt="아이오아이 로고 꾸미기" />
+                </h2>
+            </section>
+            <section className='tri_m_intropp'>
+                <p>갑자기 찾아온<br />
+                    &apos;아이오아이&apos;와 함께<br />
+                    오늘도 좋은 생각에 잠겨 보아요!
+                </p>
+            </section>
+            <section className='tri_m_img'>
+                <img src={Ioi_m_img} className='ioi_m_mem_imgborder' alt='아이오아이 이미지' />
+            </section>
+            <section className='tri_m_h2'>
+                <h2>▪ 멤버 ▪</h2>
+            </section>
+            <section className='tri_m_mem_tr_full'>
 
-                <section className='ioi_m_mem_tr4'>
+                <section className='tri_m_mem_tr4'>
                     <Link to='/IoiMemImnayoung' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemImnayoung_img} alt="임나영 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemImnayoung_img} alt="임나영 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>임나영</h4>
                             </div>
                         </article>
                     </Link>
-                    <Link to='/IoiMemKimchungha'>
+                    <Link to='/IoiMemKimchungha' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemKimchungha_img} alt="김청하 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemKimchungha_img} alt="김청하 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>김청하</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/IoiMemKimsejung'>
+                    <Link to='/IoiMemKimsejung' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemKimsejung_img} alt="김세정 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemKimsejung_img} alt="김세정 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>김세정</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/IoiMemJungcheyun'>
+                    <Link to='/IoiMemJungcheyun' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemJungcheyun_img} alt="정채연 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemJungcheyun_img} alt="정채연 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>정채연</h4>
                             </div>
                         </article>
                     </Link>
-                </section>
+                </section >
 
-                <section className='ioi_m_mem_tr4'>
-                    <Link to='/IoiMemJugyulgyoung'>
+                <section className='tri_m_mem_tr4'>
+                    <Link to='/IoiMemJugyulgyoung' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemJugyulgyoung_img} alt="주결경 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemJugyulgyoung_img} alt="주결경 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>주결경</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/IoiMemKimsohye'>
+                    <Link to='/IoiMemKimsohye' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemKimsohye_img} alt="김소혜 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemKimsohye_img} alt="김소혜 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>김소혜</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/IoiMemYuyeonjung'>
+                    <Link to='/IoiMemYuyeonjung' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemYuyeonjung_img} alt="유연정 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemYuyeonjung_img} alt="유연정 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>유연정</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/IoiMemChoiyujung'>
+                    <Link to='/IoiMemChoiyujung' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemChoiyujung_img} alt="최유정 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemChoiyujung_img} alt="최유정 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>최유정</h4>
                             </div>
                         </article>
                     </Link>
                 </section>
 
-                <section className='ioi_m_mem_tr3'>
-                    <Link to='/IoiMemGangmina'>
+                <section className='tri_m_mem_tr3'>
+                    <Link to='/IoiMemGangmina' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemGangmina_img} alt="강미나 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemGangmina_img} alt="강미나 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>강미나</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/IoiMemKimdoyeon'>
+                    <Link to='/IoiMemKimdoyeon' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemKimdoyeon_img} alt="김도연 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemKimdoyeon_img} alt="김도연 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>김도연</h4>
                             </div>
                         </article>
                     </Link>
 
-                    <Link to='/IoiMemJeonsomi'>
+                    <Link to='/IoiMemJeonsomi' className='tri_m_mem_a'>
                         <article className='tri_m_mem'>
-                            <img src={Ioi_m_MemJeonsomi_img} alt="전소미 이미지" />
-                            <div>
+                            <img src={Ioi_m_MemJeonsomi_img} alt="전소미 이미지" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>전소미</h4>
                             </div>
                         </article>
@@ -158,91 +170,18 @@ export default function IoiMain() {
 
                 </section>
 
+            </section >
+
+
+            <section className='tri_m_h2'>
+                <h2>▪ 이벤트 ▪</h2>
             </section>
-            {/* 모바일 멤버 보기 */}
-            <section className="tri_m_mem_tr_full_mobile ioi_m_mem_tr_full_mobile">
-                <section className="tri_m_mem_trtr">
-                    <section className='tri_m_mem_tr'>
-                        <Link to='/IoiMemImnayoung' className='tri_m_mem_a'>
-                            <img src={Ioi_m_MemImnayoung_img} alt="임나영 이미지" />
-                            <h4>임나영</h4>
-                        </Link>
-                        <Link to='/IoiMemKimchungha'>
-                            <img src={Ioi_m_MemKimchungha_img} alt="김청하 이미지" />
-                            <h4>김청하</h4>
-                        </Link>
-                    </section>
-                </section>
-                <section className="tri_m_mem_trtr">
-                    <section className='tri_m_mem_tr'>
-                        <Link to='/IoiMemKimsejung'>
-                            <img src={Ioi_m_MemKimsejung_img} alt="김세정 이미지" />
-                            <h4>김세정</h4>
-                        </Link>
-
-                        <Link to='/IoiMemJungcheyun'>
-                            <img src={Ioi_m_MemJungcheyun_img} alt="정채연 이미지" />
-                            <h4>정채연</h4>
-                        </Link>
-                    </section>
-                </section>
-                <section className="tri_m_mem_trtr">
-                    <section className='tri_m_mem_tr'>
-                        <Link to='/IoiMemJugyulgyoung'>
-                            <img src={Ioi_m_MemJugyulgyoung_img} alt="주결경 이미지" />
-                            <h4>주결경</h4>
-                        </Link>
-
-                        <Link to='/IoiMemKimsohye'>
-                            <img src={Ioi_m_MemKimsohye_img} alt="김소혜 이미지" />
-                            <h4>김소혜</h4>
-                        </Link>
-                    </section>
-                </section>
-                <section className="tri_m_mem_trtr">
-                    <section className='tri_m_mem_tr'>
-                        <Link to='/IoiMemYuyeonjung'>
-                            <img src={Ioi_m_MemYuyeonjung_img} alt="유연정 이미지" />
-                            <h4>유연정</h4>
-                        </Link>
-
-                        <Link to='/IoiMemChoiyujung'>
-                            <img src={Ioi_m_MemChoiyujung_img} alt="최유정 이미지" />
-                            <h4>최유정</h4>
-                        </Link>
-                    </section>
-                </section>
-                <section className="tri_m_mem_trtr">
-                    <section className='tri_m_mem_tr'>
-                        <Link to='/IoiMemGangmina'>
-                            <img src={Ioi_m_MemGangmina_img} alt="강미나 이미지" />
-                            <h4>강미나</h4>
-                        </Link>
-
-                        <Link to='/IoiMemKimdoyeon'>
-                            <img src={Ioi_m_MemKimdoyeon_img} alt="김도연 이미지" />
-                            <h4>김도연</h4>
-                        </Link>
-                    </section>
-                </section>
-                <section className="tri_m_mem_trtr">
-                    <section className='tri_m_mem_tr1'>
-                        <Link to='/IoiMemJeonsomi'>
-                            <img src={Ioi_m_MemJeonsomi_img} alt="전소미 이미지" />
-                            <h4>전소미</h4>
-                        </Link>
-                    </section>
-                </section>
-
-            </section>
-
-            <h2 className='tri_m_h2 ioi_m_h2_4'>▪ 이벤트 ▪</h2>
-            <section className='tri_m_event_tr_full ioi_m_event_tr_full'>
-                <section className='tri_m_event_tr'>
-                    <Link to='/IoiEvent' className='tri_m_event_a'>
-                        <article className='tri_m_events'>
-                            <img src={Ioi_m_img} alt="ioi event" />
-                            <div>
+            <section className='tri_m_mem_tr_full'>
+                <section className='tri_m_mem_tr1'>
+                    <Link to='/IoiEvent' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={Ioi_m_img} alt="ioi event" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>LOOP<br />
                                     이벤트</h4>
                             </div>
@@ -251,13 +190,15 @@ export default function IoiMain() {
                 </section>
             </section>
 
-            <h2 className='tri_m_h2 ioi_m_h2_5'>▪ 최신 앨범 ▪</h2>
-            <section className='tri_m_album_tr_full ioi_m_album_tr_full'>
-                <section className='tri_m_album_tr'>
-                    <Link to='/IoiIoiLoopAlbum' className='tri_m_album_a'>
-                        <article className='tri_m_albums'>
-                            <img src={IoiIoiLoopAlbum_img} alt="ioi loop" />
-                            <div>
+            <section className='tri_m_h2'>
+                <h2>▪ 최신 앨범 ▪</h2>
+            </section>
+            <section className='tri_m_mem_tr_full'>
+                <section className='tri_m_mem_tr1'>
+                    <Link to='/IoiIoiLoopAlbum' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={IoiIoiLoopAlbum_img} alt="ioi loop" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>
                                     I.O.I<br />
                                     :<br />
@@ -269,13 +210,15 @@ export default function IoiMain() {
                 </section>
             </section>
 
-            <h2 className='tri_m_h2 ioi_m_h2_6'>▪ sns ▪</h2>
-            <section className='tri_m_sns_tr_full ioi_m_sns_tr_full'>
-                <section className='tri_m_album_tr ioi_m_sns_tr'>
-                    <a href="https://www.instagram.com/ioi_10th" target='_blank' rel='noopener noreferrer' className='tri_m_sns_a ioi_m_sns_a'>
-                        <article className='tri_m_snss'>
-                            <img src={IoiInstagram_img} alt="아이오아이 인스타그램" />
-                            <div>
+            <section className='tri_m_h2'>
+                <h2>▪ sns ▪</h2>
+            </section>
+            <section className='tri_m_mem_tr_full'>
+                <section className='tri_m_mem_tr1'>
+                    <a href="https://www.instagram.com/ioi_10th" target='_blank' rel='noopener noreferrer' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={IoiInstagram_img} alt="아이오아이 인스타그램" className='ioi_m_mem_imgborder' />
+                            <div className='ioi_m_mem_imgborder'>
                                 <h4>
                                     인스타그램
                                 </h4>
@@ -286,6 +229,6 @@ export default function IoiMain() {
             </section>
 
             <FooterPart FooterPartText='ⓒ 스윙엔터테인먼트' />
-        </div>
+        </div >
     );
 };
