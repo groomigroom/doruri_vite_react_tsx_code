0c6609409586f896ccdc83958c9e01649926fbc8
groomigroom <googjohn0143@gmail.com>
2026-08-25 09:08:38 +0900

    Update FooterPart100vh.tsx

--- src/modules/FooterPart100vh.tsx
+++ src/modules/FooterPart100vh.tsx
@@ -1,1 +1,13 @@
-FooterPart100vh.tsx
+import './FooterPart.css';
+
+interface FooterPartProps {
+    FooterPartText: string;
+};
+
+export default function FooterPart100vh({ FooterPartText }: FooterPartProps) {
+    return (
+        <div className="FooterPart100vh_full">
+            <p className='FooterPart_company_pp'>{FooterPartText}</p>
+        </div>
+    );
+};
