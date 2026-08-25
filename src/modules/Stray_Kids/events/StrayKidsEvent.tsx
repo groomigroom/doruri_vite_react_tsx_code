6e29f9a54409d0f7057abdf48fa23f7949a06f14
groomigroom <googjohn0143@gmail.com>
2026-08-25 11:43:01 +0900

    Update StrayKidsEvent.tsx

--- src/modules/Stray_Kids/events/StrayKidsEvent.tsx
+++ src/modules/Stray_Kids/events/StrayKidsEvent.tsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import { useState } from "react";
 import { DoruriDocumentTitle } from "../../DoruriDocumentTitle";
 
+import '../../triples/main/TriplesMain.css';
 import '../../triples/events/TriplesEvent.css';
 import './StrayKidsEvent.css';
 import Menu from "../../Menu";
@@ -33,35 +35,66 @@ export default function StrayKidsEvent() {
 
     return (
         <div className="straykidsEvent_full">
-            <header className="triples_e_header">
+            <header className="tri_m_header">
                 <Menu />
+                <section className="home_translate">
+                    <Link to='/TxtEvent' className="on">한국어</Link>
+                    <Link to='/TxtEventEng'>English</Link>
+                </section>
             </header>
             <section className="tri_e_logo">
                 <img src={StrayKids_e_m_logo} alt="Stray Kids 이벤트 로고" />
             </section>
-            <section className="tri_e_h2">
-                <h2><img src={MainH2Deco} alt="Stray Kids" />ddd<img src={MainH2Deco} alt="Stray Kids" /></h2>
+            <section className="tri_m_h2">
+                <h2><img src={MainH2Deco} alt="Stray Kids" />노래 제목 맞히기 이벤트<img src={MainH2Deco} alt="Stray Kids" /></h2>
             </section>
-            <section className='tri_e_intropp'>
+            <section className='tri_m_intropp'>
                 <p>
-                    3줄 소개글<br />
-                    3줄 소개글<br />
-                    3줄 소개글
+                    &apos;SKZ-REPLAY 2026 Pt.1&apos; 앨범의<br />
+                    1, 2, 3번 수록곡<br />
+                    제목을 맞혀 주세요!
                 </p>
             </section>
             <form action="" className="str_e_form_part">
-                <div className="str_e_form_indiv str_e_form_indiv1">
-                    <input type="text" name="straykids_e_value1" value={straykids_e_value1} onChange={StrayKids_e_handle_input_change1} />
+                <section className="tri_m_h2">
+                    <h2>1번답 입력</h2>
+                </section>
+                <div className="str_e_form_indiv_wrap">
+                    <div className="str_e_form_indiv">
+                        <input type="text" name="straykids_e_value1" value={straykids_e_value1} onChange={StrayKids_e_handle_input_change1} />
+                    </div>
                 </div>
-                {straykids_e_value1 === "Battle Ground" && (<div className="str_e_form_indiv str_e_form_indiv2">
-                    <input type="text" name="straykids_e_value1" value={straykids_e_value2} onChange={StrayKids_e_handle_input_change2} />
-                </div>)}
-                {straykids_e_value2 === "Roman Empire" && (<div className="str_e_form_indiv str_e_form_indiv3">
-                    <input type="text" name="straykids_e_value1" value={straykids_e_value3} onChange={StrayKids_e_handle_input_change3} />
-                </div>)}
+                {straykids_e_value1 === "Battle Ground" && (
+                    <>
+                        <section className="tri_m_h2">
+                            <h2>2번답 입력</h2>
+                        </section>
+                        <div className="str_e_form_indiv_wrap">
+                            <div className="str_e_form_indiv">
+                                <input type="text" name="straykids_e_value1" value={straykids_e_value2} onChange={StrayKids_e_handle_input_change2} />
+                            </div>
+                        </div>
+                    </>
+                )}
+                {straykids_e_value2 === "Roman Empire" && (
+                    <>
+                        <section className="tri_m_h2">
+                            <h2>3번답 입력</h2>
+                        </section>
+                        <div className="str_e_form_indiv_wrap">
+                            <div className="str_e_form_indiv">
+                                <input type="text" name="straykids_e_value1" value={straykids_e_value3} onChange={StrayKids_e_handle_input_change3} />
+                            </div>
+                        </div>
+                    </>)}
                 {straykids_e_value3 === "LOVER" &&
-                    (<section className="str_e_last_result">
-                        <p>당첨</p>
+                    (<section className="tri_e_last_win_part">
+                        <div className="tri_e_ghost_40px"></div>
+                        <section className="tri_m_h2">
+                            <h2>
+                                <img src={MainH2Deco} alt="stray kids" />당첨입니다.<img src={MainH2Deco} alt="stray kids" />
+                            </h2>
+                        </section>
                     </section>)
                 }
             </form>
