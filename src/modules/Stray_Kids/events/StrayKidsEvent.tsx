75b77430119b5c09af6a8978c00f25dd56e53e89
groomigroom <googjohn0143@gmail.com>
2026-08-28 17:17:47 +0900

    Update StrayKidsEvent.tsx

--- src/modules/Stray_Kids/events/StrayKidsEvent.tsx
+++ src/modules/Stray_Kids/events/StrayKidsEvent.tsx
@@ -11,6 +11,7 @@ import Menu from "../../Menu";
 import MainH2Deco from '../../img/main_h2_deco.png';
 
 import StrayKids_e_m_logo from './img/StrayKids_e_m_logo.png';
+import FooterPart100vh from "../../FooterPart100vh";
 
 export default function StrayKidsEvent() {
     DoruriDocumentTitle('Stray Kids 이벤트');
@@ -88,6 +89,7 @@ export default function StrayKidsEvent() {
                             </div>
                         </div>
                     </>)}
+                <FooterPart100vh FooterPartText='ⓒ JYP엔터테인먼트' />
                 {straykids_e_value3 === "LOVER" &&
                     (<section className="tri_e_last_win_part">
                         <div className="tri_e_ghost_40px"></div>
@@ -97,21 +99,21 @@ export default function StrayKidsEvent() {
                             </h2>
                         </section>
                         <section className="tri_e_p_win_intropp">
-                                    <p>
-                                        당첨을 축하드려요!<br />
-                                        상품은 가사집입니다.<br />
-                                        이벤트에 관심가져 주셔서 감사합니다.<br />
-                                        이 이벤트는 가상의 이벤트입니다.<br />
-                                        참고해 주시길 부탁드리겠습니다.<br />
-                                    </p>
-                                </section>
+                            <p>
+                                당첨을 축하드려요!<br />
+                                상품은 가사집입니다.<br />
+                                이벤트에 관심가져 주셔서 감사합니다.<br />
+                                이 이벤트는 가상의 이벤트입니다.<br />
+                                참고해 주시길 부탁드리겠습니다.<br />
+                            </p>
+                        </section>
                         <section className="tri_e_p_win_present_150">
                             <div className="stray_e_win_present triples_e_p_win_present_img">
                                 <h3>▪ 가사집 ▪</h3>
-                                <p>가사 내용<br/>
-                                ----------------<br/>
-                                ----------------<br/>
-                                ----------------
+                                <p>가사 내용<br />
+                                    ----------------<br />
+                                    ----------------<br />
+                                    ----------------
                                 </p>
                             </div>
                         </section>
