46affc3d0559a67dbe0e1774d8a945872e9cd8d8
groomigroom <googjohn0143@gmail.com>
2026-08-29 15:54:03 +0900

    Update AespaEvent.tsx

--- src/modules/aespa/events/AespaEvent.tsx
+++ src/modules/aespa/events/AespaEvent.tsx
@@ -7,6 +7,7 @@ import './AespaEvent.css';
 import Menu from "../../Menu";
 import MainH2Deco from '../../img/main_h2_deco.png';
 import Aespa_e_m_logo from './img/Aespa_e_m_logo.png';
+import FooterPart100vh from "../../FooterPart100vh";
 
 export default function AespaEvent() {
     const [aespa_e_value1, setAespa_e_value1] = useState<string>("");
@@ -30,7 +31,7 @@ export default function AespaEvent() {
 
     return (
         <div className="AespaEvent_full">
-            <header className="triples_e_header">
+            <header className="tri_m_header">
                 <Menu />
                 <section className="home_translate">
                     <Link to='/AespaEvent' className="on">한국어</Link>
@@ -40,10 +41,10 @@ export default function AespaEvent() {
             <section className="tri_e_logo">
                 <img src={Aespa_e_m_logo} alt="aespa 이벤트 로고" />
             </section>
-            <section className="tri_e_h2">
+            <section className="tri_m_h2">
                 <h2><img src={MainH2Deco} alt="aespa" />앨범 제목 1글자씩 맞히기 이벤트<img src={MainH2Deco} alt="aespa" /></h2>
             </section>
-            <section className='tri_e_intropp'>
+            <section className='tri_m_intropp'>
                 <p>
                     2026년 5월 29일에<br />
                     발매된 &apos;aespa&apos;의<br />
@@ -63,9 +64,27 @@ export default function AespaEvent() {
                 {aespa_e_value6 === 'a' && <input className="aespa_e_value" type="text" name="aespa_e_value7" value={aespa_e_value7} onChange={(e) => setAespa_e_value7(e.target.value)} />}
                 {aespa_e_value7 === 'd' && <input className="aespa_e_value" type="text" name="aespa_e_value8" value={aespa_e_value8} onChange={(e) => setAespa_e_value8(e.target.value)} />}
             </form>
+            <FooterPart100vh FooterPartText='ⓒ SM엔터테인먼트' />
             {aespa_e_value8 === 'e' && (
-                <section>
-                    <p>당첨</p>
+                <section className="tri_e_last_win_part">
+                    <div className="tri_e_ghost_40px"></div>
+                    <section className="tri_m_h2">
+                        <h2>
+                            <img src={MainH2Deco} alt="stray kids" />당첨입니다.<img src={MainH2Deco} alt="stray kids" />
+                        </h2>
+                    </section>
+                    <section className="tri_e_p_win_intropp">
+                        <p>
+                            당첨을 축하드려요!<br />
+                            상품은 팔찌입니다.<br />
+                            이벤트에 관심가져 주셔서 감사합니다.<br />
+                            이 이벤트는 가상의 이벤트입니다.<br />
+                            참고해 주시길 부탁드리겠습니다.<br />
+                        </p>
+                    </section>
+                    <section className="tri_e_p_win_present_150">
+                        <img src={''} alt="" className="triples_e_p_win_present_img" />
+                    </section>
                 </section>
             )}
         </div>
