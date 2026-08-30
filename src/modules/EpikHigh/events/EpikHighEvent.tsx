70cc88b5c92314f6ab38dc9321bc47d70526eca9
groomigroom <googjohn0143@gmail.com>
2026-08-30 10:08:06 +0900

    Update EpikHighEvent.tsx

--- src/modules/EpikHigh/events/EpikHighEvent.tsx
+++ src/modules/EpikHigh/events/EpikHighEvent.tsx
@@ -4,12 +4,14 @@ import { DoruriDocumentTitle } from "../../DoruriDocumentTitle";
 
 import '../../triples/main/TriplesMain.css';
 import '../../triples/events/TriplesEvent.css';
+import '../../ALLDAY_PROJECT/events/AlldayProjectEvent.css';
 import './EpikHighEvent.css';
 import Menu from "../../Menu";
 
 import MainH2Deco from '../../img/main_h2_deco.png';
 import epikhigh_e_real_lp_img from './img/epikhigh_e_real_lp_img.png';
 import epikhigh_e_fake_lp_img from './img/epikhigh_e_fake_lp_img.png';
+import FooterPart from "../../FooterPart";
 
 export default function EpikHighEvent() {
     DoruriDocumentTitle('에픽하이 이벤트');
@@ -131,24 +133,51 @@ export default function EpikHighEvent() {
                     <div className="epikhigh_e_lp_m_divide_x epikhigh_e_lp_m_divide4"></div>
                 </section>
             </section>
-            <section className="epikhigh_e_lp_mv_buttons">
-                <button onClick={epikhigh_e_real_lp_go_up}>위로 이동</button>
-                <button onClick={epikhigh_e_real_lp_go_down}>아래로 이동</button>
-                <button onClick={epikhigh_e_real_lp_go_left}>왼쪽으로 이동</button>
-                <button onClick={epikhigh_e_real_lp_go_right}>오른쪽으로 이동</button>
+            <section className='alldayproject_e_maze_RB_buttons_full'>
+                <section className='alldayproject_e_maze_RB_buttons'>
+                    <button className='alldayproject_e_maze_RB_button alldayproject_e_maze_RB_to_top' onClick={epikhigh_e_real_lp_go_up}>위로 이동</button>
+                    <button className='alldayproject_e_maze_RB_button alldayproject_e_maze_RB_to_down' onClick={epikhigh_e_real_lp_go_down}>아래로 이동</button>
+                    <button className='alldayproject_e_maze_RB_button alldayproject_e_maze_RB_to_left' onClick={epikhigh_e_real_lp_go_left}>왼쪽으로 이동</button>
+                    <button className='alldayproject_e_maze_RB_button alldayproject_e_maze_RB_to_right' onClick={epikhigh_e_real_lp_go_right}>오른쪽으로 이동</button>
+                </section>
             </section>
-
-
-            {epikhigh_e_lp_move_count === 5 && (epikhigh_e_real_lp_top != epikhigh_e_fake_lp_top || epikhigh_e_real_lp_left != epikhigh_e_fake_lp_left) && (
-                <section className="epikhigh_e_win">
-                    <p>당첨입니다.</p>
-                </section>
-            )}
-            {epikhigh_e_real_lp_top === epikhigh_e_fake_lp_top && epikhigh_e_real_lp_left === epikhigh_e_fake_lp_left && (
-                <section className="epikhigh_e_win">
-                    <p>당첨아님</p>
-                </section>
-            )}
+            <FooterPart FooterPartText='ⓒ 아워즈' />
+            {
+                epikhigh_e_lp_move_count === 5 && (epikhigh_e_real_lp_top != epikhigh_e_fake_lp_top || epikhigh_e_real_lp_left != epikhigh_e_fake_lp_left) && (
+                    <section className="tri_e_last_win_part">
+                        <div className="tri_e_ghost_40px"></div>
+                        <section className="tri_m_h2">
+                            <h2>
+                                <img src={MainH2Deco} alt="에픽하이" />당첨입니다.<img src={MainH2Deco} alt="에픽하이" />
+                            </h2>
+                        </section>
+                        <section className="tri_e_p_win_intropp">
+                            <p>
+                                당첨을 축하드려요!<br />
+                                상품은 ---입니다.<br />
+                                이벤트에 관심가져 주셔서 감사합니다.<br />
+                                이 이벤트는 가상의 이벤트입니다.<br />
+                                참고해 주시길 부탁드리겠습니다.<br />
+                            </p>
+                        </section>
+                        <section className="tri_e_p_win_present_150">
+                            <img src={''} alt="" className="triples_e_p_win_present_img" />
+                        </section>
+                    </section>
+                )
+            }
+            {
+                epikhigh_e_real_lp_top === epikhigh_e_fake_lp_top && epikhigh_e_real_lp_left === epikhigh_e_fake_lp_left && (
+                    <section className="tri_e_last_win_part">
+                        <div className="tri_e_ghost_40px"></div>
+                        <section className="tri_m_h2">
+                            <h2>
+                                <img src={MainH2Deco} alt="에픽하이" />당첨이 아닙니다.<img src={MainH2Deco} alt="에픽하이" />
+                            </h2>
+                        </section>
+                    </section>
+                )
+            }
         </div >
     );
 }
