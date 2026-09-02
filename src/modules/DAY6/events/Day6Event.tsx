facfb733a0931ef0be9046eb5637900b0dcf4ce5
groomigroom <googjohn0143@gmail.com>
2026-09-02 14:41:21 +0900

    Update Day6Event.tsx

--- src/modules/DAY6/events/Day6Event.tsx
+++ src/modules/DAY6/events/Day6Event.tsx
@@ -7,6 +7,8 @@ import './Day6Event.css';
 import Menu from "../../Menu";
 import MainH2Deco from '../../img/main_h2_deco.png';
 import FooterPart from "../../FooterPart";
+import day6_e_logo from './img/day6_e_logo.png';
+import day6_e_clip from './img/day6_e_clip.png';
 
 export default function Day6Event() {
     DoruriDocumentTitle('DAY6 이벤트');
@@ -46,7 +48,7 @@ export default function Day6Event() {
                 </section>
             </header>
             <section className="tri_e_logo">
-                <img src={""} alt="DAY6 이벤트 로고" />
+                <img src={day6_e_logo} alt="DAY6 이벤트 로고" />
             </section>
             <section className="tri_m_h2">
                 <h2><img src={MainH2Deco} alt="DAY6" />이름 맞히기 이벤트<img src={MainH2Deco} alt="DAY6" /></h2>
@@ -106,20 +108,20 @@ export default function Day6Event() {
                     <div className="tri_e_ghost_40px"></div>
                     <section className="tri_m_h2">
                         <h2>
-                            <img src={MainH2Deco} alt="에픽하이" />당첨입니다.<img src={MainH2Deco} alt="에픽하이" />
+                            <img src={MainH2Deco} alt="DAY6" />당첨입니다.<img src={MainH2Deco} alt="DAY6" />
                         </h2>
                     </section>
                     <section className="tri_e_p_win_intropp">
                         <p>
                             당첨을 축하드려요!<br />
-                            상품은 ---입니다.<br />
+                            상품은 클립입니다.<br />
                             이벤트에 관심가져 주셔서 감사합니다.<br />
                             이 이벤트는 가상의 이벤트입니다.<br />
                             참고해 주시길 부탁드리겠습니다.<br />
                         </p>
                     </section>
                     <section className="tri_e_p_win_present_150">
-                        <img src={''} alt="" className="triples_e_p_win_present_img" />
+                        <img src={day6_e_clip} alt="" className="triples_e_p_win_present_img" />
                     </section>
                 </section>
             )}
@@ -128,7 +130,7 @@ export default function Day6Event() {
                     <div className="tri_e_ghost_40px"></div>
                     <section className="tri_m_h2">
                         <h2>
-                            <img src={MainH2Deco} alt="에픽하이" />당첨이 아닙니다.<img src={MainH2Deco} alt="에픽하이" />
+                            <img src={MainH2Deco} alt="DAY6" />당첨이 아닙니다.<img src={MainH2Deco} alt="DAY6" />
                         </h2>
                     </section>
                 </section>
