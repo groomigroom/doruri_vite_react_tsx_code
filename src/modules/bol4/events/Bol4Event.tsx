ba9d78e0acb2cce624cbe9aaea3a818736274418
groomigroom <googjohn0143@gmail.com>
2026-09-02 16:39:49 +0900

    Update Bol4Event.tsx

--- src/modules/bol4/events/Bol4Event.tsx
+++ src/modules/bol4/events/Bol4Event.tsx
@@ -9,7 +9,8 @@ import './Bol4Event.css';
 import Menu from "../../Menu";
 import MainH2Deco from '../../img/main_h2_deco.png';
 import FooterPart from "../../FooterPart";
-
+import bol4_e_logo from './img/bol4_e_logo.png';
+import bol4_e_blub from './img/bol4_e_blub.png';
 
 
 export default function Bol4Event() {
@@ -28,7 +29,7 @@ export default function Bol4Event() {
                 </section>
             </header>
             <section className="tri_e_logo">
-                <img src={""} alt="볼빨간사춘기 이벤트 로고" />
+                <img src={bol4_e_logo} alt="볼빨간사춘기 이벤트 로고" />
             </section>
             <section className="tri_m_h2">
                 <h2><img src={MainH2Deco} alt="볼빨간사춘기" />앨범 제목 클릭 이벤트<img src={MainH2Deco} alt="볼빨간사춘기" /></h2>
@@ -65,14 +66,14 @@ export default function Bol4Event() {
                     <section className="tri_e_p_win_intropp">
                         <p>
                             당첨을 축하드려요!<br />
-                            상품은 팔찌입니다.<br />
+                            상품은 전등입니다.<br />
                             이벤트에 관심가져 주셔서 감사합니다.<br />
                             이 이벤트는 가상의 이벤트입니다.<br />
                             참고해 주시길 부탁드리겠습니다.<br />
                         </p>
                     </section>
                     <section className="tri_e_p_win_present_150">
-                        <img src={''} alt="" className="triples_e_p_win_present_img" />
+                        <img src={bol4_e_blub} alt="" className="triples_e_p_win_present_img" />
                     </section>
                 </section>
             )}
