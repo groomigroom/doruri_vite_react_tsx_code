e1ce81af865f3b8096fab4e5603d70b1b3c9405e
groomigroom <googjohn0143@gmail.com>
2026-09-02 20:43:29 +0900

    Update IdnttEvent.tsx

--- src/modules/idntt/events/IdnttEvent.tsx
+++ src/modules/idntt/events/IdnttEvent.tsx
@@ -1,6 +1,12 @@
 import { useState } from "react";
 
+import '../../triples/main/TriplesMain.css';
+import '../../triples/events/TriplesEvent.css';
 import './IdnttEvent.css';
+import Menu from "../../Menu";
+import { Link } from "react-router-dom";
+import MainH2Deco from '../../img/main_h2_deco.png';
+import FooterPart from "../../FooterPart";
 
 interface IdnttEventNodeType {
     Idntt_e_mem_name: string;
@@ -8,31 +14,31 @@ interface IdnttEventNodeType {
 }
 
 export default function IdnttEvent() {
-    const [Idntt_e_node1, setIdntt_e_node1] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "1번", Idntt_e_link: null });
-    const [Idntt_e_node2, setIdntt_e_node2] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "2번", Idntt_e_link: null });
-    const [Idntt_e_node3, setIdntt_e_node3] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "3번", Idntt_e_link: null });
-    const [Idntt_e_node4, setIdntt_e_node4] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "4번", Idntt_e_link: null });
-    const [Idntt_e_node5, setIdntt_e_node5] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "5번", Idntt_e_link: null });
-    const [Idntt_e_node6, setIdntt_e_node6] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "6번", Idntt_e_link: null });
-    const [Idntt_e_node7, setIdntt_e_node7] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "7번", Idntt_e_link: null });
-    const [Idntt_e_node8, setIdntt_e_node8] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "8번", Idntt_e_link: null });
-    const [Idntt_e_node9, setIdntt_e_node9] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "9번", Idntt_e_link: null });
-    const [Idntt_e_node10, setIdntt_e_node10] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "10번", Idntt_e_link: null });
-    const [Idntt_e_node11, setIdntt_e_node11] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "11번", Idntt_e_link: null });
-    const [Idntt_e_node12, setIdntt_e_node12] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "12번", Idntt_e_link: null });
-    const [Idntt_e_node13, setIdntt_e_node13] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "13번", Idntt_e_link: null });
-    const [Idntt_e_node14, setIdntt_e_node14] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "14번", Idntt_e_link: null });
-    const [Idntt_e_node15, setIdntt_e_node15] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "15번", Idntt_e_link: null });
-    const [Idntt_e_node16, setIdntt_e_node16] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "16번", Idntt_e_link: null });
-    const [Idntt_e_node17, setIdntt_e_node17] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "17번", Idntt_e_link: null });
-    const [Idntt_e_node18, setIdntt_e_node18] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "18번", Idntt_e_link: null });
-    const [Idntt_e_node19, setIdntt_e_node19] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "19번", Idntt_e_link: null });
-    const [Idntt_e_node20, setIdntt_e_node20] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "20번", Idntt_e_link: null });
+    const [Idntt_e_node1, setIdntt_e_node1] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "김도훈", Idntt_e_link: null });
+    const [Idntt_e_node2, setIdntt_e_node2] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "김희주", Idntt_e_link: null });
+    const [Idntt_e_node3, setIdntt_e_node3] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "id3", Idntt_e_link: null });
+    const [Idntt_e_node4, setIdntt_e_node4] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "최태인", Idntt_e_link: null });
+    const [Idntt_e_node5, setIdntt_e_node5] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "이재영", Idntt_e_link: null });
+    const [Idntt_e_node6, setIdntt_e_node6] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "김주호", Idntt_e_link: null });
+    const [Idntt_e_node7, setIdntt_e_node7] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "남지운", Idntt_e_link: null });
+    const [Idntt_e_node8, setIdntt_e_node8] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "이환희", Idntt_e_link: null });
+    const [Idntt_e_node9, setIdntt_e_node9] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "이청명", Idntt_e_link: null });
+    const [Idntt_e_node10, setIdntt_e_node10] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "토와", Idntt_e_link: null });
+    const [Idntt_e_node11, setIdntt_e_node11] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "이규혁", Idntt_e_link: null });
+    const [Idntt_e_node12, setIdntt_e_node12] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "박누리", Idntt_e_link: null });
+    const [Idntt_e_node13, setIdntt_e_node13] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "김성준", Idntt_e_link: null });
+    const [Idntt_e_node14, setIdntt_e_node14] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "한예준", Idntt_e_link: null });
+    const [Idntt_e_node15, setIdntt_e_node15] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "최경빈", Idntt_e_link: null });
+    const [Idntt_e_node16, setIdntt_e_node16] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "황은수", Idntt_e_link: null });
+    const [Idntt_e_node17, setIdntt_e_node17] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "곽기웅", Idntt_e_link: null });
+    const [Idntt_e_node18, setIdntt_e_node18] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "이주헌", Idntt_e_link: null });
+    const [Idntt_e_node19, setIdntt_e_node19] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "양경호", Idntt_e_link: null });
+    const [Idntt_e_node20, setIdntt_e_node20] = useState<IdnttEventNodeType>({ Idntt_e_mem_name: "조은찬", Idntt_e_link: null });
 
     const [Idntt_e_last_win, setIdntt_e_last_win] = useState<boolean>(false);
 
     const Idntt_e_node21: IdnttEventNodeType = {
-        Idntt_e_mem_name: "21번",
+        Idntt_e_mem_name: "김은성",
         Idntt_e_link: null,
     };
 
@@ -182,7 +188,27 @@ export default function IdnttEvent() {
 
 
     return (
-        <div>
+        <div className="idntt_e_full">
+            <header className="tri_m_header">
+                <Menu />
+                <section className="home_translate">
+                    <Link to='/IdnttEvent' className="on">한국어</Link>
+                    <Link to='/IdnttEventEng'>English</Link>
+                </section>
+            </header>
+            <section className="tri_e_logo">
+                <img src={''} alt="idntt 이벤트 로고" />
+            </section>
+            <section className="tri_m_h2">
+                <h2><img src={MainH2Deco} alt="idntt" />멤버 id 순서 클릭 이벤트<img src={MainH2Deco} alt="idntt" /></h2>
+            </section>
+            <section className='tri_m_intropp'>
+                <p>
+                    &apos;idntt&apos; 멤버들의<br />
+                    id를 순서대로<br />
+                    클릭해 주세요!
+                </p>
+            </section>
             <section className="idntt_e_real_part">
                 <p onClick={Idntt_e_node1Link} className="idntt_e_mem_node1">{Idntt_e_node1.Idntt_e_mem_name}</p>
                 {Idntt_e_node1.Idntt_e_link !== null && (
@@ -246,10 +272,30 @@ export default function IdnttEvent() {
                     <p className="idntt_e_mem_node21" onClick={changeIdntt_e_last_win}>{Idntt_e_node21.Idntt_e_mem_name}</p>
                 )}
                 {Idntt_e_last_win && (
-                    <p>당첨</p>
+                    <section className="tri_e_last_win_part">
+                        <div className="tri_e_ghost_40px"></div>
+                        <section className="tri_m_h2">
+                            <h2>
+                                <img src={MainH2Deco} alt="idntt" />당첨입니다.<img src={MainH2Deco} alt="idntt" />
+                            </h2>
+                        </section>
+                        <section className="tri_e_p_win_intropp">
+                            <p>
+                                당첨을 축하드려요!<br />
+                                상품은 --입니다.<br />
+                                이벤트에 관심가져 주셔서 감사합니다.<br />
+                                이 이벤트는 가상의 이벤트입니다.<br />
+                                참고해 주시길 부탁드리겠습니다.<br />
+                            </p>
+                        </section>
+                        <section className="tri_e_p_win_present_150">
+                            <img src={''} alt="" className="triples_e_p_win_present_img" />
+                        </section>
+                    </section>
                 )}
 
             </section>
+            <FooterPart FooterPartText='ⓒ 켄버스' />
         </div>
     );
 }
