9dd9deb5b785c4d90b47717c58079be922c788a9
groomigroom <googjohn0143@gmail.com>
2026-09-17 12:24:45 +0900

    Update AespaMainEng.tsx

--- front_clean/aespaEng/main/AespaMainEng.tsx
+++ front_clean/aespaEng/main/AespaMainEng.tsx
@@ -1,1 +1,89 @@
-AespaMainEng.tsx
+import { Link } from 'react-router-dom';
+import { EngPage } from '../../HomeEng/EngPage';
+
+import '../../triples/main/TriplesMain.css'
+import '../../aespa/main/AespaMain.css';
+
+import MenuEng from '../../HomeEng/MenuEng';
+import FooterPartEng from '../../FooterPartEng';
+
+import Aes_m_logo from '../../aespa/main/img/Aes_m_logo.svg';
+import MainH2Deco from '../../img/main_h2_deco.png';
+import Aes_m_img from '../../aespa/main/img/aespa_m_aespa.jpg';
+
+import Aes_m_MemKarina_img from '../../aespa/main/img/aespa_m_mem_Karina.jpg';
+import Aes_m_MemJijell_img from '../../aespa/main/img/aespa_m_mem_Jijel.jpg';
+import Aes_m_MemWinter_img from '../../aespa/main/img/aespa_m_mem_Winter.jpg';
+import Aes_m_MemNingning_img from '../../aespa/main/img/aespa_m_mem_Ningning.jpg';
+
+//191919
+//ffffff
+
+export default function AespaMainEng() {
+    EngPage('en', 'ko');
+
+    return (
+        <div className='aes_m_full'>
+            <MenuEng />
+            <section className="home_translate">
+                <Link to='/AespaMain'>한국어</Link>
+                <Link to='/AespaMainEng' className="on">English</Link>
+            </section>
+            <img src={Aes_m_logo} alt="aespa 로고" className='tri_m_logo' />
+            <h2 className='tri_m_h2 tri_m_h2_1'><img src={MainH2Deco} alt="aespa deco" />aespa<img src={MainH2Deco} alt="aespa deco" /></h2>
+            <p className='tri_m_intropp'>
+                Let's make Lemonade<br />
+                in a different<br />
+                world with &apos;aespa&apos;!
+            </p>
+            <img src={Aes_m_img} className='tri_m_img' alt='aespa' />
+            <h2 className='tri_m_h2 tri_m_h2_2'>▪ Members ▪</h2>
+            <section className='tri_m_mem_tr_full aes_m_mem_tr_full'>
+                <section className='tri_m_mem_tr'>
+                    <Link to='/AesMemKarinaEng' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={Aes_m_MemKarina_img} alt="Karina" />
+                            <div>
+                                <h4>Karina</h4>
+                            </div>
+                        </article>
+                    </Link>
+                    <Link to='/AesMemJijellEng' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={Aes_m_MemJijell_img} alt="Giselle" />
+                            <div>
+                                <h4>Giselle</h4>
+                            </div>
+                        </article>
+                    </Link>
+
+                </section>
+
+                <section className='tri_m_mem_tr'>
+                    <Link to='/AesMemWinterEng' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={Aes_m_MemWinter_img} alt="Winter" />
+                            <div>
+                                <h4>Winter</h4>
+                            </div>
+                        </article>
+                    </Link>
+                    <Link to='/AesMemNingningEng' className='tri_m_mem_a'>
+                        <article className='tri_m_mem'>
+                            <img src={Aes_m_MemNingning_img} alt="Ningning" />
+                            <div>
+                                <h4>Ningning</h4>
+                            </div>
+                        </article>
+                    </Link>
+
+
+                </section>
+
+
+            </section>
+
+            <FooterPartEng FooterPartEngText='ⓒ SM ENTERTAINMENT' />
+        </div>
+    );
+};
