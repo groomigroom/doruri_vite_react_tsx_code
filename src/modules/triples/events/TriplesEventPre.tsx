e37e0e05384c31850885e74fce5f2ff391d41755
groomigroom <googjohn0143@gmail.com>
2026-09-18 13:25:07 +0900

    Update TriplesEventPre.tsx

--- src/modules/triples/events/TriplesEventPre.tsx
+++ src/modules/triples/events/TriplesEventPre.tsx
@@ -3,7 +3,7 @@ import '../../triples/events/TriplesEvent.css';
 import "./TriplesEventPre.css";
 
 import BabyFlowerAllImg from "./img/baby_flower_img/tri_events_pre_all_s.png"
-import TriplesEventPre_m_logo from '../main/img/tri_m_logo.svg';
+// import TriplesEventPre_m_logo from '../main/img/tri_m_logo.svg';
 import MainH2Deco from "../../img/main_h2_deco.png";
 
 interface Tri_e_popProp {
@@ -72,7 +72,8 @@ export default function TriplesEventPre({ tri_e_OnCl, tri_e_result }: Tri_e_popP
                     </section>
                     <section className="tri_e_p_ticket">
                         <img src={BabyFlowerAllImg} alt="트리플에스" className="tri_e_p_members_img" />
-                        <img src={TriplesEventPre_m_logo} alt="트리플에스 로고" className="tri_e_p_logo_img" />
+{/* TriplesEventPre_m_logo */}
+                        <img src={""} alt="트리플에스 로고" className="tri_e_p_logo_img" />
                         <p className="tri_e_p_pp"><span>tripleS Baby Flower World Tour</span><br />
                             {tri_e_p_floor_num}층 &apos;{tri_e_p_part_letter}&apos;구역 {tri_e_p_floor_num2}번<br />
                             일시 : 0000년 00월 00일<br />
