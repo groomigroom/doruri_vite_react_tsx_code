0d694aebd3f80b3b6f09c5060c8f0b0ec28bc75d
groomigroom <googjohn0143@gmail.com>
2026-09-18 12:48:43 +0900

    Comment out conditional rendering for TxtEventPre

--- src/modules/txtEng/events/TxtEventEng.tsx
+++ src/modules/txtEng/events/TxtEventEng.tsx
@@ -72,9 +72,9 @@ export default function TxtEventEng() {
                 </div>
             </form>
             <button className="txt_e_btn" onClick={txt_e_OpPopup}>submit</button>
-            {txt_e_PopopO && (
+            {/* {txt_e_PopopO && (
                 // <TxtEventPre txt_e_OnCl={txt_e_ClPopup} txt_e_result={txt_e_last_str} />
-            )}
+            )} */}
             <FooterPartEng FooterPartEngText='ⓒ BIGHIT MUSIC' />
         </div>
     );
