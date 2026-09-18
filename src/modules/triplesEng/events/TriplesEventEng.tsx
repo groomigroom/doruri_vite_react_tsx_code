7d9ec2b24a036a60c55f4500476c3637af12ed38
groomigroom <googjohn0143@gmail.com>
2026-09-18 12:50:25 +0900

    Update TriplesEventEng.tsx

--- src/modules/triplesEng/events/TriplesEventEng.tsx
+++ src/modules/triplesEng/events/TriplesEventEng.tsx
@@ -131,13 +131,13 @@ export default function TriplesEventEng() {
     if (tri_e_form.tri_e_s24 === "JiYeon") tri_e_result++;
 
     //const [tri_e_PopupO, tri_e_SetPopupO] = useState<boolean>(false);
-    const tri_e_OpPopup = () => {
-        tri_e_SetPopupO(true);
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth' // 부드럽게 올라가려면 'smooth', 즉시 이동하려면 'auto'
-        });
-    };
+    // const tri_e_OpPopup = () => {
+    //     tri_e_SetPopupO(true);
+    //     window.scrollTo({
+    //         top: 0,
+    //         behavior: 'smooth' // 부드럽게 올라가려면 'smooth', 즉시 이동하려면 'auto'
+    //     });
+    // };
     //const tri_e_OpPopup = () => tri_e_SetPopupO(true);
     //const tri_e_ClPopup = () => tri_e_SetPopupO(false);
 
